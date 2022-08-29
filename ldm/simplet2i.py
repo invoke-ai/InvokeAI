cfac71e7c010d3353488c94322b0737b8c2ae508
xra <mail@xra.dev>
2022-08-30 00:59:30 +0900

    fixed up slerp

--- ldm/simplet2i.py
+++ ldm/simplet2i.py
@@ -736,36 +736,27 @@ class T2I:
         Returns:
             v2 (np.ndarray): Interpolation vector between v0 and v1
         '''
-        c = False
+        inputs_are_torch = False
         if not isinstance(v0,np.ndarray):
-            c = True
+            inputs_are_torch = True
             v0 = v0.detach().cpu().numpy()
         if not isinstance(v1,np.ndarray):
-            c = True
+            inputs_are_torch = True
             v1 = v1.detach().cpu().numpy()
-        # Copy the vectors to reuse them later
-        v0_copy = np.copy(v0)
-        v1_copy = np.copy(v1)
-        # Normalize the vectors to get the directions and angles
-        v0 = v0 / np.linalg.norm(v0)
-        v1 = v1 / np.linalg.norm(v1)
-        # Dot product with the normalized vectors (can't use np.dot in W)
-        dot = np.sum(v0 * v1)
-        # If absolute value of dot product is almost 1, vectors are ~colineal, so use lerp
+        
+        dot = np.sum(v0 * v1 / (np.linalg.norm(v0) * np.linalg.norm(v1)))
         if np.abs(dot) > DOT_THRESHOLD:
-            return v0_copy*(1.0-t)+v1_copy*t # lerp
-        # Calculate initial angle between v0 and v1
-        theta_0 = np.arccos(dot)
-        sin_theta_0 = np.sin(theta_0)
-        # Angle at timestep t
-        theta_t = theta_0 * t
-        sin_theta_t = np.sin(theta_t)
-        # Finish the slerp algorithm
-        s0 = np.sin(theta_0 - theta_t) / sin_theta_0
-        s1 = sin_theta_t / sin_theta_0
-        v2 = s0 * v0_copy + s1 * v1_copy
-        if c:
-            res = torch.from_numpy(v2).to(self.device)
+            v2 = (1 - t) * v0 + t * v1
         else:
-            res = v2
-        return res
+            theta_0 = np.arccos(dot)
+            sin_theta_0 = np.sin(theta_0)
+            theta_t = theta_0 * t
+            sin_theta_t = np.sin(theta_t)
+            s0 = np.sin(theta_0 - theta_t) / sin_theta_0
+            s1 = sin_theta_t / sin_theta_0
+            v2 = s0 * v0 + s1 * v1
+
+        if inputs_are_torch:
+            v2 = torch.from_numpy(v2).to(self.device)
+
+        return v2
