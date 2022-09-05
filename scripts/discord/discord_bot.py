21ec87f7643d567350aea40c5ada7ab08d387169
Danny Whittaker <zallist@gmail.com>
2022-09-05 10:49:24 +0100

    Cleanup, attempted hybrid commands, failed, now using explicit types & parsing for seed if need be

--- scripts/discord/discord_bot.py
+++ scripts/discord/discord_bot.py
@@ -5,9 +5,13 @@ import threading
 import asyncio
 
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 from discord_config import settings
+
+# regex for text parsing
+import re
+text_parser = re.compile('^(?P<prompt>.+?)(?:\s+-+(?:seed|s)[\s=](?P<seed>\d+))?$', re.IGNORECASE)
 
 sys.path.append('.')
 
@@ -26,18 +30,7 @@ bot = commands.Bot(command_prefix = settings['prefix'], intents=intents)
 
 @bot.event
 async def on_ready():
-    print('Bot is ready, oauth url: {}'.format(discord.utils.oauth_url(bot.user.id)))
-
-@bot.command()
-async def dream(ctx, *, arg):
-    if ctx.author != bot.user:
-        try:
-            message = await ctx.reply('Your dream is queued')
-            dreaming_loop.call_soon_threadsafe(dreaming_queue.put_nowait, dreaming(ctx, arg, message))
-        except Exception as e:
-            error_msg = 'Dream error: {}'.format(e)
-            print(error_msg)
-            on_bot_thread(ctx.reply(error_msg))
+    print('Bot is ready, oauth url: {}'.format(discord.utils.oauth_url(bot.user.id, permissions=discord.Permissions(2147609600))))
 
 def noop():
     pass
@@ -46,15 +39,52 @@ def on_bot_thread(coroutine):
     bot.loop.create_task(coroutine) # add the coroutine as a task to the bot loop
     bot.loop.call_soon_threadsafe(noop) # Force the bot.loop to clear itself
 
-async def dreaming(ctx, arg, message):
+@bot.hybrid_command(
+    name="sync_dream_commands",
+    description="Sync slash commands for this bot"
+)
+@commands.guild_only()
+async def sync_command(ctx: commands.Context) -> None:
+    await bot.tree.sync(guild=discord.Object(id=ctx.guild.id))
+    await ctx.send("Synced")
+
+"""
+class DreamFlags(commands.FlagConverter):
+    prompt: str = commands.flag(description="The prompt to generate an image for", default=None)
+
+@bot.hybrid_command(
+    name="dream", 
+    description="Generate an image based on the given prompt"
+)
+async def dream(ctx: commands.Context, flags: DreamFlags):
+    print(f'flags: {flags}')
+"""
+@bot.command(
+    name="dream", 
+    description="Generate an image based on the given prompt"
+)
+async def dream(ctx: commands.Context, *, quote_text: str):
+    if ctx.author != bot.user:
+        try:
+            message = await ctx.reply('Your dream is queued')
+            dreaming_loop.call_soon_threadsafe(dreaming_queue.put_nowait, dreaming(ctx, quote_text, message))
+        except Exception as e:
+            error_msg = 'Dream error: {}'.format(e)
+            print(error_msg)
+            on_bot_thread(ctx.reply(error_msg))
+
+#async def dreaming(ctx: commands.Context, flags: DreamFlags, message: discord.Message):
+async def dreaming(ctx: commands.Context, quote_text: str, message: discord.Message):
     try:
         if ctx.author != bot.user:
             start = timer()
             
-            quote_text = '{}'.format(arg)
+            parsed = text_parser.match(quote_text)
 
             on_bot_thread(message.edit(content='Dreaming for {}\'s `{}`'.format(ctx.message.author.mention,quote_text)))
-            outputs = model.prompt2png(quote_text, 'outputs/img-samples')
+
+            outputs = model.prompt2png(parsed.group('prompt'), 'outputs/img-samples', seed = parsed.group('seed'))
+            #outputs = []
 
             on_bot_thread(message.delete())
 
@@ -79,4 +109,5 @@ def thread_handler():
 
 model_thread = threading.Thread(target=thread_handler, daemon=True)
 model_thread.start()
+
 bot.run(settings['token'])
