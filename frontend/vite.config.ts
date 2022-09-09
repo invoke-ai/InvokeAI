f3015889df6ecc0885c67fc72d180784671e0d0e
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2022-09-10 06:53:02 +1000

    Adds sourcemaps to dev build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1500, // we don't really care about chunk size
-  },
+export default defineConfig(({ mode }) => {
+  const common = {
+    plugins: [react()],
+    build: {
+      chunkSizeWarningLimit: 1500, // we don't really care about chunk size
+    },
+  };
+  if (mode == 'development') {
+    return {
+      ...common,
+      build: {
+        ...common.build,
+        sourcemap: true,
+      },
+    };
+  } else {
+    return {
+      ...common,
+    };
+  }
 });
