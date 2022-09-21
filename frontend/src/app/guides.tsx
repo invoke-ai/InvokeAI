458b4901834fb7aebe8589ca6f236b6ef6eae5c8
hipsterusername <kentk@hey.com>
2022-09-20 20:14:39 -0400

    Creation of initial guide files

---
+++ frontend/src/app/guides.tsx
@@ -0,0 +1,77 @@
+/**
+ * Each feature with a UI element is listed here.
+ * 
+ * 'text' is the short help text for that feature.
+ * 'href' is the link to the docs page for that feature.
+ * 'visual' is used if an image or animated asset has been referenced for display in the tooltip.
+ */
+
+ export const Guides = {
+    Prompt: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+    Gallery: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+    Steps: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+    Resolution: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+    ConfigScale: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+    Seed: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+    Seamless: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+    ESRGAN: {
+      text: "Short help text for feature 1",
+      href: "link/to/docs/feature1.html",
+      guideImage: "asset/path.gif"
+    },
+    GFPGAN: {
+      text: "Short help text for feature 2",
+      href: "link/to/docs/feature2.html",
+      guideImage: "asset/path.gif"
+    },
+    ImageToImage: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+    Sampler: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+    Variations: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+    GeneralHelp: {
+      text: "Short help text for feature 3",
+      href: "link/to/docs/feature3.html",
+      guideImage: "asset/path.gif"
+    },
+
+  };
+  
---
+++ frontend/src/common/components/GuidePopover.tsx
@@ -0,0 +1,69 @@
+import {
+    Popover,
+    PopoverArrow,
+    PopoverContent,
+    PopoverTrigger,
+    Link,
+    PopoverHeader,
+    Flex,
+    Spacer,
+    Image,
+    Button
+  } from "@chakra-ui/react";
+import { ReactElement } from "react";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Guides } from "../../app/guides";
+  /**
+   * The GuidePopover needs a child 'children' to be
+   * the trigger component you hover on. That child
+   * will be of type ReactElement.
+   *
+   * It also needs a feature name 'feature', which it
+   * uses to look up the 'text' and 'href' from 'help.ts'
+   */
+type GuideProps = {
+    children: ReactElement;
+    feature: keyof typeof Guides;
+  };
+  
+const Guide = ({ children, feature }: GuideProps) => {
+    const { text, href, guideImage } = Guides[feature];
+    if (shouldShowGuide) {    
+        return (
+            <Popover trigger={"hover"}>
+                <PopoverTrigger>{children}</PopoverTrigger>
+                <PopoverContent width={"auto"}>
+                    <PopoverArrow />
+                        <PopoverHeader>
+                            <Flex alignItems={"center"} gap={2}>
+                            <Image src={guideImage} alt={text} />
+                            </Flex>
+                        </PopoverHeader>
+                
+                    
+                        <Flex alignItems={"center"} gap={2} p={4}>
+                            {text} 
+                            
+                        </Flex>
+                        <Flex alignItems={"right"} gap={2} p={4}>
+                        <Spacer />
+                        <Spacer />
+                        <Spacer />
+                        <Spacer />
+                        <Spacer />
+                        <Spacer />   
+                            <Link href={href} isExternal>
+                            <Button leftIcon={<ExternalLinkIcon />} colorScheme='teal' variant='solid'>
+                            Learn More
+                            </Button>
+                            </Link>
+                        </Flex>           
+                </PopoverContent>
+            </Popover>
+            );
+        };
+return ;
+};
+
+    
+export default GuidePopover;
