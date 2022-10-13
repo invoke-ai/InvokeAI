759f563b6dc3a63183ef65e6c8278f2e7b8a76e7
cmdr2 <secondary.cmdr2@gmail.com>
2022-10-13 15:20:29 +0530

    Add a pause before the script ends

--- update.bat
+++ update.bat
@@ -1,8 +1,5 @@
 @echo off
 
-@rem prevent the window from closing after an error
-if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
-
 set INSTALL_ENV_DIR=%cd%\installer_files\env
 set PATH=%INSTALL_ENV_DIR%;%INSTALL_ENV_DIR%\Library\bin;%INSTALL_ENV_DIR%\Scripts;%PATH%
 
@@ -12,3 +9,5 @@ if exist ".git" (
 )
 
 conda env update
+
+pause
