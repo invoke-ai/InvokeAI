e405385e0d1edafa629249c7f3768304a1f563d0
cmdr2 <secondary.cmdr2@gmail.com>
2022-10-13 10:56:04 +0530

    Prefer the locally installed conda over any global conda installation; activate the env before updating

--- invoke.sh
+++ invoke.sh
@@ -2,7 +2,7 @@
 
 if [ "$0" == "bash" ]; then
     INSTALL_ENV_DIR="$(pwd)/installer_files/env"
-    if [ -e "$INSTALL_ENV_DIR" ]; then export PATH="$PATH:$INSTALL_ENV_DIR/bin"; fi
+    if [ -e "$INSTALL_ENV_DIR" ]; then export PATH="$INSTALL_ENV_DIR/bin:$PATH"; fi
 
     CONDA_BASEPATH=$(conda info --base)
     source "$CONDA_BASEPATH/etc/profile.d/conda.sh" # otherwise conda complains about 'shell not initialized' (needed when running in a script)
