e73a2d68b58f5058bbdb4fc4a710341d74bcc847
cmdr2 <secondary.cmdr2@gmail.com>
2022-10-26 16:39:21 +0530

    Add Library\usr\bin to the PATH

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set INSTALL_ENV_DIR=%cd%\installer_files\env
-set PATH=%INSTALL_ENV_DIR%;%INSTALL_ENV_DIR%\Library\bin;%INSTALL_ENV_DIR%\Scripts;%PATH%
+set PATH=%INSTALL_ENV_DIR%;%INSTALL_ENV_DIR%\Library\bin;%INSTALL_ENV_DIR%\Scripts;%INSTALL_ENV_DIR%\Library\usr\bin;%PATH%
 
 @rem update the repo
 if exist ".git" (
