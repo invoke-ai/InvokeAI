d05373d35a03c58b8370d8ec3d8752b99bbde24c
Kyle Schouviller <kyle0654@hotmail.com>
2022-10-26 12:09:38 -0700

    Force RGB for img2img

--- ldm/invoke/generator/img2img.py
+++ ldm/invoke/generator/img2img.py
@@ -29,7 +29,7 @@ class Img2Img(Generator):
         )
 
         if isinstance(init_image, PIL.Image.Image):
-            init_image = self._image_to_tensor(init_image)
+            init_image = self._image_to_tensor(init_image.convert('RGB'))
 
         scope = choose_autocast(self.precision)
         with scope(self.model.device.type):
