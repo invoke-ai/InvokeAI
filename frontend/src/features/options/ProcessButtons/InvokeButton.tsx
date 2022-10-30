ca882ad5ff9dc5ff06bfc6ab3fefa78230f03c0b
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2022-10-31 07:55:15 +1300

    Reworked Invoke Icon Layout when unpinned

--- frontend/src/features/options/ProcessButtons/InvokeButton.tsx
+++ frontend/src/features/options/ProcessButtons/InvokeButton.tsx
@@ -1,14 +1,20 @@
 import { useHotkeys } from 'react-hotkeys-hook';
+import { BsImageFill } from 'react-icons/bs';
 import { generateImage } from '../../../app/socketio/actions';
 import { useAppDispatch, useAppSelector } from '../../../app/store';
 import IAIButton, {
   IAIButtonProps,
 } from '../../../common/components/IAIButton';
+import IAIIconButton from '../../../common/components/IAIIconButton';
 import useCheckParameters from '../../../common/hooks/useCheckParameters';
 import { activeTabNameSelector } from '../optionsSelectors';
 
-export default function InvokeButton(props: Omit<IAIButtonProps, 'label'>) {
-  const { ...rest } = props;
+interface InvokeButton extends Omit<IAIButtonProps, 'label'> {
+  iconButton?: boolean;
+}
+
+export default function InvokeButton(props: InvokeButton) {
+  const { iconButton = false, ...rest } = props;
   const dispatch = useAppDispatch();
   const isReady = useCheckParameters();
 
@@ -28,7 +34,19 @@ export default function InvokeButton(props: Omit<IAIButtonProps, 'label'>) {
     [isReady, activeTabName]
   );
 
-  return (
+  return iconButton ? (
+    <IAIIconButton
+      aria-label="Invoke"
+      type="submit"
+      icon={<BsImageFill />}
+      isDisabled={!isReady}
+      onClick={handleClickGenerate}
+      className="invoke-btn"
+      tooltip="Invoke"
+      tooltipPlacement="bottom"
+      {...rest}
+    />
+  ) : (
     <IAIButton
       label="Invoke"
       aria-label="Invoke"
--- frontend/src/features/tabs/ShowHideOptionsPanelButton.tsx
+++ frontend/src/features/tabs/ShowHideOptionsPanelButton.tsx
@@ -1,25 +1,55 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { IoMdOptions } from 'react-icons/io';
-import { useAppDispatch } from '../../app/store';
+import { RootState, useAppDispatch, useAppSelector } from '../../app/store';
 import IAIIconButton from '../../common/components/IAIIconButton';
-import { setShouldShowOptionsPanel } from '../options/optionsSlice';
+import {
+  OptionsState,
+  setShouldShowOptionsPanel,
+} from '../options/optionsSlice';
+import CancelButton from '../options/ProcessButtons/CancelButton';
+import InvokeButton from '../options/ProcessButtons/InvokeButton';
+import _ from 'lodash';
+import LoopbackButton from '../options/ProcessButtons/Loopback';
+
+const canInvokeSelector = createSelector(
+  (state: RootState) => state.options,
+
+  (options: OptionsState) => {
+    const { shouldPinOptionsPanel, shouldShowOptionsPanel } = options;
+    return {
+      shouldShowProcessButtons:
+        !shouldPinOptionsPanel || !shouldShowOptionsPanel,
+    };
+  },
+  { memoizeOptions: { resultEqualityCheck: _.isEqual } }
+);
 
 const ShowHideOptionsPanelButton = () => {
   const dispatch = useAppDispatch();
+  const { shouldShowProcessButtons } = useAppSelector(canInvokeSelector);
 
   const handleShowOptionsPanel = () => {
     dispatch(setShouldShowOptionsPanel(true));
   };
 
   return (
-    <IAIIconButton
-      tooltip="Show Options Panel (G)"
-      tooltipPlacement="top"
-      aria-label="Show Options Panel"
-      styleClass="floating-show-hide-button btn-options"
-      onMouseOver={handleShowOptionsPanel}
-    >
-      <IoMdOptions />
-    </IAIIconButton>
+    <div className="show-hide-button-options">
+      <IAIIconButton
+        tooltip="Show Options Panel (O)"
+        tooltipPlacement="top"
+        aria-label="Show Options Panel"
+        onClick={handleShowOptionsPanel}
+      >
+        <IoMdOptions />
+      </IAIIconButton>
+      {shouldShowProcessButtons && (
+        <>
+          <InvokeButton iconButton />
+          <LoopbackButton />
+          <CancelButton />
+        </>
+      )}
+    </div>
   );
 };
 
