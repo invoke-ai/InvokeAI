5a2790a69b11e049d2507bc22eab7812915d867f
damian0815 <d@d.com>
2022-11-01 13:19:20 +0100

    convert progress display to a drop-down

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -37,3 +37,9 @@ export const NUMPY_RAND_MIN = 0;
 export const NUMPY_RAND_MAX = 4294967295;
 
 export const FACETOOL_TYPES = ['gfpgan', 'codeformer'] as const;
+
+export const IN_PROGRESS_IMAGE_TYPES: Array<{ key: string; value: string }> = [
+  { key: "None", value: 'none'}, 
+  { key: "Full-res (slow)", value: 'full-res' },
+  { key: "Latents (fast)", value: 'latents' }
+];
