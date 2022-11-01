48aff0750f97a8713d32cf0d53c566c960bd0ab3
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2022-11-02 06:08:59 +1300

    Code Split Inpaint Options
    Isolate features to their own components so they dont re-render the other stuff each time.

--- frontend/src/features/options/AdvancedOptions/Inpainting/InpaintingSettings.tsx
+++ frontend/src/features/options/AdvancedOptions/Inpainting/InpaintingSettings.tsx
@@ -1,96 +1,13 @@
-import { useToast } from '@chakra-ui/react';
-import { createSelector } from '@reduxjs/toolkit';
-import React, { ChangeEvent } from 'react';
-import {
-  RootState,
-  useAppDispatch,
-  useAppSelector,
-} from '../../../../app/store';
-import IAIButton from '../../../../common/components/IAIButton';
-import {
-  InpaintingState,
-  setClearBrushHistory,
-  setInpaintReplace,
-  setShouldUseInpaintReplace,
-} from '../../../tabs/Inpainting/inpaintingSlice';
 import BoundingBoxSettings from './BoundingBoxSettings';
-import _ from 'lodash';
-import IAINumberInput from '../../../../common/components/IAINumberInput';
-import IAISwitch from '../../../../common/components/IAISwitch';
-
-const inpaintingSelector = createSelector(
-  (state: RootState) => state.inpainting,
-  (inpainting: InpaintingState) => {
-    const { pastLines, futureLines, inpaintReplace, shouldUseInpaintReplace } =
-      inpainting;
-    return {
-      pastLines,
-      futureLines,
-      inpaintReplace,
-      shouldUseInpaintReplace,
-    };
-  },
-  {
-    memoizeOptions: {
-      resultEqualityCheck: _.isEqual,
-    },
-  }
-);
+import InpaintReplace from './InpaintReplace';
+import ClearBrushHistory from './ClearBrushHistory';
 
 export default function InpaintingSettings() {
-  const dispatch = useAppDispatch();
-  const toast = useToast();
-
-  const { pastLines, futureLines, inpaintReplace, shouldUseInpaintReplace } =
-    useAppSelector(inpaintingSelector);
-
-  const handleClearBrushHistory = () => {
-    dispatch(setClearBrushHistory());
-    toast({
-      title: 'Brush Stroke History Cleared',
-      status: 'success',
-      duration: 2500,
-      isClosable: true,
-    });
-  };
   return (
     <>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          padding: '0 1rem 0 0.2rem',
-        }}
-      >
-        <IAINumberInput
-          label="Inpaint Replace"
-          value={inpaintReplace}
-          min={0}
-          max={1.0}
-          step={0.05}
-          width={'auto'}
-          formControlProps={{ style: { paddingRight: '1rem' } }}
-          isInteger={false}
-          isDisabled={!shouldUseInpaintReplace}
-          onChange={(v: number) => {
-            dispatch(setInpaintReplace(v));
-          }}
-        />
-        <IAISwitch
-          isChecked={shouldUseInpaintReplace}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            dispatch(setShouldUseInpaintReplace(e.target.checked))
-          }
-        />
-      </div>
+      <InpaintReplace />
       <BoundingBoxSettings />
-      <IAIButton
-        label="Clear Brush History"
-        onClick={handleClearBrushHistory}
-        tooltip="Clears brush stroke history"
-        disabled={futureLines.length > 0 || pastLines.length > 0 ? false : true}
-        styleClass="inpainting-options-btn"
-      />
+      <ClearBrushHistory />
     </>
   );
 }
