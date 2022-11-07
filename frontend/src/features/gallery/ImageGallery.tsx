eb5f35d446ff755df0919b343e6f0956864d5eef
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2022-11-08 02:30:10 +1300

    Fix outpainting gallery default size to accommodate new button

--- frontend/src/features/gallery/ImageGallery.tsx
+++ frontend/src/features/gallery/ImageGallery.tsx
@@ -76,7 +76,7 @@ export default function ImageGallery() {
       return;
     }
 
-    if (activeTabName === 'inpainting') {
+    if (activeTabName === 'inpainting' || activeTabName === 'outpainting') {
       dispatch(setGalleryWidth(190));
       setGalleryMinWidth(190);
       setGalleryMaxWidth(190);
