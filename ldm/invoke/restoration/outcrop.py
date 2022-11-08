2487040ae329abd690bea84ed23640911c047f4b
Lincoln Stein <lincoln.stein@gmail.com>
2022-11-08 17:27:42 +0000

    enhance outcropping with ability to direct contents of new regions
    - When outcropping an image you can now add a `--new_prompt` option, to specify
      a new prompt to be used instead of the original one used to generate the image.
    
    - Similarly you can provide a new seed using `--seed` (or `-S`). A seed of zero
      will pick one randomly.
    
    - This PR also fixes the crash that happened when trying to outcrop an image
      that does not contain InvokeAI metadata.

--- ldm/invoke/restoration/outcrop.py
+++ ldm/invoke/restoration/outcrop.py
@@ -32,8 +32,8 @@ class Outcrop(object):
             image_callback(img,preferred_seed,use_prefix=prefix,**kwargs)
 
         result= self.generate.prompt2image(
-            orig_opt.prompt,
-            seed        = orig_opt.seed if orig_opt.seed>0 else opt.seed,
+            opt.prompt,
+            seed        = opt.seed if opt.seed else orig_opt.seed,
             sampler     = self.generate.sampler,
             steps       = opt.steps,
             cfg_scale   = opt.cfg_scale,
