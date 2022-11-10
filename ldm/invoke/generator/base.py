4c3858e079255e096327e53ad2f7fbd653755782
Kevin Turner <83819+keturn@users.noreply.github.com>
2022-11-09 17:17:52 -0800

    model_cache: add ability to load a diffusers model pipeline
    and update associated things in Generate & Generator to not instantly fail when that happens

--- ldm/invoke/generator/base.py
+++ ldm/invoke/generator/base.py
@@ -9,8 +9,8 @@ import traceback
 
 import numpy as np
 import torch
-from PIL import Image, ImageFilter, ImageChops
-import cv2 as cv
+from PIL import Image, ImageFilter
+from diffusers import DiffusionPipeline
 from einops import rearrange
 from pytorch_lightning import seed_everything
 from tqdm import trange
@@ -26,9 +26,9 @@ class Generator:
     downsampling_factor: int
     latent_channels: int
     precision: str
-    model: DiffusionWrapper
+    model: DiffusionWrapper | DiffusionPipeline
 
-    def __init__(self, model: DiffusionWrapper, precision: str):
+    def __init__(self, model: DiffusionWrapper | DiffusionPipeline, precision: str):
         self.model = model
         self.precision = precision
         self.seed = None
