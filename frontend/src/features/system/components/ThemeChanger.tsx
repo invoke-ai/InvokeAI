8f8c8869c5622c34a15632d1048b13a37ec5914c
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2022-11-19 19:01:35 +1100

    Adds theme changer popover

--- frontend/src/features/system/components/ThemeChanger.tsx
+++ frontend/src/features/system/components/ThemeChanger.tsx
@@ -1,8 +1,10 @@
-import { useColorMode } from '@chakra-ui/react';
-import React, { ChangeEvent } from 'react';
+import { useColorMode, VStack } from '@chakra-ui/react';
 import { RootState, useAppDispatch, useAppSelector } from 'app/store';
-import IAISelect from 'common/components/IAISelect';
 import { setCurrentTheme } from 'features/options/store/optionsSlice';
+import IAIPopover from 'common/components/IAIPopover';
+import IAIIconButton from 'common/components/IAIIconButton';
+import { FaCheck, FaPalette } from 'react-icons/fa';
+import IAIButton from 'common/components/IAIButton';
 
 const THEMES = ['dark', 'light', 'green'];
 
@@ -13,17 +15,39 @@ export default function ThemeChanger() {
     (state: RootState) => state.options.currentTheme
   );
 
-  const themeChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
-    setColorMode(e.target.value);
-    dispatch(setCurrentTheme(e.target.value));
+  const handleChangeTheme = (theme: string) => {
+    setColorMode(theme);
+    dispatch(setCurrentTheme(theme));
   };
 
   return (
-    <IAISelect
-      validValues={THEMES}
-      value={currentTheme}
-      onChange={themeChangeHandler}
-      styleClass="theme-changer-dropdown"
-    ></IAISelect>
+    <IAIPopover
+      trigger="hover"
+      triggerComponent={
+        <IAIIconButton
+          aria-label="Theme"
+          size={'sm'}
+          variant="link"
+          data-variant="link"
+          fontSize={20}
+          icon={<FaPalette />}
+        />
+      }
+    >
+      <VStack align={'stretch'}>
+        {THEMES.map((theme) => (
+          <IAIButton
+            style={{
+              width: '6rem',
+            }}
+            leftIcon={currentTheme === theme ? <FaCheck /> : undefined}
+            size={'sm'}
+            onClick={() => handleChangeTheme(theme)}
+          >
+            {theme.charAt(0).toUpperCase() + theme.slice(1)}
+          </IAIButton>
+        ))}
+      </VStack>
+    </IAIPopover>
   );
 }
