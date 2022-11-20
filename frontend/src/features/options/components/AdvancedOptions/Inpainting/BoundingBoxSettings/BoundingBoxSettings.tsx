80f6f9a9315f3de55754cea1987685bcfd93043d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2022-11-20 19:58:47 +1100

    First pass on Canvas options panel

--- frontend/src/features/options/components/AdvancedOptions/Inpainting/BoundingBoxSettings/BoundingBoxSettings.tsx
+++ frontend/src/features/options/components/AdvancedOptions/Inpainting/BoundingBoxSettings/BoundingBoxSettings.tsx
@@ -1,23 +1,97 @@
-import { Flex } from '@chakra-ui/react';
-import BoundingBoxDarkenOutside from './BoundingBoxDarkenOutside';
-import BoundingBoxDimensionSlider from './BoundingBoxDimensionSlider';
-import BoundingBoxLock from './BoundingBoxLock';
-import BoundingBoxVisibility from './BoundingBoxVisibility';
+import { createSelector } from '@reduxjs/toolkit';
+import { useAppDispatch, useAppSelector } from 'app/store';
+import IAISlider from 'common/components/IAISlider';
+import { canvasSelector } from 'features/canvas/store/canvasSelectors';
+import { setBoundingBoxDimensions } from 'features/canvas/store/canvasSlice';
+import _ from 'lodash';
+
+const selector = createSelector(
+  canvasSelector,
+  (canvas) => {
+    const { boundingBoxDimensions } = canvas;
+    return {
+      boundingBoxDimensions,
+    };
+  },
+  {
+    memoizeOptions: {
+      resultEqualityCheck: _.isEqual,
+    },
+  }
+);
 
 const BoundingBoxSettings = () => {
+  const dispatch = useAppDispatch();
+  const { boundingBoxDimensions } = useAppSelector(selector);
+
+  const handleChangeWidth = (v: number) => {
+    dispatch(
+      setBoundingBoxDimensions({
+        ...boundingBoxDimensions,
+        width: Math.floor(v),
+      })
+    );
+  };
+
+  const handleChangeHeight = (v: number) => {
+    dispatch(
+      setBoundingBoxDimensions({
+        ...boundingBoxDimensions,
+        height: Math.floor(v),
+      })
+    );
+  };
+
+  const handleResetWidth = () => {
+    dispatch(
+      setBoundingBoxDimensions({
+        ...boundingBoxDimensions,
+        width: Math.floor(512),
+      })
+    );
+  };
+
+  const handleResetHeight = () => {
+    dispatch(
+      setBoundingBoxDimensions({
+        ...boundingBoxDimensions,
+        height: Math.floor(512),
+      })
+    );
+  };
+
   return (
     <div className="inpainting-bounding-box-settings">
       <div className="inpainting-bounding-box-header">
-        <p>Inpaint Box</p>
-        <BoundingBoxVisibility />
+        <p>Canvas Bounding Box</p>
       </div>
       <div className="inpainting-bounding-box-settings-items">
-        <BoundingBoxDimensionSlider dimension="width" label="Box W" />
-        <BoundingBoxDimensionSlider dimension="height" label="Box H" />
-        <Flex alignItems={'center'} justifyContent={'space-between'}>
-          <BoundingBoxDarkenOutside />
-          <BoundingBoxLock />
-        </Flex>
+        <IAISlider
+          label={'Width'}
+          min={64}
+          max={1024}
+          step={64}
+          value={boundingBoxDimensions.width}
+          onChange={handleChangeWidth}
+          handleReset={handleResetWidth}
+          sliderNumberInputProps={{ max: 4096 }}
+          withSliderMarks
+          withInput
+          withReset
+        />
+        <IAISlider
+          label={'Height'}
+          min={64}
+          max={1024}
+          step={64}
+          value={boundingBoxDimensions.height}
+          onChange={handleChangeHeight}
+          handleReset={handleResetHeight}
+          sliderNumberInputProps={{ max: 4096 }}
+          withSliderMarks
+          withInput
+          withReset
+        />
       </div>
     </div>
   );
