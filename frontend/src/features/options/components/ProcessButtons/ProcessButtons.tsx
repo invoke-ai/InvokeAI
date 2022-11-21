1b589ddfc05f345fee493407c2adc4387a9c91f5
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2022-11-21 19:13:46 +1100

    Moves Loopback to app settings

--- frontend/src/features/options/components/ProcessButtons/ProcessButtons.tsx
+++ frontend/src/features/options/components/ProcessButtons/ProcessButtons.tsx
@@ -1,6 +1,5 @@
 import InvokeButton from './InvokeButton';
 import CancelButton from './CancelButton';
-import LoopbackButton from './Loopback';
 
 /**
  * Buttons to start and cancel image generation.
@@ -9,7 +8,6 @@ const ProcessButtons = () => {
   return (
     <div className="process-buttons">
       <InvokeButton />
-      <LoopbackButton />
       <CancelButton />
     </div>
   );
