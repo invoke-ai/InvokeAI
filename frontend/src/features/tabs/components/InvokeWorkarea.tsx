916e795c2653eee0150a4b6bd5eacfed1e52a380
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2022-11-25 10:48:38 +1100

    Adds gallery drag and drop to img2img/canvas

--- frontend/src/features/tabs/components/InvokeWorkarea.tsx
+++ frontend/src/features/tabs/components/InvokeWorkarea.tsx
@@ -1,16 +1,21 @@
 import { Tooltip } from '@chakra-ui/react';
 import { createSelector } from '@reduxjs/toolkit';
-import { ReactNode } from 'react';
+import { DragEvent, ReactNode } from 'react';
 import { VscSplitHorizontal } from 'react-icons/vsc';
 import { RootState, useAppDispatch, useAppSelector } from 'app/store';
 import ImageGallery from 'features/gallery/components/ImageGallery';
 import { activeTabNameSelector } from 'features/options/store/optionsSelectors';
 import {
   OptionsState,
+  setInitialImage,
   setShowDualDisplay,
 } from 'features/options/store/optionsSlice';
-import { setDoesCanvasNeedScaling } from 'features/canvas/store/canvasSlice';
+import {
+  setDoesCanvasNeedScaling,
+  setInitialCanvasImage,
+} from 'features/canvas/store/canvasSlice';
 import _ from 'lodash';
+import useGetImageByUuid from 'features/gallery/hooks/useGetImageByUuid';
 
 const workareaSelector = createSelector(
   [(state: RootState) => state.options, activeTabNameSelector],
@@ -21,6 +26,7 @@ const workareaSelector = createSelector(
       shouldPinOptionsPanel,
       isLightBoxOpen,
       shouldShowDualDisplayButton: ['inpainting'].includes(activeTabName),
+      activeTabName,
     };
   },
   {
@@ -39,12 +45,29 @@ type InvokeWorkareaProps = {
 const InvokeWorkarea = (props: InvokeWorkareaProps) => {
   const dispatch = useAppDispatch();
   const { optionsPanel, children, styleClass } = props;
-  const { showDualDisplay, isLightBoxOpen, shouldShowDualDisplayButton } =
-    useAppSelector(workareaSelector);
+  const {
+    activeTabName,
+    showDualDisplay,
+    isLightBoxOpen,
+    shouldShowDualDisplayButton,
+  } = useAppSelector(workareaSelector);
+
+  const getImageByUuid = useGetImageByUuid();
 
   const handleDualDisplay = () => {
     dispatch(setShowDualDisplay(!showDualDisplay));
     dispatch(setDoesCanvasNeedScaling(true));
+  };
+
+  const handleDrop = (e: DragEvent<HTMLDivElement>) => {
+    const uuid = e.dataTransfer.getData('invokeai/imageUuid');
+    const image = getImageByUuid(uuid);
+    if (!image) return;
+    if (activeTabName === 'img2img') {
+      dispatch(setInitialImage(image));
+    } else if (activeTabName === 'unifiedCanvas') {
+      dispatch(setInitialCanvasImage(image));
+    }
   };
 
   return (
@@ -55,7 +78,7 @@ const InvokeWorkarea = (props: InvokeWorkareaProps) => {
     >
       <div className="workarea-main">
         {optionsPanel}
-        <div className="workarea-children-wrapper">
+        <div className="workarea-children-wrapper" onDrop={handleDrop}>
           {children}
           {shouldShowDualDisplayButton && (
             <Tooltip label="Toggle Split View">
