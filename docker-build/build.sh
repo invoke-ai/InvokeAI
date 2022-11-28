3f6d0fb7dad3821864ab3d1d7b45ea3c48d07b09
Matthias Wild <40327258+mauwii@users.noreply.github.com>
2022-11-28 18:20:25 +0100

    update dockerfile (#1551)
    * update dockerfile
    
    * remove not existing file from .dockerignore
    
    * remove bloat and unecesary step
    also use --no-cache-dir for pip install
    image is now close to 2GB
    
    * make Dockerfile a variable
    
    * set base image to `ubuntu:22.10`
    
    * add build-essential
    
    * link outputs folder for persistence
    
    * update tag variable
    
    * update docs
    
    * fix not customizeable build args, add reqs output

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -6,23 +6,17 @@ set -e
 
 source ./docker-build/env.sh || echo "please run from repository root" || exit 1
 
-invokeai_conda_version=${INVOKEAI_CONDA_VERSION:-py39_4.12.0-${platform/\//-}}
-invokeai_conda_prefix=${INVOKEAI_CONDA_PREFIX:-\/opt\/conda}
-invokeai_conda_env_file=${INVOKEAI_CONDA_ENV_FILE:-environment-lin-cuda.yml}
-invokeai_git=${INVOKEAI_GIT:-invoke-ai/InvokeAI}
-invokeai_branch=${INVOKEAI_BRANCH:-main}
-huggingface_token=${HUGGINGFACE_TOKEN?}
+pip_requirements=${PIP_REQUIREMENTS:-requirements-lin-cuda.txt}
+dockerfile=${INVOKE_DOCKERFILE:-docker-build/Dockerfile}
 
 # print the settings
 echo "You are using these values:"
+echo -e "Dockerfile:\t\t ${dockerfile}"
+echo -e "requirements:\t\t ${pip_requirements}"
 echo -e "project_name:\t\t ${project_name}"
 echo -e "volumename:\t\t ${volumename}"
 echo -e "arch:\t\t\t ${arch}"
 echo -e "platform:\t\t ${platform}"
-echo -e "invokeai_conda_version:\t ${invokeai_conda_version}"
-echo -e "invokeai_conda_prefix:\t ${invokeai_conda_prefix}"
-echo -e "invokeai_conda_env_file: ${invokeai_conda_env_file}"
-echo -e "invokeai_git:\t\t ${invokeai_git}"
 echo -e "invokeai_tag:\t\t ${invokeai_tag}\n"
 
 _runAlpine() {
@@ -35,50 +29,33 @@ _runAlpine() {
     alpine "$@"
 }
 
-_copyCheckpoints() {
-  echo "creating subfolders for models and outputs"
-  _runAlpine mkdir models
-  _runAlpine mkdir outputs
-  echo "downloading v1-5-pruned-emaonly.ckpt"
-  _runAlpine wget \
-    --header="Authorization: Bearer ${huggingface_token}" \
-    -O models/v1-5-pruned-emaonly.ckpt \
-    https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt
-  echo "done"
-}
-
 _checkVolumeContent() {
   _runAlpine ls -lhA /data/models
 }
 
-_getModelMd5s() {
-  _runAlpine  \
-    alpine sh -c "md5sum /data/models/*.ckpt"
-}
-
 if [[ -n "$(docker volume ls -f name="${volumename}" -q)" ]]; then
   echo "Volume already exists"
-  if [[ -z "$(_checkVolumeContent)" ]]; then
-    echo "looks empty, copying checkpoint"
-    _copyCheckpoints
-  fi
-  echo "Models in ${volumename}:"
-  _checkVolumeContent
 else
   echo -n "createing docker volume "
   docker volume create "${volumename}"
-  _copyCheckpoints
 fi
 
 # Build Container
 docker build \
   --platform="${platform}" \
-  --tag "${invokeai_tag}" \
-  --build-arg project_name="${project_name}" \
-  --build-arg conda_version="${invokeai_conda_version}" \
-  --build-arg conda_prefix="${invokeai_conda_prefix}" \
-  --build-arg conda_env_file="${invokeai_conda_env_file}" \
-  --build-arg invokeai_git="${invokeai_git}" \
-  --build-arg invokeai_branch="${invokeai_branch}" \
-  --file ./docker-build/Dockerfile \
+  --tag="${invokeai_tag}" \
+  --build-arg="PROJECT_NAME=${project_name}" \
+  --build-arg="PIP_REQUIREMENTS=${pip_requirements}" \
+  --file="${dockerfile}" \
   .
+
+docker run \
+  --rm \
+  --platform="$platform" \
+  --name="$project_name" \
+  --hostname="$project_name" \
+  --mount="source=$volumename,target=/data" \
+  --mount="type=bind,source=$HOME/.huggingface,target=/root/.huggingface" \
+  --env="HUGGINGFACE_TOKEN=${HUGGINGFACE_TOKEN}" \
+  "${invokeai_tag}" \
+  scripts/configure_invokeai.py --yes
