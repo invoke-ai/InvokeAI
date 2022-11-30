a2d51315d8f9876f1124daab5cca3dccb9e6d2aa
Lincoln Stein <lincoln.stein@gmail.com>
2022-11-30 21:51:03 +0000

    set pull to main for release

--- source_installer/install.sh
+++ source_installer/install.sh
@@ -86,7 +86,7 @@ if [ ! -e ".git" ]; then
     git config --local init.defaultBranch main
     git remote add origin "$REPO_URL"
     git fetch
-    git checkout origin/test-source-installer -ft
+    git checkout origin/main -ft
 fi
 
 # create the environment
