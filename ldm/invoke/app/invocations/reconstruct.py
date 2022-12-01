34e3aa1f88ea2addf9bb7142d29f2a13c473f42f
Kyle Schouviller <kyle0654@hotmail.com>
2022-11-30 21:33:20 -0800

    parent 9eed1919c2071f9199996df747c8638c4a75e8fb author Kyle Schouviller <kyle0654@hotmail.com> 1669872800 -0800 committer Kyle Schouviller <kyle0654@hotmail.com> 1676240900 -0800
    Adding base node architecture
    
    Fix type annotation errors
    
    Runs and generates, but breaks in saving session
    
    Fix default model value setting. Fix deprecation warning.
    
    Fixed node api
    
    Adding markdown docs
    
    Simplifying Generate construction in apps
    
    [nodes] A few minor changes (#2510)
    
    * Pin api-related requirements
    
    * Remove confusing extra CORS origins list
    
    * Adds response models for HTTP 200
    
    [nodes] Adding graph_execution_state to soon replace session. Adding tests with pytest.
    
    Minor typing fixes
    
    [nodes] Fix some small output query hookups
    
    [node] Fixing some additional typing issues
    
    [nodes] Move and expand graph code. Add base item storage and sqlite implementation.
    
    Update startup to match new code
    
    [nodes] Add callbacks to item storage
    
    [nodes] Adding an InvocationContext object to use for invocations to provide easier extensibility
    
    [nodes] New execution model that handles iteration
    
    [nodes] Fixing the CLI
    
    [nodes] Adding a note to the CLI
    
    [nodes] Split processing thread into separate service
    
    [node] Add error message on node processing failure
    
    Removing old files and duplicated packages
    
    Adding python-multipart

---
+++ ldm/invoke/app/invocations/reconstruct.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timezone
+from typing import Literal, Union
+from pydantic import Field
+from .image import ImageField, ImageOutput
+from .baseinvocation import BaseInvocation, InvocationContext
+from ..services.image_storage import ImageType
+from ..services.invocation_services import InvocationServices
+
+
+class RestoreFaceInvocation(BaseInvocation):
+    """Restores faces in an image."""
+    type: Literal['restore_face'] = 'restore_face'
+
+    # Inputs
+    image: Union[ImageField,None] = Field(description="The input image")
+    strength: float               = Field(default=0.75, gt=0, le=1, description="The strength of the restoration")
+
+
+    def invoke(self, context: InvocationContext) -> ImageOutput: 
+        image = context.services.images.get(self.image.image_type, self.image.image_name)
+        results = context.services.generate.upscale_and_reconstruct(
+            image_list     = [[image, 0]],
+            upscale        = None,
+            strength       = self.strength, # GFPGAN strength
+            save_original  = False,
+            image_callback = None,
+        )
+
+        # Results are image and seed, unwrap for now
+        # TODO: can this return multiple results?
+        image_type = ImageType.RESULT
+        image_name = context.services.images.create_name(context.graph_execution_state_id, self.id)
+        context.services.images.save(image_type, image_name, results[0][0])
+        return ImageOutput(
+            image = ImageField(image_type = image_type, image_name = image_name)
+        )
