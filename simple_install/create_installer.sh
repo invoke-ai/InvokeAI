37369da8cab252b6204cb1c016410862c09ebd9b
Lincoln Stein <lincoln.stein@gmail.com>
2022-12-07 16:05:24 +0000

    enable update scripts to update requirements
    - Also pin requirements to known working commits.
    - This may be a breaking change; exercise with caution
    - No functional testing performed yet!

--- simple_install/create_installer.sh
+++ simple_install/create_installer.sh
@@ -4,6 +4,9 @@ cd "$(dirname "$0")"
 
 VERSION=$(grep ^VERSION ../setup.py | awk '{ print $3 }' | sed "s/'//g" )
 
+echo "Be certain that you're in the 'installer' directory before continuing."
+read -p "Press any key to continue, or CTRL-C to exit..."
+
 echo Building installer zip fles for InvokeAI v$VERSION
 
 # get rid of any old ones
@@ -22,8 +25,8 @@ mkdir InvokeAI-Installer/templates/rootdir/{outputs,embeddings,models}
 cp install.sh.in InvokeAI-Installer/install.sh
 chmod a+rx InvokeAI-Installer/install.sh
 
-zip -r InvokeAI-simple-installer-$VERSION-linux.zip InvokeAI-Installer
-zip -r InvokeAI-simple-installer-$VERSION-mac.zip InvokeAI-Installer
+zip -r InvokeAI-installer-$VERSION-linux.zip InvokeAI-Installer
+zip -r InvokeAI-installer-$VERSION-mac.zip InvokeAI-Installer
 
 # now do the windows installer
 rm InvokeAI-Installer/install.sh
@@ -33,7 +36,7 @@ cp install.bat.in InvokeAI-Installer/install.bat
 # because it is easier to do it now than in the .bat install script
 egrep -v '^-e .' InvokeAI-Installer/environments-and-requirements/requirements-win-colab-cuda.txt >requirements.txt
 mv requirements.txt InvokeAI-Installer/environments-and-requirements/requirements-win-colab-cuda.txt
-zip -r InvokeAI-simple-installer-$VERSION-windows.zip InvokeAI-Installer
+zip -r InvokeAI-installer-$VERSION-windows.zip InvokeAI-Installer
 
 # clean up
 rm -rf InvokeAI-Installer
