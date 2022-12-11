e7144055d1410ab828ff5d15d0b5b304dcc13bf2
Lincoln Stein <lincoln.stein@gmail.com>
2022-12-11 13:03:05 +0000

    make webGUI model changing work again
    - Using relative root addresses was causing problems when the
      current working directory was changed after start time.
    - This commit makes the root address absolute at start time, such
      that changing the working directory later on doesn't break anything.

--- ldm/invoke/globals.py
+++ ldm/invoke/globals.py
@@ -16,7 +16,7 @@ from argparse import Namespace
 Globals = Namespace()
 
 # This is usually overwritten by the command line and/or environment variables
-Globals.root = os.environ.get('INVOKEAI_ROOT') or os.path.expanduser('~/invokeai')
+Globals.root = os.path.abspath(os.environ.get('INVOKEAI_ROOT') or os.path.expanduser('~/invokeai'))
 
 # Where to look for the initialization file
 Globals.initfile = 'invokeai.init'
