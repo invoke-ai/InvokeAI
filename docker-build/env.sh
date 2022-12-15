eb38af9fa2e67eef613cdfa9d5e83b534cc4b095
mauwii <Mauwii@outlook.de>
2022-12-15 16:35:35 +0100

    update env.sh
    - read REPOSITORY_NAME from env if available
    - add comment to explain the intension of this file
    - remove unecesarry exports

--- docker-build/env.sh
+++ docker-build/env.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-repository_name=$(basename "$(git rev-parse --show-toplevel)")
+# Variables shared by build.sh and run.sh
+repository_name=${REPOSITORY_NAME:-$(basename "$(git rev-parse --show-toplevel)")}
 repository_name_lc=${repository_name,,}
 volumename=${VOLUMENAME:-${repository_name_lc}_data}
 arch=${ARCH:-$(arch)}
 platform=${PLATFORM:-Linux/${arch}}
 container_flavor=${CONTAINER_FLAVOR:-cuda}
 invokeai_tag=${repository_name_lc}-${container_flavor}:${INVOKEAI_TAG:-latest}
-
-export repository_name
-export volumename
-export arch
-export platform
-export invokeai_tag
