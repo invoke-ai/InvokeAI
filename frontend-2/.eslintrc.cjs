4743305e6b69fe8a1922c8f4b740138dae3333ef
Artur <necvetaev@gmail.com>
2022-12-17 17:59:33 +0100

    update, frontend-2 deleted

--- frontend-2/.eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'eslint-plugin-react-hooks'],
-  root: true,
-};
--- frontend-2/index.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'redux-socket.io-middleware';
