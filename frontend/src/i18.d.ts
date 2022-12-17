75bd11f3ee8a1dfc692ac509b162a6926012f40a
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2022-12-18 08:01:47 +1300

    Localize Gallery & Bug FIxes
    Fix a bug where the delete image from the context menu wasn't working. Removed tooltips that were broken as they don't work in context menu.

--- frontend/src/i18.d.ts
+++ frontend/src/i18.d.ts
@@ -4,6 +4,7 @@ import 'i18next';
 import common from './locales/common/en.json';
 import unifiedcanvas from './locales/unifiedcanvas/en.json';
 import options from './locales/options/en.json';
+import gallery from './locales/gallery/en.json';
 
 declare module 'i18next' {
   // Extend CustomTypeOptions
@@ -15,6 +16,7 @@ declare module 'i18next' {
       common: typeof common;
       unifiedcanvas: typeof unifiedcanvas;
       options: typeof options;
+      gallery: typeof gallery;
     };
     // Never Return Null
     returnNull: false;
