22433f07c88100bee8b26327f6067af964c05c41
Damian Stewart <null@damianstewart.com>
2022-12-18 11:50:01 +0100

    Don't crash CLI on exceptions

--- ldm/generate.py
+++ ldm/generate.py
@@ -545,7 +545,7 @@ class Generate:
                 print('**Interrupted** Partial results will be returned.')
             else:
                 raise KeyboardInterrupt
-        except RuntimeError as e:
+        except (RuntimeError, Exception) as e:
             print(traceback.format_exc(), file=sys.stderr)
             print('>> Could not generate image.')
 
