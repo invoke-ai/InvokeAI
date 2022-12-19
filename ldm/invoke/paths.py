e3ae89f0338ac4c895f5ad1163ec57f7abfbc013
Eugene Brodsky <ebr@users.noreply.github.com>
2022-12-19 00:20:04 -0500

    (paths) shared paths config root dir behaviour correct, with tests

--- ldm/invoke/paths.py
+++ ldm/invoke/paths.py
@@ -8,50 +8,52 @@ from dataclasses import dataclass
 import inspect
 import os
 
+DEFAULT_ROOT_DIR = "~/invokeai"
+
+
 @dataclass
 class PathSpec:
     kind: str
     description: str
     location: Path
 
+
 class InvokePaths:
     """
     Singleton class to manage paths
     """
+
     def __new__(cls):
         if not hasattr(cls, "instance"):
             cls.instance = super(InvokePaths, cls).__new__(cls)
         return cls.instance
 
-    # def __init__(self, path: Union[str, Path] = None) -> None:
-
-    #     rootdir = self.rootdir(path)
-
-    # def set_outputs(self, path: Union[str, Path] = None) -> Path:
-    #     self.outputs.location = self.root.location / "outputs" if path is None else Path(path)
-    #     return self.outputs.location
-
-    ### PathSpecs ###
+    def __init__(
+        self, root: Union[str, Path] = None, outdir: Union[str, Path] = None
+    ) -> None:
+        self.root = root
+        self.outdir = outdir
 
     @property
-    def rootdir(self) -> PathSpec:
+    def root(self):
         return PathSpec(
-            kind = "directory",
-            description = "InvokeAI application",
-            location = Path("~/invokeai")
+            kind="directory", description="InvokeAI application", location=self._root
         )
 
-    @rootdir.setter
-    def rootdir(self, path: Union[str, Path] = None) -> None:
+    @root.setter
+    def root(self, path: Union[str, Path] = None) -> None:
         if path is None:
             if os.environ.get("INVOKEAI_ROOT"):
                 location = Path(os.environ.get("INVOKEAI_ROOT"))
             elif os.environ.get("VIRTUAL_ENV"):
+                # how to handle this in non-automated-install situations?
+                # https://github.com/invoke-ai/InvokeAI/issues/2064
                 location = Path(os.environ.get("VIRTUAL_ENV")).parent
             else:
-                location = Path("~/invokeai")
-
-        self.rootdir.location = Path(location).expanduser().resolve()
+                location = Path(DEFAULT_ROOT_DIR)
+        else:
+            location = Path(path)
+        self._root = location
 
     @property
     def models(self) -> PathSpec:
---
+++ tests/test_paths.py
@@ -0,0 +1,135 @@
+import pytest
+from ldm.invoke.paths import InvokePaths, DEFAULT_ROOT_DIR
+from pathlib import Path
+
+@pytest.fixture(autouse=True)
+def reset_path_singleton():
+    InvokePaths._instances = {}
+
+class TestInvokePathsSingleton:
+    """
+    Test correct singleton behaviour
+    """
+
+    def test_identity(self):
+        p1 = InvokePaths()
+        p2 = InvokePaths()
+
+        assert p1 is p2
+
+    def test_override_envvar_transfers(self, monkeypatch):
+        monkeypatch.delenv("VIRTUAL_ENV", raising = False)
+
+        monkeypatch.setenv("INVOKEAI_ROOT", "/someplace")
+        p1 = InvokePaths()
+        p2 = InvokePaths()
+
+        assert p1.root.location == Path("/someplace")
+
+        p2.root = "~/some-other-place"
+
+        assert p1.root.location == Path("~/some-other-place")
+        assert p1.root.location is p2.root.location
+
+class TestInvokePathsRootDir:
+
+    def test_fallback(self, monkeypatch):
+        """
+        When root dir isn't set and no env vars are set, it should fall back to default
+        """
+
+        monkeypatch.delenv("VIRTUAL_ENV", raising = False)
+        monkeypatch.delenv("INVOKEAI_ROOT", raising = False)
+        p = InvokePaths()
+        assert p.root.location == Path(DEFAULT_ROOT_DIR)
+
+    def test_root_envvar(self, monkeypatch):
+        """
+        When INVOKEAI_ROOT env var is set, it should be used
+        """
+
+        monkeypatch.delenv("VIRTUAL_ENV", raising = False)
+
+        for location in [
+            "/absolute/path/invokeai",
+            "relative/path/invokeai",
+            "~/home/path/invokeai",
+        ]:
+            monkeypatch.setenv("INVOKEAI_ROOT", location)
+            p = InvokePaths()
+            assert p.root.location == Path(location)
+
+
+    def test_virtualenv_envvar(self, monkeypatch):
+        """
+        When VIRTUAL_ENV env var is set, it should be used
+        """
+
+        monkeypatch.delenv("INVOKEAI_ROOT", raising = False)
+        monkeypatch.setenv("VIRTUAL_ENV", f"{DEFAULT_ROOT_DIR}/.venv")
+        p = InvokePaths()
+        assert p.root.location == Path(DEFAULT_ROOT_DIR)
+
+    def test_both_envvars(self, monkeypatch):
+        """
+        When both envvars are set, the INVOKEAI_ROOT env var should take precedence
+        """
+
+        monkeypatch.setenv("INVOKEAI_ROOT", "/someplace")
+        monkeypatch.setenv("VIRTUAL_ENV", f"{DEFAULT_ROOT_DIR}/.venv")
+        p = InvokePaths()
+        assert p.root.location == Path("/someplace")
+
+# class TestInvokePathsOutputsDir:
+
+    # def test_changing_root_dir_default_outdir():
+
+    # def test_changing_root_dir_custom_outdir():
+
+
+    # def test_fallback(self, monkeypatch):
+    #     """
+    #     When root dir isn't set and no env vars are set, it should fall back to default
+    #     """
+
+    #     monkeypatch.delenv("VIRTUAL_ENV", raising = False)
+    #     monkeypatch.delenv("INVOKEAI_ROOT", raising = False)
+    #     p = InvokePaths()
+    #     assert p.root.location == Path(DEFAULT_ROOT_DIR)
+
+    # def test_root_envvar(self, monkeypatch):
+    #     """
+    #     When INVOKEAI_ROOT env var is set, it should be used
+    #     """
+
+    #     monkeypatch.delenv("VIRTUAL_ENV", raising = False)
+
+    #     for location in [
+    #         "/absolute/path/invokeai",
+    #         "relative/path/invokeai",
+    #         "~/home/path/invokeai",
+    #     ]:
+    #         monkeypatch.setenv("INVOKEAI_ROOT", location)
+    #         p = InvokePaths()
+    #         assert p.root.location == Path(location)
+
+
+    # def test_virtualenv_envvar(self, monkeypatch):
+    #     """
+    #     When VIRTUAL_ENV env var is set, it should be used
+    #     """
+
+    #     monkeypatch.delenv("INVOKEAI_ROOT", raising = False)
+    #     monkeypatch.setenv("VIRTUAL_ENV", f"{DEFAULT_ROOT_DIR}/.venv")
+    #     p = InvokePaths()
+    #     assert p.root.location == Path(DEFAULT_ROOT_DIR)
+
+    # def test_both_envvars(self, monkeypatch):
+    #     """
+    #     When both envvars are set, the INVOKEAI_ROOT env var should take precedence
+    #     """
+
+    #     monkeypatch.setenv("INVOKEAI_ROOT", "/someplace")
+    #     monkeypatch.setenv("VIRTUAL_ENV", f"{DEFAULT_ROOT_DIR}/.venv")
+    #     p = InvokePaths()
+    #     assert p.root.location == Path("/someplace")
