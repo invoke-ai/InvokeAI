b30eee190a4e37b3416afb489b2bea5d9a84dd0e
Kent Keirsey <31807370+hipsterusername@users.noreply.github.com>
2022-12-23 10:45:38 -0500

    Fix dispatch dependency error from prettier

--- frontend/src/features/tabs/components/ImageToImage/ImageToImagePanel.tsx
+++ frontend/src/features/tabs/components/ImageToImage/ImageToImagePanel.tsx
@@ -60,7 +60,7 @@ export default function ImageToImagePanel() {
       const handleChangeHiresFix = () => dispatch(setHiresFix(false));
       handleChangeHiresFix();
     }
-  }, [activeTabName]);
+  }, [activeTabName, dispatch]);
 
   return (
     <InvokeOptionsPanel>
