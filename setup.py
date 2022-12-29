ee354a14be9c9b21aded79ffa8faad0efa8039c3
mauwii <Mauwii@outlook.de>
2022-12-29 04:17:37 +0100

    update pyproject.toml, add legacy setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+To be compatible with legacy builds
+"""
+import os
+from setuptools import setup
+
+def list_files(directory):
+    return [os.path.join(directory,x) for x in os.listdir(directory) if os.path.isfile(os.path.join(directory,x))]
+
+setup(
+    data_files=[('frontend/dist',list_files('frontend/dist')),
+            ('frontend/dist/assets',list_files('frontend/dist/assets')),
+            ('assets',['assets/caution.png']),
+            ('configs',list_files('configs')),
+            ('configs/stable-diffusion',list_files('configs/stable-diffusion')),
+    ],
+)
