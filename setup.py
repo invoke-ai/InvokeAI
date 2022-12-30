0b5d2610cdc3a7d62397b4c2b480a5c9897c1207
mauwii <Mauwii@outlook.de>
2022-12-30 08:47:43 +0100

    only use pyproject.toml - recommended for new projects and will soon be the default - setup.py is only needed as editable build hook - setup.cfg can't do things which pyproject.toml cant

--- setup.py
+++ setup.py
@@ -1,32 +1,28 @@
 """
 To be compatible with legacy builds
 """
-import os
 from setuptools import setup
 
-
-def list_files(directory):
-    """
-    returns all files in a provided directory
-    """
-    return [
-        os.path.join(directory, x)
-        for x in os.listdir(directory)
-        if os.path.isfile(os.path.join(directory, x))
-    ]
+setup()
 
 
-setup(
-    data_files=[
-        ("frontend/dist", list_files("frontend/dist")),
-        ("frontend/dist/assets", list_files("frontend/dist/assets")),
-        ("assets", ["assets/caution.png"]),
-        ("configs", list_files("configs")),
-        ("configs/stable-diffusion", list_files("configs/stable-diffusion")),
-        ("scripts", list_files("scripts")),
-        # ("environments-and-requirements",[
-        #     "environments-and-requirements/requirements-base.txt"
-        #     ],
-        # ),
-    ],
-)
+# def list_files(directory):
+#     """
+#     returns all files in a provided directory
+#     """
+#     return [
+#         os.path.join(directory, x)
+#         for x in os.listdir(directory)
+#         if os.path.isfile(os.path.join(directory, x))
+#     ]
+
+
+# setup(
+#     data_files=[
+#         ("frontend/dist", "./frontend/dist"),
+#         ("assets", ["assets/caution.png"]),
+#         ("configs", list_files("configs")),
+#         ("configs/stable-diffusion", list_files("configs/stable-diffusion")),
+#         ("scripts", list_files("scripts")),
+#     ],
+# )
