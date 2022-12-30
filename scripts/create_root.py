f5badd19df5db0ba158d91e6e2c235bb1e8841d9
mauwii <Mauwii@outlook.de>
2022-12-30 16:17:18 +0100

    update create_root.py

--- scripts/create_root.py
+++ scripts/create_root.py
@@ -6,40 +6,36 @@ import os
 import shutil
 import sys
 
-from ldm.invoke.globals import Globals
-
 
 def main():
     """
     main function
     """
     osp = os.path
-    source_prefix = os.path.join(
-        sys.prefix,
-        sys.platlibdir,
-        os.listdir(os.path.join(sys.prefix, sys.platlibdir))[0],
-        "site-packages",
-    )
-    dirs_to_copy = "configs", "frontend", "scripts"
-    done: int = 0
 
     def copy_from_venv(folder):
         """
-        function to copy the requested directory tree from source_prefix to Globals.root
+        function to copy directorys from the venv's  tree from source_prefix to Globals.root
         usefull since the config script will not work in a empty directory.
         """
-        # src_folder = osp.abspath(osp.join(os.environ.get("VIRTUAL_ENV"), folder))
-        src_folder = osp.abspath(osp.join(source_prefix, folder))
-        if Globals.root:
-            dest_folder = Globals.root
+        if os.getenv("INVOKEAI_ROOT"):
+            dest_folder = osp.abspath(osp.join(os.getenv("INVOKEAI_ROOT"), folder))
         else:
-            dest_folder = osp.abspath(
-                osp.join(os.environ.get("VIRTUAL_ENV"), "..", folder)
-            )
-        # dest_folder = osp.join(Globals.root, folder)
+            dest_folder = osp.abspath(osp.join(os.getcwd(), folder))
+
+        source_prefix = os.path.join(
+            sys.prefix,
+            sys.platlibdir,
+            os.listdir(os.path.join(sys.prefix, sys.platlibdir))[0],
+            "site-packages",
+        )
+        src_folder = osp.abspath(osp.join(source_prefix, folder))
         print(f"trying to copy {src_folder} to {dest_folder}")
         shutil.copytree(src_folder, dest_folder)
 
+    # run the copy job
+    dirs_to_copy = "configs", "frontend", "scripts"
+    done = 0
     for tree in dirs_to_copy:
         try:
             copy_from_venv(folder=tree)
@@ -51,6 +47,7 @@ def main():
         except RuntimeError as err:
             print(err)
 
+    # if succeesfull, run configure_invokeai afterwards◊
     if int(len(dirs_to_copy)) == done:
         from scripts import configure_invokeai
 
