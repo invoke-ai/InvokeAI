08c5d4383fcdcc50212f824548c20820b13f1572
mauwii <Mauwii@outlook.de>
2022-12-30 12:27:17 +0100

    further cleaning of pyproject.toml

--- setup.py
+++ setup.py
@@ -1,28 +1,6 @@
 """
-To be compatible with legacy builds
+necesarry to enable editable builds
 """
 from setuptools import setup
 
 setup()
-
-
-# def list_files(directory):
-#     """
-#     returns all files in a provided directory
-#     """
-#     return [
-#         os.path.join(directory, x)
-#         for x in os.listdir(directory)
-#         if os.path.isfile(os.path.join(directory, x))
-#     ]
-
-
-# setup(
-#     data_files=[
-#         ("frontend/dist", "./frontend/dist"),
-#         ("assets", ["assets/caution.png"]),
-#         ("configs", list_files("configs")),
-#         ("configs/stable-diffusion", list_files("configs/stable-diffusion")),
-#         ("scripts", list_files("scripts")),
-#     ],
-# )
