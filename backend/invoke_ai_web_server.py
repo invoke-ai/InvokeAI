ff9d21eae39661793a3906a0e6b0304fb35f5a99
mauwii <Mauwii@outlook.de>
2023-01-01 04:04:07 +0100

    replace find_frontend

--- backend/invoke_ai_web_server.py
+++ backend/invoke_ai_web_server.py
@@ -1,4 +1,5 @@
 import eventlet
+import frontend
 import glob
 import os
 import shutil
@@ -87,7 +88,7 @@ class InvokeAIWebServer:
         if opt.cors:
             socketio_args["cors_allowed_origins"] = opt.cors
 
-        frontend_path = self.find_frontend()
+        frontend_path = frontend.__path__[0]
         self.app = Flask(
             __name__, static_url_path="", static_folder=frontend_path
         )
@@ -246,18 +247,6 @@ class InvokeAIWebServer:
                     certfile=args.certfile,
                     keyfile=args.keyfile,
                 )
-
-    def find_frontend(self):
-        my_dir = os.path.dirname(__file__)
-        # LS: setup.py seems to put the frontend in different places on different systems, so
-        # this is fragile and needs to be replaced with a better way of finding the front end.
-        for candidate in (os.path.join(my_dir,'..','frontend','dist'),          # pip install -e .
-                          os.path.join(my_dir,'../../../../frontend','dist'),   # pip install . (Linux, Mac)
-                          os.path.join(my_dir,'../../../frontend','dist'),      # pip install . (Windows)
-        ):
-            if os.path.exists(candidate):
-                return candidate
-        assert "Frontend files cannot be found. Cannot continue"
 
     def setup_app(self):
         self.result_url = "outputs/"
