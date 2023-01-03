f77f61205120c3b3a112e870f83942aff1b1d755
mauwii <Mauwii@outlook.de>
2023-01-03 07:52:38 +0100

    update `ldm/invoke/__init__.py` - initialize pytorch variable if needed

--- ldm/invoke/__init__.py
+++ ldm/invoke/__init__.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+if sys.platform == 'darwin':
+    os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+
 __app_id__ = "invoke-ai/InvokeAI"
 __app_name__ = "InvokeAI"
 __version__ = "2.2.4.7"
