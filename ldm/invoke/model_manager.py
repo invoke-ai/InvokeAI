e735652a13ce1e637eaff1fcd9dabae7613c4726
Lincoln Stein <lincoln.stein@gmail.com>
2023-01-10 03:25:00 +0000

    fix dictconfig-not-serializable issue

--- ldm/invoke/model_manager.py
+++ ldm/invoke/model_manager.py
@@ -163,7 +163,6 @@ class ModelManager(object):
         for name in self.config:
             stanza = self.config[name]
             models[name] = dict()
-            
             format = stanza.get('format','ckpt') # Determine Format
 
             # Common Attribs
@@ -177,36 +176,26 @@ class ModelManager(object):
             
             # Checkpoint Config Parse
             if format == 'ckpt':
-                config = stanza.get('config', None)
-                weights = stanza.get('weights', None)
-                vae = stanza.get('vae', None)
-                width = stanza.get('width', 512)
-                height = stanza.get('height', 512)
                 models[name].update(
                     description = description,
                     format = format,
-                    config = config,
-                    weights = weights,
-                    vae = vae,
-                    width = width,
-                    height = height,
-                    status = status
+                    config = str(stanza.get('config', None)),
+                    weights = str(stanza.get('weights', None)),
+                    vae = str(stanza.get('vae', None)),
+                    width = str(stanza.get('width', 512)),
+                    height = str(stanza.get('height', 512)),
+                    status = status,
                 )
                 
             # Diffusers Config Parse
             if format == 'diffusers':
-                repo_id = stanza.get('repo_id', None)
-                vae = None
-                if 'vae' in stanza:
-                    vae = stanza['vae'].get('repo_id',None)
                 models[name].update(
                     description = description,
-                    format = format,
-                    repo_id = repo_id,
-                    vae = vae,
-                    status = status
+                    repo_id = str(stanza.get('repo_id', None)),
+                    path = str(stanza.get('path',None)),
+                    status = status,
                 )
-
+        
         return models
 
     def print_models(self) -> None:
