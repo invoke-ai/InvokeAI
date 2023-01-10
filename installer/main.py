2961d6af648b0664a109bfa3dc8cd684eab71adb
Eugene Brodsky <ebr@users.noreply.github.com>
2023-01-10 16:55:57 -0500

    (installer) use prompt-toolkit for directory picking instead of tkinter

--- installer/main.py
+++ installer/main.py
@@ -2,12 +2,20 @@
 InvokeAI Installer
 """
 
+import argparse
 from installer import Installer
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-d", "--destination", type=str, help="Destination path for installation", default="~/invokeai")
+
+    args = parser.parse_args()
+
     inst = Installer()
+
     try:
-        inst.install()
+        inst.install(path=args.destination)
     except KeyboardInterrupt as exc:
         print("\n")
         print("Ctrl-C pressed. Aborting.")
