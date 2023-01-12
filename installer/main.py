93129fde325495050d343f7368e8ceca366483e6
Eugene Brodsky <ebr@users.noreply.github.com>
2023-01-12 00:56:47 -0500

    (installer) run configure_invokeai from within the installer

--- installer/main.py
+++ installer/main.py
@@ -8,14 +8,14 @@ from installer import Installer
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-d", "--destination", type=str, help="Destination path for installation", default="~/invokeai")
+    parser.add_argument("-r", "--root", type=str, help="Destination path for installation", default="~/invokeai")
 
     args = parser.parse_args()
 
     inst = Installer()
 
     try:
-        inst.install(path=args.destination)
+        inst.install(path=args.root)
     except KeyboardInterrupt as exc:
         print("\n")
         print("Ctrl-C pressed. Aborting.")
