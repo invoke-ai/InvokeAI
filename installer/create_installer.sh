2e7a002308b7d9855b8ebe23483e28758aa18057
Eugene Brodsky <ebr@users.noreply.github.com>
2023-01-29 20:10:51 -0500

    (installer) remove unnecessary shell options from the install wrapper script

--- installer/create_installer.sh
+++ installer/create_installer.sh
@@ -57,8 +57,8 @@ mv dist/*.whl InvokeAI-Installer/
 
 # Install scripts
 # Mac/Linux
-perl -p -e "s/^INVOKEAI_VERSION=.*/INVOKEAI_VERSION=\"$VERSION\"/" install.sh.in > InvokeAI-Installer/install.sh
-chmod a+rx InvokeAI-Installer/install.sh
+cp install.sh.in InvokeAI-Installer/install.sh
+chmod a+x InvokeAI-Installer/install.sh
 
 # Windows
 perl -p -e "s/^set INVOKEAI_VERSION=.*/set INVOKEAI_VERSION=$VERSION/" install.bat.in > InvokeAI-Installer/install.bat
