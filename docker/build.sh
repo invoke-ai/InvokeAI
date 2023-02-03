01a2b8c05bfd73308843152b8b2eefcfb1666356
Matthias Wild <40327258+mauwii@users.noreply.github.com>
2023-02-03 23:34:47 +0100

    Adapt latest changes to Dockerfile (#2478)
    * remove non maintained Dockerfile
    
    * adapt Docker related files to latest changes
    - also build the frontend when building the image
    - skip user response if INVOKE_MODEL_RECONFIGURE is set
    - split INVOKE_MODEL_RECONFIGURE to support more than one argument
    
    * rename `docker-build` dir to `docker`
    
    * update build-container.yml
    - rename image to invokeai
    - add cpu flavor
    - add metadata to build summary
    - enable caching
    - remove build-cloud-img.yml
    
    * fix yarn cache path, link copyjob

--- docker/build.sh
+++ docker/build.sh
@@ -2,30 +2,31 @@
 set -e
 
 # How to use: https://invoke-ai.github.io/InvokeAI/installation/INSTALL_DOCKER/#setup
-#
 # Some possible pip extra-index urls (cuda 11.7 is available without extra url):
-#
 #   CUDA 11.6:  https://download.pytorch.org/whl/cu116
 #   ROCm 5.2:   https://download.pytorch.org/whl/rocm5.2
 #   CPU:        https://download.pytorch.org/whl/cpu
-#
 #   as found on https://pytorch.org/get-started/locally/
 
-cd "$(dirname "$0")" || exit 1
+SCRIPTDIR=$(dirname "$0")
+cd "$SCRIPTDIR" || exit 1
 
 source ./env.sh
 
-DOCKERFILE=${INVOKE_DOCKERFILE:-"./Dockerfile"}
+DOCKERFILE=${INVOKE_DOCKERFILE:-Dockerfile}
 
 # print the settings
 echo -e "You are using these values:\n"
-echo -e "Dockerfile:\t ${DOCKERFILE}"
-echo -e "extra-index-url: ${PIP_EXTRA_INDEX_URL:-none}"
-echo -e "Volumename:\t ${VOLUMENAME}"
-echo -e "arch:\t\t ${ARCH}"
-echo -e "Platform:\t ${PLATFORM}"
-echo -e "Invokeai_tag:\t ${INVOKEAI_TAG}\n"
+echo -e "Dockerfile: \t${DOCKERFILE}"
+echo -e "index-url: \t${PIP_EXTRA_INDEX_URL:-none}"
+echo -e "Volumename: \t${VOLUMENAME}"
+echo -e "Platform: \t${PLATFORM}"
+echo -e "Registry: \t${CONTAINER_REGISTRY}"
+echo -e "Repository: \t${CONTAINER_REPOSITORY}"
+echo -e "Container Tag: \t${CONTAINER_TAG}"
+echo -e "Container Image: ${CONTAINER_IMAGE}\n"
 
+# Create docker volume
 if [[ -n "$(docker volume ls -f name="${VOLUMENAME}" -q)" ]]; then
     echo -e "Volume already exists\n"
 else
@@ -36,7 +37,7 @@ fi
 # Build Container
 docker build \
     --platform="${PLATFORM}" \
-    --tag="${INVOKEAI_TAG}" \
-    ${PIP_EXTRA_INDEX_URL:+--build-arg=PIP_EXTRA_INDEX_URL="${PIP_EXTRA_INDEX_URL}"} \
+    --tag="${CONTAINER_IMAGE}" \
+    ${PIP_EXTRA_INDEX_URL:+--build-arg="PIP_EXTRA_INDEX_URL=${PIP_EXTRA_INDEX_URL}"} \
     --file="${DOCKERFILE}" \
     ..
