0583d9c481384ec4e01500ebf5148ff679e0c065
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-02-03 17:29:14 +1300

    Add Ukranian & Update Italian

--- invokeai/frontend/src/features/system/components/LanguagePicker.tsx
+++ invokeai/frontend/src/features/system/components/LanguagePicker.tsx
@@ -22,6 +22,7 @@ export default function LanguagePicker() {
     es: t('common:langSpanish'),
     ja: t('common:langJapanese'),
     nl: t('common:langDutch'),
+    ua: t('common:langUkranian'),
   };
 
   const renderLanguagePicker = () => {
