dba2c88c85b20d77456d59588e96bd5c69d39736
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-02-03 12:10:33 +0800

    feat: add copy image in share menu

--- invokeai/frontend/src/features/gallery/components/CurrentImageButtons.tsx
+++ invokeai/frontend/src/features/gallery/components/CurrentImageButtons.tsx
@@ -131,6 +131,22 @@ const CurrentImageButtons = () => {
     dispatch(setActiveTab('img2img'));
   };
 
+  const handleCopyImage = async () => {
+    if (!currentImage) return;
+
+    const blob = await fetch(currentImage.url).then((res) => res.blob());
+    const data = [new ClipboardItem({ [blob.type]: blob })];
+
+    await navigator.clipboard.write(data);
+
+    toast({
+      title: t('toast:imageCopied'),
+      status: 'success',
+      duration: 2500,
+      isClosable: true,
+    });
+  };
+
   const handleCopyImageLink = () => {
     navigator.clipboard
       .writeText(
@@ -400,6 +416,14 @@ const CurrentImageButtons = () => {
               leftIcon={<FaShare />}
             >
               {t('parameters:sendToUnifiedCanvas')}
+            </IAIButton>
+
+            <IAIButton
+              size={'sm'}
+              onClick={handleCopyImage}
+              leftIcon={<FaCopy />}
+            >
+              {t('parameters:copyImage')}
             </IAIButton>
             <IAIButton
               size={'sm'}
