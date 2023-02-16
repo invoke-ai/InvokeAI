caf07479ece6c8844169bf95d55d45cc6cda94b9
Eugene Brodsky <ebr@users.noreply.github.com>
2023-02-15 19:55:27 -0500

    fix spelling mistake

--- scripts/configure_invokeai.py
+++ scripts/configure_invokeai.py
@@ -5,5 +5,5 @@ import warnings
 from ldm.invoke.config import invokeai_configure
 
 if __name__ == '__main__':
-    warnings.warn("configure_invokeai.py is deprecated, running 'invokai-configure'...", DeprecationWarning)
+    warnings.warn("configure_invokeai.py is deprecated, running 'invokeai-configure'...", DeprecationWarning)
     invokeai_configure.main()
