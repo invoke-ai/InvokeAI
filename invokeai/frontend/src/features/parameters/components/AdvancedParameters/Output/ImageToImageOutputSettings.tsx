b313cf8afd015df94c943298f90f989007fb9715
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-02-21 02:27:55 +1300

    Add Symmetry Settings

--- invokeai/frontend/src/features/parameters/components/AdvancedParameters/Output/ImageToImageOutputSettings.tsx
+++ invokeai/frontend/src/features/parameters/components/AdvancedParameters/Output/ImageToImageOutputSettings.tsx
@@ -1,10 +1,12 @@
 import { Flex } from '@chakra-ui/react';
 import SeamlessSettings from './SeamlessSettings';
+import SymmetrySettings from './SymmetrySettings';
 
 const ImageToImageOutputSettings = () => {
   return (
     <Flex gap={2} direction="column">
       <SeamlessSettings />
+      <SymmetrySettings />
     </Flex>
   );
 };
--- invokeai/frontend/src/features/parameters/components/AdvancedParameters/Output/OutputSettings.tsx
+++ invokeai/frontend/src/features/parameters/components/AdvancedParameters/Output/OutputSettings.tsx
@@ -1,12 +1,14 @@
 import { Flex } from '@chakra-ui/react';
 import HiresSettings from './HiresSettings';
 import SeamlessSettings from './SeamlessSettings';
+import SymmetrySettings from './SymmetrySettings';
 
 const OutputSettings = () => {
   return (
     <Flex gap={2} direction="column">
       <SeamlessSettings />
       <HiresSettings />
+      <SymmetrySettings />
     </Flex>
   );
 };
