cd98d88fe73c67a20313e39e1b2b9c62401d60a5
Kyle Schouviller <kyle0654@hotmail.com>
2023-02-24 20:11:28 -0800

    [nodes] Removed InvokerServices, simplying service model

--- ldm/invoke/app/services/invocation_services.py
+++ ldm/invoke/app/services/invocation_services.py
@@ -1,4 +1,6 @@
 # Copyright (c) 2022 Kyle Schouviller (https://github.com/kyle0654)
+from .invocation_queue import InvocationQueueABC
+from .item_storage import ItemStorageABC
 from .image_storage import ImageStorageBase
 from .events import EventServiceBase
 from ....generate import Generate
@@ -9,12 +11,23 @@ class InvocationServices():
     generate: Generate # TODO: wrap Generate, or split it up from model?
     events: EventServiceBase
     images: ImageStorageBase
+    queue: InvocationQueueABC
+
+    # NOTE: we must forward-declare any types that include invocations, since invocations can use services
+    graph_execution_manager: ItemStorageABC['GraphExecutionState']
+    processor: 'InvocationProcessorABC'
 
     def __init__(self,
         generate: Generate,
         events: EventServiceBase,
-        images: ImageStorageBase
+        images: ImageStorageBase,
+        queue: InvocationQueueABC,
+        graph_execution_manager: ItemStorageABC['GraphExecutionState'],
+        processor: 'InvocationProcessorABC'
     ):
         self.generate = generate
         self.events = events
         self.images = images
+        self.queue = queue
+        self.graph_execution_manager = graph_execution_manager
+        self.processor = processor
