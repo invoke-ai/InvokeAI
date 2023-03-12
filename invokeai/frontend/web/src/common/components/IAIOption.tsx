f4b7f80d59cab71bbd94e5a7a1daeffa05e44ced
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-03-12 14:59:13 +1100

    fix(ui): remove key prop

--- invokeai/frontend/web/src/common/components/IAIOption.tsx
+++ invokeai/frontend/web/src/common/components/IAIOption.tsx
@@ -4,19 +4,14 @@ import { ReactNode } from 'react';
 type IAIOptionProps = {
   children: ReactNode | string | number;
   value: string | number;
-  key: string | number;
 };
 
 export default function IAIOption(props: IAIOptionProps) {
-  const { children, value, key } = props;
+  const { children, value } = props;
   const [base800, base200] = useToken('colors', ['base.800', 'base.200']);
 
   return (
-    <option
-      key={key}
-      value={value}
-      style={{ background: base800, color: base200 }}
-    >
+    <option value={value} style={{ background: base800, color: base200 }}>
       {children}
     </option>
   );
