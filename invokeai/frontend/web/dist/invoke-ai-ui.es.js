de01c38bbe360db3ed803c22273a427d0c830cb4
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2023-03-14 14:11:42 -0400

    fresh build

--- invokeai/frontend/web/dist/invoke-ai-ui.es.js
+++
@@ -1,8 +0,0 @@
-import { aE as r, aH as t, eh as n } from "./exports-21bec2df.mjs";
-import "react";
-import "react-dom";
-export {
-  r as InvokeAiLogoComponent,
-  t as ThemeChanger,
-  n as default
-};
