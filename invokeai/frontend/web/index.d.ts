eb7ae2588c6019786632c449bb00554e4f0c464e
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2023-03-21 10:21:58 -0400

    unused var

--- invokeai/frontend/web/index.d.ts
+++ invokeai/frontend/web/index.d.ts
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from 'react';
+import { PropsWithChildren } from 'react';
 import { IAIPopoverProps } from '../web/src/common/components/IAIPopover';
 import { IAIIconButtonProps } from '../web/src/common/components/IAIIconButton';
 
