0ede3218ba68b929f14fd7fe55154fc8a1531097
maryhipp <maryhipp@gmail.com>
2023-03-29 11:18:33 -0700

    add optional apiUrl prop

--- invokeai/frontend/web/src/component.tsx
+++ invokeai/frontend/web/src/component.tsx
@@ -1,8 +1,9 @@
-import React, { lazy, PropsWithChildren } from 'react';
+import React, { lazy, PropsWithChildren, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store } from './app/store';
 import { persistor } from './persistor';
+import { OpenAPI } from 'services/api';
 import '@fontsource/inter/100.css';
 import '@fontsource/inter/200.css';
 import '@fontsource/inter/300.css';
@@ -21,14 +22,22 @@ import './i18n';
 const App = lazy(() => import('./app/App'));
 const ThemeLocaleProvider = lazy(() => import('./app/ThemeLocaleProvider'));
 
-export default function Component(props: PropsWithChildren) {
+interface Props extends PropsWithChildren {
+  apiUrl?: string;
+}
+
+export default function Component({ apiUrl, children }: Props) {
+  useEffect(() => {
+    if (apiUrl) OpenAPI.BASE = apiUrl;
+  }, [apiUrl]);
+
   return (
     <React.StrictMode>
       <Provider store={store}>
         <PersistGate loading={<Loading />} persistor={persistor}>
           <React.Suspense fallback={<Loading showText />}>
             <ThemeLocaleProvider>
-              <App>{props.children}</App>
+              <App>{children}</App>
             </ThemeLocaleProvider>
           </React.Suspense>
         </PersistGate>
