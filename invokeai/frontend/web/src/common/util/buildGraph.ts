1b286c60c3c60f11d71a7e582e0c235f04734972
maryhipp <maryhipp@gmail.com>
2023-03-29 13:32:22 -0700

    feat(ui): start hooking up dynamic txt2img node generation, create middleware for session invocation

---
+++ invokeai/frontend/web/src/common/util/buildGraph.ts
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { RootState } from 'app/store';
+import { InvokeTabName, tabMap } from 'features/ui/store/tabMap';
+import { Graph } from 'services/api';
+import { buildTxt2ImgNode } from './buildNodes';
+
+function mapTabToFunction(activeTabName: InvokeTabName) {
+  switch (activeTabName) {
+    case 'txt2img':
+      return buildTxt2ImgNode;
+
+    default:
+      return buildTxt2ImgNode;
+  }
+}
+
+export const buildGraph = (state: RootState): Graph => {
+  const { activeTab } = state.ui;
+  const activeTabName = tabMap[activeTab];
+  const nodeId = uuidv4();
+
+  return {
+    nodes: {
+      [nodeId]: {
+        id: nodeId,
+        ...mapTabToFunction(activeTabName)(state),
+      },
+    },
+  };
+};
