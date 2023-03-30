6fd98406084aef9b503da0d1649a54f207d5247f
maryhipp <maryhipp@gmail.com>
2023-03-30 10:32:42 -0700

    feat(ui): img2img implementation

--- invokeai/frontend/web/src/common/util/buildGraph.ts
+++ invokeai/frontend/web/src/common/util/buildGraph.ts
@@ -3,12 +3,15 @@ import { v4 as uuidv4 } from 'uuid';
 import { RootState } from 'app/store';
 import { InvokeTabName, tabMap } from 'features/ui/store/tabMap';
 import { Graph } from 'services/api';
-import { buildTxt2ImgNode } from './buildNodes';
+import { buildImg2ImgNode, buildTxt2ImgNode } from './buildNodes';
 
 function mapTabToFunction(activeTabName: InvokeTabName) {
   switch (activeTabName) {
     case 'txt2img':
       return buildTxt2ImgNode;
+
+    case 'img2img':
+      return buildImg2ImgNode;
 
     default:
       return buildTxt2ImgNode;
