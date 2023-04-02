c2a92d125413efc13c66ed8dd30c6d2b83dcb4ff
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-04-02 19:38:37 +1000

    fix(ui): fix middleware types

--- invokeai/frontend/web/src/services/invokeMiddleware.ts
+++ invokeai/frontend/web/src/services/invokeMiddleware.ts
@@ -1,20 +1,21 @@
-import { Middleware } from '@reduxjs/toolkit';
+import { Middleware, MiddlewareAPI } from '@reduxjs/toolkit';
 import { emitSubscribe } from 'app/nodesSocketio/actions';
+import { AppDispatch, RootState } from 'app/store';
 import { setSessionId } from './apiSlice';
 import { invokeSession } from './thunks/session';
 
-export const invokeMiddleware: Middleware = (store) => (next) => (action) => {
-  const { dispatch } = store;
+export const invokeMiddleware: Middleware =
+  (store: MiddlewareAPI<AppDispatch, RootState>) => (next) => (action) => {
+    const { dispatch } = store;
 
-  if (action.type === 'api/createSession/fulfilled' && action?.payload?.id) {
-    const sessionId = action.payload.id;
-    console.log('createSession.fulfilled');
+    if (action.type === 'api/createSession/fulfilled' && action?.payload?.id) {
+      const sessionId = action.payload.id;
+      console.log('createSession.fulfilled');
 
-    dispatch(setSessionId(sessionId));
-    dispatch(emitSubscribe(sessionId));
-    // types are wrong but this works
-    dispatch(invokeSession({ sessionId }));
-  } else {
-    next(action);
-  }
-};
+      dispatch(setSessionId(sessionId));
+      dispatch(emitSubscribe(sessionId));
+      dispatch(invokeSession({ sessionId }));
+    } else {
+      next(action);
+    }
+  };
