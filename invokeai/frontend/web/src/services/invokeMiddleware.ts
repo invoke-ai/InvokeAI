85111e8d76414150640980eca7b0048afa724042
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2023-04-03 14:32:43 -0400

    feat(ui): POST upload working

--- invokeai/frontend/web/src/services/invokeMiddleware.ts
+++ invokeai/frontend/web/src/services/invokeMiddleware.ts
@@ -1,8 +1,16 @@
 import { isFulfilled, Middleware, MiddlewareAPI } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+
 import { emitSubscribe } from 'app/nodesSocketio/actions';
 import { AppDispatch, RootState } from 'app/store';
 import { setSessionId } from './apiSlice';
+import { uploadImage } from './thunks/image';
 import { createSession, invokeSession } from './thunks/session';
+import * as InvokeAI from 'app/invokeai';
+import { addImage } from 'features/gallery/store/gallerySlice';
+import { tabMap } from 'features/ui/store/tabMap';
+import { setInitialCanvasImage } from 'features/canvas/store/canvasSlice';
+import { setInitialImage } from 'features/parameters/store/generationSlice';
 
 /**
  * `redux-toolkit` provides nice matching utilities, which can be used as type guards
@@ -10,10 +18,11 @@ import { createSession, invokeSession } from './thunks/session';
  */
 
 const isFulfilledCreateSession = isFulfilled(createSession);
+const isFulfilledUploadImage = isFulfilled(uploadImage);
 
 export const invokeMiddleware: Middleware =
   (store: MiddlewareAPI<AppDispatch, RootState>) => (next) => (action) => {
-    const { dispatch } = store;
+    const { dispatch, getState } = store;
 
     if (isFulfilledCreateSession(action)) {
       const sessionId = action.payload.id;
@@ -22,6 +31,29 @@ export const invokeMiddleware: Middleware =
       dispatch(setSessionId(sessionId));
       dispatch(emitSubscribe(sessionId));
       dispatch(invokeSession({ sessionId }));
+    } else if (isFulfilledUploadImage(action)) {
+      const uploadLocation = action.payload;
+      console.log('uploadImage.fulfilled');
+
+      const newImage: InvokeAI.Image = {
+        uuid: uuidv4(),
+        category: 'user',
+        url: uploadLocation,
+        width: 512,
+        height: 512,
+        mtime: new Date().getTime(),
+        thumbnail: uploadLocation,
+      };
+
+      dispatch(addImage({ image: newImage, category: 'user' }));
+
+      const { activeTab } = getState().ui;
+      const activeTabName = tabMap[activeTab];
+      if (activeTabName === 'unifiedCanvas') {
+        dispatch(setInitialCanvasImage(newImage));
+      } else if (activeTabName === 'img2img') {
+        dispatch(setInitialImage(newImage));
+      }
     } else {
       next(action);
     }
