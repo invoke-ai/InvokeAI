d6104a3cea041c0a0bbf7f3a0cde1b4e8ba47ac1
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-04-05 23:48:39 +1000

    feat(nodes): add script to generate openapi.json

---
+++ invokeai/app/util/generate_openapi_json.py
@@ -0,0 +1,15 @@
+# Generate the OpenAPI schema json
+
+import json
+from invokeai.app.api_app import app
+from fastapi.openapi.utils import get_openapi
+
+openapi_doc = get_openapi(
+    title=app.title,
+    version=app.version,
+    openapi_version=app.openapi_version,
+    routes=app.routes,
+)
+
+with open("./openapi.json", "w") as f:
+    json.dump(openapi_doc, f)
