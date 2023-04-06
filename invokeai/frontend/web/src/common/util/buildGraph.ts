152d4e76aabd6d25d5409c5a09b1c18c5225e81b
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-04-06 22:40:46 +1000

    feat(ui): add exampleGraphs object w/ iterations example

--- invokeai/frontend/web/src/common/util/buildGraph.ts
+++ invokeai/frontend/web/src/common/util/buildGraph.ts
@@ -18,10 +18,67 @@ function mapTabToFunction(activeTabName: InvokeTabName) {
   }
 }
 
+const exampleGraphs: Record<string, Graph> = {
+  iterations: {
+    nodes: {
+      '0': {
+        id: '0',
+        type: 'range',
+        start: 0,
+        stop: 5,
+        step: 1,
+      },
+      '1': {
+        collection: [],
+        id: '1',
+        index: 0,
+        type: 'iterate',
+      },
+      '2': {
+        cfg_scale: 7.5,
+        height: 512,
+        id: '2',
+        model: '',
+        progress_images: false,
+        prompt: 'dog',
+        sampler_name: 'k_lms',
+        seamless: false,
+        steps: 11,
+        type: 'txt2img',
+        width: 512,
+      },
+    },
+    edges: [
+      {
+        source: {
+          field: 'collection',
+          node_id: '0',
+        },
+        destination: {
+          field: 'collection',
+          node_id: '1',
+        },
+      },
+      {
+        source: {
+          field: 'item',
+          node_id: '1',
+        },
+        destination: {
+          field: 'seed',
+          node_id: '2',
+        },
+      },
+    ],
+  },
+};
+
 export const buildGraph = (state: RootState): Graph => {
   const { activeTab } = state.ui;
   const activeTabName = tabMap[activeTab];
   const nodeId = uuidv4();
+
+  // return exampleGraphs.iterations;
 
   return {
     nodes: {
