9e1809127331e14816340871d773de491ebe3764
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2023-04-06 11:17:06 -0400

    feat(ui): generate iterations graph

--- invokeai/frontend/web/src/common/util/buildGraph.ts
+++ invokeai/frontend/web/src/common/util/buildGraph.ts
@@ -1,9 +1,13 @@
-import { v4 as uuidv4 } from 'uuid';
-
 import { RootState } from 'app/store';
 import { InvokeTabName, tabMap } from 'features/ui/store/tabMap';
-import { Graph } from 'services/api';
-import { buildImg2ImgNode, buildTxt2ImgNode } from './buildNodes';
+import {
+  Graph,
+  ImageToImageInvocation,
+  TextToImageInvocation,
+} from 'services/api';
+import { buildTxt2ImgNode } from './nodes/textToimage';
+import { buildImg2ImgNode } from './nodes/imageToImage';
+import { buildIteration } from './nodes/iteration';
 
 function mapTabToFunction(activeTabName: InvokeTabName) {
   switch (activeTabName) {
@@ -18,74 +22,22 @@ function mapTabToFunction(activeTabName: InvokeTabName) {
   }
 }
 
-const exampleGraphs: Record<string, Graph> = {
-  iterations: {
-    nodes: {
-      '0': {
-        id: '0',
-        type: 'range',
-        start: 0,
-        stop: 5,
-        step: 1,
-      },
-      '1': {
-        collection: [],
-        id: '1',
-        index: 0,
-        type: 'iterate',
-      },
-      '2': {
-        cfg_scale: 7.5,
-        height: 512,
-        id: '2',
-        model: '',
-        progress_images: false,
-        prompt: 'dog',
-        sampler_name: 'k_lms',
-        seamless: false,
-        steps: 11,
-        type: 'txt2img',
-        width: 512,
-      },
-    },
-    edges: [
-      {
-        source: {
-          field: 'collection',
-          node_id: '0',
-        },
-        destination: {
-          field: 'collection',
-          node_id: '1',
-        },
-      },
-      {
-        source: {
-          field: 'item',
-          node_id: '1',
-        },
-        destination: {
-          field: 'seed',
-          node_id: '2',
-        },
-      },
-    ],
-  },
+const buildBaseNode = (
+  state: RootState
+): Record<string, TextToImageInvocation | ImageToImageInvocation> => {
+  const { activeTab } = state.ui;
+  const activeTabName = tabMap[activeTab];
+
+  return mapTabToFunction(activeTabName)(state);
 };
 
 export const buildGraph = (state: RootState): Graph => {
-  const { activeTab } = state.ui;
-  const activeTabName = tabMap[activeTab];
-  const nodeId = uuidv4();
+  const { iterations } = state.generation;
+  const baseNode = buildBaseNode(state);
 
-  // return exampleGraphs.iterations;
+  if (iterations > 1) {
+    return buildIteration({ baseNode, iterations });
+  }
 
-  return {
-    nodes: {
-      [nodeId]: {
-        id: nodeId,
-        ...mapTabToFunction(activeTabName)(state),
-      },
-    },
-  };
+  return { nodes: baseNode };
 };
