2386d5d7863e13c7d25f22fad4388080e8cf19c7
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2023-04-06 11:17:06 -0400

    feat(ui): generate iterations graph

---
+++ invokeai/frontend/web/src/common/util/nodes/iteration.ts
@@ -0,0 +1,79 @@
+import { v4 as uuidv4 } from 'uuid';
+import {
+  Edge,
+  Graph,
+  ImageToImageInvocation,
+  IterateInvocation,
+  RangeInvocation,
+  TextToImageInvocation,
+} from 'services/api';
+
+type BuildIteration = {
+  baseNode: Record<string, TextToImageInvocation | ImageToImageInvocation>;
+  iterations: number;
+};
+
+const buildRangeNode = (
+  iterations: number
+): Record<string, RangeInvocation> => {
+  const nodeId = uuidv4();
+  return {
+    [nodeId]: {
+      id: nodeId,
+      type: 'range',
+      start: 0,
+      stop: iterations,
+      step: 1,
+    },
+  };
+};
+
+const buildIterateNode = (): Record<string, IterateInvocation> => {
+  const nodeId = uuidv4();
+  return {
+    [nodeId]: {
+      id: nodeId,
+      type: 'iterate',
+      collection: [],
+      index: 0,
+    },
+  };
+};
+
+export const buildIteration = ({
+  baseNode,
+  iterations,
+}: BuildIteration): Graph => {
+  const rangeNode = buildRangeNode(iterations);
+  const iterateNode = buildIterateNode();
+  const edges: Edge[] = [
+    {
+      source: {
+        field: 'collection',
+        node_id: Object.keys(rangeNode)[0],
+      },
+      destination: {
+        field: 'collection',
+        node_id: Object.keys(iterateNode)[0],
+      },
+    },
+    {
+      source: {
+        field: 'item',
+        node_id: Object.keys(iterateNode)[0],
+      },
+      destination: {
+        field: 'seed',
+        node_id: Object.keys(baseNode)[0],
+      },
+    },
+  ];
+  return {
+    nodes: {
+      ...rangeNode,
+      ...iterateNode,
+      ...baseNode,
+    },
+    edges,
+  };
+};
