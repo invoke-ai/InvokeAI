a428c473ae7ba0bf8d377adde9cbdf33f7cfc10a
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-04-12 12:32:58 +1000

    feat(ui): handle already-connected fields

--- invokeai/frontend/web/src/features/nodes/components/FieldHandle.tsx
+++ invokeai/frontend/web/src/features/nodes/components/FieldHandle.tsx
@@ -24,7 +24,7 @@ const inputHandleStyles: CSSProperties = {
 };
 
 const outputHandleStyles: CSSProperties = {
-  right: '-0.5rem',
+  right: '-1.7rem',
 };
 
 const requiredConnectionStyles: CSSProperties = {
