ae172b74a40494348136e2231ad8fe7d744b4224
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-04-17 12:20:31 +1000

    feat(nodes): roll back sqlalchemy

--- invokeai/app/services/sqlite.py
+++ invokeai/app/services/sqlite.py
@@ -1,25 +1,23 @@
-from typing import Any, Generic, TypeVar, Union, get_args
+import sqlite3
+from threading import Lock
+from typing import Generic, TypeVar, Union, get_args
+
 from pydantic import BaseModel, parse_raw_as
 
 from .item_storage import ItemStorageABC, PaginatedResults
 
-from sqlalchemy import create_engine, String, TEXT, Engine, select
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Session
-
-
 T = TypeVar("T", bound=BaseModel)
 
-
-class Base(DeclarativeBase):
-    pass
+sqlite_memory = ":memory:"
 
 
 class SqliteItemStorage(ItemStorageABC, Generic[T]):
     _filename: str
     _table_name: str
+    _conn: sqlite3.Connection
+    _cursor: sqlite3.Cursor
     _id_field: str
-    _engine: Engine
-    _table: Any # TODO: figure out how to type this
+    _lock: Lock
 
     def __init__(self, filename: str, table_name: str, id_field: str = "id"):
         super().__init__()
@@ -27,79 +25,86 @@ class SqliteItemStorage(ItemStorageABC, Generic[T]):
         self._filename = filename
         self._table_name = table_name
         self._id_field = id_field  # TODO: validate that T has this field
+        self._lock = Lock()
 
-        self._engine = create_engine(f"sqlite+pysqlite:///{self._filename}")
+        self._conn = sqlite3.connect(
+            self._filename, check_same_thread=False
+        )  # TODO: figure out a better threading solution
+        self._cursor = self._conn.cursor()
+
         self._create_table()
 
     def _create_table(self):
-        # dynamically create the ORM model class to avoid name collisions
-
-        # cannot access `self.__orig_class__` in `__init__` or `__new__` so
-        # format the table name into the class name
-        pascal_table_name = self._table_name.replace("_", " ").title()
-        pascal_table_name = pascal_table_name.replace(" ", "")
-
-        table_dict = dict(
-            __tablename__=self._table_name,
-            id=mapped_column(String, primary_key=True),
-            item=mapped_column(TEXT, nullable=False),
-        )
-
-        self._table = type(pascal_table_name, (Base,), table_dict)
-
-        Base.metadata.create_all(self._engine)
+        try:
+            self._lock.acquire()
+            self._cursor.execute(
+                f"""CREATE TABLE IF NOT EXISTS {self._table_name} (
+                item TEXT,
+                id TEXT GENERATED ALWAYS AS (json_extract(item, '$.{self._id_field}')) VIRTUAL NOT NULL);"""
+            )
+            self._cursor.execute(
+                f"""CREATE UNIQUE INDEX IF NOT EXISTS {self._table_name}_id ON {self._table_name}(id);"""
+            )
+        finally:
+            self._lock.release()
 
     def _parse_item(self, item: str) -> T:
         item_type = get_args(self.__orig_class__)[0]
         return parse_raw_as(item_type, item)
 
     def set(self, item: T):
-        session = Session(self._engine)
-
-        item_id = str(getattr(item, self._id_field))
-        new_item = self._table(id=item_id, item=item.json())
-
-        session.merge(new_item)
-
-        session.commit()
-        session.close()
-
+        try:
+            self._lock.acquire()
+            self._cursor.execute(
+                f"""INSERT OR REPLACE INTO {self._table_name} (item) VALUES (?);""",
+                (item.json(),),
+            )
+            self._conn.commit()
+        finally:
+            self._lock.release()
         self._on_changed(item)
 
     def get(self, id: str) -> Union[T, None]:
-        session = Session(self._engine)
+        try:
+            self._lock.acquire()
+            self._cursor.execute(
+                f"""SELECT item FROM {self._table_name} WHERE id = ?;""", (str(id),)
+            )
+            result = self._cursor.fetchone()
+        finally:
+            self._lock.release()
 
-        item = session.get(self._table, str(id))
-
-        session.close()
-
-        if not item:
+        if not result:
             return None
 
-        return self._parse_item(item.item)
+        return self._parse_item(result[0])
 
     def delete(self, id: str):
-        session = Session(self._engine)
-
-        item = session.get(self._table, id)
-        session.delete(item)
-
-        session.commit()
-        session.close()
-
+        try:
+            self._lock.acquire()
+            self._cursor.execute(
+                f"""DELETE FROM {self._table_name} WHERE id = ?;""", (str(id),)
+            )
+            self._conn.commit()
+        finally:
+            self._lock.release()
         self._on_deleted(id)
 
     def list(self, page: int = 0, per_page: int = 10) -> PaginatedResults[T]:
-        session = Session(self._engine)
+        try:
+            self._lock.acquire()
+            self._cursor.execute(
+                f"""SELECT item FROM {self._table_name} LIMIT ? OFFSET ?;""",
+                (per_page, page * per_page),
+            )
+            result = self._cursor.fetchall()
 
-        stmt = select(self._table.item).limit(per_page).offset(page * per_page)
-        result = session.execute(stmt)
+            items = list(map(lambda r: self._parse_item(r[0]), result))
 
-        items = list(map(lambda r: self._parse_item(r[0]), result))
-        count = session.query(self._table.item).count()
-
-        session.commit()
-        session.close()
+            self._cursor.execute(f"""SELECT count(*) FROM {self._table_name};""")
+            count = self._cursor.fetchone()[0]
+        finally:
+            self._lock.release()
 
         pageCount = int(count / per_page) + 1
 
@@ -110,19 +115,23 @@ class SqliteItemStorage(ItemStorageABC, Generic[T]):
     def search(
         self, query: str, page: int = 0, per_page: int = 10
     ) -> PaginatedResults[T]:
-        session = Session(self._engine)
+        try:
+            self._lock.acquire()
+            self._cursor.execute(
+                f"""SELECT item FROM {self._table_name} WHERE item LIKE ? LIMIT ? OFFSET ?;""",
+                (f"%{query}%", per_page, page * per_page),
+            )
+            result = self._cursor.fetchall()
 
-        stmt = (
-            session.query(self._table)
-            .where(self._table.item.like(f"%{query}%"))
-            .limit(per_page)
-            .offset(page * per_page)
-        )
+            items = list(map(lambda r: self._parse_item(r[0]), result))
 
-        result = session.execute(stmt)
-
-        items = list(map(lambda r: self._parse_item(r[0].item), result))
-        count = session.query(self._table.item).count()
+            self._cursor.execute(
+                f"""SELECT count(*) FROM {self._table_name} WHERE item LIKE ?;""",
+                (f"%{query}%",),
+            )
+            count = self._cursor.fetchone()[0]
+        finally:
+            self._lock.release()
 
         pageCount = int(count / per_page) + 1
 
