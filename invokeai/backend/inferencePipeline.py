1b813afd3b4e088118de9c010fdadbf68cfd6efa
Sai Krishna <saikrishna@multicorewareinc.com>
2023-04-24 03:29:23 -0700

    Cleanup files based on comments

--- invokeai/backend/inferencePipeline.py
+++ invokeai/backend/inferencePipeline.py
@@ -3,16 +3,11 @@ This file implements base class Inference Model for model selection
 Implements abstract methods for inference related operations
 """
 from abc import ABC, abstractmethod
-class inferenceModel(ABC):
+class inferencePipeline(ABC):
 
     """
     Instantiation of Inference model class
     """
-    def __init__(
-        self,
-        model_type = "Pytorch"
-    ):
-        self.model_type = model_type
 
     @abstractmethod
     def prompt2image(self):
