0fb8474c816fbda6d224f1a63409af1c6720233e
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-04-27 13:54:10 +1000

    feat(ui): simplify image display

--- invokeai/frontend/web/src/features/gallery/components/CurrentImageDisplay.tsx
+++ invokeai/frontend/web/src/features/gallery/components/CurrentImageDisplay.tsx
@@ -1,23 +1,11 @@
-import { Box, Collapse, Flex, Icon, useDisclosure } from '@chakra-ui/react';
+import { Flex, Icon } from '@chakra-ui/react';
 import { createSelector } from '@reduxjs/toolkit';
 import { useAppSelector } from 'app/storeHooks';
 import { systemSelector } from 'features/system/store/systemSelectors';
-import IAIButton from 'common/components/IAIButton';
-import ImageToImageSettings from 'features/parameters/components/AdvancedParameters/ImageToImage/ImageToImageSettings';
 import { isEqual } from 'lodash';
-import { useState } from 'react';
-import {
-  AnimatePresence,
-  motion,
-  useMotionValue,
-  useTransform,
-} from 'framer-motion';
 
 import { MdPhoto } from 'react-icons/md';
-import {
-  gallerySelector,
-  selectedImageSelector,
-} from '../store/gallerySelectors';
+import { selectedImageSelector } from '../store/gallerySelectors';
 import CurrentImageButtons from './CurrentImageButtons';
 import CurrentImagePreview from './CurrentImagePreview';
 
@@ -42,9 +30,6 @@ export const currentImageDisplaySelector = createSelector(
  */
 const CurrentImageDisplay = () => {
   const { hasAnImageToDisplay } = useAppSelector(currentImageDisplaySelector);
-  const [shouldHideImageToImage, setShouldHideImageToImage] = useState(false);
-  const w = useMotionValue(0);
-  const width = useTransform(w, [0, 100], [`0px`, `100px`]);
 
   return (
     <Flex
@@ -65,27 +50,23 @@ const CurrentImageDisplay = () => {
           w: 'full',
           h: 'full',
           alignItems: 'center',
+          justifyContent: 'center',
           gap: 4,
         }}
       >
-        <CurrentImageButtons />
         {hasAnImageToDisplay ? (
-          <CurrentImagePreview />
+          <>
+            <CurrentImageButtons />
+            <CurrentImagePreview />
+          </>
         ) : (
-          <Flex
-            width="full"
-            height="full"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <Icon
-              as={MdPhoto}
-              sx={{
-                boxSize: 24,
-                color: 'base.500',
-              }}
-            />
-          </Flex>
+          <Icon
+            as={MdPhoto}
+            sx={{
+              boxSize: 24,
+              color: 'base.500',
+            }}
+          />
         )}
       </Flex>
     </Flex>
