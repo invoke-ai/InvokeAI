bd491e897fca5dd4c4aef743622912545e113883
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-05-01 16:55:05 +1000

    feat(ui): disable w/h when img2img & not fit

--- invokeai/frontend/web/src/features/parameters/components/MainParameters/WidthSlider.tsx
+++ invokeai/frontend/web/src/features/parameters/components/MainParameters/WidthSlider.tsx
@@ -13,17 +13,34 @@ const selector = createSelector(
   (generation, hotkeys, config) => {
     const { initial, min, sliderMax, inputMax, fineStep, coarseStep } =
       config.sd.width;
-    const { width } = generation;
+    const { width, shouldFitToWidthHeight, isImageToImageEnabled } = generation;
 
     const step = hotkeys.shift ? fineStep : coarseStep;
 
-    return { width, initial, min, sliderMax, inputMax, step };
+    return {
+      width,
+      initial,
+      min,
+      sliderMax,
+      inputMax,
+      step,
+      shouldFitToWidthHeight,
+      isImageToImageEnabled,
+    };
   }
 );
 
 const WidthSlider = () => {
-  const { width, initial, min, sliderMax, inputMax, step } =
-    useAppSelector(selector);
+  const {
+    width,
+    initial,
+    min,
+    sliderMax,
+    inputMax,
+    step,
+    shouldFitToWidthHeight,
+    isImageToImageEnabled,
+  } = useAppSelector(selector);
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
 
@@ -40,6 +57,7 @@ const WidthSlider = () => {
 
   return (
     <IAISlider
+      isDisabled={!shouldFitToWidthHeight && isImageToImageEnabled}
       label={t('parameters.width')}
       value={width}
       min={min}
