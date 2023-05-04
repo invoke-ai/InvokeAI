fdef53b2de5395e39da0262faf7e2fdf1f7a5f71
user1 <gregghelt@gmail.com>
2023-05-04 14:21:11 -0700

    Switching to ControlField for output from controlnet nodes.

--- invokeai/app/invocations/controlnet_image_processors.py
+++ invokeai/app/invocations/controlnet_image_processors.py
@@ -400,3 +400,4 @@ class ZoeDepthImageProcessorInvocation(ImageProcessorInvocation, PILInvocationCo
         zoe_depth_processor = ZoeDetector.from_pretrained("lllyasviel/Annotators")
         processed_image = zoe_depth_processor(image)
         return processed_image
+
