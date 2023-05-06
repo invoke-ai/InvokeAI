d78cd6d3d4540bd754d8286bd4b9172aa8396700
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-05-06 19:57:45 +1000

    feat(nodes): improve default model choosing output

--- invokeai/app/invocations/util/choose_model.py
+++ invokeai/app/invocations/util/choose_model.py
@@ -4,10 +4,11 @@ from invokeai.backend.model_management.model_manager import ModelManager
 def choose_model(model_manager: ModelManager, model_name: str):
     """Returns the default model if the `model_name` not a valid model, else returns the selected model."""
     logger = model_manager.logger
-    if model_manager.valid_model(model_name):
-        model = model_manager.get_model(model_name)
-    else:
+    if model_name and not model_manager.valid_model(model_name):
+        default_model_name = model_manager.default_model()
+        logger.warning(f"\'{model_name}\' is not a valid model name. Using default model \'{default_model_name}\' instead.")
         model = model_manager.get_model()
-        logger.warning(f"\'{model_name}\' is not a valid model name. Using default model \'{model['model_name']}\' instead.")
+    else:
+        model = model_manager.get_model(model_name)
 
     return model
