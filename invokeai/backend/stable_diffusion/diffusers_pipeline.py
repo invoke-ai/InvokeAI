e2a94be33680fa539f1dbd192266667be2d6df62
user1 <gregghelt@gmail.com>
2023-05-06 13:10:56 -0700

    Added resizing of controlnet image based on noise latent. Fixes a tensor mismatch issue.

--- invokeai/backend/stable_diffusion/diffusers_pipeline.py
+++ invokeai/backend/stable_diffusion/diffusers_pipeline.py
@@ -1030,6 +1030,7 @@ class StableDiffusionGeneratorPipeline(StableDiffusionPipeline):
         dtype=torch.float16,
         do_classifier_free_guidance=True,
     ):
+
         if not isinstance(image, torch.Tensor):
             if isinstance(image, PIL.Image.Image):
                 image = [image]
