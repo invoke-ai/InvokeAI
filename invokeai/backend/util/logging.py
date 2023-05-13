691e1bf829f455b2c97f00375c010b92e164e437
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-05-14 09:06:57 +1200

    Make debug messages cyan/blue

--- invokeai/backend/util/logging.py
+++ invokeai/backend/util/logging.py
@@ -71,6 +71,7 @@ class InvokeAILogFormatter(logging.Formatter):
     grey = "\x1b[38;20m"
     yellow = "\x1b[33;20m"
     red = "\x1b[31;20m"
+    cyan = "\x1b[36;20m"
     bold_red = "\x1b[31;1m"
     reset = "\x1b[0m"
 
@@ -80,7 +81,7 @@ class InvokeAILogFormatter(logging.Formatter):
 
     # Format Map
     FORMATS = {
-        logging.DEBUG: grey + format + reset,
+        logging.DEBUG: cyan + format + reset,
         logging.INFO: grey + format + reset,
         logging.WARNING: yellow + format + reset,
         logging.ERROR: red + format + reset,
