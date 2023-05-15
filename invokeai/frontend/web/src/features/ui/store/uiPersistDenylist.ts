d68c14516cf3d5a30702e32fc5672c368c784681
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-05-16 09:46:03 +1000

    chore(ui): clean up persist denylists

--- invokeai/frontend/web/src/features/ui/store/uiPersistDenylist.ts
+++ invokeai/frontend/web/src/features/ui/store/uiPersistDenylist.ts
@@ -3,11 +3,6 @@ import { UIState } from './uiTypes';
 /**
  * UI slice persist denylist
  */
-const itemsToDenylist: (keyof UIState)[] = ['floatingProgressImageRect'];
 export const uiPersistDenylist: (keyof UIState)[] = [
   'floatingProgressImageRect',
 ];
-
-export const uiDenylist = itemsToDenylist.map(
-  (denylistItem) => `ui.${denylistItem}`
-);
