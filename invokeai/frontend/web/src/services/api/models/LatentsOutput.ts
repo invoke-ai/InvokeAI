da87378713bd9eeeebf74f28335e6d638b44185a
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-05-16 10:39:40 +1000

    chore(ui): regen api client

--- invokeai/frontend/web/src/services/api/models/LatentsOutput.ts
+++ invokeai/frontend/web/src/services/api/models/LatentsOutput.ts
@@ -8,10 +8,18 @@ import type { LatentsField } from './LatentsField';
  * Base class for invocations that output latents
  */
 export type LatentsOutput = {
-  type?: 'latent_output';
+  type?: 'latents_output';
   /**
    * The output latents
    */
   latents?: LatentsField;
+  /**
+   * The width of the latents in pixels
+   */
+  width: number;
+  /**
+   * The height of the latents in pixels
+   */
+  height: number;
 };
 
