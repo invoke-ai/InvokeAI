71003be6b864e1b5a554e623ab78c23b4421fc9c
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-05-19 15:55:15 +1000

    fix(ui): add value to conditioning field

--- invokeai/frontend/web/src/features/nodes/types/types.ts
+++ invokeai/frontend/web/src/features/nodes/types/types.ts
@@ -180,6 +180,7 @@ export type LatentsInputFieldValue = FieldValueBase & {
 
 export type ConditioningInputFieldValue = FieldValueBase & {
   type: 'conditioning';
+  value?: string;
 };
 
 export type ControlInputFieldValue = FieldValueBase & {
