4a7a5234df60c3f57b1f51455a9891fab624e0b1
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-05-22 20:05:20 +1000

    fix(ui): fix image nodes losing image

--- invokeai/frontend/web/src/features/nodes/types/types.ts
+++ invokeai/frontend/web/src/features/nodes/types/types.ts
@@ -1,6 +1,6 @@
 import { OpenAPIV3 } from 'openapi-types';
 import { RgbaColor } from 'react-colorful';
-import { ImageField } from 'services/api';
+import { ImageDTO } from 'services/api';
 import { AnyInvocationType } from 'services/events/types';
 
 export type InvocationValue = {
@@ -179,7 +179,7 @@ export type ConditioningInputFieldValue = FieldValueBase & {
 
 export type ImageInputFieldValue = FieldValueBase & {
   type: 'image';
-  value?: Pick<ImageField, 'image_name' | 'image_type'>;
+  value?: ImageDTO;
 };
 
 export type ModelInputFieldValue = FieldValueBase & {
@@ -245,7 +245,7 @@ export type BooleanInputFieldTemplate = InputFieldTemplateBase & {
 };
 
 export type ImageInputFieldTemplate = InputFieldTemplateBase & {
-  default: Pick<ImageField, 'image_name' | 'image_type'>;
+  default: ImageDTO;
   type: 'image';
 };
 
