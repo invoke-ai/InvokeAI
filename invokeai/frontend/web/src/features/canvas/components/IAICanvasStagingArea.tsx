1c54841434b3e435d62f2dc7a7101c6c61ff68d6
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-05-29 18:40:01 +1000

    fix(ui): fix progress images in canvas

--- invokeai/frontend/web/src/features/canvas/components/IAICanvasStagingArea.tsx
+++ invokeai/frontend/web/src/features/canvas/components/IAICanvasStagingArea.tsx
@@ -62,7 +62,7 @@ const IAICanvasStagingArea = (props: Props) => {
     <Group {...rest}>
       {shouldShowStagingImage && currentStagingAreaImage && (
         <IAICanvasImage
-          url={getUrl(currentStagingAreaImage.image.image_url)}
+          url={getUrl(currentStagingAreaImage.image.image_url) ?? ''}
           x={x}
           y={y}
         />
