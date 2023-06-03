faa69f8a4723f9334138c6babb1b31d4381de849
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-06-03 12:41:31 +1000

    feat(ui): add alpha colors

--- invokeai/frontend/web/src/theme/colors/greenTea.ts
+++ invokeai/frontend/web/src/theme/colors/greenTea.ts
@@ -3,10 +3,16 @@ import { generateColorPalette } from '../util/generateColorPalette';
 
 export const greenTeaThemeColors: InvokeAIThemeColors = {
   base: generateColorPalette(223, 10),
+  baseAlpha: generateColorPalette(223, 10, false, true),
   accent: generateColorPalette(155, 80),
+  accentAlpha: generateColorPalette(155, 80, false, true),
   working: generateColorPalette(47, 68),
+  workingAlpha: generateColorPalette(47, 68, false, true),
   warning: generateColorPalette(28, 75),
+  warningAlpha: generateColorPalette(28, 75, false, true),
   ok: generateColorPalette(122, 49),
+  okAlpha: generateColorPalette(122, 49, false, true),
   error: generateColorPalette(0, 50),
+  errorAlpha: generateColorPalette(0, 50, false, true),
   gridLineColor: 'rgba(255, 255, 255, 0.2)',
 };
