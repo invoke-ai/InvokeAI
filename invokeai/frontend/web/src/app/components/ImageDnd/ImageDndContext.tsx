ecb8eb0dde3aa865869789d2a6f9b6f15e779898
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-06-07 15:40:43 +1000

    fix(ui): improve dragging activation
    - delay of 250ms
    - prevent gallery images from accidentally activating native drag and drop

--- invokeai/frontend/web/src/app/components/ImageDnd/ImageDndContext.tsx
+++ invokeai/frontend/web/src/app/components/ImageDnd/ImageDndContext.tsx
@@ -40,11 +40,11 @@ const ImageDndContext = (props: ImageDndContextProps) => {
   );
 
   const mouseSensor = useSensor(MouseSensor, {
-    activationConstraint: { distance: 15 },
+    activationConstraint: { delay: 250, tolerance: 5 },
   });
 
   const touchSensor = useSensor(TouchSensor, {
-    activationConstraint: { distance: 15 },
+    activationConstraint: { delay: 250, tolerance: 5 },
   });
   // TODO: Use KeyboardSensor - needs composition of multiple collisionDetection algos
   // Alternatively, fix `rectIntersection` collection detection to work with the drag overlay
