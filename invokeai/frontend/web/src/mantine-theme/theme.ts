e981a10eb61ac991493e38d6810e8632c9e1bf35
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-06-12 19:48:26 +1200

    chore: Move Mantine Theme Override to own file

---
+++ invokeai/frontend/web/src/mantine-theme/theme.ts
@@ -0,0 +1,21 @@
+import { MantineThemeOverride } from '@mantine/core';
+
+export const mantineTheme: MantineThemeOverride = {
+  colorScheme: 'dark',
+  colors: {
+    ocean: [
+      '#dcf5ff',
+      '#afddff',
+      '#80c4fd',
+      '#50acf9',
+      '#2295f6',
+      '#097bdd',
+      '#0060ad',
+      '#00447d',
+      '#00294e',
+      '#000f20',
+    ],
+  },
+  primaryColor: 'ocean',
+  primaryShade: 6,
+};
