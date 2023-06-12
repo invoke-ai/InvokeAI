302d28ad2c3d14a9bec7f1d9f6825e50c738ff47
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-06-12 19:50:34 +1200

    feat: Use Mantine Select for Scheduler

--- invokeai/frontend/web/src/features/parameters/components/Parameters/Core/ParamScheduler.tsx
+++ invokeai/frontend/web/src/features/parameters/components/Parameters/Core/ParamScheduler.tsx
@@ -1,9 +1,8 @@
+import { Select } from '@mantine/core';
 import { createSelector } from '@reduxjs/toolkit';
 import { Scheduler } from 'app/constants';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
 import { defaultSelectorOptions } from 'app/store/util/defaultMemoizeOptions';
-import IAICustomSelect from 'common/components/IAICustomSelect';
-import IAISelect from 'common/components/IAISelect';
 import { generationSelector } from 'features/parameters/store/generationSelectors';
 import { setScheduler } from 'features/parameters/store/generationSlice';
 import { uiSelector } from 'features/ui/store/uiSelectors';
@@ -52,11 +51,12 @@ const ParamScheduler = () => {
   // );
 
   return (
-    <IAISelect
+    <Select
       label={t('parameters.scheduler')}
       value={scheduler}
       validValues={allSchedulers}
       onChange={handleChange}
+      searchable
     />
   );
 
