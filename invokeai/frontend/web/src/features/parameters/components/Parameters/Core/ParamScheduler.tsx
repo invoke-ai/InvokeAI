c71e850be8123b7f74d3420aa45fe0107b1ea237
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-06-14 13:01:43 +1000

    fix(ui): fix rebase conflicts

--- invokeai/frontend/web/src/features/parameters/components/Parameters/Core/ParamScheduler.tsx
+++ invokeai/frontend/web/src/features/parameters/components/Parameters/Core/ParamScheduler.tsx
@@ -6,19 +6,13 @@ import IAIMantineSelect from 'common/components/IAIMantineSelect';
 import { generationSelector } from 'features/parameters/store/generationSelectors';
 import { setScheduler } from 'features/parameters/store/generationSlice';
 import { uiSelector } from 'features/ui/store/uiSelectors';
-import { ChangeEvent, memo, useCallback } from 'react';
+import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const selector = createSelector(
   [uiSelector, generationSelector],
   (ui, generation) => {
-    // TODO: DPMSolverSinglestepScheduler is fixed in https://github.com/huggingface/diffusers/pull/3413
-    // but we need to wait for the next release before removing this special handling.
-    const allSchedulers = ui.schedulers
-      .filter((scheduler) => {
-        return !['dpmpp_2s'].includes(scheduler);
-      })
-      .sort((a, b) => a.localeCompare(b));
+    const allSchedulers = ui.schedulers.sort((a, b) => a.localeCompare(b));
 
     return {
       scheduler: generation.scheduler,
@@ -35,39 +29,23 @@ const ParamScheduler = () => {
   const { t } = useTranslation();
 
   const handleChange = useCallback(
-    (e: ChangeEvent<HTMLSelectElement>) => {
-      dispatch(setScheduler(e.target.value as Scheduler));
+    (v: string | null) => {
+      if (!v) {
+        return;
+      }
+      dispatch(setScheduler(v as Scheduler));
     },
     [dispatch]
   );
-  // const handleChange = useCallback(
-  //   (v: string | null | undefined) => {
-  //     if (!v) {
-  //       return;
-  //     }
-  //     dispatch(setScheduler(v as Scheduler));
-  //   },
-  //   [dispatch]
-  // );
 
   return (
     <IAIMantineSelect
       label={t('parameters.scheduler')}
       value={scheduler}
-      validValues={allSchedulers}
+      data={allSchedulers}
       onChange={handleChange}
     />
   );
-
-  // return (
-  //   <IAICustomSelect
-  //     label={t('parameters.scheduler')}
-  //     value={scheduler}
-  //     data={allSchedulers}
-  //     onChange={handleChange}
-  //     withCheckIcon
-  //   />
-  // );
 };
 
 export default memo(ParamScheduler);
