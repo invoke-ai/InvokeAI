dd1b3c9f35db137e3ed02a56d6b1e68f68645a29
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-06-15 14:42:18 +1000

    fix(api): update API models to use BoardDTOs

--- invokeai/app/api/routers/boards.py
+++ invokeai/app/api/routers/boards.py
@@ -1,6 +1,6 @@
 from fastapi import Body, HTTPException, Path, Query
 from fastapi.routing import APIRouter
-from invokeai.app.services.board_record_storage import BoardRecord, BoardChanges
+from invokeai.app.services.board_record_storage import BoardChanges
 from invokeai.app.services.image_record_storage import OffsetPaginatedResults
 from invokeai.app.services.models.board_record import BoardDTO
 
@@ -16,32 +16,39 @@ boards_router = APIRouter(prefix="/v1/boards", tags=["boards"])
         201: {"description": "The board was created successfully"},
     },
     status_code=201,
+    response_model=BoardDTO,
 )
 async def create_board(
     board_name: str = Body(description="The name of the board to create"),
-):
+) -> BoardDTO:
     """Creates a board"""
     try:
         result = ApiDependencies.invoker.services.boards.create(board_name=board_name)
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail="Failed to create board")
-    
+
+
 @boards_router.patch(
     "/{board_id}",
     operation_id="update_board",
     responses={
-        201: {"description": "The board was updated successfully"},
+        201: {
+            "description": "The board was updated successfully",
+        },
     },
     status_code=201,
+    response_model=BoardDTO,
 )
 async def update_board(
     board_id: str = Path(description="The id of board to update"),
     changes: BoardChanges = Body(description="The changes to apply to the board"),
-):
-    """Creates a board"""
+) -> BoardDTO:
+    """Updates a board"""
     try:
-        result = ApiDependencies.invoker.services.boards.update(board_id=board_id, changes=changes)
+        result = ApiDependencies.invoker.services.boards.update(
+            board_id=board_id, changes=changes
+        )
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail="Failed to update board")
@@ -63,7 +70,7 @@ async def delete_board(
 @boards_router.get(
     "/",
     operation_id="list_boards",
-    response_model=OffsetPaginatedResults[BoardRecord],
+    response_model=OffsetPaginatedResults[BoardDTO],
 )
 async def list_boards(
     offset: int = Query(default=0, description="The page offset"),
@@ -76,4 +83,3 @@ async def list_boards(
         limit,
     )
     return results
-
