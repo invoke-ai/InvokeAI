421c23d3ea6200e30e58cc5e44603a807d8a5794
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-06-21 21:27:15 +1000

    fix(ui): fix gallery image fetching for board categories

--- invokeai/frontend/web/src/app/store/middleware/listenerMiddleware/listeners/boardIdSelected.ts
+++ invokeai/frontend/web/src/app/store/middleware/listenerMiddleware/listeners/boardIdSelected.ts
@@ -2,7 +2,8 @@ import { log } from 'app/logging/useLogger';
 import { startAppListening } from '..';
 import { boardIdSelected } from 'features/gallery/store/boardSlice';
 import { selectImagesAll } from 'features/gallery/store/imagesSlice';
-import { receivedPageOfImages } from 'services/thunks/image';
+import { IMAGES_PER_PAGE, receivedPageOfImages } from 'services/thunks/image';
+import { api } from 'services/apiSlice';
 
 const moduleLog = log.child({ namespace: 'boards' });
 
@@ -11,16 +12,36 @@ export const addBoardIdSelectedListener = () => {
     actionCreator: boardIdSelected,
     effect: (action, { getState, dispatch }) => {
       const boardId = action.payload;
+
+      // we need to check if we need to fetch more images
+
+      if (!boardId) {
+        // a board was unselected - we don't need to do anything
+        return;
+      }
+
       const state = getState();
       const { categories } = state.images;
 
-      const images = selectImagesAll(state).filter((i) => {
+      const filteredImages = selectImagesAll(state).filter((i) => {
         const isInCategory = categories.includes(i.image_category);
         const isInSelectedBoard = boardId ? i.board_id === boardId : true;
         return isInCategory && isInSelectedBoard;
       });
 
-      if (images.length === 0) {
+      // get the board from the cache
+      const { data: boards } = api.endpoints.listAllBoards.select()(state);
+      const board = boards?.find((b) => b.board_id === boardId);
+      if (!board) {
+        // can't find the board in cache...
+        return;
+      }
+
+      // if we haven't loaded one full page of images from this board, load more
+      if (
+        filteredImages.length < board.image_count &&
+        filteredImages.length < IMAGES_PER_PAGE
+      ) {
         dispatch(receivedPageOfImages({ categories, boardId }));
       }
     },
