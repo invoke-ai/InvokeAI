674f42ba9af16898cc7834084aedd9ec863dbbe7
Brandon Rising <brandonrising@Brandons-MacBook-Pro.local>
2023-06-27 00:33:58 -0400

    Pass env vars as build-args, ensure node modules isn't getting passed in

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-docker-compose build
+build_args=""
+
+[[ -f ".env" ]] && build_args=$(awk '$1 ~ /\=/ {print "--build-arg " $0 " "}' .env)
+
+echo "docker-compose build args:"
+echo $build_args
+
+docker-compose build $build_args
