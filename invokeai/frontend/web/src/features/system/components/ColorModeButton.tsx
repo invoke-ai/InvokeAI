fb015332f29317f484843833e57a585362b1487f
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-06-30 06:05:08 +1200

    feat: Add tooltips to color mode switcher

--- invokeai/frontend/web/src/features/system/components/ColorModeButton.tsx
+++ invokeai/frontend/web/src/features/system/components/ColorModeButton.tsx
@@ -1,13 +1,20 @@
 import { useColorMode } from '@chakra-ui/react';
 import IAIIconButton from 'common/components/IAIIconButton';
+import { useTranslation } from 'react-i18next';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
 const ColorModeButton = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const { t } = useTranslation();
 
   return (
     <IAIIconButton
-      aria-label="Toggle Color Mode"
+      aria-label={
+        colorMode === 'dark' ? t('common.lightMode') : t('common.darkMode')
+      }
+      tooltip={
+        colorMode === 'dark' ? t('common.lightMode') : t('common.darkMode')
+      }
       size="sm"
       icon={
         colorMode === 'dark' ? (
