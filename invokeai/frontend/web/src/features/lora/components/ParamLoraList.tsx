1323a45e30e95d671851b0cbbcc36d07dc4821d5
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-07-04 22:45:45 +1000

    fix(ui): fix selector memoization
    Every `GalleryImage` was rerendering any time the app rerendered bc the selector function itself was not memoized. This resulted in the memoization cache inside the selector constantly being reset.
    
    Same for `BatchImage`.
    
    Also updated memoization for a few other selectors.

--- invokeai/frontend/web/src/features/lora/components/ParamLoraList.tsx
+++ invokeai/frontend/web/src/features/lora/components/ParamLoraList.tsx
@@ -1,14 +1,19 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { stateSelector } from 'app/store/store';
 import { useAppSelector } from 'app/store/storeHooks';
+import { defaultSelectorOptions } from 'app/store/util/defaultMemoizeOptions';
 import { map } from 'lodash-es';
 import ParamLora from './ParamLora';
 
-const selector = createSelector(stateSelector, ({ lora }) => {
-  const { loras } = lora;
+const selector = createSelector(
+  stateSelector,
+  ({ lora }) => {
+    const { loras } = lora;
 
-  return { loras };
-});
+    return { loras };
+  },
+  defaultSelectorOptions
+);
 
 const ParamLoraList = () => {
   const { loras } = useAppSelector(selector);
