e41e8606b579b61c1ce2d34c102e6b5da728b79e
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-07-05 17:33:03 +1000

    feat(ui): improve accordion ux
    - Accordions now may be opened or closed regardless of whether or not their contents are enabled or active
    - Accordions have a short text indicator alerting the user if their contents are enabled, either a simple `Enabled` or, for accordions like LoRA or ControlNet, `X Active` if any are active

--- invokeai/frontend/web/src/features/lora/components/ParamLoraCollapse.tsx
+++ invokeai/frontend/web/src/features/lora/components/ParamLoraCollapse.tsx
@@ -1,14 +1,30 @@
-import { Flex, useDisclosure } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
+import { createSelector } from '@reduxjs/toolkit';
+import { stateSelector } from 'app/store/store';
+import { useAppSelector } from 'app/store/storeHooks';
+import { defaultSelectorOptions } from 'app/store/util/defaultMemoizeOptions';
 import IAICollapse from 'common/components/IAICollapse';
+import { size } from 'lodash-es';
 import { memo } from 'react';
 import ParamLoraList from './ParamLoraList';
 import ParamLoraSelect from './ParamLoraSelect';
 
+const selector = createSelector(
+  stateSelector,
+  (state) => {
+    const loraCount = size(state.lora.loras);
+    return {
+      activeLabel: loraCount > 0 ? `${loraCount} Active` : undefined,
+    };
+  },
+  defaultSelectorOptions
+);
+
 const ParamLoraCollapse = () => {
-  const { isOpen, onToggle } = useDisclosure();
+  const { activeLabel } = useAppSelector(selector);
 
   return (
-    <IAICollapse label="LoRAs" isOpen={isOpen} onToggle={onToggle}>
+    <IAICollapse label={'LoRA'} activeLabel={activeLabel}>
       <Flex sx={{ flexDir: 'column', gap: 2 }}>
         <ParamLoraSelect />
         <ParamLoraList />
--- invokeai/frontend/web/src/features/ui/components/tabs/TextToImage/TextToImageTabCoreParameters.tsx
+++ invokeai/frontend/web/src/features/ui/components/tabs/TextToImage/TextToImageTabCoreParameters.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex, useDisclosure } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { createSelector } from '@reduxjs/toolkit';
+import { stateSelector } from 'app/store/store';
 import { useAppSelector } from 'app/store/storeHooks';
 import { defaultSelectorOptions } from 'app/store/util/defaultMemoizeOptions';
 import IAICollapse from 'common/components/IAICollapse';
@@ -11,25 +12,30 @@ import ParamScheduler from 'features/parameters/components/Parameters/Core/Param
 import ParamSteps from 'features/parameters/components/Parameters/Core/ParamSteps';
 import ParamWidth from 'features/parameters/components/Parameters/Core/ParamWidth';
 import ParamSeedFull from 'features/parameters/components/Parameters/Seed/ParamSeedFull';
-import { uiSelector } from 'features/ui/store/uiSelectors';
 import { memo } from 'react';
 
 const selector = createSelector(
-  uiSelector,
-  (ui) => {
+  stateSelector,
+  ({ ui, generation }) => {
     const { shouldUseSliders } = ui;
+    const { shouldRandomizeSeed } = generation;
 
-    return { shouldUseSliders };
+    const activeLabel = !shouldRandomizeSeed ? 'Manual Seed' : undefined;
+
+    return { shouldUseSliders, activeLabel };
   },
   defaultSelectorOptions
 );
 
 const TextToImageTabCoreParameters = () => {
-  const { shouldUseSliders } = useAppSelector(selector);
-  const { isOpen, onToggle } = useDisclosure({ defaultIsOpen: true });
+  const { shouldUseSliders, activeLabel } = useAppSelector(selector);
 
   return (
-    <IAICollapse label={'General'} isOpen={isOpen} onToggle={onToggle}>
+    <IAICollapse
+      label={'General'}
+      activeLabel={activeLabel}
+      defaultIsOpen={true}
+    >
       <Flex
         sx={{
           flexDirection: 'column',
