c866598d41b52ed4e77843d1f54ff03a61939933
mickr777 <115216705+mickr777@users.noreply.github.com>
2023-07-12 08:41:15 +1000

    Some Tweaks to In Progress Image Node

--- invokeai/frontend/web/src/features/nodes/components/ProgressImageNode.tsx
+++ invokeai/frontend/web/src/features/nodes/components/ProgressImageNode.tsx
@@ -4,14 +4,27 @@ import { InvocationValue } from '../types/types';
 
 import { useAppSelector } from 'app/store/storeHooks';
 import { IAINoContentFallback } from 'common/components/IAIImageFallback';
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 import IAINodeHeader from './IAINode/IAINodeHeader';
-import IAINodeResizer from './IAINode/IAINodeResizer';
+import IAINodeResizer, { OnResize } from './IAINode/IAINodeResizer';
 import NodeWrapper from './NodeWrapper';
 
 const ProgressImageNode = (props: NodeProps<InvocationValue>) => {
   const progressImage = useAppSelector((state) => state.system.progressImage);
   const { selected } = props;
+
+  const [nodeSize, setNodeSize] = useState(() => {
+    const storedSize = localStorage.getItem('nodeSize');
+    return storedSize ? JSON.parse(storedSize) : { width: 512, height: 512 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem('nodeSize', JSON.stringify(nodeSize));
+  }, [nodeSize]);
+
+  const handleResize: OnResize = (_, { width, height }) => {
+    setNodeSize({ width, height });
+  };
 
   return (
     <NodeWrapper selected={selected}>
@@ -27,6 +40,10 @@ const ProgressImageNode = (props: NodeProps<InvocationValue>) => {
           p: 2,
           bg: 'base.200',
           _dark: { bg: 'base.800' },
+          width: nodeSize.width,
+          height: nodeSize.height,
+          overflow: 'hidden',
+          flexShrink: 0,
         }}
       >
         {progressImage ? (
@@ -36,6 +53,8 @@ const ProgressImageNode = (props: NodeProps<InvocationValue>) => {
               w: 'full',
               h: 'full',
               objectFit: 'contain',
+              maxWidth: '100%',
+              maxHeight: '100%',
             }}
           />
         ) : (
@@ -53,10 +72,7 @@ const ProgressImageNode = (props: NodeProps<InvocationValue>) => {
           </Flex>
         )}
       </Flex>
-      <IAINodeResizer
-        maxHeight={progressImage?.height ?? 512}
-        maxWidth={progressImage?.width ?? 512}
-      />
+      <IAINodeResizer onResize={handleResize} />
     </NodeWrapper>
   );
 };
