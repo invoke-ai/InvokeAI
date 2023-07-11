b6b22dc7994942534a2b7b39bbcc4597cc679544
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-07-12 07:50:11 +1200

    feat: Update Reload Schema button

--- invokeai/frontend/web/src/features/nodes/components/panels/TopCenterPanel.tsx
+++ invokeai/frontend/web/src/features/nodes/components/panels/TopCenterPanel.tsx
@@ -1,27 +1,19 @@
 import { HStack } from '@chakra-ui/react';
-import { useAppDispatch } from 'app/store/storeHooks';
-import IAIButton from 'common/components/IAIButton';
 import CancelButton from 'features/parameters/components/ProcessButtons/CancelButton';
-import { memo, useCallback } from 'react';
+import { memo } from 'react';
 import { Panel } from 'reactflow';
-import { receivedOpenAPISchema } from 'services/api/thunks/schema';
 import LoadNodesButton from '../ui/LoadNodesButton';
 import NodeInvokeButton from '../ui/NodeInvokeButton';
+import ReloadSchemaButton from '../ui/ReloadSchemaButton';
 import SaveNodesButton from '../ui/SaveNodesButton';
 
 const TopCenterPanel = () => {
-  const dispatch = useAppDispatch();
-
-  const handleReloadSchema = useCallback(() => {
-    dispatch(receivedOpenAPISchema());
-  }, [dispatch]);
-
   return (
     <Panel position="top-center">
       <HStack>
         <NodeInvokeButton />
         <CancelButton />
-        <IAIButton onClick={handleReloadSchema}>Reload Schema</IAIButton>
+        <ReloadSchemaButton />
         <SaveNodesButton />
         <LoadNodesButton />
       </HStack>
