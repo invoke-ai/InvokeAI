e1f275696764f947bd3db16736d8892996f8f4c3
maryhipp <maryhipp@gmail.com>
2023-07-12 08:54:42 -0700

    add new route for app config

--- invokeai/app/api/routers/app_info.py
+++ invokeai/app/api/routers/app_info.py
@@ -1,5 +1,6 @@
 from fastapi.routing import APIRouter
 from pydantic import BaseModel
+from invokeai.backend.image_util.patchmatch import PatchMatch
 
 from invokeai.version import __version__
 
@@ -10,9 +11,19 @@ class AppVersion(BaseModel):
     """App Version Response"""
     version: str
 
+class AppConfig(BaseModel):
+    """App Config Response"""
+    patchmatch_enabled: bool
+
 
 @app_router.get('/version', operation_id="app_version",
                 status_code=200,
                 response_model=AppVersion)
 async def get_version() -> AppVersion:
     return AppVersion(version=__version__)
+
+@app_router.get('/config', operation_id="get_config",
+                status_code=200,
+                response_model=AppConfig)
+async def get_config() -> AppConfig:
+    return AppConfig(patchmatch_enabled=PatchMatch.patchmatch_available())
