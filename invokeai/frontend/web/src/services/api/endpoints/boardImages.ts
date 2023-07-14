307409bfe3ed69535174f9b3d38a234f2304ba87
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2023-07-14 14:50:29 -0400

    adding and removing multiple images from board works via drag n drop

--- invokeai/frontend/web/src/services/api/endpoints/boardImages.ts
+++ invokeai/frontend/web/src/services/api/endpoints/boardImages.ts
@@ -2,6 +2,7 @@ import { OffsetPaginatedResults_ImageDTO_ } from 'services/api/types';
 import { api } from '..';
 import { paths } from '../schema';
 import { imagesApi } from './images';
+import { PatchCollection } from '@reduxjs/toolkit/dist/query/core/buildThunks';
 
 type ListBoardImagesArg =
   paths['/api/v1/board_images/{board_id}']['get']['parameters']['path'] &
@@ -14,6 +15,14 @@ type AddImageToBoardArg =
 type RemoveImageFromBoardArg =
   paths['/api/v1/board_images/{board_id}']['delete']['parameters']['path'] &
   { image_name: paths['/api/v1/board_images/{board_id}']['delete']['requestBody']['content']['application/json'] };
+
+type AddImagesToBoardArg =
+  paths['/api/v1/board_images/{board_id}/add_images']['post']['parameters']['path'] &
+  { image_names: paths['/api/v1/board_images/{board_id}/add_images']['post']['requestBody']['content']['application/json'] };
+
+type RemoveImagesFromBoardArg =
+  paths['/api/v1/board_images/{board_id}/remove_images']['post']['parameters']['path'] &
+  { image_names: paths['/api/v1/board_images/{board_id}/remove_images']['post']['requestBody']['content']['application/json'] };
 
 export const boardImagesApi = api.injectEndpoints({
   endpoints: (build) => ({
@@ -85,6 +94,60 @@ export const boardImagesApi = api.injectEndpoints({
         } catch {
           patchResult.undo();
         }
+      },
+    }),
+    addMultipleImagesToBoard: build.mutation<void, AddImagesToBoardArg>({
+      query: ({ board_id, image_names }) => ({
+        url: `board_images/${board_id}/add_images`,
+        method: 'POST',
+        body: image_names,
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'Board', id: arg.board_id },
+      ],
+      async onQueryStarted(
+        { image_names, ...patch },
+        { dispatch, queryFulfilled }
+      ) {
+        const patchResults: PatchCollection[] = [];
+
+        image_names.forEach(image_name => {
+          patchResults.push(dispatch(
+            imagesApi.util.updateQueryData('getImageDTO', image_name, (draft) => {
+              Object.assign(draft, patch);
+            })
+          ));
+        })
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResults.forEach(patch => patch.undo());
+        }
+      },
+    }),
+
+    removeMultipleImagesFromBoard: build.mutation<void, RemoveImagesFromBoardArg>({
+      query: ({ board_id, image_names }) => ({
+        url: `board_images/${board_id}/remove_images`,
+        method: 'POST',
+        body: image_names,
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'Board', id: arg.board_id },
+      ],
+      async onQueryStarted(
+        { image_names, ...patch },
+        { dispatch, queryFulfilled }
+      ) {
+        const patchResults: PatchCollection[] = [];
+
+        image_names.forEach(image_name => {
+          patchResults.push(dispatch(
+            imagesApi.util.updateQueryData('getImageDTO', image_name, (draft) => {
+              Object.assign(draft, { board_id: null });
+            })
+          ));
+        })
       },
     }),
   }),
