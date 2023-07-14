5c58bc6348c662373f815e7815f875b105d8f49d
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-07-14 18:04:44 +1200

    fix: Missing VAE Input Field Component

---
+++ invokeai/frontend/web/src/features/nodes/components/fields/VaeInputFieldComponent.tsx
@@ -0,0 +1,15 @@
+import {
+  VaeInputFieldTemplate,
+  VaeInputFieldValue,
+} from 'features/nodes/types/types';
+import { memo } from 'react';
+import { FieldComponentProps } from './types';
+
+const VaeInputFieldComponent = (
+  props: FieldComponentProps<VaeInputFieldValue, VaeInputFieldTemplate>
+) => {
+  const { nodeId, field } = props;
+  return null;
+};
+
+export default memo(VaeInputFieldComponent);
