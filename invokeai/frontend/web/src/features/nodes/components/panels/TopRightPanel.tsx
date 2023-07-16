570bb41d9811e35914321c24a8b8bb8c6892c9ce
mickr777 <115216705+mickr777@users.noreply.github.com>
2023-07-16 23:19:02 +1000

    Add button to hide Legend

--- invokeai/frontend/web/src/features/nodes/components/panels/TopRightPanel.tsx
+++ invokeai/frontend/web/src/features/nodes/components/panels/TopRightPanel.tsx
@@ -1,18 +1,50 @@
+import { useState } from 'react';
 import { RootState } from 'app/store/store';
 import { useAppSelector } from 'app/store/storeHooks';
 import { memo } from 'react';
 import { Panel } from 'reactflow';
 import FieldTypeLegend from '../FieldTypeLegend';
 import NodeGraphOverlay from '../NodeGraphOverlay';
+import IAIIconButton from 'common/components/IAIIconButton';
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+import { useTranslation } from 'react-i18next';
 
 const TopRightPanel = () => {
+  const { t } = useTranslation();
+  const [isLegendVisible, setIsLegendVisible] = useState(true);
   const shouldShowGraphOverlay = useAppSelector(
     (state: RootState) => state.nodes.shouldShowGraphOverlay
   );
 
+  const toggleLegendVisibility = () => {
+    setIsLegendVisible(!isLegendVisible);
+  };
+
   return (
     <Panel position="top-right">
-      <FieldTypeLegend />
+      <div style={{ position: 'relative' }}>
+        <IAIIconButton
+          onClick={toggleLegendVisibility}
+          isChecked={isLegendVisible}
+          tooltip={
+            isLegendVisible ? t('nodes.closeLegend') : t('nodes.openLegend')
+          }
+        >
+          {isLegendVisible ? <FaArrowUp /> : <FaArrowDown />}
+        </IAIIconButton>
+        {isLegendVisible && (
+          <div
+            style={{
+              position: 'absolute',
+              top: 'calc(100% + 5px)',
+              right: 0,
+              zIndex: 1,
+            }}
+          >
+            <FieldTypeLegend />
+          </div>
+        )}
+      </div>
       {shouldShowGraphOverlay && <NodeGraphOverlay />}
     </Panel>
   );
