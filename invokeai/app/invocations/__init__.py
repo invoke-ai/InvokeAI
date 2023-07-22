68c4adea6441830ba19daaeeb2b3902e59f78b8d
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-07-22 18:04:35 +1200

    feat: Community Extensions Easy Install

--- invokeai/app/invocations/__init__.py
+++ invokeai/app/invocations/__init__.py
@@ -1,12 +1,26 @@
-import os
+import pathlib
+
+from ..services.config import get_invokeai_config
 
 __all__ = []
 
-dirname = os.path.dirname(os.path.abspath(__file__))
-for f in os.listdir(dirname):
-    if (
-        f != "__init__.py"
-        and os.path.isfile("%s/%s" % (dirname, f))
-        and f[-3:] == ".py"
-    ):
-        __all__.append(f[:-3])
+
+def load_nodes(directory: str | pathlib.Path, extensions=False):
+    directory = directory if isinstance(
+        directory, pathlib.Path) else pathlib.Path(directory)
+    assert directory.is_dir()
+    for file in directory.iterdir():
+        if (file.is_file() and file.name != '__init__.py' and file.name[-3:] == ".py"):
+            if extensions:
+                __all__.append((file.parent / file.name[:-3]).__str__())
+            else:
+                __all__.append(file.name[:-3])
+
+
+# Load Native Nodes
+native_nodes_dir = pathlib.Path(__file__).parent
+load_nodes(native_nodes_dir)
+
+# Load Community Nodes
+community_nodes_dir = pathlib.Path(get_invokeai_config().root / 'nodes')
+load_nodes(community_nodes_dir, extensions=True)
