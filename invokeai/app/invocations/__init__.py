d97af4b1f5af927cda8ae657031b4b38915de51a
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-07-22 19:19:40 +1200

    fix: Make the extensions be more organized

--- invokeai/app/invocations/__init__.py
+++ invokeai/app/invocations/__init__.py
@@ -1,26 +1,47 @@
 import pathlib
 
+from ...backend.util.logging import InvokeAILogger
 from ..services.config import get_invokeai_config
+
+logger = InvokeAILogger.getLogger()
 
 __all__ = []
 
 
-def load_nodes(directory: str | pathlib.Path, extensions=False):
-    directory = directory if isinstance(
-        directory, pathlib.Path) else pathlib.Path(directory)
-    assert directory.is_dir()
-    for file in directory.iterdir():
-        if (file.is_file() and file.name != '__init__.py' and file.name[-3:] == ".py"):
-            if extensions:
-                __all__.append((file.parent / file.name[:-3]).__str__())
-            else:
-                __all__.append(file.name[:-3])
+def load_nodes():
+    native_nodes_dir = pathlib.Path(__file__).parent
+    assert native_nodes_dir.is_dir()
+
+    community_nodes_dir = pathlib.Path(get_invokeai_config().root / 'nodes')
+    assert community_nodes_dir.is_dir()
+
+    # Load Native Nodes
+    for file in native_nodes_dir.iterdir():
+        if file.is_file() and file.name != '__init__.py' and file.suffix == ".py":
+            __all__.append(file.stem)
+
+    # Load Community Nodes
+    for extension in community_nodes_dir.iterdir():
+        if extension.is_dir() and extension.name != '__pycache__':
+            # Search for py files that are not named __init__.py in extensions root directory
+            py_files = list(extension.glob('*.py'))
+            py_files = [
+                file for file in py_files
+                if not file.name.endswith("__init__.py")]
+
+            # If no py files are found, extensions is not loaded.
+            if len(py_files) == 0:
+                logger.warn(
+                    f'Extension: "{extension.name}" failed to load. No nodes found.')
+                return
+
+            # Every py file in the root directory of the extension is loaded as a node
+            # This will allow people to pack multiple nodes in different files in the same extension
+            # All subfolders are ignored. These can be used for secondary operations needed for the node.
+            for file in extension.iterdir():
+                if file.is_file() and file.name != '__init__.py' and file.suffix == ".py":
+                    __all__.append((file.parent / file.stem).__str__())
+                    logger.info(f'Extension: "{extension.name}" loaded')
 
 
-# Load Native Nodes
-native_nodes_dir = pathlib.Path(__file__).parent
-load_nodes(native_nodes_dir)
-
-# Load Community Nodes
-community_nodes_dir = pathlib.Path(get_invokeai_config().root / 'nodes')
-load_nodes(community_nodes_dir, extensions=True)
+load_nodes()
