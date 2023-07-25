37103b66e8ca9ea1dca73dcb6b7fc69007a505c4
ymgenesis <janik.matthew@gmail.com>
2023-07-26 00:00:39 +0200

    remove mask & output class
    - Remove "ImageMaskOutput" where both image and mask are output
    - Remove ability to output mask from node

--- invokeai/app/invocations/image.py
+++ invokeai/app/invocations/image.py
@@ -60,21 +60,6 @@ class MaskOutput(BaseInvocationOutput):
         }
 
 
-class ImageMaskOutput(BaseInvocationOutput):
-    """Base class for invocations that output an image and a mask"""
-
-    # fmt: off
-    type: Literal["image_mask_output"] = "image_mask_output"
-    image:      ImageField = Field(default=None, description="The output image")
-    width:             int = Field(description="The width of the image in pixels")
-    height:            int = Field(description="The height of the image in pixels")
-    mask:       ImageField = Field(default=None, description="The output mask")
-    # fmt: on
-
-    class Config:
-        schema_extra = {"required": ["type", "image", "width", "height", "mask"]}
-
-
 class LoadImageInvocation(BaseInvocation):
     """Load an image and provide it as output."""
 
@@ -205,17 +190,17 @@ class CenterPadCropInvocation(BaseInvocation, PILInvocationConfig):
             },
         }
 
-    def invoke(self, context: InvocationContext) -> ImageMaskOutput:
+    def invoke(self, context: InvocationContext) -> ImageOutput:
         image = context.services.images.get_pil_image(self.image.image_name)
         imgwidth, imgheight = image.size
 
-        new_width = imgwidth + self.right + self.left
-        new_height = imgheight + self.top + self.bottom
+        # Calculate and create new image dimensions
+        new_width = image.width + self.right + self.left
+        new_height = image.height + self.top + self.bottom
         image_crop = Image.new(mode="RGBA", size=(new_width, new_height), color=(0, 0, 0, 0))
 
+        # Paste new image onto input
         image_crop.paste(image, (self.left, self.top))
-
-        white_mask = Image.new("L", image_crop.size, color=255)
 
         image_dto = context.services.images.create(
             image=image_crop,
@@ -225,20 +210,11 @@ class CenterPadCropInvocation(BaseInvocation, PILInvocationConfig):
             session_id=context.graph_execution_state_id,
             is_intermediate=self.is_intermediate,
         )
-        white_mask_dto = context.services.images.create(
-            image=white_mask,
-            image_origin=ResourceOrigin.INTERNAL,
-            image_category=ImageCategory.MASK,
-            node_id=self.id,
-            session_id=context.graph_execution_state_id,
-            is_intermediate=self.is_intermediate,
-        )
 
-        return ImageMaskOutput(
+        return ImageOutput(
             image=ImageField(image_name=image_dto.image_name),
             width=image_dto.width,
             height=image_dto.height,
-            mask=ImageField(image_name=white_mask_dto.image_name),
         )
 
 
