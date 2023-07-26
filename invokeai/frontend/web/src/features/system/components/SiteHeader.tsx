0f0e0344ee16f26975959b4d10af9b8a70b26aef
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-07-26 18:05:57 +1200

    fix: Fix Settings dropdown menu icons being too small

--- invokeai/frontend/web/src/features/system/components/SiteHeader.tsx
+++ invokeai/frontend/web/src/features/system/components/SiteHeader.tsx
@@ -2,10 +2,10 @@ import { Flex, MenuGroup, Spacer } from '@chakra-ui/react';
 import { memo } from 'react';
 import StatusIndicator from './StatusIndicator';
 
-import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
+import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import IAIIconButton from 'common/components/IAIIconButton';
 import { useTranslation } from 'react-i18next';
-import { FaBug, FaDiscord, FaGithub, FaKeyboard, FaBars } from 'react-icons/fa';
+import { FaBars, FaBug, FaDiscord, FaGithub, FaKeyboard } from 'react-icons/fa';
 import { MdSettings } from 'react-icons/md';
 import { useFeatureStatus } from '../hooks/useFeatureStatus';
 import HotkeysModal from './HotkeysModal/HotkeysModal';
@@ -44,29 +44,41 @@ const SiteHeader = () => {
         <MenuList>
           <MenuGroup title={t('common.communityLabel')}>
             {isGithubLinkEnabled && (
-              <MenuItem as="a" href={githubLink} icon={<FaGithub />}>
+              <MenuItem
+                as="a"
+                href={githubLink}
+                icon={<FaGithub fontSize={20} />}
+              >
                 {t('common.githubLabel')}
               </MenuItem>
             )}
             {isBugLinkEnabled && (
-              <MenuItem as="a" href={`${githubLink}/issues}`} icon={<FaBug />}>
+              <MenuItem
+                as="a"
+                href={`${githubLink}/issues}`}
+                icon={<FaBug fontSize={20} />}
+              >
                 {t('common.reportBugLabel')}
               </MenuItem>
             )}
             {isDiscordLinkEnabled && (
-              <MenuItem as="a" href={discordLink} icon={<FaDiscord />}>
+              <MenuItem
+                as="a"
+                href={discordLink}
+                icon={<FaDiscord fontSize={20} />}
+              >
                 {t('common.discordLabel')}
               </MenuItem>
             )}
           </MenuGroup>
           <MenuGroup title={t('common.settingsLabel')}>
             <HotkeysModal>
-              <MenuItem as="button" icon={<FaKeyboard />}>
+              <MenuItem as="button" icon={<FaKeyboard fontSize={20} />}>
                 {t('common.hotkeysLabel')}
               </MenuItem>
             </HotkeysModal>
             <SettingsModal>
-              <MenuItem as="button" icon={<MdSettings />}>
+              <MenuItem as="button" icon={<MdSettings fontSize={20} />}>
                 {t('common.settingsLabel')}
               </MenuItem>
             </SettingsModal>
