fd75a1dd1058cc59dfa5247d9ca8c5efaa0d11e6
Lincoln Stein <lstein@gmail.com>
2023-07-27 15:01:00 -0400

    reformat with black

--- scripts/probe-model.py
+++ scripts/probe-model.py
@@ -7,13 +7,10 @@ from invokeai.backend.model_management.model_probe import ModelProbe
 
 parser = argparse.ArgumentParser(description="Probe model type")
 parser.add_argument(
-    'model_path',
+    "model_path",
     type=Path,
 )
-args=parser.parse_args()
+args = parser.parse_args()
 
 info = ModelProbe().probe(args.model_path)
 print(info)
-
-      
-      
