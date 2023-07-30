ba817b56489254ef556dd14c02954017c0531743
Lincoln Stein <lstein@gmail.com>
2023-07-30 09:14:26 -0400

    added popup for translation service

--- invokeai/app/invocations/translate.py
+++ invokeai/app/invocations/translate.py
@@ -4,10 +4,11 @@ from typing import Literal
 from pydantic import Field
 from .baseinvocation import (
     BaseInvocation,
+    BaseInvocationOutput,
     InvocationContext,
     InvocationConfig,
 )
-from .params import StringOutput
+# from .params import StringOutput
 
 translate_available = False
 try:
@@ -19,6 +20,10 @@ except:
 
 DEFAULT_PROMPT = "" if translate_available else "To use this node, please 'pip install --upgrade translators'"
 
+class TranslateOutput(BaseInvocationOutput):
+    """Translated string output"""
+    type: Literal["translated_string_output"] = "translated_string_output"
+    prompt: str = Field(default=None, description="The translated prompt string")
 
 class TranslateInvocation(BaseInvocation):
     """Use the translators package to translate 330 languages into English prompts"""
@@ -27,7 +32,8 @@ class TranslateInvocation(BaseInvocation):
     type: Literal["translate"] = "translate"
 
     # Inputs
-    prompt: str = Field(default=DEFAULT_PROMPT, description="Prompt in any language")
+    text: str = Field(default=DEFAULT_PROMPT, description="Prompt in any language")
+    translator: Literal[tuple(ts.translators_pool)] = Field(default="google", description="The translator service to use")
     # fmt: on
 
     # Schema customisation
@@ -36,5 +42,6 @@ class TranslateInvocation(BaseInvocation):
             "ui": {"title": "Translate", "tags": ["prompt", "translate", "translator"]},
         }
 
-    def invoke(self, context: InvocationContext) -> StringOutput:
-        return StringOutput(text=ts.translate_text(self.prompt))
+    def invoke(self, context: InvocationContext) -> TranslateOutput:
+        translation: str= ts.translate_text(self.text, translator=self.translator)
+        return TranslateOutput(prompt=translation)
