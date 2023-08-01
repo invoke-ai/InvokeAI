fb8f218901ead1658562106c420b7d340fceba88
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-08-01 14:26:02 +1000

    fix(ui): post-onnx fixes

--- invokeai/frontend/web/src/features/nodes/util/graphBuilders/addVAEToGraph.ts
+++ invokeai/frontend/web/src/features/nodes/util/graphBuilders/addVAEToGraph.ts
@@ -17,7 +17,7 @@ import {
 export const addVAEToGraph = (
   state: RootState,
   graph: NonNullableGraph,
-  modelLoader: string = MAIN_MODEL_LOADER
+  modelLoaderNodeId: string = MAIN_MODEL_LOADER
 ): void => {
   const { vae } = state.generation;
 
@@ -34,11 +34,11 @@ export const addVAEToGraph = (
       vae_model: vae,
     };
   }
-  const isOnnxModel = modelLoader == ONNX_MODEL_LOADER;
+  const isOnnxModel = modelLoaderNodeId == ONNX_MODEL_LOADER;
   if (graph.id === TEXT_TO_IMAGE_GRAPH || graph.id === IMAGE_TO_IMAGE_GRAPH) {
     graph.edges.push({
       source: {
-        node_id: isAutoVae ? modelLoader : VAE_LOADER,
+        node_id: isAutoVae ? modelLoaderNodeId : VAE_LOADER,
         field: isAutoVae && isOnnxModel ? 'vae_decoder' : 'vae',
       },
       destination: {
@@ -51,7 +51,7 @@ export const addVAEToGraph = (
   if (graph.id === IMAGE_TO_IMAGE_GRAPH) {
     graph.edges.push({
       source: {
-        node_id: isAutoVae ? modelLoader : VAE_LOADER,
+        node_id: isAutoVae ? modelLoaderNodeId : VAE_LOADER,
         field: isAutoVae && isOnnxModel ? 'vae_decoder' : 'vae',
       },
       destination: {
@@ -64,7 +64,7 @@ export const addVAEToGraph = (
   if (graph.id === INPAINT_GRAPH) {
     graph.edges.push({
       source: {
-        node_id: isAutoVae ? modelLoader : VAE_LOADER,
+        node_id: isAutoVae ? modelLoaderNodeId : VAE_LOADER,
         field: isAutoVae && isOnnxModel ? 'vae_decoder' : 'vae',
       },
       destination: {
