44cfebe5ad0896bd312d1a0496aad6a0e472ca65
Zerdoumi <3418725+sohelzerdoumi@users.noreply.github.com>
2023-08-01 15:57:13 +0200

    fix https/wss behind reverse proxy

--- invokeai/frontend/web/src/services/events/middleware.ts
+++ invokeai/frontend/web/src/services/events/middleware.ts
@@ -12,8 +12,9 @@ import { socketSubscribed, socketUnsubscribed } from './actions';
 
 export const socketMiddleware = () => {
   let areListenersSet = false;
-
-  let socketUrl = `ws://${window.location.host}`;
+  
+  let wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+  let socketUrl = `${wsProtocol}://${window.location.host}`;
 
   const socketOptions: Parameters<typeof io>[0] = {
     timeout: 60000,
