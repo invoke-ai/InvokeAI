7f4c3870808541746e4f4bc51fdc601b8e19056f
Kevin Turner <83819+keturn@users.noreply.github.com>
2023-08-05 15:46:46 -0700

    test(model_management): factor out name strings

--- tests/test_model_manager.py
+++ tests/test_model_manager.py
@@ -5,6 +5,9 @@ import pytest
 from invokeai.app.services.config import InvokeAIAppConfig
 from invokeai.backend import ModelManager, BaseModelType, ModelType, SubModelType
 
+BASIC_MODEL_NAME = ("SDXL base", BaseModelType.StableDiffusionXL, ModelType.Main)
+VAE_OVERRIDE_MODEL_NAME = ("SDXL with VAE", BaseModelType.StableDiffusionXL, ModelType.Main)
+
 
 @pytest.fixture
 def model_manager(datadir) -> ModelManager:
@@ -14,14 +17,11 @@ def model_manager(datadir) -> ModelManager:
 
 def test_get_model_names(model_manager: ModelManager):
     names = model_manager.model_names()
-    assert names[:2] == [
-        ("SDXL base", BaseModelType.StableDiffusionXL, ModelType.Main),
-        ("SDXL with VAE", BaseModelType.StableDiffusionXL, ModelType.Main),
-    ]
+    assert names[:2] == [BASIC_MODEL_NAME, VAE_OVERRIDE_MODEL_NAME]
 
 
 def test_get_model_path_for_diffusers(model_manager: ModelManager, datadir: Path):
-    model_config = model_manager._get_model_config(BaseModelType.StableDiffusionXL, "SDXL base", ModelType.Main)
+    model_config = model_manager._get_model_config(BASIC_MODEL_NAME[1], BASIC_MODEL_NAME[0], BASIC_MODEL_NAME[2])
     top_model_path, is_override = model_manager._get_model_path(model_config)
     expected_model_path = datadir / "models" / "sdxl" / "main" / "SDXL base 1_0"
     assert top_model_path == expected_model_path
@@ -29,7 +29,9 @@ def test_get_model_path_for_diffusers(model_manager: ModelManager, datadir: Path
 
 
 def test_get_model_path_for_overridden_vae(model_manager: ModelManager, datadir: Path):
-    model_config = model_manager._get_model_config(BaseModelType.StableDiffusionXL, "SDXL with VAE", ModelType.Main)
+    model_config = model_manager._get_model_config(
+        VAE_OVERRIDE_MODEL_NAME[1], VAE_OVERRIDE_MODEL_NAME[0], VAE_OVERRIDE_MODEL_NAME[2]
+    )
     vae_model_path, is_override = model_manager._get_model_path(model_config, SubModelType.Vae)
     expected_vae_path = datadir / "models" / "sdxl" / "vae" / "sdxl-vae-fp16-fix"
     assert vae_model_path == expected_vae_path
