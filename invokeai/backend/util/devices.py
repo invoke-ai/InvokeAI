bb5a685f16afa2e97ebe8660a58d12c136a1354e
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-08-05 12:21:35 +1000

    chore: black

--- invokeai/backend/util/devices.py
+++ invokeai/backend/util/devices.py
@@ -32,7 +32,7 @@ def choose_precision(device: torch.device) -> str:
         device_name = torch.cuda.get_device_name(device)
         if not ("GeForce GTX 1660" in device_name or "GeForce GTX 1650" in device_name):
             return "float16"
-    elif device.type == "mps" and version.parse(platform.mac_ver()[0]) < version.parse('14.0.0'):
+    elif device.type == "mps" and version.parse(platform.mac_ver()[0]) < version.parse("14.0.0"):
         return "float16"
     return "float32"
 
