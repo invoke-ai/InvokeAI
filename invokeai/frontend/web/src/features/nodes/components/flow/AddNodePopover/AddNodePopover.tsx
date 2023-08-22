5cf9b75d77297abff2aaf8e5bb97c9485d05e818
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-08-22 23:56:08 +1200

    fix: Remove / as hotkey for add node and add tooltip

--- invokeai/frontend/web/src/features/nodes/components/flow/AddNodePopover/AddNodePopover.tsx
+++ invokeai/frontend/web/src/features/nodes/components/flow/AddNodePopover/AddNodePopover.tsx
@@ -140,7 +140,7 @@ const AddNodePopover = () => {
     onClose();
   }, [onClose]);
 
-  useHotkeys(['shift+a', 'space', '/'], handleHotkeyOpen);
+  useHotkeys(['shift+a', 'space'], handleHotkeyOpen);
   useHotkeys(['escape'], handleHotkeyClose);
 
   return (
--- invokeai/frontend/web/src/features/nodes/components/flow/panels/TopLeftPanel/TopLeftPanel.tsx
+++ invokeai/frontend/web/src/features/nodes/components/flow/panels/TopLeftPanel/TopLeftPanel.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from '@chakra-ui/react';
 import { useAppDispatch } from 'app/store/storeHooks';
 import IAIButton from 'common/components/IAIButton';
 import { addNodePopoverOpened } from 'features/nodes/store/nodesSlice';
@@ -13,9 +14,15 @@ const TopLeftPanel = () => {
 
   return (
     <Panel position="top-left">
-      <IAIButton aria-label="Add Node" onClick={handleOpenAddNodePopover}>
-        Add Node
-      </IAIButton>
+      <Tooltip label="Add New Node (Shift+A, Space)">
+        <IAIButton
+          size="sm"
+          aria-label="Add Node"
+          onClick={handleOpenAddNodePopover}
+        >
+          Add Node
+        </IAIButton>
+      </Tooltip>
     </Panel>
   );
 };
