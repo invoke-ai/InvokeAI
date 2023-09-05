86185f2fe3c6a6847d17001fb11f962cb4f2ef13
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-09-05 22:37:14 +1200

    feat: Scaled Bounding Box Dimensions now respect Aspect Ratio

--- invokeai/frontend/web/src/features/parameters/components/Parameters/Canvas/InfillAndScaling/ParamScaledHeight.tsx
+++ invokeai/frontend/web/src/features/parameters/components/Parameters/Canvas/InfillAndScaling/ParamScaledHeight.tsx
@@ -2,6 +2,7 @@ import { createSelector } from '@reduxjs/toolkit';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
 import { defaultSelectorOptions } from 'app/store/util/defaultMemoizeOptions';
 import IAISlider from 'common/components/IAISlider';
+import { roundToMultiple } from 'common/util/roundDownToMultiple';
 import { canvasSelector } from 'features/canvas/store/canvasSelectors';
 import { setScaledBoundingBoxDimensions } from 'features/canvas/store/canvasSlice';
 import { generationSelector } from 'features/parameters/store/generationSelectors';
@@ -12,12 +13,13 @@ const selector = createSelector(
   [generationSelector, canvasSelector],
   (generation, canvas) => {
     const { scaledBoundingBoxDimensions, boundingBoxScaleMethod } = canvas;
-    const { model } = generation;
+    const { model, aspectRatio } = generation;
 
     return {
       model,
       scaledBoundingBoxDimensions,
       isManual: boundingBoxScaleMethod === 'manual',
+      aspectRatio,
     };
   },
   defaultSelectorOptions
@@ -25,7 +27,7 @@ const selector = createSelector(
 
 const ParamScaledHeight = () => {
   const dispatch = useAppDispatch();
-  const { model, isManual, scaledBoundingBoxDimensions } =
+  const { model, isManual, scaledBoundingBoxDimensions, aspectRatio } =
     useAppSelector(selector);
 
   const initial = ['sdxl', 'sdxl-refiner'].includes(model?.base_model as string)
@@ -35,19 +37,33 @@ const ParamScaledHeight = () => {
   const { t } = useTranslation();
 
   const handleChangeScaledHeight = (v: number) => {
+    let newWidth = scaledBoundingBoxDimensions.width;
+    const newHeight = Math.floor(v);
+
+    if (aspectRatio) {
+      newWidth = roundToMultiple(newHeight * aspectRatio, 8);
+    }
+
     dispatch(
       setScaledBoundingBoxDimensions({
-        ...scaledBoundingBoxDimensions,
-        height: Math.floor(v),
+        width: newWidth,
+        height: newHeight,
       })
     );
   };
 
   const handleResetScaledHeight = () => {
+    let resetWidth = scaledBoundingBoxDimensions.width;
+    const resetHeight = Math.floor(initial);
+
+    if (aspectRatio) {
+      resetWidth = roundToMultiple(resetHeight * aspectRatio, 8);
+    }
+
     dispatch(
       setScaledBoundingBoxDimensions({
-        ...scaledBoundingBoxDimensions,
-        height: Math.floor(initial),
+        width: resetWidth,
+        height: resetHeight,
       })
     );
   };
