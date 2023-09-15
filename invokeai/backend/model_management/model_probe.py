16664da5b6689ae6cbd61e4fee932a00019acec8
Ryan Dick <ryanjdick3@gmail.com>
2023-09-14 23:49:02 -0400

    black

--- invokeai/backend/model_management/model_probe.py
+++ invokeai/backend/model_management/model_probe.py
@@ -511,7 +511,9 @@ class ControlNetFolderProbe(FolderProbeBase):
             else (
                 BaseModelType.StableDiffusion2
                 if dimension == 1024
-                else BaseModelType.StableDiffusionXL if dimension == 2048 else None
+                else BaseModelType.StableDiffusionXL
+                if dimension == 2048
+                else None
             )
         )
         if not base_model:
