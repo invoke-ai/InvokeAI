508c7ca9ebec9910eaa7c63850ab3a1255eef15b
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-09-19 18:52:41 +1000

    feat(ui): fix up param collapse labels

--- invokeai/frontend/web/src/features/dynamicPrompts/components/ParamDynamicPromptsCollapse.tsx
+++ invokeai/frontend/web/src/features/dynamicPrompts/components/ParamDynamicPromptsCollapse.tsx
@@ -2,29 +2,33 @@ import { Flex } from '@chakra-ui/react';
 import { createSelector } from '@reduxjs/toolkit';
 import { stateSelector } from 'app/store/store';
 import { useAppSelector } from 'app/store/storeHooks';
-import { defaultSelectorOptions } from 'app/store/util/defaultMemoizeOptions';
 import IAICollapse from 'common/components/IAICollapse';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useFeatureStatus } from '../../system/hooks/useFeatureStatus';
 import ParamDynamicPromptsMaxPrompts from './ParamDynamicPromptsMaxPrompts';
 import ParamDynamicPromptsPreview from './ParamDynamicPromptsPreview';
 import ParamDynamicPromptsSeedBehaviour from './ParamDynamicPromptsSeedBehaviour';
 
-const selector = createSelector(
-  stateSelector,
-  (state) => {
-    const { prompts } = state.dynamicPrompts;
-    return {
-      activeLabel: `${prompts.length} Prompt${prompts.length !== 1 ? 's' : ''}`,
-    };
-  },
-  defaultSelectorOptions
-);
-
 const ParamDynamicPromptsCollapse = () => {
-  const { activeLabel } = useAppSelector(selector);
   const { t } = useTranslation();
+  const selectActiveLabel = useMemo(
+    () =>
+      createSelector(stateSelector, ({ dynamicPrompts }) => {
+        const count = dynamicPrompts.prompts.length;
+        if (count === 1) {
+          return t('dynamicPrompts.promptsWithCount_one', {
+            count,
+          });
+        } else {
+          return t('dynamicPrompts.promptsWithCount_other', {
+            count,
+          });
+        }
+      }),
+    [t]
+  );
+  const activeLabel = useAppSelector(selectActiveLabel);
 
   const isDynamicPromptingEnabled =
     useFeatureStatus('dynamicPrompting').isFeatureEnabled;
@@ -34,7 +38,10 @@ const ParamDynamicPromptsCollapse = () => {
   }
 
   return (
-    <IAICollapse label={t('prompt.dynamicPrompts')} activeLabel={activeLabel}>
+    <IAICollapse
+      label={t('dynamicPrompts.dynamicPrompts')}
+      activeLabel={activeLabel}
+    >
       <Flex sx={{ gap: 2, flexDir: 'column' }}>
         <ParamDynamicPromptsSeedBehaviour />
         <ParamDynamicPromptsPreview />
