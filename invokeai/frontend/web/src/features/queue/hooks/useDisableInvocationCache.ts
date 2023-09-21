1227f054bfc1a5062255faab011e035912ab7304
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-09-21 18:57:52 +1000

    feat(nodes): add enable, disable, status to invocation cache
    - New routes to clear, enable, disable and get the status of the cache
    - Status includes hits, misses, size, max size, enabled
    - Add client cache queries and mutations, abstracted into hooks
    - Add invocation cache status area (next to queue status) w/ buttons

---
+++ invokeai/frontend/web/src/features/queue/hooks/useDisableInvocationCache.ts
@@ -0,0 +1,48 @@
+import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
+import { addToast } from 'features/system/store/systemSlice';
+import { useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import {
+  useDisableInvocationCacheMutation,
+  useGetInvocationCacheStatusQuery,
+} from 'services/api/endpoints/appInfo';
+
+export const useDisableInvocationCache = () => {
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const { data: cacheStatus } = useGetInvocationCacheStatusQuery();
+  const isConnected = useAppSelector((state) => state.system.isConnected);
+  const [trigger, { isLoading }] = useDisableInvocationCacheMutation({
+    fixedCacheKey: 'disableInvocationCache',
+  });
+
+  const isDisabled = useMemo(
+    () => !cacheStatus?.enabled || !isConnected,
+    [cacheStatus?.enabled, isConnected]
+  );
+
+  const disableInvocationCache = useCallback(async () => {
+    if (isDisabled) {
+      return;
+    }
+
+    try {
+      await trigger().unwrap();
+      dispatch(
+        addToast({
+          title: t('invocationCache.disableSucceeded'),
+          status: 'success',
+        })
+      );
+    } catch {
+      dispatch(
+        addToast({
+          title: t('invocationCache.disableFailed'),
+          status: 'error',
+        })
+      );
+    }
+  }, [isDisabled, trigger, dispatch, t]);
+
+  return { disableInvocationCache, isLoading, cacheStatus, isDisabled };
+};
