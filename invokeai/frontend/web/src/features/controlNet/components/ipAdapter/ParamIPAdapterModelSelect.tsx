830c1f7fc5243b71edecbb5e3a7831db8d988363
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2023-10-03 13:28:15 -0400

    IP adapter UI

--- invokeai/frontend/web/src/features/controlNet/components/ipAdapter/ParamIPAdapterModelSelect.tsx
+++ invokeai/frontend/web/src/features/controlNet/components/ipAdapter/ParamIPAdapterModelSelect.tsx
@@ -6,7 +6,7 @@ import { ipAdapterModelChanged } from 'features/controlNet/store/controlNetSlice
 import { MODEL_TYPE_MAP } from 'features/parameters/types/constants';
 import { modelIdToIPAdapterModelParam } from 'features/parameters/util/modelIdToIPAdapterModelParams';
 import { forEach } from 'lodash-es';
-import { memo, useCallback, useMemo } from 'react';
+import { memo, useCallback, useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useGetIPAdapterModelsQuery } from 'services/api/endpoints/models';
 
@@ -23,6 +23,27 @@ const ParamIPAdapterModelSelect = () => {
   const { t } = useTranslation();
 
   const { data: ipAdapterModels } = useGetIPAdapterModelsQuery();
+
+  const firstModel = useMemo(() => {
+    if (!ipAdapterModels || !Object.keys(ipAdapterModels.entities).length) {
+      return undefined;
+    }
+    const firstModelId = Object.keys(ipAdapterModels.entities)[0];
+
+    if (!firstModelId) {
+      return undefined;
+    }
+
+    const firstModel = ipAdapterModels.entities[firstModelId];
+
+    return firstModel ? firstModel : undefined;
+  }, [ipAdapterModels]);
+
+  useEffect(() => {
+    if (firstModel) {
+      dispatch(ipAdapterModelChanged(firstModel));
+    }
+  }, [firstModel, dispatch]);
 
   // grab the full model entity from the RTK Query cache
   const selectedModel = useMemo(
