55ad4feb5c2f1e15db17756ce22994402f0cb0ca
Kent Keirsey <31807370+hipsterusername@users.noreply.github.com>
2023-10-17 11:59:19 -0400

    Revert "Revert "feat(ui): remove special handling for t2i vs controlnet""
    This reverts commit bdf4c4944cf0e9e827ee026fddfd1bd74f3c40e0.

--- invokeai/frontend/web/src/features/controlAdapters/store/controlAdaptersSlice.ts
+++ invokeai/frontend/web/src/features/controlAdapters/store/controlAdaptersSlice.ts
@@ -88,61 +88,6 @@ export const selectValidT2IAdapters = (controlAdapters: ControlAdaptersState) =>
           (ca.processorType === 'none' && Boolean(ca.controlImage)))
     );
 
-// TODO: I think we can safely remove this?
-// const disableAllIPAdapters = (
-//   state: ControlAdaptersState,
-//   exclude?: string
-// ) => {
-//   const updates: Update<ControlAdapterConfig>[] = selectAllIPAdapters(state)
-//     .filter((ca) => ca.id !== exclude)
-//     .map((ca) => ({
-//       id: ca.id,
-//       changes: { isEnabled: false },
-//     }));
-//   caAdapter.updateMany(state, updates);
-// };
-
-const disableAllControlNets = (
-  state: ControlAdaptersState,
-  exclude?: string
-) => {
-  const updates: Update<ControlAdapterConfig>[] = selectAllControlNets(state)
-    .filter((ca) => ca.id !== exclude)
-    .map((ca) => ({
-      id: ca.id,
-      changes: { isEnabled: false },
-    }));
-  caAdapter.updateMany(state, updates);
-};
-
-const disableAllT2IAdapters = (
-  state: ControlAdaptersState,
-  exclude?: string
-) => {
-  const updates: Update<ControlAdapterConfig>[] = selectAllT2IAdapters(state)
-    .filter((ca) => ca.id !== exclude)
-    .map((ca) => ({
-      id: ca.id,
-      changes: { isEnabled: false },
-    }));
-  caAdapter.updateMany(state, updates);
-};
-
-const disableIncompatibleControlAdapters = (
-  state: ControlAdaptersState,
-  type: ControlAdapterType,
-  exclude?: string
-) => {
-  if (type === 'controlnet') {
-    // we cannot do controlnet + t2i adapter, if we are enabled a controlnet, disable all t2is
-    disableAllT2IAdapters(state, exclude);
-  }
-  if (type === 't2i_adapter') {
-    // we cannot do controlnet + t2i adapter, if we are enabled a t2i, disable controlnets
-    disableAllControlNets(state, exclude);
-  }
-};
-
 export const controlAdaptersSlice = createSlice({
   name: 'controlAdapters',
   initialState: initialControlAdapterState,
@@ -158,7 +103,6 @@ export const controlAdaptersSlice = createSlice({
       ) => {
         const { id, type, overrides } = action.payload;
         caAdapter.addOne(state, buildControlAdapter(id, type, overrides));
-        disableIncompatibleControlAdapters(state, type, id);
       },
       prepare: ({
         type,
@@ -175,8 +119,6 @@ export const controlAdaptersSlice = createSlice({
       action: PayloadAction<ControlAdapterConfig>
     ) => {
       caAdapter.addOne(state, action.payload);
-      const { type, id } = action.payload;
-      disableIncompatibleControlAdapters(state, type, id);
     },
     controlAdapterDuplicated: {
       reducer: (
@@ -196,8 +138,6 @@ export const controlAdaptersSlice = createSlice({
           isEnabled: true,
         });
         caAdapter.addOne(state, newControlAdapter);
-        const { type } = newControlAdapter;
-        disableIncompatibleControlAdapters(state, type, newId);
       },
       prepare: (id: string) => {
         return { payload: { id, newId: uuidv4() } };
@@ -217,7 +157,6 @@ export const controlAdaptersSlice = createSlice({
           state,
           buildControlAdapter(id, type, { controlImage })
         );
-        disableIncompatibleControlAdapters(state, type, id);
       },
       prepare: (payload: {
         type: ControlAdapterType;
@@ -235,12 +174,6 @@ export const controlAdaptersSlice = createSlice({
     ) => {
       const { id, isEnabled } = action.payload;
       caAdapter.updateOne(state, { id, changes: { isEnabled } });
-      if (isEnabled) {
-        // we are enabling a control adapter. due to limitations in the current system, we may need to disable other adapters
-        // TODO: disable when multiple IP adapters are supported
-        const ca = selectControlAdapterById(state, id);
-        ca && disableIncompatibleControlAdapters(state, ca.type, id);
-      }
     },
     controlAdapterImageChanged: (
       state,
