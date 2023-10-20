824702de99a09d18e5fae065bda2cc67568908f2
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-10-20 12:50:55 +1100

    feat(nodes): change expected structure for custom nodes

--- invokeai/app/invocations/__init__.py
+++ invokeai/app/invocations/__init__.py
@@ -7,10 +7,13 @@ from invokeai.app.services.config.config_default import InvokeAIAppConfig
 
 custom_nodes_path = Path(InvokeAIAppConfig.get_config().custom_nodes_path.absolute())
 custom_nodes_path.mkdir(parents=True, exist_ok=True)
+
 custom_nodes_init_path = str(custom_nodes_path / "__init__.py")
+custom_nodes_readme_path = str(custom_nodes_path / "README.md")
 
 # copy our custom nodes __init__.py to the custom nodes directory
-shutil.copy(Path(__file__).parent / "_custom_nodes_init.py", custom_nodes_init_path)
+shutil.copy(Path(__file__).parent / "custom_nodes/init.py", custom_nodes_init_path)
+shutil.copy(Path(__file__).parent / "custom_nodes/README.md", custom_nodes_readme_path)
 
 # Import custom nodes, see https://docs.python.org/3/library/importlib.html#importing-programmatically
 spec = spec_from_file_location("custom_nodes", custom_nodes_init_path)
@@ -21,5 +24,5 @@ sys.modules[spec.name] = module
 spec.loader.exec_module(module)
 
 # add core nodes to __all__
-python_files = filter(lambda f: not f.name.startswith("_"), Path(__file__).parent.rglob("*.py"))
+python_files = filter(lambda f: not f.name.startswith("_"), Path(__file__).parent.glob("*.py"))
 __all__ = list(f.stem for f in python_files)  # type: ignore
