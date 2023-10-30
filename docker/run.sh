d1d8ee71fc5ce3639d5463927909849b719ba2ab
Wilson E. Alvarez <wilson.e.alvarez@rubonnek.com>
2023-10-30 16:53:02 -0400

    Simplify docker compose setup

--- docker/run.sh
+++ docker/run.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
-# This script is provided for backwards compatibility with the old docker setup.
-# it doesn't do much aside from wrapping the usual docker compose CLI.
+run() {
+  local scriptdir=$(dirname "${BASH_SOURCE[0]}")
+  cd "$scriptdir" || exit 1
 
-SCRIPTDIR=$(dirname "${BASH_SOURCE[0]}")
-cd "$SCRIPTDIR" || exit 1
+  local build_args=""
+  local service_name="invokeai-cpu"
 
-docker compose up -d
-docker compose logs -f
+  [[ -f ".env" ]] &&
+    build_args=$(awk '$1 ~ /=[^$]/ {print "--build-arg " $0 " "}' .env) &&
+    service_name="invokeai-$(awk -F '=' '/GPU_DRIVER/ {print $2}' .env)"
+
+  printf "%s\n" "docker compose build args:"
+  printf "%s\n" "$build_args"
+
+  docker compose build $build_args
+  unset build_args
+
+  printf "%s\n" "starting service $service_name"
+  docker compose up -d $service_name
+  docker compose logs -f
+}
+
+run
