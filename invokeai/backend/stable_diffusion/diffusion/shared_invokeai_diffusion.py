defcd0a18ac1e8f6d598c3a13a96d9560bed1cf3
Damian Stewart <d@damianstewart.com>
2023-11-09 10:02:14 +0100

    cleanup unused code

--- invokeai/backend/stable_diffusion/diffusion/shared_invokeai_diffusion.py
+++ invokeai/backend/stable_diffusion/diffusion/shared_invokeai_diffusion.py
@@ -34,18 +34,6 @@ ModelForwardCallback: TypeAlias = Union[
     Callable[[torch.Tensor, torch.Tensor, torch.Tensor], torch.Tensor],
 ]
 
-def rescale_noise_cfg(noise_cfg, noise_pred_text, guidance_rescale=0.0):
-     """
-     Rescale `noise_cfg` according to `guidance_rescale`. Based on findings of [Common Diffusion Noise Schedules and
-     Sample Steps are Flawed](https://arxiv.org/pdf/2305.08891.pdf). See Section 3.4
-     """
-     std_text = noise_pred_text.std(dim=list(range(1, noise_pred_text.ndim)), keepdim=True)
-     std_cfg = noise_cfg.std(dim=list(range(1, noise_cfg.ndim)), keepdim=True)
-     # rescale the results from guidance (fixes overexposure)
-     noise_pred_rescaled = noise_cfg * (std_text / std_cfg)
-     # mix with the original results from guidance by factor guidance_rescale to avoid "plain looking" images
-     noise_cfg = guidance_rescale * noise_pred_rescaled + (1 - guidance_rescale) * noise_cfg
-     return noise_cfg
 
 class InvokeAIDiffuserComponent:
     """
