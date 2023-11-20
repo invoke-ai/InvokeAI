1a596a568453ec67bee3ad155ee7c954262899f8
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-11-21 07:22:02 +1100

    fix(backend): fix unintentional change to import orders
    - Ignore I001 (isort rules) for this file
    - Ignore F401 (unused imports) for this file

--- invokeai/backend/model_management/__init__.py
+++ invokeai/backend/model_management/__init__.py
@@ -1,14 +1,13 @@
+# ruff: noqa: I001, F401
 """
 Initialization file for invokeai.backend.model_management
 """
 # This import must be first
-from .lora import ModelPatcher, ONNXModelPatcher  # noqa: F401
-from .model_cache import ModelCache  # noqa: F401
-from .model_manager import AddModelResult, ModelInfo, ModelManager, SchedulerPredictionType  # noqa: F401 isort: split
+from .model_manager import AddModelResult, ModelInfo, ModelManager, SchedulerPredictionType
+from .lora import ModelPatcher, ONNXModelPatcher
+from .model_cache import ModelCache
 
-# This import must be last
-from .model_merge import MergeInterpolationMethod, ModelMerger  # noqa: F401 isort: split
-from .models import (  # noqa: F401
+from .models import (
     BaseModelType,
     DuplicateModelException,
     ModelNotFoundException,
@@ -16,3 +15,6 @@ from .models import (  # noqa: F401
     ModelVariantType,
     SubModelType,
 )
+
+# This import must be last
+from .model_merge import MergeInterpolationMethod, ModelMerger
