b51c80894b29f236bfb73bdd718e2ddf2e16d03a
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-11-25 21:10:22 +1100

    feat(ui): improved UI for missing node field templates
    When a node is updated with new fields and workflow needs to be updated, the fields now display "Unknown input/output: FieldName".

---
+++ invokeai/frontend/web/src/features/nodes/hooks/useFieldInputInstance.ts
@@ -0,0 +1,28 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { stateSelector } from 'app/store/store';
+import { useAppSelector } from 'app/store/storeHooks';
+import { defaultSelectorOptions } from 'app/store/util/defaultMemoizeOptions';
+import { useMemo } from 'react';
+import { isInvocationNode } from '../types/invocation';
+
+export const useFieldInputInstance = (nodeId: string, fieldName: string) => {
+  const selector = useMemo(
+    () =>
+      createSelector(
+        stateSelector,
+        ({ nodes }) => {
+          const node = nodes.nodes.find((node) => node.id === nodeId);
+          if (!isInvocationNode(node)) {
+            return;
+          }
+          return node.data.inputs[fieldName];
+        },
+        defaultSelectorOptions
+      ),
+    [fieldName, nodeId]
+  );
+
+  const fieldTemplate = useAppSelector(selector);
+
+  return fieldTemplate;
+};
