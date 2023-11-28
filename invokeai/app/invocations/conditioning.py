8f69e20a30bccf8c2050765aaff0667748643d96
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2023-11-28 22:39:27 +0530

    wip: CAC Concat (possibly)

--- invokeai/app/invocations/conditioning.py
+++ invokeai/app/invocations/conditioning.py
@@ -40,7 +40,29 @@ class ConditioningConcatInvocation(BaseInvocation):
             c1_extra_conditioning_info.tokens_count_including_eos_bos
             + c2_extra_conditioning_info.tokens_count_including_eos_bos
         )
+
+        c1_ec_cross_control_args = c1_extra_conditioning_info.cross_attention_control_args
+        c2_ec_cross_control_args = c2_extra_conditioning_info.cross_attention_control_args
+
         ec_cross_attention_control_args = None
+
+        if c1_ec_cross_control_args and not c2_ec_cross_control_args:
+            ec_cross_attention_control_args = c1_ec_cross_control_args
+
+        if c2_ec_cross_control_args and not c1_ec_cross_control_args:
+            ec_cross_attention_control_args = c2_ec_cross_control_args
+
+        if c1_ec_cross_control_args and c2_ec_cross_control_args:
+            original_conditioning = torch.cat(
+                (c1_ec_cross_control_args.original_conditioning, c2_ec_cross_control_args.original_conditioning), dim=1
+            )
+            edited_conditioning = torch.cat(
+                (c1_ec_cross_control_args.edited_conditioning, c2_ec_cross_control_args.edited_conditioning), dim=1
+            )
+            edit_opcodes = c1_ec_cross_control_args.edit_opcodes + c2_ec_cross_control_args.edit_opcodes
+            edit_options = c1_ec_cross_control_args.edit_options.update(c2_ec_cross_control_args.edit_options)
+
+            ec_cross_attention_control_args = (original_conditioning, edited_conditioning, edit_opcodes, edit_options)
 
         ec_concat = ExtraConditioningInfo(
             tokens_count_including_eos_bos=ec_concat_tokens_count_including_eos_bos,
