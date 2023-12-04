3e66b4f8902b9d8e69df1942afe4b0a0d9f8d622
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2023-12-04 15:22:56 -0500

    cleanup

--- invokeai/frontend/web/src/app/hooks/useSocketIO.ts
+++ invokeai/frontend/web/src/app/hooks/useSocketIO.ts
@@ -3,8 +3,8 @@ import { $authToken } from 'app/store/nanostores/authToken';
 import { $baseUrl } from 'app/store/nanostores/baseUrl';
 import { $isDebugging } from 'app/store/nanostores/isDebugging';
 import { useAppDispatch } from 'app/store/storeHooks';
-import { MapStore, WritableAtom, atom, map } from 'nanostores';
-import { useEffect } from 'react';
+import { MapStore, atom, map } from 'nanostores';
+import { useEffect, useMemo } from 'react';
 import {
   ClientToServerEvents,
   ServerToClientEvents,
@@ -16,57 +16,8 @@ import { ManagerOptions, Socket, SocketOptions, io } from 'socket.io-client';
 declare global {
   interface Window {
     $socketOptions?: MapStore<Partial<ManagerOptions & SocketOptions>>;
-    $socketUrl?: WritableAtom<string>;
   }
 }
-
-const makeSocketOptions = (): Partial<ManagerOptions & SocketOptions> => {
-  const socketOptions: Parameters<typeof io>[0] = {
-    timeout: 60000,
-    path: '/ws/socket.io',
-    autoConnect: false, // achtung! removing this breaks the dynamic middleware
-    forceNew: true,
-  };
-
-  // if building in package mode, replace socket url with open api base url minus the http protocol
-  if (['nodes', 'package'].includes(import.meta.env.MODE)) {
-    const authToken = $authToken.get();
-    if (authToken) {
-      // TODO: handle providing jwt to socket.io
-      socketOptions.auth = { token: authToken };
-    }
-
-    socketOptions.transports = ['websocket', 'polling'];
-  }
-
-  return socketOptions;
-};
-
-const makeSocketUrl = (): string => {
-  const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-  let socketUrl = `${wsProtocol}://${window.location.host}`;
-  // if (['nodes', 'package'].includes(import.meta.env.MODE)) {
-  const baseUrl = $baseUrl.get();
-  console.log({ baseUrl });
-  if (baseUrl) {
-    //eslint-disable-next-line
-    socketUrl = baseUrl.replace(/^https?\:\/\//i, '');
-  }
-  // }
-  console.log({ socketUrl });
-  return socketUrl;
-};
-
-const makeSocket = (
-  socketUrl: string
-): Socket<ServerToClientEvents, ClientToServerEvents> => {
-  const socketOptions = makeSocketOptions();
-  const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-    socketUrl,
-    { ...socketOptions, ...$socketOptions.get() }
-  );
-  return socket;
-};
 
 export const $socketOptions = map<Partial<ManagerOptions & SocketOptions>>({});
 export const $isSocketInitialized = atom<boolean>(false);
@@ -76,23 +27,51 @@ export const $isSocketInitialized = atom<boolean>(false);
  */
 export const useSocketIO = () => {
   const dispatch = useAppDispatch();
-  const socketOptions = useStore($socketOptions);
-  const socketUrl = makeSocketUrl();
   const baseUrl = useStore($baseUrl);
   const authToken = useStore($authToken);
+  const addlSocketOptions = useStore($socketOptions);
+
+  const socketUrl = useMemo(() => {
+    const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+    let builtUrl = `${wsProtocol}://${window.location.host}`;
+    if (baseUrl) {
+      builtUrl = baseUrl.replace(/^https?:\/\//i, '');
+    }
+
+    return builtUrl;
+  }, [baseUrl]);
+
+  const socketOptions = useMemo(() => {
+    const options: Parameters<typeof io>[0] = {
+      timeout: 60000,
+      path: '/ws/socket.io',
+      autoConnect: false, // achtung! removing this breaks the dynamic middleware
+      forceNew: true,
+    };
+
+    if (authToken) {
+      options.auth = { token: authToken };
+      options.transports = ['websocket', 'polling'];
+    }
+
+    return { ...options, ...addlSocketOptions };
+  }, [authToken, addlSocketOptions]);
 
   useEffect(() => {
     if ($isSocketInitialized.get()) {
       // Singleton!
       return;
     }
-    const socket = makeSocket(socketUrl);
+
+    const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+      socketUrl,
+      socketOptions
+    );
     setEventListeners({ dispatch, socket });
     socket.connect();
 
     if ($isDebugging.get()) {
       window.$socketOptions = $socketOptions;
-      // window.$socketUrl = socketUrl;
       console.log('Socket initialized', socket);
     }
 
@@ -101,11 +80,10 @@ export const useSocketIO = () => {
     return () => {
       if ($isDebugging.get()) {
         window.$socketOptions = undefined;
-        window.$socketUrl = undefined;
         console.log('Socket teardown', socket);
       }
       socket.disconnect();
       $isSocketInitialized.set(false);
     };
-  }, [dispatch, socketOptions, socketUrl, baseUrl, authToken]);
+  }, [dispatch, socketOptions, socketUrl]);
 };
