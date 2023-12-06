72b93b8039063d4e1c9a5b3c57f9c5aaa768a8fd
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-12-06 23:48:12 +1100

    fix(ui): use translation for unnamed workflows

--- invokeai/frontend/web/src/features/nodes/components/flow/panels/TopPanel/WorkflowName.tsx
+++ invokeai/frontend/web/src/features/nodes/components/flow/panels/TopPanel/WorkflowName.tsx
@@ -5,9 +5,7 @@ import { useTranslation } from 'react-i18next';
 
 const TopCenterPanel = () => {
   const { t } = useTranslation();
-  const name = useAppSelector(
-    (state) => state.workflow.name || 'Untitled Workflow'
-  );
+  const name = useAppSelector((state) => state.workflow.name);
   const isTouched = useAppSelector((state) => state.workflow.isTouched);
 
   return (
@@ -20,7 +18,7 @@ const TopCenterPanel = () => {
       fontWeight={600}
       opacity={0.8}
     >
-      {name}
+      {name || t('workflows.unnamedWorkflow')}
       {isTouched ? ` (${t('common.unsaved')})` : ''}
     </Text>
   );
