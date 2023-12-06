770cd36167c0099c22db6ea7d2742d5149b88b15
maryhipp <maryhipp@gmail.com>
2023-12-06 14:07:49 -0500

    add optional actions to image and board DTO

--- invokeai/app/services/boards/boards_common.py
+++ invokeai/app/services/boards/boards_common.py
@@ -12,12 +12,23 @@ class BoardDTO(BoardRecord):
     """The URL of the thumbnail of the most recent image in the board."""
     image_count: int = Field(description="The number of images in the board.")
     """The number of images in the board."""
+    actions: Optional[dict[str, bool]] = Field(
+        default=None,
+        description="Allowed actions on board."
+    )
+    """Allowed actions on board."""
 
 
-def board_record_to_dto(board_record: BoardRecord, cover_image_name: Optional[str], image_count: int) -> BoardDTO:
+def board_record_to_dto(
+        board_record: BoardRecord,
+        cover_image_name: Optional[str],
+        image_count: int,
+        actions: Optional[dict[str, bool]] = None
+    ) -> BoardDTO:
     """Converts a board record to a board DTO."""
     return BoardDTO(
         **board_record.model_dump(exclude={"cover_image_name"}),
         cover_image_name=cover_image_name,
         image_count=image_count,
+        actions=actions
     )
