296060db6389c6417b65e7a9a62cd91096c588ac
Wilson E. Alvarez <wilson.e.alvarez@rubonnek.com>
2023-12-07 11:17:11 -0500

    Add cpu and rocm profiles. Let invokeai-nvidia service be the default.

--- docker/run.sh
+++ docker/run.sh
@@ -6,11 +6,13 @@ run() {
   cd "$scriptdir" || exit 1
 
   local build_args=""
-  local service_name="invokeai-cpu"
+  local profile=""
 
   [[ -f ".env" ]] &&
-    build_args=$(awk '$1 ~ /=[^$]/ {print "--build-arg " $0 " "}' .env) &&
-    service_name="invokeai-$(awk -F '=' '/GPU_DRIVER/ {print $2}' .env)"
+    build_args=$(awk '$1 ~ /=[^$]/ && $0 !~ /^#/ {print "--build-arg " $0 " "}' .env) &&
+    profile="$(awk -F '=' '/GPU_DRIVER/ {print $2}' .env)"
+
+  local service_name="invokeai-$profile"
 
   printf "%s\n" "docker compose build args:"
   printf "%s\n" "$build_args"
@@ -19,7 +21,7 @@ run() {
   unset build_args
 
   printf "%s\n" "starting service $service_name"
-  docker compose up -d $service_name
+  docker compose --profile "$profile" up -d "$service_name"
   docker compose logs -f
 }
 
