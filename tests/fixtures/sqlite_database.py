386b65653016200a926ad64fc8c82cbf74317091
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-12-13 10:13:03 +1100

    feat(db): remove unnecessary fixture declaration
    Also revert the change to `conftest.py` in which the file was flagged for pytest to crawl for fixtures.

--- tests/fixtures/sqlite_database.py
+++ tests/fixtures/sqlite_database.py
@@ -1,8 +1,5 @@
 from logging import Logger
-from typing import Callable
 from unittest import mock
-
-import pytest
 
 from invokeai.app.services.config.config_default import InvokeAIAppConfig
 from invokeai.app.services.image_files.image_files_base import ImageFileStorageBase
@@ -11,23 +8,17 @@ from invokeai.app.services.shared.sqlite_migrator.migrations.migration_1 import 
 from invokeai.app.services.shared.sqlite_migrator.migrations.migration_2 import migration_2
 from invokeai.app.services.shared.sqlite_migrator.sqlite_migrator_impl import SQLiteMigrator
 
-CreateSqliteDatabaseFunction = Callable[[InvokeAIAppConfig, Logger], SqliteDatabase]
 
+def create_sqlite_database(config: InvokeAIAppConfig, logger: Logger) -> SqliteDatabase:
+    db_path = None if config.use_memory_db else config.db_path
+    db = SqliteDatabase(db_path=db_path, logger=logger, verbose=config.log_sql)
 
-@pytest.fixture
-def create_sqlite_database() -> CreateSqliteDatabaseFunction:
-    def _create_sqlite_database(config: InvokeAIAppConfig, logger: Logger) -> SqliteDatabase:
-        db_path = None if config.use_memory_db else config.db_path
-        db = SqliteDatabase(db_path=db_path, logger=logger, verbose=config.log_sql)
+    image_files = mock.Mock(spec=ImageFileStorageBase)
 
-        image_files = mock.Mock(spec=ImageFileStorageBase)
-
-        migrator = SQLiteMigrator(db=db)
-        migration_2.provide_dependency("logger", logger)
-        migration_2.provide_dependency("image_files", image_files)
-        migrator.register_migration(migration_1)
-        migrator.register_migration(migration_2)
-        migrator.run_migrations()
-        return db
-
-    return _create_sqlite_database
+    migrator = SQLiteMigrator(db=db)
+    migration_2.provide_dependency("logger", logger)
+    migration_2.provide_dependency("image_files", image_files)
+    migrator.register_migration(migration_1)
+    migrator.register_migration(migration_2)
+    migrator.run_migrations()
+    return db
