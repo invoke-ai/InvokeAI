821e0326c93bb9b6797ac2d3363f8d66ca104033
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-12-13 11:25:57 +1100

    fix(db): formatting

--- tests/fixtures/sqlite_database.py
+++ tests/fixtures/sqlite_database.py
@@ -7,10 +7,7 @@ from invokeai.app.services.shared.sqlite.sqlite_database import SqliteDatabase
 from invokeai.app.services.shared.sqlite.sqlite_util import init_db
 
 
-def create_mock_sqlite_database(
-    config: InvokeAIAppConfig,
-    logger: Logger,
-) -> SqliteDatabase:
+def create_mock_sqlite_database(config: InvokeAIAppConfig, logger: Logger) -> SqliteDatabase:
     image_files = mock.Mock(spec=ImageFileStorageBase)
     db = init_db(config=config, logger=logger, image_files=image_files)
     return db
