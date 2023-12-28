e99b715e9e41041a947247e09b36bcd7938d7e28
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-12-29 08:50:29 +1100

    fix(ui): board collapse button styling

--- invokeai/frontend/web/src/features/gallery/components/GalleryBoardName.tsx
+++ invokeai/frontend/web/src/features/gallery/components/GalleryBoardName.tsx
@@ -1,9 +1,8 @@
 import { ChevronUpIcon } from '@chakra-ui/icons';
-import { Button, Flex } from '@chakra-ui/react';
+import { Button, Flex, Spacer } from '@chakra-ui/react';
 import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
 import { stateSelector } from 'app/store/store';
 import { useAppSelector } from 'app/store/storeHooks';
-import { InvText } from 'common/components/InvText/wrapper';
 import { memo, useMemo } from 'react';
 import { useBoardName } from 'services/api/hooks/useBoardName';
 
@@ -35,32 +34,20 @@ const GalleryBoardName = (props: Props) => {
       as={Button}
       onClick={onToggle}
       size="sm"
-      sx={{
-        position: 'relative',
-        gap: 2,
-        w: 'full',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        px: 2,
-      }}
+      position="relative"
+      gap={2}
+      w="full"
+      justifyContent="center"
+      alignItems="center"
+      px={2}
     >
-      <InvText
-        noOfLines={1}
-        sx={{
-          fontWeight: 'semibold',
-          w: '100%',
-          textAlign: 'center',
-          color: 'base.200',
-        }}
-      >
-        {formattedBoardName}
-      </InvText>
+      <Spacer />
+      {formattedBoardName}
+      <Spacer />
       <ChevronUpIcon
-        sx={{
-          transform: isOpen ? 'rotate(0deg)' : 'rotate(180deg)',
-          transitionProperty: 'common',
-          transitionDuration: 'normal',
-        }}
+        transform={isOpen ? 'rotate(0deg)' : 'rotate(180deg)'}
+        transitionProperty="common"
+        transitionDuration="normal"
       />
     </Flex>
   );
