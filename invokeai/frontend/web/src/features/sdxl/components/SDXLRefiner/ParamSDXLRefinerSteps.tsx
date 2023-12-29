c1a443a4942fddcc51aec43cb36ed9486651349d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-12-29 17:22:03 +1100

    feat(ui): add refiner cfg scale & steps defaults & marks

--- invokeai/frontend/web/src/features/sdxl/components/SDXLRefiner/ParamSDXLRefinerSteps.tsx
+++ invokeai/frontend/web/src/features/sdxl/components/SDXLRefiner/ParamSDXLRefinerSteps.tsx
@@ -1,3 +1,5 @@
+import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
+import { stateSelector } from 'app/store/store';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
 import { InvControl } from 'common/components/InvControl/InvControl';
 import { InvSlider } from 'common/components/InvSlider/InvSlider';
@@ -5,26 +7,51 @@ import { setRefinerSteps } from 'features/sdxl/store/sdxlSlice';
 import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 
+const selector = createMemoizedSelector([stateSelector], ({ config }) => {
+  const { initial, min, sliderMax, inputMax, fineStep, coarseStep } =
+    config.sd.steps;
+
+  return {
+    marks: [min, Math.floor(sliderMax / 2), sliderMax],
+    initial,
+    min,
+    sliderMax,
+    inputMax,
+    step: coarseStep,
+    fineStep,
+  };
+});
 const ParamSDXLRefinerSteps = () => {
+  const { initial, min, sliderMax, inputMax, step, fineStep, marks } =
+    useAppSelector(selector);
   const refinerSteps = useAppSelector((state) => state.sdxl.refinerSteps);
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
-  const handleChange = useCallback(
+
+  const onChange = useCallback(
     (v: number) => {
       dispatch(setRefinerSteps(v));
     },
     [dispatch]
   );
 
+  const onReset = useCallback(() => {
+    dispatch(setRefinerSteps(initial));
+  }, [dispatch, initial]);
+
   return (
     <InvControl label={t('sdxl.steps')}>
       <InvSlider
-        min={1}
-        max={100}
-        step={1}
-        onChange={handleChange}
         value={refinerSteps}
+        min={min}
+        max={sliderMax}
+        step={step}
+        fineStep={fineStep}
+        onChange={onChange}
+        onReset={onReset}
         withNumberInput
+        marks={marks}
+        numberInputMax={inputMax}
       />
     </InvControl>
   );
