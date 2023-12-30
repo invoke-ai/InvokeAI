844276e13d8eb9e48a4cf5a2ea0a150b86397154
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-12-30 23:55:39 +1100

    feat(ui): misc perf/rerender improvements
    More efficient selectors, memoized/stable references to objects, lazy popover/menu rendering.

--- invokeai/frontend/web/src/features/canvas/components/IAICanvasStatusText/IAICanvasStatusTextCursorPos.tsx
+++ invokeai/frontend/web/src/features/canvas/components/IAICanvasStatusText/IAICanvasStatusTextCursorPos.tsx
@@ -1,30 +1,23 @@
 import { Box } from '@chakra-ui/react';
-import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
+import { createSelector } from '@reduxjs/toolkit';
 import { stateSelector } from 'app/store/store';
 import { useAppSelector } from 'app/store/storeHooks';
 import roundToHundreth from 'features/canvas/util/roundToHundreth';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
-const cursorPositionSelector = createMemoizedSelector(
-  [stateSelector],
-  ({ canvas }) => {
-    const { cursorPosition } = canvas;
+const cursorPositionSelector = createSelector([stateSelector], ({ canvas }) => {
+  const { cursorPosition } = canvas;
 
-    const { cursorX, cursorY } = cursorPosition
-      ? { cursorX: cursorPosition.x, cursorY: cursorPosition.y }
-      : { cursorX: -1, cursorY: -1 };
+  const { cursorX, cursorY } = cursorPosition
+    ? { cursorX: cursorPosition.x, cursorY: cursorPosition.y }
+    : { cursorX: -1, cursorY: -1 };
 
-    return {
-      cursorCoordinatesString: `(${roundToHundreth(cursorX)}, ${roundToHundreth(
-        cursorY
-      )})`,
-    };
-  }
-);
+  return `(${roundToHundreth(cursorX)}, ${roundToHundreth(cursorY)})`;
+});
 
 const IAICanvasStatusTextCursorPos = () => {
-  const { cursorCoordinatesString } = useAppSelector(cursorPositionSelector);
+  const cursorCoordinatesString = useAppSelector(cursorPositionSelector);
   const { t } = useTranslation();
 
   return (
