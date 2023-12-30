89b7082bc05ba89f6f1c13cb8b7531be4b1be8ec
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-12-30 19:16:12 +1100

    fix(ui): remove debug stmts

--- invokeai/frontend/web/src/features/dynamicPrompts/hooks/useDynamicPromptsModal.ts
+++ invokeai/frontend/web/src/features/dynamicPrompts/hooks/useDynamicPromptsModal.ts
@@ -7,11 +7,9 @@ const $isDynamicPromptsModalOpen = atom<boolean>(false);
 export const useDynamicPromptsModal = () => {
   const isOpen = useStore($isDynamicPromptsModalOpen);
   const onOpen = useCallback(() => {
-    console.log('onOpen');
     $isDynamicPromptsModalOpen.set(true);
   }, []);
   const onClose = useCallback(() => {
-    console.log('onClose');
     $isDynamicPromptsModalOpen.set(false);
   }, []);
 
