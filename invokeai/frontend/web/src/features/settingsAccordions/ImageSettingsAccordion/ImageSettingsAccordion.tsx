19fff8db25e0001b526d89f6154b6a23acbbe880
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-01 11:44:42 +1100

    fix(ui): show denoising strength on canvas

--- invokeai/frontend/web/src/features/settingsAccordions/ImageSettingsAccordion/ImageSettingsAccordion.tsx
+++ invokeai/frontend/web/src/features/settingsAccordions/ImageSettingsAccordion/ImageSettingsAccordion.tsx
@@ -81,7 +81,8 @@ export const ImageSettingsAccordion = memo(() => {
             </Flex>
             {activeTabName === 'txt2img' && <HrfSettings />}
             {activeTabName === 'img2img' && <ImageToImageFit />}
-            {activeTabName === 'img2img' && <ImageToImageStrength />}
+            {activeTabName === 'img2img' ||
+              (activeTabName === 'unifiedCanvas' && <ImageToImageStrength />)}
             {activeTabName === 'unifiedCanvas' && (
               <>
                 <ParamScaleBeforeProcessing />
