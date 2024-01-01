dfd9c448aaea5290dc732a053744696033de18fc
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-01 20:37:14 +1100

    feat(ui): use zod-less workflow builder when appropriate

--- invokeai/frontend/web/src/features/nodes/hooks/useWorkflowWatcher.ts
+++ invokeai/frontend/web/src/features/nodes/hooks/useWorkflowWatcher.ts
@@ -1,7 +1,7 @@
 import { useAppSelector } from 'app/store/storeHooks';
 import type { WorkflowV2 } from 'features/nodes/types/workflow';
 import type { BuildWorkflowArg } from 'features/nodes/util/workflow/buildWorkflow';
-import { buildWorkflow } from 'features/nodes/util/workflow/buildWorkflow';
+import { buildWorkflowFast } from 'features/nodes/util/workflow/buildWorkflow';
 import { debounce } from 'lodash-es';
 import { atom } from 'nanostores';
 import { useEffect } from 'react';
@@ -9,7 +9,7 @@ import { useEffect } from 'react';
 export const $builtWorkflow = atom<WorkflowV2 | null>(null);
 
 const debouncedBuildWorkflow = debounce((arg: BuildWorkflowArg) => {
-  $builtWorkflow.set(buildWorkflow(arg));
+  $builtWorkflow.set(buildWorkflowFast(arg));
 }, 300);
 
 export const useWorkflowWatcher = () => {
