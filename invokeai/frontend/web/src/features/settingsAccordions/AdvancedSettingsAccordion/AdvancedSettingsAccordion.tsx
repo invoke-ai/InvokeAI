930de519100101969ce2f48b2e005b9c190db276
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-03 09:23:10 +1100

    feat(ui): add badges for advanced settings

--- invokeai/frontend/web/src/features/settingsAccordions/AdvancedSettingsAccordion/AdvancedSettingsAccordion.tsx
+++ invokeai/frontend/web/src/features/settingsAccordions/AdvancedSettingsAccordion/AdvancedSettingsAccordion.tsx
@@ -1,4 +1,7 @@
 import { Flex } from '@chakra-ui/layout';
+import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
+import { stateSelector } from 'app/store/store';
+import { useAppSelector } from 'app/store/storeHooks';
 import { InvControlGroup } from 'common/components/InvControl/InvControlGroup';
 import type { InvLabelProps } from 'common/components/InvControl/types';
 import { InvSingleAccordion } from 'common/components/InvSingleAccordion/InvSingleAccordion';
@@ -19,11 +22,35 @@ const labelProps2: InvLabelProps = {
   flexGrow: 1,
 };
 
+const selectBadges = createMemoizedSelector(stateSelector, (state) => {
+  const badges: (string | number)[] = [];
+  if (state.generation.vae) {
+    let vaeBadge = state.generation.vae.model_name;
+    if (state.generation.vaePrecision === 'fp16') {
+      vaeBadge += ` ${state.generation.vaePrecision}`;
+    }
+    badges.push(vaeBadge);
+  } else if (state.generation.vaePrecision === 'fp16') {
+    badges.push(`VAE ${state.generation.vaePrecision}`);
+  }
+  if (state.generation.clipSkip) {
+    badges.push(`Skip ${state.generation.clipSkip}`);
+  }
+  if (state.generation.cfgRescaleMultiplier) {
+    badges.push(`Rescale ${state.generation.cfgRescaleMultiplier}`);
+  }
+  if (state.generation.seamlessXAxis || state.generation.seamlessYAxis) {
+    badges.push('seamless');
+  }
+  return badges;
+});
+
 export const AdvancedSettingsAccordion = memo(() => {
+  const badges = useAppSelector(selectBadges);
   const { t } = useTranslation();
 
   return (
-    <InvSingleAccordion label={t('accordions.advanced.title')}>
+    <InvSingleAccordion label={t('accordions.advanced.title')} badges={badges}>
       <Flex gap={4} alignItems="center" p={4} flexDir="column">
         <Flex gap={4} w="full">
           <ParamVAEModelSelect />
