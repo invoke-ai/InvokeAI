bca7ea1674430b8ee76c6d63d6f4d81090b5fd43
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-01-04 12:46:57 -0500

    option to override logo component

--- invokeai/frontend/web/src/app/store/nanostores/customAppInfo.ts
+++
@@ -1,3 +0,0 @@
-import { atom } from 'nanostores';
-
-export const $customAppInfo = atom<string | undefined>();
--- invokeai/frontend/web/src/features/system/components/InvokeAILogoComponent.tsx
+++ invokeai/frontend/web/src/features/system/components/InvokeAILogoComponent.tsx
@@ -1,27 +1,27 @@
 /* eslint-disable i18next/no-literal-string */
 import { Image } from '@chakra-ui/react';
 import { useStore } from '@nanostores/react';
-import { $customAppInfo } from 'app/store/nanostores/customAppInfo';
 import InvokeLogoYellow from 'assets/images/invoke-key-ylw-sm.svg';
 import { InvText } from 'common/components/InvText/wrapper';
 import { InvTooltip } from 'common/components/InvTooltip/InvTooltip';
 import { memo, useMemo, useRef } from 'react';
 import { useGetAppVersionQuery } from 'services/api/endpoints/appInfo';
+import { $logo } from '../../../app/store/nanostores/logo';
 
 const InvokeAILogoComponent = () => {
   const { data: appVersion } = useGetAppVersionQuery();
   const ref = useRef(null);
-  const customAppInfo = useStore($customAppInfo);
+  const logoOverride = useStore($logo);
   const tooltip = useMemo(() => {
-    if (customAppInfo) {
-      return <InvText fontWeight="semibold">{customAppInfo}</InvText>;
-    }
-
     if (appVersion) {
       return <InvText fontWeight="semibold">v{appVersion.version}</InvText>;
     }
     return null;
-  }, [appVersion, customAppInfo]);
+  }, [appVersion]);
+
+  if (logoOverride) {
+    return logoOverride;
+  }
 
   return (
     <InvTooltip placement="right" label={tooltip} p={1} px={2} gutter={16}>
