10eefccaf40fbd779137f1487aa6a98f39a49d44
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-08 20:42:21 +1100

    feat(ui): change diff util, update debuggerLoggerMiddleware
    The previous diff library would present deleted things as `undefined`. Unfortunately, a JSON.stringify cycle will strip those values out. The ROARR logger does this and so the diffs end up being a lot less useful, not showing removed keys.
    
    The new diff library uses a different format for the delta that serializes nicely.

--- invokeai/frontend/web/src/app/store/middleware/debugLoggerMiddleware.ts
+++ invokeai/frontend/web/src/app/store/middleware/debugLoggerMiddleware.ts
@@ -1,16 +1,16 @@
 import type { Middleware, MiddlewareAPI } from '@reduxjs/toolkit';
-import { diff } from 'deep-object-diff';
+import { diff } from 'jsondiffpatch';
 
 /**
  * Super simple logger middleware. Useful for debugging when the redux devtools are awkward.
  */
-export const debuggingLoggerMiddleware: Middleware =
+export const debugLoggerMiddleware: Middleware =
   (api: MiddlewareAPI) => (next) => (action) => {
     const originalState = api.getState();
-    console.debug('dispatching', action);
+    console.log('REDUX: dispatching', action);
     const result = next(action);
     const nextState = api.getState();
-    const stateDiff = diff(originalState, nextState);
-    console.debug('diff', stateDiff);
+    console.log('REDUX: next state', nextState);
+    console.log('REDUX: diff', diff(originalState, nextState));
     return result;
   };
