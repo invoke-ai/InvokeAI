f2e8b66be4ecd3f4b408e5affefd0a2a5b76a959
Lincoln Stein <lstein@gmail.com>
2024-01-08 17:15:35 -0500

    Fix "Cannot import name 'PagingArgumentParser' error when starting textual inversion
    - Closes #5395

--- invokeai/app/services/config/__init__.py
+++ invokeai/app/services/config/__init__.py
@@ -1,5 +1,7 @@
 """Init file for InvokeAI configure package."""
 
+from invokeai.app.services.config.config_common import PagingArgumentParser
+
 from .config_default import InvokeAIAppConfig, get_invokeai_config
 
-__all__ = ["InvokeAIAppConfig", "get_invokeai_config"]
+__all__ = ["InvokeAIAppConfig", "get_invokeai_config", "PagingArgumentParser"]
