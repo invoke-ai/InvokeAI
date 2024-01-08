4f1b803d48e3aac431a5c17df0b0f87f07e80785
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-08 13:30:15 +1100

    feat(ui): add $socket to window if in dev mode

--- invokeai/frontend/web/src/app/hooks/useSocketIO.ts
+++ invokeai/frontend/web/src/app/hooks/useSocketIO.ts
@@ -71,7 +71,7 @@ export const useSocketIO = () => {
     setEventListeners({ dispatch, socket });
     socket.connect();
 
-    if ($isDebugging.get()) {
+    if ($isDebugging.get() || import.meta.env.MODE === 'development') {
       window.$socketOptions = $socketOptions;
       console.log('Socket initialized', socket);
     }
@@ -79,7 +79,7 @@ export const useSocketIO = () => {
     $isSocketInitialized.set(true);
 
     return () => {
-      if ($isDebugging.get()) {
+      if ($isDebugging.get() || import.meta.env.MODE === 'development') {
         window.$socketOptions = undefined;
         console.log('Socket teardown', socket);
       }
