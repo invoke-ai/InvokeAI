795fbf0e81ec0529ca807cf39f2309bf1f7a51ac
Stefan Tobler <toblerlstefan@gmail.com>
2024-01-14 03:09:35 -0500

    refactoring bulkdownload to consider image category

--- invokeai/app/services/bulk_download/bulk_download_base.py
+++ invokeai/app/services/bulk_download/bulk_download_base.py
@@ -8,6 +8,17 @@ from invokeai.app.services.invoker import Invoker
 
 class BulkDownloadBase(ABC):
     @abstractmethod
+    def start(self, invoker: Invoker) -> None:
+        """
+        Starts the BulkDownloadService.
+
+        This method is responsible for starting the BulkDownloadService and performing any necessary initialization
+        operations to prepare the service for use.
+
+        param: invoker: The Invoker that holds all the services, required to be passed as a parameter to avoid circular dependencies.
+        """
+
+    @abstractmethod
     def __init__(
         self,
         output_folder: Union[str, Path],
@@ -21,7 +32,7 @@ class BulkDownloadBase(ABC):
         """
 
     @abstractmethod
-    def handler(self, invoker: Invoker, image_names: list[str], board_id: Optional[str]) -> None:
+    def handler(self, image_names: list[str], board_id: Optional[str]) -> None:
         """
         Starts a a bulk download job.
 
