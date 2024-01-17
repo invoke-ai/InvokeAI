8d07b7ad5d4f33d2414b8b4c4a2c8554e7591d9e
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-01-17 15:13:01 -0500

    lint

--- invokeai/frontend/web/src/common/components/InvExpander/types.ts
+++ invokeai/frontend/web/src/common/components/InvExpander/types.ts
@@ -3,6 +3,5 @@ import type { PropsWithChildren } from 'react';
 export type InvExpanderProps = PropsWithChildren<{
   label?: string;
   defaultIsOpen?: boolean;
-  onClick?: (isOpen: boolean) => void;
   id?: string;
 }>;
