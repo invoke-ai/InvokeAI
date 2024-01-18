1ab0e86085158b3ee1fb176fcedecb47c63fb1ef
Rohinish <92542124+rohinish404@users.noreply.github.com>
2024-01-18 19:00:56 +0530

    feat(ui): add about modal w/ app deps (#5462)
    * resolved conflicts
    
    * changed logo and some design changes
    
    * feedback changes
    
    * resolved conflicts
    
    * changed logo and some design changes
    
    * feedback changes
    
    * lint fixed
    
    * added translations
    
    * some requested changes done
    
    * all feedback changes done and replace links in settingsmenu comp
    
    * fixed the gap between deps verisons & chnaged heights
    
    * feat(ui): minor about modal styling
    
    * feat(ui): tag app endpoints with FetchOnReconnect
    
    * fix(ui): remove unused translation string
    
    ---------
    
    Co-authored-by: psychedelicious <4822129+psychedelicious@users.noreply.github.com>
    Co-authored-by: Kent Keirsey <31807370+hipsterusername@users.noreply.github.com>

--- invokeai/frontend/web/src/features/system/components/SettingsModal/SettingsMenu.tsx
+++ invokeai/frontend/web/src/features/system/components/SettingsModal/SettingsMenu.tsx
@@ -8,12 +8,15 @@ import {
   InvMenuGroup,
 } from 'common/components/InvMenu/wrapper';
 import { useGlobalMenuClose } from 'common/hooks/useGlobalMenuClose';
+import AboutModal from 'features/system/components/AboutModal/AboutModal';
 import HotkeysModal from 'features/system/components/HotkeysModal/HotkeysModal';
 import { useFeatureStatus } from 'features/system/hooks/useFeatureStatus';
+import { discordLink, githubLink } from 'features/system/store/constants';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
   PiBugBeetleBold,
+  PiInfoBold,
   PiKeyboardBold,
   PiToggleRightFill,
 } from 'react-icons/pi';
@@ -28,9 +31,6 @@ const SettingsMenu = () => {
   const isBugLinkEnabled = useFeatureStatus('bugLink').isFeatureEnabled;
   const isDiscordLinkEnabled = useFeatureStatus('discordLink').isFeatureEnabled;
   const isGithubLinkEnabled = useFeatureStatus('githubLink').isFeatureEnabled;
-
-  const githubLink = 'http://github.com/invoke-ai/InvokeAI';
-  const discordLink = 'https://discord.gg/ZmtBAhwWhy';
 
   return (
     <InvMenu isOpen={isOpen} onOpen={onOpen} onClose={onClose}>
@@ -86,6 +86,13 @@ const SettingsMenu = () => {
               {t('common.settingsLabel')}
             </InvMenuItem>
           </SettingsModal>
+        </InvMenuGroup>
+        <InvMenuGroup title={t('accessibility.about')}>
+          <AboutModal>
+            <InvMenuItem as="button" icon={<PiInfoBold />}>
+              {t('accessibility.about')}
+            </InvMenuItem>
+          </AboutModal>
         </InvMenuGroup>
       </InvMenuList>
     </InvMenu>
