a1307b9f2e591dd01b04f9dbf248d6b94b1cb04b
Lincoln Stein <lstein@gmail.com>
2024-01-22 14:37:23 -0500

    add concept of repo variant

--- tests/test_model_probe.py
+++ tests/test_model_probe.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 
 from invokeai.backend import BaseModelType
-from invokeai.backend.model_management.model_probe import VaeFolderProbe
+from invokeai.backend.model_manager.probe import VaeFolderProbe
 
 
 @pytest.mark.parametrize(
@@ -20,3 +20,10 @@ def test_get_base_type(vae_path: str, expected_type: BaseModelType, datadir: Pat
     probe = VaeFolderProbe(sd1_vae_path)
     base_type = probe.get_base_type()
     assert base_type == expected_type
+    repo_variant = probe.get_repo_variant()
+    assert repo_variant == 'default'
+
+def test_repo_variant(datadir: Path):
+    probe = VaeFolderProbe(datadir / "vae" / "taesdxl-fp16")
+    repo_variant = probe.get_repo_variant()
+    assert repo_variant == 'fp16'
