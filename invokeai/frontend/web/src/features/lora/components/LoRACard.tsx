e055ee077cb7c246efda41446faa3e72f87bcfea
Josh Corbett <joshwcorbett@icloud.com>
2024-01-28 16:35:09 -0700

    style: :rotating_light: linting

--- invokeai/frontend/web/src/features/lora/components/LoRACard.tsx
+++ invokeai/frontend/web/src/features/lora/components/LoRACard.tsx
@@ -29,7 +29,7 @@ export const LoRACard = memo((props: LoRACardProps) => {
   const handleChange = useCallback(
     (v: number) => {
       if (v !== 0) {
-        setPrevWeight(v)
+        setPrevWeight(v);
       }
       v === 0 ? setIsEnabled(false) : setIsEnabled(true);
       dispatch(loraWeightChanged({ id: lora.id, weight: v }));
@@ -37,14 +37,17 @@ export const LoRACard = memo((props: LoRACardProps) => {
     [dispatch, lora.id]
   );
 
-  const handleSetDisabled = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.checked) {
-      handleChange(prevWeight);
-    } else {
-      setPrevWeight(lora.weight);
-      handleChange(0);
-    }
-  }, [handleChange, lora.weight, prevWeight])
+  const handleSetDisabled = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.checked) {
+        handleChange(prevWeight);
+      } else {
+        setPrevWeight(lora.weight);
+        handleChange(0);
+      }
+    },
+    [handleChange, lora.weight, prevWeight]
+  );
 
   const handleRemoveLora = useCallback(() => {
     dispatch(loraRemoved(lora.id));
@@ -53,12 +56,12 @@ export const LoRACard = memo((props: LoRACardProps) => {
   return (
     <Card variant="lora">
       <CardHeader>
-        <Flex alignItems='center' justifyContent='space-between' width='100%' gap={2}>
+        <Flex alignItems="center" justifyContent="space-between" width="100%" gap={2}>
           <Text noOfLines={1} wordBreak="break-all" color={isEnabled ? 'base.200' : 'base.500'}>
             {lora.model_name}
           </Text>
-          <Flex alignItems='center' gap={2}>
-            <Switch size='sm' onChange={handleSetDisabled} isChecked={isEnabled} />
+          <Flex alignItems="center" gap={2}>
+            <Switch size="sm" onChange={handleSetDisabled} isChecked={isEnabled} />
             <IconButton
               aria-label="Remove LoRA"
               variant="ghost"
