7ad95639f4f9a52f174fa9d4210af9ae2bd0fd44
Josh Corbett <joshwcorbett@icloud.com>
2024-01-28 16:25:55 -0700

    feat: :sparkles: disable/enable LorRas with a switch

--- invokeai/frontend/web/src/features/lora/components/LoRACard.tsx
+++ invokeai/frontend/web/src/features/lora/components/LoRACard.tsx
@@ -4,13 +4,15 @@ import {
   CardHeader,
   CompositeNumberInput,
   CompositeSlider,
+  Flex,
   IconButton,
+  Switch,
   Text,
 } from '@invoke-ai/ui-library';
 import { useAppDispatch } from 'app/store/storeHooks';
 import type { LoRA } from 'features/lora/store/loraSlice';
 import { loraRemoved, loraWeightChanged } from 'features/lora/store/loraSlice';
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useState } from 'react';
 import { PiTrashSimpleBold } from 'react-icons/pi';
 
 type LoRACardProps = {
@@ -21,12 +23,28 @@ export const LoRACard = memo((props: LoRACardProps) => {
   const dispatch = useAppDispatch();
   const { lora } = props;
 
+  const [prevWeight, setPrevWeight] = useState(lora.weight);
+  const [isEnabled, setIsEnabled] = useState(lora.weight !== 0);
+
   const handleChange = useCallback(
     (v: number) => {
+      if (v !== 0) {
+        setPrevWeight(v)
+      }
+      v === 0 ? setIsEnabled(false) : setIsEnabled(true);
       dispatch(loraWeightChanged({ id: lora.id, weight: v }));
     },
     [dispatch, lora.id]
   );
+
+  const handleSetDisabled = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      handleChange(prevWeight);
+    } else {
+      setPrevWeight(lora.weight);
+      handleChange(0);
+    }
+  }, [handleChange, lora.weight, prevWeight])
 
   const handleRemoveLora = useCallback(() => {
     dispatch(loraRemoved(lora.id));
@@ -35,16 +53,21 @@ export const LoRACard = memo((props: LoRACardProps) => {
   return (
     <Card variant="lora">
       <CardHeader>
-        <Text noOfLines={1} wordBreak="break-all" color="base.200">
-          {lora.model_name}
-        </Text>
-        <IconButton
-          aria-label="Remove LoRA"
-          variant="ghost"
-          size="sm"
-          onClick={handleRemoveLora}
-          icon={<PiTrashSimpleBold />}
-        />
+        <Flex alignItems='center' justifyContent='space-between' width='100%' gap={2}>
+          <Text noOfLines={1} wordBreak="break-all" color={isEnabled ? 'base.200' : 'base.500'}>
+            {lora.model_name}
+          </Text>
+          <Flex alignItems='center' gap={2}>
+            <Switch size='sm' onChange={handleSetDisabled} isChecked={isEnabled} />
+            <IconButton
+              aria-label="Remove LoRA"
+              variant="ghost"
+              size="sm"
+              onClick={handleRemoveLora}
+              icon={<PiTrashSimpleBold />}
+            />
+          </Flex>
+        </Flex>
       </CardHeader>
       <CardBody>
         <CompositeSlider
