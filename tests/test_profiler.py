3fb2e48ac89f6161cbd1eb062a7520a733d845d0
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-31 21:38:50 +1100

    tests(profiler): update tests

--- tests/test_profiler.py
+++ tests/test_profiler.py
@@ -8,38 +8,45 @@ import pytest
 from invokeai.app.util.profiler import Profiler
 
 
-def test_profiler_new():
+def test_profiler_starts():
     with TemporaryDirectory() as tempdir:
         profiler = Profiler(logger=Logger("test_profiler"), output_dir=Path(tempdir))
-        assert not profiler.profiler
+        assert not profiler._profiler
         assert not profiler.profile_id
-        profiler.new("test")
-        assert profiler.profiler
+        profiler.start("test")
+        assert profiler._profiler
         assert profiler.profile_id == "test"
-        profiler.new("test2")
-        assert profiler.profiler
+        profiler.stop()
+        assert not profiler._profiler
+        assert not profiler.profile_id
+        profiler.start("test2")
+        assert profiler._profiler
         assert profiler.profile_id == "test2"
+        profiler.stop()
 
 
 def test_profiler_profiles():
     with TemporaryDirectory() as tempdir:
         profiler = Profiler(logger=Logger("test_profiler"), output_dir=Path(tempdir))
-        profiler.new("test")
-        profiler.enable()
+        profiler.start("test")
         for _ in range(1000000):
             pass
-        profiler.disable()
-        profiler.dump()
+        profiler.stop()
         assert (Path(tempdir) / "test.prof").exists()
+
+def test_profiler_profiles_with_prefix():
+    with TemporaryDirectory() as tempdir:
+        profiler = Profiler(logger=Logger("test_profiler"), output_dir=Path(tempdir), prefix="prefix")
+        profiler.start("test")
+        for _ in range(1000000):
+            pass
+        profiler.stop()
+        assert (Path(tempdir) / "prefix_test.prof").exists()
 
 
 def test_profile_fails_if_not_set_up():
     with TemporaryDirectory() as tempdir:
         profiler = Profiler(logger=Logger("test_profiler"), output_dir=Path(tempdir))
-        match = re.escape("Profiler not initialized. Call Profiler.new() first.")
+        match = re.escape("Profiler not initialized. Call start() first.")
         with pytest.raises(RuntimeError, match=match):
-            profiler.enable()
-        with pytest.raises(RuntimeError, match=match):
-            profiler.disable()
-        with pytest.raises(RuntimeError, match=match):
-            profiler.dump()
+            profiler.stop()
