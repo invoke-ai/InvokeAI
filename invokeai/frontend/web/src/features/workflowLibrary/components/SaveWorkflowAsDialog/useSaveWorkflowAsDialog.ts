371cf5e0b99b66b82d43d143032702d30f8440c4
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-01-31 15:34:21 +1100

    fix(ui): when saving a never-before-saved workflow, do not append " (copy)" to the name

--- invokeai/frontend/web/src/features/workflowLibrary/components/SaveWorkflowAsDialog/useSaveWorkflowAsDialog.ts
+++ invokeai/frontend/web/src/features/workflowLibrary/components/SaveWorkflowAsDialog/useSaveWorkflowAsDialog.ts
@@ -1,5 +1,7 @@
 import { useStore } from '@nanostores/react';
+import { createSelector } from '@reduxjs/toolkit';
 import { useAppSelector } from 'app/store/storeHooks';
+import { selectWorkflowSlice } from 'features/nodes/store/workflowSlice';
 import { getWorkflowCopyName } from 'features/workflowLibrary/util/getWorkflowCopyName';
 import { atom } from 'nanostores';
 import { useCallback } from 'react';
@@ -8,14 +10,30 @@ const $isOpen = atom(false);
 const $workflowName = atom('');
 const $shouldSaveToProject = atom(false);
 
+const selectNewWorkflowName = createSelector(selectWorkflowSlice, ({ name, id }): string => {
+  // If the workflow has no ID, it's a new workflow that has never been saved to the server. The dialog should use
+  // whatever the user has entered in the workflow name field.
+  if (!id) {
+    return name;
+  }
+  // Else, the workflow is already saved to the server. The dialog should use the workflow's name with " (copy)"
+  // appended to it.
+  if (name.length) {
+    return getWorkflowCopyName(name);
+  }
+  // Else, we have a workflow that has been saved to the server, but has no name. This should never happen, but if
+  // it does, we just return an empty string and let the dialog use the default name.
+  return '';
+});
+
 export const useSaveWorkflowAsDialog = () => {
-  const currentWorkflowName = useAppSelector((s) => s.workflow.name);
+  const newWorkflowName = useAppSelector(selectNewWorkflowName);
 
   const isOpen = useStore($isOpen);
   const onOpen = useCallback(() => {
-    $workflowName.set(currentWorkflowName.length ? getWorkflowCopyName(currentWorkflowName) : '');
+    $workflowName.set(newWorkflowName);
     $isOpen.set(true);
-  }, [currentWorkflowName]);
+  }, [newWorkflowName]);
   const onClose = useCallback(() => {
     $isOpen.set(false);
     $workflowName.set('');
