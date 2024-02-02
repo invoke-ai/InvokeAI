dd3b955b8aa69211c02755074da146c708c76571
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-02 17:44:30 +1100

    feat(ui): flesh out ViewerInfo

--- invokeai/frontend/web/src/features/viewer/components/ViewerInfo.tsx
+++ invokeai/frontend/web/src/features/viewer/components/ViewerInfo.tsx
@@ -1,7 +1,29 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { skipToken } from '@reduxjs/toolkit/query';
+import { useAppSelector } from 'app/store/storeHooks';
+import { IAINoContentFallback } from 'common/components/IAIImageFallback';
+import ImageMetadataViewer from 'features/gallery/components/ImageMetadataViewer/ImageMetadataViewer';
+import { selectLastSelectedImage } from 'features/gallery/store/gallerySelectors';
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { PiImageBold } from 'react-icons/pi';
+import { useGetImageDTOQuery } from 'services/api/endpoints/images';
+
+const selectLastSelectedImageName = createSelector(
+  selectLastSelectedImage,
+  (lastSelectedImage) => lastSelectedImage?.image_name
+);
 
 export const ViewerInfo = memo(() => {
-  return null;
+  const { t } = useTranslation();
+  const imageName = useAppSelector(selectLastSelectedImageName);
+  const { currentData: imageDTO } = useGetImageDTOQuery(imageName ?? skipToken);
+
+  if (!imageDTO) {
+    return <IAINoContentFallback icon={PiImageBold} label={t('gallery.noImageSelected')} />;
+  }
+
+  return <ImageMetadataViewer image={imageDTO} />;
 });
 
 ViewerInfo.displayName = 'ViewerInfo';
