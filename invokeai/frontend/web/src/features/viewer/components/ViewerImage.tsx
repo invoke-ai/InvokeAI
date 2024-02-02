fba7f360382642b239377acc2c39ae4e4ededc19
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-02 17:41:01 +1100

    feat(ui): flesh our ViewerImage
    It displays the currently selected image.

--- invokeai/frontend/web/src/features/viewer/components/ViewerImage.tsx
+++ invokeai/frontend/web/src/features/viewer/components/ViewerImage.tsx
@@ -1,7 +1,48 @@
-import { memo } from 'react';
+import { createSelector } from '@reduxjs/toolkit';
+import { skipToken } from '@reduxjs/toolkit/query';
+import { useAppSelector } from 'app/store/storeHooks';
+import IAIDndImage from 'common/components/IAIDndImage';
+import { IAINoContentFallback } from 'common/components/IAIImageFallback';
+import type { TypesafeDraggableData } from 'features/dnd/types';
+import { selectLastSelectedImage } from 'features/gallery/store/gallerySelectors';
+import { memo, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { PiImageBold } from 'react-icons/pi';
+import { useGetImageDTOQuery } from 'services/api/endpoints/images';
+
+const selectLastSelectedImageName = createSelector(
+  selectLastSelectedImage,
+  (lastSelectedImage) => lastSelectedImage?.image_name
+);
 
 export const ViewerImage = memo(() => {
-  return null;
+  const imageName = useAppSelector(selectLastSelectedImageName);
+
+  const { currentData: imageDTO } = useGetImageDTOQuery(imageName ?? skipToken);
+
+  const draggableData = useMemo<TypesafeDraggableData | undefined>(() => {
+    if (imageDTO) {
+      return {
+        id: 'current-image',
+        payloadType: 'IMAGE_DTO',
+        payload: { imageDTO },
+      };
+    }
+  }, [imageDTO]);
+
+  const { t } = useTranslation();
+
+  return (
+    <IAIDndImage
+      imageDTO={imageDTO}
+      draggableData={draggableData}
+      isUploadDisabled={true}
+      fitContainer
+      useThumbailFallback
+      noContentFallback={<IAINoContentFallback icon={PiImageBold} label={t('gallery.noImageSelected')} />}
+      dataTestId="image-preview"
+    />
+  );
 });
 
 ViewerImage.displayName = 'ViewerImage';
