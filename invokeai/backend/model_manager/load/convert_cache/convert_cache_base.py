60aa3d4893d42bdd80c33aea40f21eb94a3caef9
Lincoln Stein <lstein@gmail.com>
2024-02-03 22:55:09 -0500

    model loading and conversion implemented for vaes

---
+++ invokeai/backend/model_manager/load/convert_cache/convert_cache_base.py
@@ -0,0 +1,28 @@
+"""
+Disk-based converted model cache.
+"""
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+class ModelConvertCacheBase(ABC):
+
+    @property
+    @abstractmethod
+    def max_size(self) -> float:
+        """Return the maximum size of this cache directory."""
+        pass
+
+    @abstractmethod
+    def make_room(self, size: float) -> None:
+        """
+        Make sufficient room in the cache directory for a model of max_size.
+
+        :param size: Size required (GB)
+        """
+        pass
+
+    @abstractmethod
+    def cache_path(self, key: str) -> Path:
+        """Return the path for a model with the indicated key."""
+        pass
+
