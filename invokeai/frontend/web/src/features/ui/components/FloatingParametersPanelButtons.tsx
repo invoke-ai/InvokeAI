5fe40d228edaf3bb6f1fdbb68ad525ec9d5eade3
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-06 20:51:31 +1100

    fix(ui): floating panel buttons have spinner when not processing
    Fixes the symptoms of #5666 but doesn't address the underlying issue.

--- invokeai/frontend/web/src/features/ui/components/FloatingParametersPanelButtons.tsx
+++ invokeai/frontend/web/src/features/ui/components/FloatingParametersPanelButtons.tsx
@@ -4,13 +4,13 @@ import CancelCurrentQueueItemIconButton from 'features/queue/components/CancelCu
 import ClearQueueConfirmationAlertDialog from 'features/queue/components/ClearQueueConfirmationAlertDialog';
 import { ClearAllQueueIconButton } from 'features/queue/components/ClearQueueIconButton';
 import { QueueButtonTooltip } from 'features/queue/components/QueueButtonTooltip';
+import { useIsProcessing } from 'features/queue/hooks/useIsProcessing';
 import { useQueueBack } from 'features/queue/hooks/useQueueBack';
 import type { UsePanelReturn } from 'features/ui/hooks/usePanel';
 import { memo, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiCircleNotchBold, PiSlidersHorizontalBold } from 'react-icons/pi';
 import { RiSparklingFill } from 'react-icons/ri';
-import { useGetQueueStatusQuery } from 'services/api/endpoints/queue';
 
 const floatingButtonStyles: SystemStyleObject = {
   borderStartRadius: 0,
@@ -24,16 +24,16 @@ type Props = {
 const FloatingSidePanelButtons = (props: Props) => {
   const { t } = useTranslation();
   const { queueBack, isLoading, isDisabled } = useQueueBack();
-  const { data: queueStatus } = useGetQueueStatusQuery();
+  const isProcessing = useIsProcessing();
 
   const queueButtonIcon = useMemo(
     () =>
-      !isDisabled && queueStatus?.processor.is_processing ? (
+      !isDisabled && isProcessing ? (
         <Icon boxSize={6} as={PiCircleNotchBold} animation={spinAnimation} />
       ) : (
         <RiSparklingFill size="16px" />
       ),
-    [isDisabled, queueStatus?.processor.is_processing]
+    [isDisabled, isProcessing]
   );
 
   const disclosure = useDisclosure();
