54a67459bf1b6d5ea1a21d2f8cdb8533ed05a7c6
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-07 19:51:04 +1100

    feat(nodes): support custom save and load functions in `ItemStorageEphemeralDisk`

--- invokeai/app/services/item_storage/item_storage_common.py
+++ invokeai/app/services/item_storage/item_storage_common.py
@@ -1,5 +1,15 @@
+from pathlib import Path
+from typing import Callable, TypeAlias, TypeVar
+
+
 class ItemNotFoundError(KeyError):
     """Raised when an item is not found in storage"""
 
     def __init__(self, item_id: str) -> None:
         super().__init__(f"Item with id {item_id} not found")
+
+
+T = TypeVar("T")
+
+SaveFunc: TypeAlias = Callable[[T, Path], None]
+LoadFunc: TypeAlias = Callable[[Path], T]
