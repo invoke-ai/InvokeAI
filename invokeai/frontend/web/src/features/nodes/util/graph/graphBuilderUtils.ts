bb8c71f7064c376e0f6e71f0f590b2f0dd5b0947
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-07 16:41:24 +1100

    feat(ui): revise graphs to not use `LinearUIOutputInvocation`
    See this comment for context: https://github.com/invoke-ai/InvokeAI/pull/5491#discussion_r1480760629
    
    - Remove this now-unnecessary node from all graphs
    - Update graphs' terminal image-outputting nodes' `is_intermediate` and `board` fields appropriately
    - Add util function to prepare the `board` field, tidy the utils
    - Update `socketInvocationComplete` listener to work correctly with this change
    
    I've manually tested all graph permutations that were changed (I think this is all...) to ensure images go to the gallery as expected:
    - ad-hoc upscaling
    - t2i w/ sd1.5
    - t2i w/ sd1.5 & hrf
    - t2i w/ sdxl
    - t2i w/ sdxl + refiner
    - i2i w/ sd1.5
    - i2i w/ sdxl
    - i2i w/ sdxl + refiner
    - canvas t2i w/ sd1.5
    - canvas t2i w/ sdxl
    - canvas t2i w/ sdxl + refiner
    - canvas i2i w/ sd1.5
    - canvas i2i w/ sdxl
    - canvas i2i w/ sdxl + refiner
    - canvas inpaint w/ sd1.5
    - canvas inpaint w/ sdxl
    - canvas inpaint w/ sdxl + refiner
    - canvas outpaint w/ sd1.5
    - canvas outpaint w/ sdxl
    - canvas outpaint w/ sdxl + refiner

---
+++ invokeai/frontend/web/src/features/nodes/util/graph/graphBuilderUtils.ts
@@ -0,0 +1,38 @@
+import type { RootState } from 'app/store/store';
+import type { BoardField } from 'features/nodes/types/common';
+import { activeTabNameSelector } from 'features/ui/store/uiSelectors';
+
+/**
+ * Gets the board field, based on the autoAddBoardId setting.
+ */
+export const getBoardField = (state: RootState): BoardField | undefined => {
+  const { autoAddBoardId } = state.gallery;
+  if (autoAddBoardId === 'none') {
+    return undefined;
+  }
+  return { board_id: autoAddBoardId };
+};
+
+/**
+ * Gets the SDXL style prompts, based on the concat setting.
+ */
+export const getSDXLStylePrompts = (state: RootState): { positiveStylePrompt: string; negativeStylePrompt: string } => {
+  const { positivePrompt, negativePrompt } = state.generation;
+  const { positiveStylePrompt, negativeStylePrompt, shouldConcatSDXLStylePrompt } = state.sdxl;
+
+  return {
+    positiveStylePrompt: shouldConcatSDXLStylePrompt ? positivePrompt : positiveStylePrompt,
+    negativeStylePrompt: shouldConcatSDXLStylePrompt ? negativePrompt : negativeStylePrompt,
+  };
+};
+
+/**
+ * Gets the is_intermediate field, based on the active tab and shouldAutoSave setting.
+ */
+export const getIsIntermediate = (state: RootState) => {
+  const activeTabName = activeTabNameSelector(state);
+  if (activeTabName === 'unifiedCanvas') {
+    return !state.canvas.shouldAutoSave;
+  }
+  return false;
+};
