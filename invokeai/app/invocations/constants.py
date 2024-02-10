acc50d9bd27b1d5a24907a3e853a31e24f69a396
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-11 09:51:25 +1100

    chore(nodes): "SAMPLER_NAME_VALUES" -> "SCHEDULER_NAME_VALUES"
    This was named inaccurately.

--- invokeai/app/invocations/constants.py
+++ invokeai/app/invocations/constants.py
@@ -1,3 +1,7 @@
+from typing import Literal
+
+from invokeai.backend.stable_diffusion.schedulers import SCHEDULER_MAP
+
 LATENT_SCALE_FACTOR = 8
 """
 HACK: Many nodes are currently hard-coded to use a fixed latent scale factor of 8. This is fragile, and will need to
@@ -5,3 +9,6 @@ be addressed if future models use a different latent scale factor. Also, note th
 factor is hard-coded to a literal '8' rather than using this constant.
 The ratio of image:latent dimensions is LATENT_SCALE_FACTOR:1, or 8:1.
 """
+
+SCHEDULER_NAME_VALUES = Literal[tuple(SCHEDULER_MAP.keys())]
+"""A literal type representing the valid scheduler names."""
