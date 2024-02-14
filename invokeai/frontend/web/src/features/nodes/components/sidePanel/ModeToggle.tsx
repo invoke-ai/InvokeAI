d51b69660138a7b5f3a087613d9fd4b69bfc24b0
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-14 18:17:02 +1100

    feat(ui): use IconButton for workflow view/edit button

--- invokeai/frontend/web/src/features/nodes/components/sidePanel/ModeToggle.tsx
+++ invokeai/frontend/web/src/features/nodes/components/sidePanel/ModeToggle.tsx
@@ -1,75 +1,43 @@
-import { Button, Flex, Menu, MenuButton, MenuItem, MenuList, Text } from '@invoke-ai/ui-library';
+import { Flex, IconButton } from '@invoke-ai/ui-library';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
-import type { WorkflowMode } from 'features/nodes/store/types';
 import { workflowModeChanged } from 'features/nodes/store/workflowSlice';
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import { MdBolt } from 'react-icons/md';
-import { PiCaretDownBold, PiPencil } from 'react-icons/pi';
+import { PiEyeBold, PiPencilBold } from 'react-icons/pi';
 
 export const ModeToggle = () => {
   const dispatch = useAppDispatch();
   const mode = useAppSelector((s) => s.workflow.mode);
   const { t } = useTranslation();
 
-  const modeOptions = useMemo(() => {
-    return [
-      {
-        value: 'view',
-        label: t('nodes.viewMode'),
-        icon: <MdBolt />,
-        description: 'Use the configured linear Workflow in the studio',
-      },
-      {
-        value: 'edit',
-        label: t('nodes.editMode'),
-        icon: <PiPencil />,
-        description: 'Update and edit the workflow in the Workflow Editor',
-      },
-    ];
-  }, [t]);
+  const onClickEdit = useCallback(() => {
+    dispatch(workflowModeChanged('edit'));
+  }, [dispatch]);
 
-  const handleClickMode = useCallback(
-    (selectedMode: WorkflowMode) => {
-      dispatch(workflowModeChanged(selectedMode));
-    },
-    [dispatch]
-  );
-
-  const value = useMemo(() => modeOptions.find((o) => o.value === mode), [mode, modeOptions]);
+  const onClickView = useCallback(() => {
+    dispatch(workflowModeChanged('view'));
+  }, [dispatch]);
 
   return (
     <Flex justifyContent="flex-end">
-      <Menu>
-        <MenuButton
-          size="md"
-          as={Button}
+      {mode === 'view' && (
+        <IconButton
+          aria-label={t('nodes.editMode')}
+          tooltip={t('nodes.editMode')}
+          onClick={onClickEdit}
+          icon={<PiPencilBold />}
           colorScheme="invokeBlue"
-          rightIcon={<PiCaretDownBold size="10" />}
-          textAlign="left"
-        >
-          <Flex gap="1">
-            {value?.icon}
-            <Text fontSize="sm">{value?.label}</Text>
-          </Flex>
-        </MenuButton>
-        <MenuList maxW="200px">
-          {modeOptions.map((option) => (
-            <MenuItem
-              key={option.value}
-              fontSize="sm"
-              icon={option.icon}
-              onClick={handleClickMode.bind(null, option.value as WorkflowMode)}
-              alignItems="flex-start"
-            >
-              <Flex flexDir="column">
-                <Text mt="-2px">{option.label}</Text>
-                <Text fontSize="xs">{option.description}</Text>
-              </Flex>
-            </MenuItem>
-          ))}
-        </MenuList>
-      </Menu>
+          />
+          )}
+      {mode === 'edit' && (
+        <IconButton
+        aria-label={t('nodes.viewMode')}
+        tooltip={t('nodes.viewMode')}
+        onClick={onClickView}
+        icon={<PiEyeBold />}
+        colorScheme="invokeBlue"
+        />
+      )}
     </Flex>
   );
 };
