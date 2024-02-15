cc45007dc4ff55575b622d0a1992bf151dfb0744
Ryan Dick <ryanjdick3@gmail.com>
2024-02-15 17:28:55 -0500

    Remove unused code for attention map saving.

--- invokeai/backend/stable_diffusion/__init__.py
+++ invokeai/backend/stable_diffusion/__init__.py
@@ -4,13 +4,11 @@ Initialization file for the invokeai.backend.stable_diffusion package
 
 from .diffusers_pipeline import PipelineIntermediateState, StableDiffusionGeneratorPipeline  # noqa: F401
 from .diffusion import InvokeAIDiffuserComponent  # noqa: F401
-from .diffusion.cross_attention_map_saving import AttentionMapSaver  # noqa: F401
 from .seamless import set_seamless  # noqa: F401
 
 __all__ = [
     "PipelineIntermediateState",
     "StableDiffusionGeneratorPipeline",
     "InvokeAIDiffuserComponent",
-    "AttentionMapSaver",
     "set_seamless",
 ]
