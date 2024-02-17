276a95ae8e910201c1f64a630725380d5489cf12
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-18 01:41:04 +1100

    refactor(nodes): merge processors
    Consolidate graph processing logic into session processor.
    
    With graphs as the unit of work, and the session queue distributing graphs, we no longer need the invocation queue or processor.
    
    Instead, the session processor dequeues the next session and processes it in a simple loop, greatly simplifying the app.
    
    - Remove `graph_execution_manager` service.
    - Remove `queue` (invocation queue) service.
    - Remove `processor` (invocation processor) service.
    - Remove queue-related logic from `Invoker`. It now only starts and stops the services, providing them with access to other services.
    - Remove unused `invocation_retrieval_error` and `session_retrieval_error` events, these are no longer needed.
    - Clean up stats service now that it is less coupled to the rest of the app.
    - Refactor cancellation logic - cancellations now originate from session queue (i.e. HTTP cancel endpoint) and are emitted as events. Processor gets the events and sets the canceled event. Access to this event is provided to the invocation context for e.g. the step callback.
    - Remove `sessions` router; it provided access to `graph_executions` but that no longer exists.

--- invokeai/app/services/session_processor/session_processor_common.py
+++ invokeai/app/services/session_processor/session_processor_common.py
@@ -4,3 +4,17 @@ from pydantic import BaseModel, Field
 class SessionProcessorStatus(BaseModel):
     is_started: bool = Field(description="Whether the session processor is started")
     is_processing: bool = Field(description="Whether a session is being processed")
+
+
+class CanceledException(Exception):
+    """Execution canceled by user."""
+
+    pass
+
+
+class ProgressImage(BaseModel):
+    """The progress image sent intermittently during processing"""
+
+    width: int = Field(description="The effective width of the image in pixels")
+    height: int = Field(description="The effective height of the image in pixels")
+    dataURL: str = Field(description="The image data as a b64 data URL")
