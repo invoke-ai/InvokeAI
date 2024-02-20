9068400433240dc25bee43f951da659f368e64c4
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-02-20 10:09:40 -0500

    workspace for mary and jenn

---
+++ invokeai/frontend/web/src/features/modelManagerV2/subpanels/ImportModels.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@invoke-ai/ui-library';
+
+//jenn's workspace
+export const ImportModels = () => {
+  return (
+    <Box layerStyle="first" p={2} borderRadius="base" w="full" h="full">
+      Import Models
+    </Box>
+  );
+};
