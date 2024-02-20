c99eaee6f36a256cd5f2a55453b691d4e0cadcc6
Jennifer Player <chainchompa@gmail.com>
2024-02-20 13:39:27 -0500

    added import model form and importqueue

--- invokeai/frontend/web/src/features/modelManagerV2/subpanels/ImportModels.tsx
+++ invokeai/frontend/web/src/features/modelManagerV2/subpanels/ImportModels.tsx
@@ -1,10 +1,85 @@
-import { Box } from '@invoke-ai/ui-library';
+import { Button, Box, Flex, FormControl, FormLabel, Heading, Input, Text, Divider } from '@invoke-ai/ui-library';
+import { t } from 'i18next';
+import { CSSProperties } from 'react';
+import { useImportMainModelsMutation } from '../../../services/api/endpoints/models';
+import { useForm } from '@mantine/form';
+import { useAppDispatch } from 'app/store/storeHooks';
+import { addToast } from 'features/system/store/systemSlice';
+import { makeToast } from 'features/system/util/makeToast';
+import { ImportQueue } from './ImportQueue';
 
-//jenn's workspace
+const formStyles: CSSProperties = {
+  width: '100%',
+};
+
+type ExtendedImportModelConfig = {
+  location: string;
+};
+
 export const ImportModels = () => {
+  const dispatch = useAppDispatch();
+
+  const [importMainModel, { isLoading }] = useImportMainModelsMutation();
+
+  const addModelForm = useForm({
+    initialValues: {
+      location: '',
+    },
+  });
+
+  console.log('addModelForm', addModelForm.values.location)
+
+  const handleAddModelSubmit = (values: ExtendedImportModelConfig) => {
+    importMainModel({ source: values.location, config: undefined })
+      .unwrap()
+      .then((_) => {
+        dispatch(
+          addToast(
+            makeToast({
+              title: t('toast.modelAddedSimple'),
+              status: 'success',
+            })
+          )
+        );
+        addModelForm.reset();
+      })
+      .catch((error) => {
+        if (error) {
+          dispatch(
+            addToast(
+              makeToast({
+                title: `${error.data.detail} `,
+                status: 'error',
+              })
+            )
+          );
+        }
+      });
+  };
+
   return (
-    <Box layerStyle="first" p={2} borderRadius="base" w="full" h="full">
-      Import Models
+    <Box layerStyle="first" p={3} borderRadius="base" w="full" h="full">
+      <Box w="full" p={4}>
+        <Heading fontSize="xl">Add Model</Heading>
+      </Box>
+      <Box layerStyle="second" p={3} borderRadius="base" w="full" h="full">
+        <form onSubmit={addModelForm.onSubmit((v) => handleAddModelSubmit(v))} style={formStyles}>
+          <Flex gap={2} alignItems="flex-end" justifyContent="space-between">
+            <FormControl>
+              <Flex direction="column" w="full">
+                <FormLabel>{t('modelManager.modelLocation')}</FormLabel>
+                <Input {...addModelForm.getInputProps('location')} />
+              </Flex>
+            </FormControl>
+            <Button isDisabled={!addModelForm.values.location} isLoading={isLoading} type="submit">
+              {t('modelManager.addModel')}
+            </Button>
+          </Flex>
+        </form>
+        <Divider mt="5" mb="3" />
+        <Text>{t('modelManager.importQueue')}</Text>
+        <ImportQueue />
+      </Box>
     </Box>
   );
 };
