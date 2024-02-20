8abb57eedb4696ea1f9fc94087fc4271a3f5d208
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-20 17:48:51 +1100

    tidy(bulk_download): remove extraneous abstract methods
    `start`, `stop` and `__init__` are not required in implementations of an ABC or service.

--- invokeai/app/services/bulk_download/bulk_download_base.py
+++ invokeai/app/services/bulk_download/bulk_download_base.py
@@ -1,26 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
-from invokeai.app.services.invoker import Invoker
-
 
 class BulkDownloadBase(ABC):
-    @abstractmethod
-    def start(self, invoker: Invoker) -> None:
-        """
-        Starts the BulkDownloadService.
-
-        This method is responsible for starting the BulkDownloadService and performing any necessary initialization
-        operations to prepare the service for use.
-
-        param: invoker: The Invoker that holds all the services, required to be passed as a parameter to avoid circular dependencies.
-        """
-
-    @abstractmethod
-    def __init__(self):
-        """
-        Create BulkDownloadBase object.
-        """
+    """Responsible for creating a zip file containing the images specified by the given image names or board id."""
 
     @abstractmethod
     def handler(
@@ -51,10 +34,6 @@ class BulkDownloadBase(ABC):
         :param board_id: The ID of the board whose name is to be included in the item id.
         :return: The generated item ID.
         """
-
-    @abstractmethod
-    def stop(self, *args, **kwargs) -> None:
-        """Stops the BulkDownloadService and cleans up."""
 
     @abstractmethod
     def delete(self, bulk_download_item_name: str) -> None:
