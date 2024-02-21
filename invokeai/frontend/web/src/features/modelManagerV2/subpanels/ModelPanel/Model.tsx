dd8e5e4ed778ee41dcb654f09ad82f13397947a3
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-02-21 13:56:14 -0500

    edit view for model, depending on type and valid values

---
+++ invokeai/frontend/web/src/features/modelManagerV2/subpanels/ModelPanel/Model.tsx
@@ -0,0 +1,8 @@
+import { useAppSelector } from '../../../../app/store/storeHooks';
+import { ModelEdit } from './ModelEdit';
+import { ModelView } from './ModelView';
+
+export const Model = () => {
+  const selectedModelMode = useAppSelector((s) => s.modelmanagerV2.selectedModelMode);
+  return selectedModelMode === 'view' ? <ModelView /> : <ModelEdit />;
+};
