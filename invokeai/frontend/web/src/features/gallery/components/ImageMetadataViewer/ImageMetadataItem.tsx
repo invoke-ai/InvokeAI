acd70f0d7fd01b6b79c518d2382918139bb4788a
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-21 19:42:49 +1100

    fix(ui): handle new model format for metadata

--- invokeai/frontend/web/src/features/gallery/components/ImageMetadataViewer/ImageMetadataItem.tsx
+++ invokeai/frontend/web/src/features/gallery/components/ImageMetadataViewer/ImageMetadataItem.tsx
@@ -1,8 +1,10 @@
 import { ExternalLink, Flex, IconButton, Text, Tooltip } from '@invoke-ai/ui-library';
-import { memo, useCallback } from 'react';
+import { skipToken } from '@reduxjs/toolkit/query';
+import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { IoArrowUndoCircleOutline } from 'react-icons/io5';
 import { PiCopyBold } from 'react-icons/pi';
+import { useGetModelConfigQuery } from 'services/api/endpoints/models';
 
 type MetadataItemProps = {
   isLink?: boolean;
@@ -18,8 +20,9 @@ type MetadataItemProps = {
  */
 const ImageMetadataItem = ({ label, value, onClick, isLink, labelPosition, withCopy = false }: MetadataItemProps) => {
   const { t } = useTranslation();
-
-  const handleCopy = useCallback(() => navigator.clipboard.writeText(value.toString()), [value]);
+  const handleCopy = useCallback(() => {
+    navigator.clipboard.writeText(value?.toString());
+  }, [value]);
 
   if (!value) {
     return null;
@@ -68,3 +71,40 @@ const ImageMetadataItem = ({ label, value, onClick, isLink, labelPosition, withC
 };
 
 export default memo(ImageMetadataItem);
+
+type VAEMetadataItemProps = {
+  label: string;
+  modelKey?: string;
+  onClick: () => void;
+};
+
+export const VAEMetadataItem = memo(({ label, modelKey, onClick }: VAEMetadataItemProps) => {
+  const { data: modelConfig } = useGetModelConfigQuery(modelKey ?? skipToken);
+
+  return (
+    <ImageMetadataItem label={label} value={modelKey ? modelConfig?.name ?? modelKey : 'Default'} onClick={onClick} />
+  );
+});
+
+VAEMetadataItem.displayName = 'VAEMetadataItem';
+
+type ModelMetadataItemProps = {
+  label: string;
+  modelKey?: string;
+  
+  extra?: string;
+  onClick: () => void;
+};
+
+export const ModelMetadataItem = memo(({ label, modelKey, extra, onClick }: ModelMetadataItemProps) => {
+  const { data: modelConfig } = useGetModelConfigQuery(modelKey ?? skipToken);
+  const value = useMemo(() => {
+    if (modelConfig) {
+      return `${modelConfig.name}${extra ?? ''}`;
+    }
+    return `${modelKey}${extra ?? ''}`;
+  }, [extra, modelConfig, modelKey]);
+  return <ImageMetadataItem label={label} value={value} onClick={onClick} />;
+});
+
+ModelMetadataItem.displayName = 'ModelMetadataItem';
