f7324e81c1929afb0ba324be4a61b47a6f53c1c9
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-02-23 14:58:36 -0500

    fix logic to see if scanned models are already installed, style tweaks

---
+++ invokeai/frontend/web/src/features/modelManagerV2/hooks/useIsImported.ts
@@ -0,0 +1,54 @@
+import { useCallback } from "react";
+import { ALL_BASE_MODELS } from "../../../services/api/constants";
+import { useGetMainModelsQuery, useGetLoRAModelsQuery, useGetTextualInversionModelsQuery, useGetControlNetModelsQuery, useGetT2IAdapterModelsQuery, useGetIPAdapterModelsQuery, useGetVaeModelsQuery, } from "../../../services/api/endpoints/models";
+import { EntityState } from "@reduxjs/toolkit";
+import { forEach } from "lodash-es";
+import { AnyModelConfig } from "../../../services/api/types";
+
+export const useIsImported = () => {
+    const { data: mainModels } = useGetMainModelsQuery(ALL_BASE_MODELS);
+    const { data: loras } = useGetLoRAModelsQuery();
+    const { data: embeddings } = useGetTextualInversionModelsQuery();
+    const { data: controlnets } = useGetControlNetModelsQuery();
+    const { data: ipAdapters } = useGetIPAdapterModelsQuery();
+    const { data: t2is } = useGetT2IAdapterModelsQuery();
+    const { data: vaes } = useGetVaeModelsQuery();
+
+    const isImported = useCallback(({ name }: { name: string }) => {
+        const data = [mainModels, loras, embeddings, controlnets, ipAdapters, t2is, vaes]
+        let isMatch = false;
+        for (let index = 0; index < data.length; index++) {
+            const modelType: EntityState<AnyModelConfig, string> | undefined = data[index];
+
+            const match = modelsFilter(modelType, name)
+
+            if (!!match.length) {
+                isMatch = true
+                break;
+            }
+        }
+        return isMatch
+    }, [mainModels, loras, embeddings, controlnets, ipAdapters, t2is, vaes])
+
+    return { isImported }
+}
+
+const modelsFilter = <T extends AnyModelConfig>(
+    data: EntityState<T, string> | undefined,
+    nameFilter: string,
+): T[] => {
+    const filteredModels: T[] = [];
+
+    forEach(data?.entities, (model) => {
+        if (!model) {
+            return;
+        }
+
+        const matchesFilter = model.path.toLowerCase().includes(nameFilter.toLowerCase());
+
+        if (matchesFilter) {
+            filteredModels.push(model);
+        }
+    });
+    return filteredModels;
+};
