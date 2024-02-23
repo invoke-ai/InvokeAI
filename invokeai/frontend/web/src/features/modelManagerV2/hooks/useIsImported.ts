9afb4f9eb4106e701707e14f80e9b06149142d41
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-02-23 16:12:49 -0500

    lint fix

--- invokeai/frontend/web/src/features/modelManagerV2/hooks/useIsImported.ts
+++ invokeai/frontend/web/src/features/modelManagerV2/hooks/useIsImported.ts
@@ -1,54 +1,62 @@
-import { useCallback } from "react";
-import { ALL_BASE_MODELS } from "../../../services/api/constants";
-import { useGetMainModelsQuery, useGetLoRAModelsQuery, useGetTextualInversionModelsQuery, useGetControlNetModelsQuery, useGetT2IAdapterModelsQuery, useGetIPAdapterModelsQuery, useGetVaeModelsQuery, } from "../../../services/api/endpoints/models";
-import { EntityState } from "@reduxjs/toolkit";
-import { forEach } from "lodash-es";
-import { AnyModelConfig } from "../../../services/api/types";
+import type { EntityState } from '@reduxjs/toolkit';
+import { forEach } from 'lodash-es';
+import { useCallback } from 'react';
+import { ALL_BASE_MODELS } from 'services/api/constants';
+import {
+  useGetControlNetModelsQuery,
+  useGetIPAdapterModelsQuery,
+  useGetLoRAModelsQuery,
+  useGetMainModelsQuery,
+  useGetT2IAdapterModelsQuery,
+  useGetTextualInversionModelsQuery,
+  useGetVaeModelsQuery,
+} from 'services/api/endpoints/models';
+import type { AnyModelConfig } from 'services/api/types';
 
 export const useIsImported = () => {
-    const { data: mainModels } = useGetMainModelsQuery(ALL_BASE_MODELS);
-    const { data: loras } = useGetLoRAModelsQuery();
-    const { data: embeddings } = useGetTextualInversionModelsQuery();
-    const { data: controlnets } = useGetControlNetModelsQuery();
-    const { data: ipAdapters } = useGetIPAdapterModelsQuery();
-    const { data: t2is } = useGetT2IAdapterModelsQuery();
-    const { data: vaes } = useGetVaeModelsQuery();
+  const { data: mainModels } = useGetMainModelsQuery(ALL_BASE_MODELS);
+  const { data: loras } = useGetLoRAModelsQuery();
+  const { data: embeddings } = useGetTextualInversionModelsQuery();
+  const { data: controlnets } = useGetControlNetModelsQuery();
+  const { data: ipAdapters } = useGetIPAdapterModelsQuery();
+  const { data: t2is } = useGetT2IAdapterModelsQuery();
+  const { data: vaes } = useGetVaeModelsQuery();
 
-    const isImported = useCallback(({ name }: { name: string }) => {
-        const data = [mainModels, loras, embeddings, controlnets, ipAdapters, t2is, vaes]
-        let isMatch = false;
-        for (let index = 0; index < data.length; index++) {
-            const modelType: EntityState<AnyModelConfig, string> | undefined = data[index];
+  const isImported = useCallback(
+    ({ name }: { name: string }) => {
+      const data = [mainModels, loras, embeddings, controlnets, ipAdapters, t2is, vaes];
+      let isMatch = false;
+      for (let index = 0; index < data.length; index++) {
+        const modelType: EntityState<AnyModelConfig, string> | undefined = data[index];
 
-            const match = modelsFilter(modelType, name)
+        const match = modelsFilter(modelType, name);
 
-            if (!!match.length) {
-                isMatch = true
-                break;
-            }
+        if (match.length) {
+          isMatch = true;
+          break;
         }
-        return isMatch
-    }, [mainModels, loras, embeddings, controlnets, ipAdapters, t2is, vaes])
+      }
+      return isMatch;
+    },
+    [mainModels, loras, embeddings, controlnets, ipAdapters, t2is, vaes]
+  );
 
-    return { isImported }
-}
+  return { isImported };
+};
 
-const modelsFilter = <T extends AnyModelConfig>(
-    data: EntityState<T, string> | undefined,
-    nameFilter: string,
-): T[] => {
-    const filteredModels: T[] = [];
+const modelsFilter = <T extends AnyModelConfig>(data: EntityState<T, string> | undefined, nameFilter: string): T[] => {
+  const filteredModels: T[] = [];
 
-    forEach(data?.entities, (model) => {
-        if (!model) {
-            return;
-        }
+  forEach(data?.entities, (model) => {
+    if (!model) {
+      return;
+    }
 
-        const matchesFilter = model.path.toLowerCase().includes(nameFilter.toLowerCase());
+    const matchesFilter = model.path.toLowerCase().includes(nameFilter.toLowerCase());
 
-        if (matchesFilter) {
-            filteredModels.push(model);
-        }
-    });
-    return filteredModels;
+    if (matchesFilter) {
+      filteredModels.push(model);
+    }
+  });
+  return filteredModels;
 };
