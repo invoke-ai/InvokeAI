5496699d6c752b8571566e464aa8dceb24b07168
Jennifer Player <chainchompa@gmail.com>
2024-02-23 16:16:42 -0500

    refactored and fixed issues with advanced import form

--- invokeai/frontend/web/src/features/modelManagerV2/subpanels/AddModelPanel/AdvancedImport.tsx
+++ invokeai/frontend/web/src/features/modelManagerV2/subpanels/AddModelPanel/AdvancedImport.tsx
@@ -1,56 +1,238 @@
-import type { ComboboxOnChange, ComboboxOption } from '@invoke-ai/ui-library';
-import { Combobox, Flex, FormLabel,Text } from '@invoke-ai/ui-library';
+import { Button, Flex, FormControl, FormErrorMessage, FormLabel, Input, Text, Textarea } from '@invoke-ai/ui-library';
+import { useAppDispatch } from 'app/store/storeHooks';
 import ScrollableContent from 'common/components/OverlayScrollbars/ScrollableContent';
-import { useCallback, useMemo, useState } from 'react';
+import BaseModelSelect from 'features/modelManagerV2/subpanels/ModelPanel/Fields/BaseModelSelect';
+import BooleanSelect from 'features/modelManagerV2/subpanels/ModelPanel/Fields/BooleanSelect';
+import ModelFormatSelect from 'features/modelManagerV2/subpanels/ModelPanel/Fields/ModelFormatSelect';
+import ModelTypeSelect from 'features/modelManagerV2/subpanels/ModelPanel/Fields/ModelTypeSelect';
+import ModelVariantSelect from 'features/modelManagerV2/subpanels/ModelPanel/Fields/ModelVariantSelect';
+import PredictionTypeSelect from 'features/modelManagerV2/subpanels/ModelPanel/Fields/PredictionTypeSelect';
+import RepoVariantSelect from 'features/modelManagerV2/subpanels/ModelPanel/Fields/RepoVariantSelect';
+import { addToast } from 'features/system/store/systemSlice';
+import { makeToast } from 'features/system/util/makeToast';
+import { useCallback, useEffect } from 'react';
+import type { SubmitHandler} from 'react-hook-form';
+import {useForm  } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { useImportAdvancedModelMutation } from 'services/api/endpoints/models';
+import type { AnyModelConfig } from 'services/api/types';
 import { z } from 'zod';
-
-import { AdvancedImportCheckpoint } from './AdvancedImportCheckpoint';
-import { AdvancedImportDiffusers } from './AdvancedImportDiffusers';
 
 export const zManualAddMode = z.enum(['diffusers', 'checkpoint']);
 export type ManualAddMode = z.infer<typeof zManualAddMode>;
 export const isManualAddMode = (v: unknown): v is ManualAddMode => zManualAddMode.safeParse(v).success;
 
 export const AdvancedImport = () => {
-  const [advancedAddMode, setAdvancedAddMode] = useState<ManualAddMode>('diffusers');
+  const dispatch = useAppDispatch();
+
+  const [importAdvancedModel] = useImportAdvancedModelMutation();
 
   const { t } = useTranslation();
-  const handleChange: ComboboxOnChange = useCallback((v) => {
-    if (!isManualAddMode(v?.value)) {
-      return;
-    }
-    setAdvancedAddMode(v.value);
-  }, []);
 
-  const options: ComboboxOption[] = useMemo(
-    () => [
-      { label: t('modelManager.diffusersModels'), value: 'diffusers' },
-      { label: t('modelManager.checkpointOrSafetensors'), value: 'checkpoint' },
-    ],
-    [t]
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+    setValue,
+    resetField,
+    reset,
+    watch,
+  } = useForm<AnyModelConfig>({
+    defaultValues: {
+      name: '',
+      base: 'sd-1',
+      type: 'main',
+      path: '',
+      description: '',
+      format: 'diffusers',
+      vae: '',
+      variant: 'normal',
+    },
+    mode: 'onChange',
+  });
+
+  const onSubmit = useCallback<SubmitHandler<AnyModelConfig>>(
+    (values) => {
+      const cleanValues = Object.fromEntries(
+        Object.entries(values).filter(([value]) => value !== null && value !== undefined)
+      );
+      importAdvancedModel({
+        source: {
+          path: cleanValues.path as string,
+          type: 'local',
+        },
+        config: cleanValues,
+      })
+        .unwrap()
+        .then((_) => {
+          dispatch(
+            addToast(
+              makeToast({
+                title: t('modelManager.modelAdded', {
+                  modelName: values.name,
+                }),
+                status: 'success',
+              })
+            )
+          );
+          reset();
+        })
+        .catch((error) => {
+          if (error) {
+            dispatch(
+              addToast(
+                makeToast({
+                  title: t('toast.modelAddFailed'),
+                  status: 'error',
+                })
+              )
+            );
+          }
+        });
+    },
+    [dispatch, reset, t, importAdvancedModel]
   );
 
-  const value = useMemo(() => options.find((o) => o.value === advancedAddMode), [options, advancedAddMode]);
+  const watchedModelType = watch('type');
+  const watchedModelFormat = watch('format');
+
+  useEffect(() => {
+    if (watchedModelType === 'main') {
+      setValue('format', 'diffusers');
+      setValue('repo_variant', '');
+      setValue('variant', 'normal');
+    }
+    if (watchedModelType === 'lora') {
+      setValue('format', 'lycoris');
+    } else if (watchedModelType === 'embedding') {
+      setValue('format', 'embedding_file');
+    } else if (watchedModelType === 'ip_adapter') {
+      setValue('format', 'invokeai');
+    } else {
+      setValue('format', 'diffusers');
+    }
+    resetField('upcast_attention');
+    resetField('ztsnr_training');
+    resetField('vae');
+    resetField('config');
+    resetField('prediction_type');
+    resetField('image_encoder_model_id');
+  }, [watchedModelType, resetField, setValue]);
 
   return (
     <ScrollableContent>
-      <Flex flexDirection="column" gap={4} width="100%" pb={10}>
-        <Flex alignItems="flex-end" gap="4">
-          <Flex direction="column" gap="3" width="full">
-            <FormLabel>{t('modelManager.modelType')}</FormLabel>
-            <Combobox value={value} options={options} onChange={handleChange} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Flex flexDirection="column" gap={4} width="100%" pb={10}>
+          <Flex alignItems="flex-end" gap="4">
+            <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+              <FormLabel>Model Type</FormLabel>
+              <ModelTypeSelect<AnyModelConfig> control={control} name="type" />
+            </FormControl>
+            <Text px="2" fontSize="xs" textAlign="center">
+              {t('modelManager.advancedImportInfo')}
+            </Text>
           </Flex>
-          <Text px="2" fontSize="xs" textAlign="center">
-            {t('modelManager.advancedImportInfo')}
-          </Text>
-        </Flex>
 
-        <Flex p={4} borderRadius={4} bg="base.850" height="100%">
-          {advancedAddMode === 'diffusers' && <AdvancedImportDiffusers />}
-          {advancedAddMode === 'checkpoint' && <AdvancedImportCheckpoint />}
+          <Flex p={4} borderRadius={4} bg="base.850" height="100%" direction="column" gap="3">
+            <FormControl isInvalid={Boolean(errors.name)}>
+              <Flex direction="column" width="full">
+                <FormLabel>{t('modelManager.name')}</FormLabel>
+                <Input
+                  {...register('name', {
+                    validate: (value) => value.trim().length >= 3 || 'Must be at least 3 characters',
+                  })}
+                />
+                {errors.name?.message && <FormErrorMessage>{errors.name?.message}</FormErrorMessage>}
+              </Flex>
+            </FormControl>
+            <Flex>
+              <FormControl>
+                <Flex direction="column" width="full">
+                  <FormLabel>{t('modelManager.description')}</FormLabel>
+                  <Textarea size="sm" {...register('description')} />
+                  {errors.name?.message && <FormErrorMessage>{errors.name?.message}</FormErrorMessage>}
+                </Flex>
+              </FormControl>
+            </Flex>
+            <Flex gap={4}>
+              <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+                <FormLabel>Base Model</FormLabel>
+                <BaseModelSelect<AnyModelConfig> control={control} name="base" />
+              </FormControl>
+              <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+                <FormLabel>Format</FormLabel>
+                <ModelFormatSelect<AnyModelConfig> control={control} name="format" />
+              </FormControl>
+            </Flex>
+            <Flex gap={4}>
+              <FormControl flexDir="column" alignItems="flex-start" gap={1} isInvalid={Boolean(errors.path)}>
+                <FormLabel>Path</FormLabel>
+                <Input
+                  {...register('path', {
+                    validate: (value) => value.trim().length > 0 || 'Must provide a path',
+                  })}
+                />
+                {errors.path?.message && <FormErrorMessage>{errors.path?.message}</FormErrorMessage>}
+              </FormControl>
+            </Flex>
+            {watchedModelType === 'main' && (
+              <>
+                <Flex gap={4}>
+                  {watchedModelFormat === 'diffusers' && (
+                    <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+                      <FormLabel>Repo Variant</FormLabel>
+                      <RepoVariantSelect<AnyModelConfig> control={control} name="repo_variant" />
+                    </FormControl>
+                  )}
+                  {watchedModelFormat === 'checkpoint' && (
+                    <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+                      <FormLabel>Config Path</FormLabel>
+                      <Input {...register('config')} />
+                    </FormControl>
+                  )}
+
+                  <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+                    <FormLabel>Variant</FormLabel>
+                    <ModelVariantSelect<AnyModelConfig> control={control} name="variant" />
+                  </FormControl>
+                </Flex>
+                <Flex gap={4}>
+                  <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+                    <FormLabel>Prediction Type</FormLabel>
+                    <PredictionTypeSelect<AnyModelConfig> control={control} name="prediction_type" />
+                  </FormControl>
+                  <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+                    <FormLabel>Upcast Attention</FormLabel>
+                    <BooleanSelect<AnyModelConfig> control={control} name="upcast_attention" />
+                  </FormControl>
+                </Flex>
+                <Flex gap={4}>
+                  <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+                    <FormLabel>ZTSNR Training</FormLabel>
+                    <BooleanSelect<AnyModelConfig> control={control} name="ztsnr_training" />
+                  </FormControl>
+                  <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+                    <FormLabel>VAE Path</FormLabel>
+                    <Input {...register('vae')} />
+                  </FormControl>
+                </Flex>
+              </>
+            )}
+            {watchedModelType === 'ip_adapter' && (
+              <Flex gap={4}>
+                <FormControl flexDir="column" alignItems="flex-start" gap={1}>
+                  <FormLabel>Image Encoder Model ID</FormLabel>
+                  <Input {...register('image_encoder_model_id')} />
+                </FormControl>
+              </Flex>
+            )}
+            <Button mt={2} type="submit">
+              {t('modelManager.addModel')}
+            </Button>
+          </Flex>
         </Flex>
-      </Flex>
+      </form>
     </ScrollableContent>
   );
 };
