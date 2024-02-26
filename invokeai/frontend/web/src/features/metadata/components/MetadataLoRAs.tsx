04545e792c60b91b42e712d8787ba8b07b1bcb25
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-26 11:51:37 +1100

    fix(ui): fix lora metadata rendering

--- invokeai/frontend/web/src/features/metadata/components/MetadataLoRAs.tsx
+++ invokeai/frontend/web/src/features/metadata/components/MetadataLoRAs.tsx
@@ -1,3 +1,4 @@
+import { Text } from '@invoke-ai/ui-library';
 import type { LoRA } from 'features/lora/store/loraSlice';
 import { MetadataItemView } from 'features/metadata/components/MetadataItemView';
 import type { MetadataHandlers } from 'features/metadata/types';
@@ -54,7 +55,7 @@ const MetadataViewLoRA = ({
     if (!handlers.renderItemValue) {
       return null;
     }
-    return handlers.renderItemValue(lora);
+    return <Text>{handlers.renderItemValue(lora)}</Text>;
   }, [handlers, lora]);
 
   return <MetadataItemView label={label} isDisabled={false} onRecall={onRecall} renderedValue={renderedValue} />;
