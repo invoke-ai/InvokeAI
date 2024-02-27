32b847897495b73aca760a88b7f66bb53f014c6c
Jennifer Player <chainchompa@gmail.com>
2024-02-27 09:43:02 -0500

    added add all button to scan models

--- invokeai/frontend/web/src/features/modelManagerV2/subpanels/AddModelPanel/ScanModels/ScanModelsResults.tsx
+++ invokeai/frontend/web/src/features/modelManagerV2/subpanels/AddModelPanel/ScanModels/ScanModelsResults.tsx
@@ -1,10 +1,22 @@
-import { Divider, Flex, Heading, IconButton, Input, InputGroup, InputRightElement } from '@invoke-ai/ui-library';
+import {
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from '@invoke-ai/ui-library';
+import { useAppDispatch } from 'app/store/storeHooks';
 import ScrollableContent from 'common/components/OverlayScrollbars/ScrollableContent';
+import { addToast } from 'features/system/store/systemSlice';
+import { makeToast } from 'features/system/util/makeToast';
 import type { ChangeEventHandler } from 'react';
 import { useCallback, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiXBold } from 'react-icons/pi';
-import type { ScanFolderResponse } from 'services/api/endpoints/models';
+import { type ScanFolderResponse, useImportMainModelsMutation } from 'services/api/endpoints/models';
 
 import { ScanModelResultItem } from './ScanModelResultItem';
 
@@ -15,6 +27,9 @@ type ScanModelResultsProps = {
 export const ScanModelsResults = ({ results }: ScanModelResultsProps) => {
   const { t } = useTranslation();
   const [searchTerm, setSearchTerm] = useState('');
+  const dispatch = useAppDispatch();
+
+  const [importMainModel] = useImportMainModelsMutation();
 
   const filteredResults = useMemo(() => {
     return results.filter((result) => {
@@ -31,6 +46,38 @@ export const ScanModelsResults = ({ results }: ScanModelResultsProps) => {
     setSearchTerm('');
   }, []);
 
+  const handleAddAll = useCallback(() => {
+    for (const result of filteredResults) {
+      if (result.is_installed) {
+        continue;
+      }
+      importMainModel({ source: result.path, config: undefined })
+        .unwrap()
+        .then((_) => {
+          dispatch(
+            addToast(
+              makeToast({
+                title: t('toast.modelAddedSimple'),
+                status: 'success',
+              })
+            )
+          );
+        })
+        .catch((error) => {
+          if (error) {
+            dispatch(
+              addToast(
+                makeToast({
+                  title: `${error.data.detail} `,
+                  status: 'error',
+                })
+              )
+            );
+          }
+        });
+    }
+  }, [importMainModel, filteredResults, dispatch, t]);
+
   return (
     <>
       <Divider mt={4} />
@@ -39,27 +86,32 @@ export const ScanModelsResults = ({ results }: ScanModelResultsProps) => {
           <Heading fontSize="md" as="h4">
             {t('modelManager.scanResults')}
           </Heading>
-          <InputGroup maxW="300px" size="xs">
-            <Input
-              placeholder={t('modelManager.search')}
-              value={searchTerm}
-              data-testid="board-search-input"
-              onChange={handleSearch}
-              size="xs"
-            />
+          <Flex alignItems="center" gap="4">
+            <Button onClick={handleAddAll} isDisabled={filteredResults.length === 0}>
+              {t('modelManager.addAll')}
+            </Button>
+            <InputGroup maxW="300px" size="xs">
+              <Input
+                placeholder={t('modelManager.search')}
+                value={searchTerm}
+                data-testid="board-search-input"
+                onChange={handleSearch}
+                size="xs"
+              />
 
-            {searchTerm && (
-              <InputRightElement h="full" pe={2}>
-                <IconButton
-                  size="sm"
-                  variant="link"
-                  aria-label={t('boards.clearSearch')}
-                  icon={<PiXBold />}
-                  onClick={clearSearch}
-                />
-              </InputRightElement>
-            )}
-          </InputGroup>
+              {searchTerm && (
+                <InputRightElement h="full" pe={2}>
+                  <IconButton
+                    size="sm"
+                    variant="link"
+                    aria-label={t('boards.clearSearch')}
+                    icon={<PiXBold />}
+                    onClick={clearSearch}
+                  />
+                </InputRightElement>
+              )}
+            </InputGroup>
+          </Flex>
         </Flex>
         <Flex height="100%" layerStyle="third" borderRadius="base" p={4} mt={4} mb={4}>
           <ScrollableContent>
