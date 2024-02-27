f0ecbfddab3cb50de920999d06c95f2c9f509c9c
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-02-27 15:33:11 -0500

    UI in MM to create trigger phrases

--- invokeai/frontend/web/src/features/modelManagerV2/subpanels/ModelPanel/Metadata/ModelMetadata.tsx
+++ invokeai/frontend/web/src/features/modelManagerV2/subpanels/ModelPanel/Metadata/ModelMetadata.tsx
@@ -1,8 +1,10 @@
-import { Flex } from '@invoke-ai/ui-library';
+import { Box, Flex } from '@invoke-ai/ui-library';
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useAppSelector } from 'app/store/storeHooks';
 import DataViewer from 'features/gallery/components/ImageMetadataViewer/DataViewer';
 import { useGetModelConfigQuery } from 'services/api/endpoints/models';
+
+import { TriggerPhrases } from './TriggerPhrases';
 
 export const ModelMetadata = () => {
   const selectedModelKey = useAppSelector((s) => s.modelmanagerV2.selectedModelKey);
@@ -11,6 +13,9 @@ export const ModelMetadata = () => {
   return (
     <>
       <Flex flexDir="column" height="full" gap="3">
+        <Box layerStyle="second" borderRadius="base" p={3}>
+          <TriggerPhrases />
+        </Box>
         <DataViewer label="metadata" data={data?.source_api_response || {}} />
       </Flex>
     </>
