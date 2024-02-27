f3c59e6062aecda50e268ad885a4f10a6db515fc
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-02-27 18:25:50 +1100

    feat(ui): configure knip

--- invokeai/frontend/web/knip.ts
+++ invokeai/frontend/web/knip.ts
@@ -6,6 +6,20 @@ const config: KnipConfig = {
     'src/app/store/middleware/debugLoggerMiddleware.ts',
   ],
   ignoreBinaries: ['only-allow'],
+  rules: {
+    files: 'warn',
+    dependencies: 'warn',
+    unlisted: 'warn',
+    binaries: 'warn',
+    unresolved: 'warn',
+    exports: 'warn',
+    types: 'warn',
+    nsExports: 'warn',
+    nsTypes: 'warn',
+    enumMembers: 'warn',
+    classMembers: 'warn',
+    duplicates: 'warn',
+  },
 };
 
 export default config;
