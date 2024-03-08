fce136fe91fd6c19fbe4cafa34104f691b86e29a
blessedcoolant <54517381+blessedcoolant@users.noreply.github.com>
2024-03-08 09:57:09 +0530

    Add Custom Loading Animation

--- invokeai/frontend/web/src/common/components/Loading/Loading.tsx
+++ invokeai/frontend/web/src/common/components/Loading/Loading.tsx
@@ -1,23 +1,38 @@
-import { Flex, Image, Spinner } from '@invoke-ai/ui-library';
-import InvokeLogoWhite from 'public/assets/images/invoke-symbol-wht-lrg.svg';
+import { Flex, Text } from '@invoke-ai/ui-library';
+// import AnimatedVideoSpinner from 'public/assets/videos/LogoAnim.webm';
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import type { Options } from 'react-lottie';
+import Lottie from 'react-lottie';
+
+import { LoadingAnimationData } from './LoadingAnimationData';
+
+const options: Options = {
+  loop: true,
+  autoplay: true,
+  animationData: LoadingAnimationData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
 
 // This component loads before the theme so we cannot use theme tokens here
-
 const Loading = () => {
+  const { t } = useTranslation();
   return (
-    <Flex position="relative" width="100vw" height="100vh" alignItems="center" justifyContent="center" bg="#151519">
-      <Image src={InvokeLogoWhite} w="8rem" h="8rem" />
-      <Spinner
-        label="Loading"
-        color="grey"
-        position="absolute"
-        size="sm"
-        width="24px !important"
-        height="24px !important"
-        right="1.5rem"
-        bottom="1.5rem"
-      />
+    <Flex
+      position="relative"
+      flexDir="column"
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      bg="#151519"
+    >
+      <Lottie options={options} width={200} height={200} />
+      <Text color="#535a65" fontFamily="sans-serif" fontSize="small" fontWeight={600}>
+        {t('common.initializingInvokeAI')}
+      </Text>
     </Flex>
   );
 };
