4f97192c053735d9c3d7f2fbf5f2b1000a8af3ad
Ryan Dick <ryanjdick3@gmail.com>
2024-03-11 09:45:25 -0400

    (minor) The latest ruff version has _slightly_ different formatting preferences.

--- invokeai/app/invocations/mask.py
+++ invokeai/app/invocations/mask.py
@@ -28,9 +28,9 @@ class RectangleMaskInvocation(BaseInvocation, WithMetadata):
 
     def invoke(self, context: InvocationContext) -> MaskOutput:
         mask = torch.zeros((1, self.height, self.width), dtype=torch.bool)
-        mask[
-            :, self.y_top : self.y_top + self.rectangle_height, self.x_left : self.x_left + self.rectangle_width
-        ] = True
+        mask[:, self.y_top : self.y_top + self.rectangle_height, self.x_left : self.x_left + self.rectangle_width] = (
+            True
+        )
 
         mask_name = context.tensors.save(mask)
         return MaskOutput(
