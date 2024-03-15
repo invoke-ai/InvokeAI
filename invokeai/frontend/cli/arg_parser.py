5179587b5afc95f457f2c7d3b22ffd3f09137d51
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-03-15 17:04:39 +1100

    feat(config): restore ignore_missing_core_models arg

--- invokeai/frontend/cli/arg_parser.py
+++ invokeai/frontend/cli/arg_parser.py
@@ -3,13 +3,19 @@ from typing import Optional
 
 from invokeai.version import __version__
 
-_root_help = r"""Sets a root directory for the app. If omitted, the app will search for the root directory in the following order:
+_root_help = r"""Sets a root directory for the app.
+If omitted, the app will search for the root directory in the following order:
 - The `$INVOKEAI_ROOT` environment variable
 - The currently active virtual environment's parent directory
 - `$HOME/invokeai`"""
 
+_ignore_missing_core_models_help = r"""If set, the app will ignore missing core diffusers conversion models.
+These are required to use checkpoint/safetensors models.
+If you only use diffusers models, you can safely enable this."""
+
 _parser = ArgumentParser(description="Invoke Studio", formatter_class=RawTextHelpFormatter)
 _parser.add_argument("--root", type=str, help=_root_help)
+_parser.add_argument("--ignore_missing_core_models", action="store_true", help=_ignore_missing_core_models_help)
 _parser.add_argument("--version", action="version", version=__version__, help="Displays the version and exits.")
 
 
