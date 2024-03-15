ce9aeeece39116c2d7bd7371c990b3d4805324e5
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-03-15 16:33:52 +1100

    feat: single app entrypoint with CLI arg parsing
    We have two problems with how argparse is being utilized:
    - We parse CLI args as the `api_app.py` file is read. This causes a problem pytest, which has an incompatible set of CLI args. Some tests import the FastAPI app, which triggers the config to parse CLI args, which receives the pytest args and fails.
    - We've repeatedly had problems when something that uses the config is imported before the CLI args are parsed. When this happens, the root dir may not be set correctly, so we attempt to operate on incorrect paths.
    
    To resolve these issues, we need to lift CLI arg parsing outside of the application code, but still let the application access the CLI args. We can create a external app entrypoint to do this.
    
    - `InvokeAIArgs` is a simple helper class that parses CLI args and stores the result.
    - `run_app()` is the new entrypoint. It first parses CLI args, then runs `invoke_api` to start the app.
    
    The `invokeai-web` project script and `invokeai-web.py` dev script now call `run_app()` instead of `invoke_api()`.
    
    The first time `get_config()` is called to get the singleton config object, it retrieves the args from `InvokeAIArgs`, sets the root dir if provided, then merges settings in from `invokeai.yaml`.
    
    CLI arg parsing is now safely insulated from application code, but still accessible. And we don't need to worry about import order having an impact on anything, because by the time the app is running, we have already parsed CLI args. Whew!

---
+++ invokeai/frontend/cli/arg_parser.py
@@ -0,0 +1,41 @@
+from argparse import ArgumentParser, Namespace, RawTextHelpFormatter
+from typing import Optional
+
+from invokeai.version import __version__
+
+_root_help = r"""Sets a root directory for the app. If omitted, the app will search for the root directory in the following order:
+- The `$INVOKEAI_ROOT` environment variable
+- The currently active virtual environment's parent directory
+- `$HOME/invokeai`"""
+
+_parser = ArgumentParser(description="Invoke Studio", formatter_class=RawTextHelpFormatter)
+_parser.add_argument("--root", type=str, help=_root_help)
+_parser.add_argument("--version", action="version", version=__version__, help="Displays the version and exits.")
+
+
+class InvokeAIArgs:
+    """Helper class for parsing CLI args.
+
+    Args should never be parsed within the application code, only in the CLI entrypoints. Parsing args within the
+    application creates conflicts when running tests or when using application modules directly.
+
+    If the args are needed within the application, the consumer should access them from this class.
+
+    Example:
+    ```
+    # In a CLI wrapper
+    from invokeai.frontend.cli.app_arg_parser import InvokeAIArgs
+    InvokeAIArgs.parse_args()
+
+    # In the application
+    from invokeai.frontend.cli.app_arg_parser import InvokeAIArgs
+    args = InvokeAIArgs.args
+    """
+
+    args: Optional[Namespace] = None
+
+    @staticmethod
+    def parse_args() -> Optional[Namespace]:
+        """Parse CLI args and store the result."""
+        InvokeAIArgs.args = _parser.parse_args()
+        return InvokeAIArgs.args
