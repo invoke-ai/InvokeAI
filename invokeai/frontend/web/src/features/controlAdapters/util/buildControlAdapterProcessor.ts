fe563f05fca2e96df8bad4f28164aff5787a676e
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-03-18 15:06:38 -0400

    tsc

--- invokeai/frontend/web/src/features/controlAdapters/util/buildControlAdapterProcessor.ts
+++ invokeai/frontend/web/src/features/controlAdapters/util/buildControlAdapterProcessor.ts
@@ -5,7 +5,7 @@ import type { ControlNetModelConfig, T2IAdapterModelConfig } from 'services/api/
 export const buildControlAdapterProcessor = (modelConfig: ControlNetModelConfig | T2IAdapterModelConfig) => {
   const defaultPreprocessor = modelConfig.default_settings?.preprocessor;
   const processorType = isControlAdapterProcessorType(defaultPreprocessor) ? defaultPreprocessor : 'none';
-  const processorNode = CONTROLNET_PROCESSORS[processorType].default;
+  const processorNode = CONTROLNET_PROCESSORS[processorType].buildDefaults(modelConfig.base);
 
   return { processorType, processorNode };
 };
