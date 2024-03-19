55a43808ae9b5ede1446c5a3dfebbf64fd7ccf16
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-03-19 16:54:04 +1100

    cleanup: remove unused scripts, cruft
    App runs & tests pass.

--- invokeai/frontend/merge/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Initialization file for invokeai.frontend.merge
-"""
-
-from .merge_diffusers import main as invokeai_merge_diffusers  # noqa: F401
--- scripts/invokeai-model-install.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from invokeai.frontend.install.model_install import main
-
-main()
