48e1a0c2178e271c3d637e86bb7e06547fc28b0d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-03-27 15:46:20 +1100

    fix(ui): remove sync models functionality
    The backend functionality was removed in the previous couple commits. Removing the frontend endpoints and components.

--- invokeai/frontend/web/src/services/api/endpoints/models.ts
+++ invokeai/frontend/web/src/services/api/endpoints/models.ts
@@ -188,15 +188,6 @@ export const modelsApi = api.injectEndpoints({
       },
       serializeQueryArgs: ({ queryArgs }) => `${queryArgs.name}.${queryArgs.base}.${queryArgs.type}`,
     }),
-    syncModels: build.mutation<void, void>({
-      query: () => {
-        return {
-          url: buildModelsUrl('sync'),
-          method: 'PATCH',
-        };
-      },
-      invalidatesTags: [{ type: 'ModelConfig', id: LIST_TAG }],
-    }),
     scanFolder: build.query<ScanFolderResponse, ScanFolderArg>({
       query: (arg) => {
         const folderQueryStr = arg ? queryString.stringify(arg, {}) : '';
@@ -278,7 +269,6 @@ export const {
   useUpdateModelImageMutation,
   useInstallModelMutation,
   useConvertModelMutation,
-  useSyncModelsMutation,
   useLazyScanFolderQuery,
   useLazyGetHuggingFaceModelsQuery,
   useListModelInstallsQuery,
