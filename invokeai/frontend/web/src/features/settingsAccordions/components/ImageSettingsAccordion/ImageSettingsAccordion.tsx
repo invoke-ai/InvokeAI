4927d1b7c9f3822c0c4586de4dfd9f1a30f989d3
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-04-03 11:26:18 -0400

    add some test IDs for accordion targeting

--- invokeai/frontend/web/src/features/settingsAccordions/components/ImageSettingsAccordion/ImageSettingsAccordion.tsx
+++ invokeai/frontend/web/src/features/settingsAccordions/components/ImageSettingsAccordion/ImageSettingsAccordion.tsx
@@ -83,7 +83,7 @@ export const ImageSettingsAccordion = memo(() => {
       isOpen={isOpenAccordion}
       onToggle={onToggleAccordion}
     >
-      <Flex px={4} pt={4} w="full" h="full" flexDir="column">
+      <Flex px={4} pt={4} w="full" h="full" flexDir="column" data-testid="image-settings-accordion">
         {activeTabName === 'unifiedCanvas' ? <ImageSizeCanvas /> : <ImageSizeLinear />}
         <Expander label={t('accordions.advanced.options')} isOpen={isOpenExpander} onToggle={onToggleExpander}>
           <Flex gap={4} pb={4} flexDir="column">
