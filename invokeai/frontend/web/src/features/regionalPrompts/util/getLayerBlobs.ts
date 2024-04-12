7d561aa5f4650a17b7ba3ec3bc2daef4a13b681e
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-04-12 16:28:00 +1000

    fix(ui): fix layer debug

--- invokeai/frontend/web/src/features/regionalPrompts/util/getLayerBlobs.ts
+++ invokeai/frontend/web/src/features/regionalPrompts/util/getLayerBlobs.ts
@@ -1,26 +1,51 @@
 import { getStore } from 'app/store/nanostores/store';
 import openBase64ImageInTab from 'common/util/openBase64ImageInTab';
 import { blobToDataURL } from 'features/canvas/util/blobToDataURL';
-import { $stage } from 'features/regionalPrompts/store/regionalPromptsSlice';
+import { selectPromptLayerObjectGroup } from 'features/regionalPrompts/components/LayerComponent';
+import { $stage, REGIONAL_PROMPT_LAYER_NAME } from 'features/regionalPrompts/store/regionalPromptsSlice';
+import Konva from 'konva';
 import { assert } from 'tsafe';
 
-export const getLayerBlobs = async () => {
+export const getRegionalPromptLayerBlobs = async (preview: boolean = false): Promise<Record<string, Blob>> => {
   const state = getStore().getState();
   const stage = $stage.get();
   assert(stage !== null, 'Stage is null');
-  const stageLayers = stage.getLayers().filter((l) => l.name() === 'regionalPromptLayer');
-  for (const layer of stageLayers) {
+  const regionalPromptLayers = stage.getLayers().filter((l) => l.name() === REGIONAL_PROMPT_LAYER_NAME);
+
+  // We need to reconstruct each layer to only output the desired data. This logic mirrors the logic in
+  // `getKonvaLayerBbox()` in `invokeai/frontend/web/src/features/regionalPrompts/util/bbox.ts`
+  const offscreenStageContainer = document.createElement('div');
+  const offscreenStage = new Konva.Stage({
+    container: offscreenStageContainer,
+    width: stage.width(),
+    height: stage.height(),
+  });
+
+  const blobs: Record<string, Blob> = {};
+
+  for (const layer of regionalPromptLayers) {
+    const layerClone = layer.clone();
+    for (const child of layerClone.getChildren(selectPromptLayerObjectGroup)) {
+      child.destroy();
+    }
+    offscreenStage.add(layerClone);
     const blob = await new Promise<Blob>((resolve) => {
-      layer.toBlob({
+      offscreenStage.toBlob({
         callback: (blob) => {
           assert(blob, 'Blob is null');
           resolve(blob);
         },
       });
     });
-    const base64 = await blobToDataURL(blob);
-    const prompt = state.regionalPrompts.layers.find((l) => l.id === layer.id())?.prompt;
-    assert(prompt !== undefined, 'Prompt is undefined');
-    openBase64ImageInTab([{ base64, caption: prompt }]);
+    blobs[layer.id()] = blob;
+
+    if (preview) {
+      const base64 = await blobToDataURL(blob);
+      const prompt = state.regionalPrompts.layers.find((l) => l.id === layer.id())?.prompt;
+      openBase64ImageInTab([{ base64, caption: prompt ?? '' }]);
+    }
+    layerClone.destroy();
   }
+
+  return blobs;
 };
