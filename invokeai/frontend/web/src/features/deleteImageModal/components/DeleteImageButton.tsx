f706a13230f925d85add0ab04ecbeec2deaeaf2b
Daniel Abrao <danielpaladar@gmail.com>
2024-04-14 19:51:20 -0300

    Adjust gallery image length handling

--- invokeai/frontend/web/src/features/deleteImageModal/components/DeleteImageButton.tsx
+++ invokeai/frontend/web/src/features/deleteImageModal/components/DeleteImageButton.tsx
@@ -13,11 +13,8 @@ export const DeleteImageButton = memo((props: DeleteImageButtonProps) => {
   const { onClick, isDisabled } = props;
   const { t } = useTranslation();
   const isConnected = useAppSelector((s) => s.system.isConnected);
-  const imageSelectionLength: number = (useAppSelector((s) => s.gallery.selection) || []).length;
-  const labelMessage: string =
-    imageSelectionLength > 1
-      ? t('gallery.deleteImage_plural', { count: imageSelectionLength })
-      : t('gallery.deleteImage');
+  const imageSelectionLength: number = useAppSelector((s) => s.gallery.selection.length);
+  const labelMessage: string = `${t('gallery.deleteImage', { count: imageSelectionLength })} (Del)`;
 
   return (
     <IconButton
