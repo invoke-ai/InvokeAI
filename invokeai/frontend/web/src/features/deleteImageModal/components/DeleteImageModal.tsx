56ac35b739976315272495bc3510ed95d100924f
Daniel Abrao <danielpaladar@gmail.com>
2024-04-14 19:51:05 -0300

    Refactor i18n pluralization

--- invokeai/frontend/web/src/features/deleteImageModal/components/DeleteImageModal.tsx
+++ invokeai/frontend/web/src/features/deleteImageModal/components/DeleteImageModal.tsx
@@ -80,9 +80,7 @@ const DeleteImageModal = () => {
 
   return (
     <ConfirmationAlertDialog
-      title={t(imagesToDelete.length > 1 ? 'gallery.deleteImage_plural' : 'gallery.deleteImage', {
-        count: imagesToDelete.length,
-      })}
+      title={t('gallery.deleteImage', { count: imagesToDelete.length })}
       isOpen={isModalOpen}
       onClose={handleClose}
       cancelButtonText={t('boards.cancel')}
