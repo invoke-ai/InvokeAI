b508945b110ac731ab3f8875c57cef85b16ece65
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-04-15 19:26:29 +1000

    feat(ui): edge labels
    Add setting to render labels with format `Source Node label -> Target Node label` on edges.

--- invokeai/frontend/web/src/features/nodes/components/flow/edges/InvocationDefaultEdge.tsx
+++ invokeai/frontend/web/src/features/nodes/components/flow/edges/InvocationDefaultEdge.tsx
@@ -1,8 +1,9 @@
+import { Flex, Text } from '@invoke-ai/ui-library';
 import { useAppSelector } from 'app/store/storeHooks';
 import type { CSSProperties } from 'react';
 import { memo, useMemo } from 'react';
 import type { EdgeProps } from 'reactflow';
-import { BaseEdge, getBezierPath } from 'reactflow';
+import { BaseEdge, EdgeLabelRenderer, getBezierPath } from 'reactflow';
 
 import { makeEdgeSelector } from './util/makeEdgeSelector';
 
@@ -25,9 +26,10 @@ const InvocationDefaultEdge = ({
     [source, sourceHandleId, target, targetHandleId, selected]
   );
 
-  const { isSelected, shouldAnimate, stroke } = useAppSelector(selector);
+  const { isSelected, shouldAnimate, stroke, label } = useAppSelector(selector);
+  const shouldShowEdgeLabels = useAppSelector((s) => s.nodes.shouldShowEdgeLabels);
 
-  const [edgePath] = getBezierPath({
+  const [edgePath, labelX, labelY] = getBezierPath({
     sourceX,
     sourceY,
     sourcePosition,
@@ -47,7 +49,33 @@ const InvocationDefaultEdge = ({
     [isSelected, shouldAnimate, stroke]
   );
 
-  return <BaseEdge path={edgePath} markerEnd={markerEnd} style={edgeStyles} />;
+  return (
+    <>
+      <BaseEdge path={edgePath} markerEnd={markerEnd} style={edgeStyles} />
+      {label && shouldShowEdgeLabels && (
+        <EdgeLabelRenderer>
+          <Flex
+            className="nodrag nopan"
+            pointerEvents="all"
+            position="absolute"
+            transform={`translate(-50%, -50%) translate(${labelX}px,${labelY}px)`}
+            bg="base.800"
+            borderRadius="base"
+            borderWidth={1}
+            borderColor={isSelected ? 'undefined' : 'transparent'}
+            opacity={isSelected ? 1 : 0.5}
+            py={1}
+            px={3}
+            shadow="md"
+          >
+            <Text size="sm" fontWeight="semibold" color={isSelected ? 'base.100' : 'base.300'}>
+              {label}
+            </Text>
+          </Flex>
+        </EdgeLabelRenderer>
+      )}
+    </>
+  );
 };
 
 export default memo(InvocationDefaultEdge);
--- invokeai/frontend/web/src/features/nodes/components/flow/edges/util/makeEdgeSelector.ts
+++ invokeai/frontend/web/src/features/nodes/components/flow/edges/util/makeEdgeSelector.ts
@@ -1,7 +1,7 @@
 import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
 import { colorTokenToCssVar } from 'common/util/colorTokenToCssVar';
 import { selectNodesSlice } from 'features/nodes/store/nodesSlice';
-import { selectFieldOutputTemplate } from 'features/nodes/store/selectors';
+import { selectFieldOutputTemplate, selectNodeTemplate } from 'features/nodes/store/selectors';
 import { isInvocationNode } from 'features/nodes/types/invocation';
 
 import { getFieldColor } from './getEdgeColor';
@@ -10,6 +10,7 @@ const defaultReturnValue = {
   isSelected: false,
   shouldAnimate: false,
   stroke: colorTokenToCssVar('base.500'),
+  label: '',
 };
 
 export const makeEdgeSelector = (
@@ -19,25 +20,34 @@ export const makeEdgeSelector = (
   targetHandleId: string | null | undefined,
   selected?: boolean
 ) =>
-  createMemoizedSelector(selectNodesSlice, (nodes): { isSelected: boolean; shouldAnimate: boolean; stroke: string } => {
-    const sourceNode = nodes.nodes.find((node) => node.id === source);
-    const targetNode = nodes.nodes.find((node) => node.id === target);
+  createMemoizedSelector(
+    selectNodesSlice,
+    (nodes): { isSelected: boolean; shouldAnimate: boolean; stroke: string; label: string } => {
+      const sourceNode = nodes.nodes.find((node) => node.id === source);
+      const targetNode = nodes.nodes.find((node) => node.id === target);
 
-    const isInvocationToInvocationEdge = isInvocationNode(sourceNode) && isInvocationNode(targetNode);
+      const isInvocationToInvocationEdge = isInvocationNode(sourceNode) && isInvocationNode(targetNode);
 
-    const isSelected = Boolean(sourceNode?.selected || targetNode?.selected || selected);
-    if (!sourceNode || !sourceHandleId) {
-      return defaultReturnValue;
+      const isSelected = Boolean(sourceNode?.selected || targetNode?.selected || selected);
+      if (!sourceNode || !sourceHandleId || !targetNode || !targetHandleId) {
+        return defaultReturnValue;
+      }
+
+      const outputFieldTemplate = selectFieldOutputTemplate(nodes, sourceNode.id, sourceHandleId);
+      const sourceType = isInvocationToInvocationEdge ? outputFieldTemplate?.type : undefined;
+
+      const stroke = sourceType && nodes.shouldColorEdges ? getFieldColor(sourceType) : colorTokenToCssVar('base.500');
+
+      const sourceNodeTemplate = selectNodeTemplate(nodes, sourceNode.id);
+      const targetNodeTemplate = selectNodeTemplate(nodes, targetNode.id);
+
+      const label = `${sourceNodeTemplate?.title || sourceNode.data?.label} -> ${targetNodeTemplate?.title || targetNode.data?.label}`;
+
+      return {
+        isSelected,
+        shouldAnimate: nodes.shouldAnimateEdges && isSelected,
+        stroke,
+        label,
+      };
     }
-
-    const outputFieldTemplate = selectFieldOutputTemplate(nodes, sourceNode.id, sourceHandleId);
-    const sourceType = isInvocationToInvocationEdge ? outputFieldTemplate?.type : undefined;
-
-    const stroke = sourceType && nodes.shouldColorEdges ? getFieldColor(sourceType) : colorTokenToCssVar('base.500');
-
-    return {
-      isSelected,
-      shouldAnimate: nodes.shouldAnimateEdges && isSelected,
-      stroke,
-    };
-  });
+  );
