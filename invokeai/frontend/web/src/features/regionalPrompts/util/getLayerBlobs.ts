0d4923da1c971fa0ef6d7aaa715358be7b2cd636
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-04-17 13:59:18 +1000

    feat(ui): restore invoke button (wip)

--- invokeai/frontend/web/src/features/regionalPrompts/util/getLayerBlobs.ts
+++ invokeai/frontend/web/src/features/regionalPrompts/util/getLayerBlobs.ts
@@ -1,7 +1,8 @@
+import { getStore } from 'app/store/nanostores/store';
 import openBase64ImageInTab from 'common/util/openBase64ImageInTab';
 import { blobToDataURL } from 'features/canvas/util/blobToDataURL';
-import { selectPromptLayerObjectGroup } from 'features/regionalPrompts/components/LayerComponent';
-import { getStage, REGIONAL_PROMPT_LAYER_NAME } from 'features/regionalPrompts/store/regionalPromptsSlice';
+import { renderLayers } from 'features/regionalPrompts/components/imperative/konvaApiDraft';
+import { REGIONAL_PROMPT_LAYER_NAME } from 'features/regionalPrompts/store/regionalPromptsSlice';
 import Konva from 'konva';
 import { assert } from 'tsafe';
 
@@ -15,40 +16,27 @@ export const getRegionalPromptLayerBlobs = async (
   layerIds?: string[],
   preview: boolean = false
 ): Promise<Record<string, Blob>> => {
-  const stage = getStage();
+  const state = getStore().getState();
+  const container = document.createElement('div');
+  const stage = new Konva.Stage({ container, width: state.generation.width, height: state.generation.height });
+  renderLayers(stage, state.regionalPrompts.layers, state.regionalPrompts.selectedLayer);
 
-  // This automatically omits layers that are not rendered. Rendering is controlled by the layer's `isVisible` flag in redux.
-  const regionalPromptLayers = stage.getLayers().filter((l) => {
-    console.log(l.name(), l.id());
-    const isRegionalPromptLayer = l.name() === REGIONAL_PROMPT_LAYER_NAME;
-    const isRequestedLayerId = layerIds ? layerIds.includes(l.id()) : true;
-    return isRegionalPromptLayer && isRequestedLayerId;
-  });
-
-  // We need to reconstruct each layer to only output the desired data. This logic mirrors the logic in
-  // `getKonvaLayerBbox()` in `invokeai/frontend/web/src/features/regionalPrompts/util/bbox.ts`
-  const offscreenStageContainer = document.createElement('div');
-  const offscreenStage = new Konva.Stage({
-    container: offscreenStageContainer,
-    width: stage.width(),
-    height: stage.height(),
-  });
-
+  const layers = stage.find<Konva.Layer>(`.${REGIONAL_PROMPT_LAYER_NAME}`);
   const blobs: Record<string, Blob> = {};
 
-  for (const layer of regionalPromptLayers) {
-    const layerClone = layer.clone();
-    for (const child of layerClone.getChildren()) {
-      if (selectPromptLayerObjectGroup(child)) {
-        child.destroy();
-      } else {
-        // We need to re-cache to handle children with transparency and multiple objects - like prompt region layers.
-        child.cache();
-      }
+  // First remove all layers
+  for (const layer of layers) {
+    layer.remove();
+  }
+
+  // Next render each layer to a blob
+  for (const layer of layers) {
+    if (layerIds && !layerIds.includes(layer.id())) {
+      continue;
     }
-    offscreenStage.add(layerClone);
+    stage.add(layer);
     const blob = await new Promise<Blob>((resolve) => {
-      offscreenStage.toBlob({
+      stage.toBlob({
         callback: (blob) => {
           assert(blob, 'Blob is null');
           resolve(blob);
@@ -60,7 +48,7 @@ export const getRegionalPromptLayerBlobs = async (
       const base64 = await blobToDataURL(blob);
       openBase64ImageInTab([{ base64, caption: layer.id() }]);
     }
-    layerClone.destroy();
+    layer.remove();
     blobs[layer.id()] = blob;
   }
 
