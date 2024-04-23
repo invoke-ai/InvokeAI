229423b370c9d2c157c92adddf5bb4a5e77c46a6
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-04-23 10:50:48 +1000

    tidy(ui): memo aspectratiopreview

--- invokeai/frontend/web/src/features/parameters/components/ImageSize/AspectRatioPreview.tsx
+++ invokeai/frontend/web/src/features/parameters/components/ImageSize/AspectRatioPreview.tsx
@@ -1,10 +1,13 @@
 import { Flex } from '@invoke-ai/ui-library';
 import { StageComponent } from 'features/regionalPrompts/components/StageComponent';
+import { memo } from 'react';
 
-export const AspectRatioPreview = () => {
+export const AspectRatioPreview = memo(() => {
   return (
-    <Flex w="full" h="full" alignItems="center" justifyContent="center">
+    <Flex w="full" h="full" alignItems="center" justifyContent="center" position="relative">
       <StageComponent asPreview />
     </Flex>
   );
-};
+});
+
+AspectRatioPreview.displayName = 'AspectRatioPreview';
