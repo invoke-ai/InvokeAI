9ee7cad613c8e5fd4982095b3ed5d1593fabf729
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-04-30 10:48:35 +1000

    feat(ui): make control layer ui exclusive to txt2img tab

--- invokeai/frontend/web/src/features/ui/components/tabs/TextToImageTab.tsx
+++ invokeai/frontend/web/src/features/ui/components/tabs/TextToImageTab.tsx
@@ -10,7 +10,7 @@ const TextToImageTab = () => {
   const regionalControlTitle = useRegionalControlTitle();
 
   return (
-    <Box position="relative" w="full" h="full" p={2} borderRadius="base">
+    <Box layerStyle="first" position="relative" w="full" h="full" p={2} borderRadius="base">
       <Tabs variant="line" isLazy={true} display="flex" flexDir="column" w="full" h="full">
         <TabList>
           <Tab>{t('common.viewer')}</Tab>
