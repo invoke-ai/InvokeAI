61957418142c24b027b0d1da6046874c3beab9e8
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-04-30 12:39:43 +1000

    feat(ui): move global mask opacity to settings popover

--- invokeai/frontend/web/src/features/regionalPrompts/components/RegionalPromptsToolbar.tsx
+++ invokeai/frontend/web/src/features/regionalPrompts/components/RegionalPromptsToolbar.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable i18next/no-literal-string */
 import { Flex } from '@invoke-ai/ui-library';
 import { BrushSize } from 'features/regionalPrompts/components/BrushSize';
-import { GlobalMaskLayerOpacity } from 'features/regionalPrompts/components/GlobalMaskLayerOpacity';
+import ControlLayersSettingsPopover from 'features/regionalPrompts/components/ControlLayersSettingsPopover';
 import { ToolChooser } from 'features/regionalPrompts/components/ToolChooser';
 import { UndoRedoButtonGroup } from 'features/regionalPrompts/components/UndoRedoButtonGroup';
 import { memo } from 'react';
@@ -10,9 +10,9 @@ export const RegionalPromptsToolbar = memo(() => {
   return (
     <Flex gap={4}>
       <BrushSize />
-      <GlobalMaskLayerOpacity />
-      <UndoRedoButtonGroup />
       <ToolChooser />
+      <UndoRedoButtonGroup />
+      <ControlLayersSettingsPopover />
     </Flex>
   );
 });
