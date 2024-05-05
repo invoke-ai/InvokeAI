fbf5efa44ead8eb83510ee3528f8c8af74ab0355
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-05-05 16:26:30 +1000

    feat(ui): use graph utils in builders (wip)

---
+++ invokeai/frontend/web/src/features/nodes/util/graph/buildGenerationTabGraph2.ts
@@ -0,0 +1,167 @@
+import { logger } from 'app/logging/logger';
+import type { RootState } from 'app/store/store';
+import { fetchModelConfigWithTypeGuard } from 'features/metadata/util/modelFetchingHelpers';
+import { addControlLayersToGraph } from 'features/nodes/util/graph/addControlLayersToGraph';
+import { addInitialImageToGenerationTabGraph } from 'features/nodes/util/graph/addInitialImageToGenerationTabGraph';
+import { addSeamlessToGenerationTabGraph } from 'features/nodes/util/graph/addSeamlessToGenerationTabGraph';
+import { Graph } from 'features/nodes/util/graph/Graph';
+import { getBoardField } from 'features/nodes/util/graph/graphBuilderUtils';
+import { MetadataUtil } from 'features/nodes/util/graph/MetadataUtil';
+import { isNonRefinerMainModelConfig } from 'services/api/types';
+
+import { addHrfToGraph } from './addHrfToGraph';
+import { addLoRAsToGraph } from './addLoRAsToGraph';
+import { addNSFWCheckerToGraph } from './addNSFWCheckerToGraph';
+import { addVAEToGraph } from './addVAEToGraph';
+import { addWatermarkerToGraph } from './addWatermarkerToGraph';
+import {
+  CLIP_SKIP,
+  CONTROL_LAYERS_GRAPH,
+  DENOISE_LATENTS,
+  LATENTS_TO_IMAGE,
+  MAIN_MODEL_LOADER,
+  NEGATIVE_CONDITIONING,
+  NEGATIVE_CONDITIONING_COLLECT,
+  NOISE,
+  POSITIVE_CONDITIONING,
+  POSITIVE_CONDITIONING_COLLECT,
+} from './constants';
+import { getModelMetadataField } from './metadata';
+
+const log = logger('nodes');
+export const buildGenerationTabGraph = async (state: RootState): Promise<Graph> => {
+  const {
+    model,
+    cfgScale: cfg_scale,
+    cfgRescaleMultiplier: cfg_rescale_multiplier,
+    scheduler,
+    steps,
+    clipSkip: skipped_layers,
+    shouldUseCpuNoise,
+    vaePrecision,
+    seamlessXAxis,
+    seamlessYAxis,
+    seed,
+  } = state.generation;
+  const { positivePrompt, negativePrompt } = state.controlLayers.present;
+  const { width, height } = state.controlLayers.present.size;
+
+  if (!model) {
+    log.error('No model found in state');
+    throw new Error('No model found in state');
+  }
+
+  const g = new Graph(CONTROL_LAYERS_GRAPH);
+  const modelLoader = g.addNode({
+    type: 'main_model_loader',
+    id: MAIN_MODEL_LOADER,
+    model,
+  });
+  const clipSkip = g.addNode({
+    type: 'clip_skip',
+    id: CLIP_SKIP,
+    skipped_layers,
+  });
+  const posCond = g.addNode({
+    type: 'compel',
+    id: POSITIVE_CONDITIONING,
+    prompt: positivePrompt,
+  });
+  const posCondCollect = g.addNode({
+    type: 'collect',
+    id: POSITIVE_CONDITIONING_COLLECT,
+  });
+  const negCond = g.addNode({
+    type: 'compel',
+    id: NEGATIVE_CONDITIONING,
+    prompt: negativePrompt,
+  });
+  const negCondCollect = g.addNode({
+    type: 'collect',
+    id: NEGATIVE_CONDITIONING_COLLECT,
+  });
+  const noise = g.addNode({
+    type: 'noise',
+    id: NOISE,
+    seed,
+    width,
+    height,
+    use_cpu: shouldUseCpuNoise,
+  });
+  const denoise = g.addNode({
+    type: 'denoise_latents',
+    id: DENOISE_LATENTS,
+    cfg_scale,
+    cfg_rescale_multiplier,
+    scheduler,
+    steps,
+    denoising_start: 0,
+    denoising_end: 1,
+  });
+  const l2i = g.addNode({
+    type: 'l2i',
+    id: LATENTS_TO_IMAGE,
+    fp32: vaePrecision === 'fp32',
+    board: getBoardField(state),
+    // This is the terminal node and must always save to gallery.
+    is_intermediate: false,
+    use_cache: false,
+  });
+
+  g.addEdge(modelLoader, 'unet', denoise, 'unet');
+  g.addEdge(modelLoader, 'clip', clipSkip, 'clip');
+  g.addEdge(clipSkip, 'clip', posCond, 'clip');
+  g.addEdge(clipSkip, 'clip', negCond, 'clip');
+  g.addEdge(posCond, 'conditioning', posCondCollect, 'item');
+  g.addEdge(negCond, 'conditioning', negCondCollect, 'item');
+  g.addEdge(noise, 'noise', denoise, 'noise');
+  g.addEdge(denoise, 'latents', l2i, 'latents');
+
+  const modelConfig = await fetchModelConfigWithTypeGuard(model.key, isNonRefinerMainModelConfig);
+
+  MetadataUtil.add(g, {
+    generation_mode: 'txt2img',
+    cfg_scale,
+    cfg_rescale_multiplier,
+    height,
+    width,
+    positive_prompt: positivePrompt,
+    negative_prompt: negativePrompt,
+    model: getModelMetadataField(modelConfig),
+    seed,
+    steps,
+    rand_device: shouldUseCpuNoise ? 'cpu' : 'cuda',
+    scheduler,
+    clip_skip: skipped_layers,
+  });
+  MetadataUtil.setMetadataReceivingNode(g, l2i);
+
+  const didAddInitialImage = addInitialImageToGenerationTabGraph(state, g, denoise, noise);
+  const terminalModelLoader = addSeamlessToGenerationTabGraph(state, g, denoise, modelLoader);
+
+  // optionally add custom VAE
+  await addVAEToGraph(state, graph, modelLoaderNodeId);
+
+  // add LoRA support
+  await addLoRAsToGraph(state, graph, DENOISE_LATENTS, modelLoaderNodeId);
+
+  await addControlLayersToGraph(state, graph, DENOISE_LATENTS);
+
+  // High resolution fix.
+  if (state.hrf.hrfEnabled && !didAddInitialImage) {
+    addHrfToGraph(state, graph);
+  }
+
+  // NSFW & watermark - must be last thing added to graph
+  if (state.system.shouldUseNSFWChecker) {
+    // must add before watermarker!
+    addNSFWCheckerToGraph(state, graph);
+  }
+
+  if (state.system.shouldUseWatermarker) {
+    // must add after nsfw checker!
+    addWatermarkerToGraph(state, graph);
+  }
+
+  return graph;
+};
