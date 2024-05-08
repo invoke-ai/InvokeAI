5ca794b94f3ee58ed8592c6e18d7e30e9e097b64
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-05-08 19:41:39 +1000

    feat(ui): show progress toggle on control layers toolbar

--- invokeai/frontend/web/src/features/controlLayers/components/ControlLayersToolbar.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/ControlLayersToolbar.tsx
@@ -4,6 +4,7 @@ import { BrushSize } from 'features/controlLayers/components/BrushSize';
 import ControlLayersSettingsPopover from 'features/controlLayers/components/ControlLayersSettingsPopover';
 import { ToolChooser } from 'features/controlLayers/components/ToolChooser';
 import { UndoRedoButtonGroup } from 'features/controlLayers/components/UndoRedoButtonGroup';
+import { ToggleProgressButton } from 'features/gallery/components/ImageViewer/ToggleProgressButton';
 import { ViewerToggleMenu } from 'features/gallery/components/ImageViewer/ViewerToggleMenu';
 import { memo } from 'react';
 
@@ -11,7 +12,9 @@ export const ControlLayersToolbar = memo(() => {
   return (
     <Flex w="full" gap={2}>
       <Flex flex={1} justifyContent="center">
-        <Flex gap={2} marginInlineEnd="auto" />
+        <Flex gap={2} marginInlineEnd="auto">
+          <ToggleProgressButton />
+        </Flex>
       </Flex>
       <Flex flex={1} gap={2} justifyContent="center">
         <BrushSize />
