b0ed2f355cc3c73ac2eaf884aa7d8d21168f2a69
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-05-14 20:01:55 +1000

    tidy(ui): move testing-only types to test file

--- invokeai/frontend/web/src/features/controlLayers/util/controlAdapters.test.ts
+++ invokeai/frontend/web/src/features/controlLayers/util/controlAdapters.test.ts
@@ -4,20 +4,6 @@ import { assert } from 'tsafe';
 import { describe, test } from 'vitest';
 
 import type {
-  _CannyProcessorConfig,
-  _ColorMapProcessorConfig,
-  _ContentShuffleProcessorConfig,
-  _DepthAnythingProcessorConfig,
-  _DWOpenposeProcessorConfig,
-  _HedProcessorConfig,
-  _LineartAnimeProcessorConfig,
-  _LineartProcessorConfig,
-  _MediapipeFaceProcessorConfig,
-  _MidasDepthProcessorConfig,
-  _MlsdProcessorConfig,
-  _NormalbaeProcessorConfig,
-  _PidiProcessorConfig,
-  _ZoeDepthProcessorConfig,
   CannyProcessorConfig,
   CLIPVisionModelV2,
   ColorMapProcessorConfig,
@@ -75,3 +61,33 @@ describe('Control Adapter Types', () => {
     assert<Equals<_ZoeDepthProcessorConfig, ZoeDepthProcessorConfig>>();
   });
 });
+
+// Types derived from OpenAPI
+type _CannyProcessorConfig = Required<
+  Pick<Invocation<'canny_image_processor'>, 'id' | 'type' | 'low_threshold' | 'high_threshold'>
+>;
+type _ColorMapProcessorConfig = Required<
+  Pick<Invocation<'color_map_image_processor'>, 'id' | 'type' | 'color_map_tile_size'>
+>;
+type _ContentShuffleProcessorConfig = Required<
+  Pick<Invocation<'content_shuffle_image_processor'>, 'id' | 'type' | 'w' | 'h' | 'f'>
+>;
+type _DepthAnythingProcessorConfig = Required<
+  Pick<Invocation<'depth_anything_image_processor'>, 'id' | 'type' | 'model_size'>
+>;
+type _HedProcessorConfig = Required<Pick<Invocation<'hed_image_processor'>, 'id' | 'type' | 'scribble'>>;
+type _LineartAnimeProcessorConfig = Required<Pick<Invocation<'lineart_anime_image_processor'>, 'id' | 'type'>>;
+type _LineartProcessorConfig = Required<Pick<Invocation<'lineart_image_processor'>, 'id' | 'type' | 'coarse'>>;
+type _MediapipeFaceProcessorConfig = Required<
+  Pick<Invocation<'mediapipe_face_processor'>, 'id' | 'type' | 'max_faces' | 'min_confidence'>
+>;
+type _MidasDepthProcessorConfig = Required<
+  Pick<Invocation<'midas_depth_image_processor'>, 'id' | 'type' | 'a_mult' | 'bg_th'>
+>;
+type _MlsdProcessorConfig = Required<Pick<Invocation<'mlsd_image_processor'>, 'id' | 'type' | 'thr_v' | 'thr_d'>>;
+type _NormalbaeProcessorConfig = Required<Pick<Invocation<'normalbae_image_processor'>, 'id' | 'type'>>;
+type _DWOpenposeProcessorConfig = Required<
+  Pick<Invocation<'dw_openpose_image_processor'>, 'id' | 'type' | 'draw_body' | 'draw_face' | 'draw_hands'>
+>;
+type _PidiProcessorConfig = Required<Pick<Invocation<'pidi_image_processor'>, 'id' | 'type' | 'safe' | 'scribble'>>;
+type _ZoeDepthProcessorConfig = Required<Pick<Invocation<'zoe_depth_image_processor'>, 'id' | 'type'>>;
