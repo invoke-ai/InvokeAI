9ff55969639db30961f2ba69d5cc0e8f0f4c4724
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-05-16 22:13:20 +1000

    feat(ui): hide values for connected fields

--- invokeai/frontend/web/src/features/nodes/hooks/useConnectionInputFieldNames.ts
+++ invokeai/frontend/web/src/features/nodes/hooks/useConnectionInputFieldNames.ts
@@ -1,4 +1,7 @@
+import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
+import { useAppSelector } from 'app/store/storeHooks';
 import { useNodeTemplate } from 'features/nodes/hooks/useNodeTemplate';
+import { selectNodesSlice } from 'features/nodes/store/nodesSlice';
 import { getSortedFilteredFieldNames } from 'features/nodes/util/node/getSortedFilteredFieldNames';
 import { TEMPLATE_BUILDER_MAP } from 'features/nodes/util/schema/buildFieldInputTemplate';
 import { keys, map } from 'lodash-es';
@@ -6,15 +9,31 @@ import { useMemo } from 'react';
 
 export const useConnectionInputFieldNames = (nodeId: string): string[] => {
   const template = useNodeTemplate(nodeId);
+  const selectConnectedFieldNames = useMemo(
+    () =>
+      createMemoizedSelector(selectNodesSlice, (nodesSlice) =>
+        nodesSlice.edges
+          .filter((e) => e.target === nodeId)
+          .map((e) => e.targetHandle)
+          .filter(Boolean)
+      ),
+    [nodeId]
+  );
+  const connectedFieldNames = useAppSelector(selectConnectedFieldNames);
+
   const fieldNames = useMemo(() => {
     // get the visible fields
-    const fields = map(template.inputs).filter(
-      (field) =>
+    const fields = map(template.inputs).filter((field) => {
+      if (connectedFieldNames.includes(field.name)) {
+        return true;
+      }
+      return (
         (field.input === 'connection' && !field.type.isCollectionOrScalar) ||
         !keys(TEMPLATE_BUILDER_MAP).includes(field.type.name)
-    );
+      );
+    });
 
     return getSortedFilteredFieldNames(fields);
-  }, [template]);
+  }, [connectedFieldNames, template.inputs]);
   return fieldNames;
 };
