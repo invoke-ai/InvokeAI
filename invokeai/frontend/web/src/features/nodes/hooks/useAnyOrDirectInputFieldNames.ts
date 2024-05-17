6b7b0b3777e1b761cf7ac3e70a2c8863f1fef23c
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-05-18 08:16:51 +1000

    fix(ui): do not rearrange fields when connection/disconnecting

--- invokeai/frontend/web/src/features/nodes/hooks/useAnyOrDirectInputFieldNames.ts
+++ invokeai/frontend/web/src/features/nodes/hooks/useAnyOrDirectInputFieldNames.ts
@@ -1,7 +1,5 @@
-import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
-import { useAppSelector } from 'app/store/storeHooks';
+import { EMPTY_ARRAY } from 'app/store/constants';
 import { useNodeTemplate } from 'features/nodes/hooks/useNodeTemplate';
-import { selectNodesSlice } from 'features/nodes/store/nodesSlice';
 import { getSortedFilteredFieldNames } from 'features/nodes/util/node/getSortedFilteredFieldNames';
 import { TEMPLATE_BUILDER_MAP } from 'features/nodes/util/schema/buildFieldInputTemplate';
 import { keys, map } from 'lodash-es';
@@ -9,31 +7,20 @@ import { useMemo } from 'react';
 
 export const useAnyOrDirectInputFieldNames = (nodeId: string): string[] => {
   const template = useNodeTemplate(nodeId);
-  const selectConnectedFieldNames = useMemo(
-    () =>
-      createMemoizedSelector(selectNodesSlice, (nodesSlice) =>
-        nodesSlice.edges
-          .filter((e) => e.target === nodeId)
-          .map((e) => e.targetHandle)
-          .filter(Boolean)
-      ),
-    [nodeId]
-  );
-  const connectedFieldNames = useAppSelector(selectConnectedFieldNames);
 
   const fieldNames = useMemo(() => {
     const fields = map(template.inputs).filter((field) => {
-      if (connectedFieldNames.includes(field.name)) {
-        return false;
-      }
-
       return (
         (['any', 'direct'].includes(field.input) || field.type.isCollectionOrScalar) &&
         keys(TEMPLATE_BUILDER_MAP).includes(field.type.name)
       );
     });
-    return getSortedFilteredFieldNames(fields);
-  }, [connectedFieldNames, template.inputs]);
+    const _fieldNames = getSortedFilteredFieldNames(fields);
+    if (_fieldNames.length === 0) {
+      return EMPTY_ARRAY;
+    }
+    return _fieldNames;
+  }, [template.inputs]);
 
   return fieldNames;
 };
