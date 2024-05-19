c70392d92d2933a66afc3a450ddb4b1e680b34bd
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-05-19 11:53:05 +1000

    chore(ui): knip

--- invokeai/frontend/web/src/features/nodes/hooks/useFieldType.ts.ts
+++
@@ -1,9 +0,0 @@
-import { useFieldTemplate } from 'features/nodes/hooks/useFieldTemplate';
-import type { FieldType } from 'features/nodes/types/field';
-import { useMemo } from 'react';
-
-export const useFieldType = (nodeId: string, fieldName: string, kind: 'inputs' | 'outputs'): FieldType => {
-  const fieldTemplate = useFieldTemplate(nodeId, fieldName, kind);
-  const fieldType = useMemo(() => fieldTemplate.type, [fieldTemplate]);
-  return fieldType;
-};
