d0c40a8b5b19958c3e2bf32dd35239ddbcd3effc
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-06-05 21:24:26 +1000

    feat(ui): add raster layer rendering and interaction (WIP)

---
+++ invokeai/frontend/web/src/features/controlLayers/components/BrushColorPicker.tsx
@@ -0,0 +1,48 @@
+import { Flex, Popover, PopoverBody, PopoverContent, PopoverTrigger, Tooltip } from '@invoke-ai/ui-library';
+import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
+import IAIColorPicker from 'common/components/IAIColorPicker';
+import { rgbaColorToString } from 'features/canvas/util/colorToString';
+import { brushColorChanged } from 'features/controlLayers/store/controlLayersSlice';
+import type { RgbaColor } from 'features/controlLayers/store/types';
+import { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const BrushColorPicker = memo(() => {
+  const { t } = useTranslation();
+  const brushColor = useAppSelector((s) => s.controlLayers.present.brushColor);
+  const dispatch = useAppDispatch();
+  const onChange = useCallback(
+    (color: RgbaColor) => {
+      dispatch(brushColorChanged(color));
+    },
+    [dispatch]
+  );
+  return (
+    <Popover isLazy>
+      <PopoverTrigger>
+        <span>
+          <Tooltip label={t('controlLayers.brushColor')}>
+            <Flex
+              as="button"
+              aria-label={t('controlLayers.brushColor')}
+              borderRadius="full"
+              borderWidth={1}
+              bg={rgbaColorToString(brushColor)}
+              w={8}
+              h={8}
+              cursor="pointer"
+              tabIndex={-1}
+            />
+          </Tooltip>
+        </span>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverBody minH={64}>
+          <IAIColorPicker color={brushColor} onChange={onChange} withNumberInput />
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  );
+});
+
+BrushColorPicker.displayName = 'BrushColorPicker';
