8e6469d9d7f8651266d547d53578f15e60e17cff
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-06-06 09:26:29 +1000

    feat(ui): raster layer reset, object group util

--- invokeai/frontend/web/src/features/controlLayers/konva/renderers/objects.ts
+++ invokeai/frontend/web/src/features/controlLayers/konva/renderers/objects.ts
@@ -1,8 +1,9 @@
 import { rgbaColorToString } from 'features/canvas/util/colorToString';
-import { RG_LAYER_LINE_NAME, RG_LAYER_RECT_NAME } from 'features/controlLayers/konva/naming';
+import { getObjectGroupId } from 'features/controlLayers/konva/naming';
 import type { BrushLine, EraserLine, RectShape } from 'features/controlLayers/store/types';
 import { DEFAULT_RGBA_COLOR } from 'features/controlLayers/store/types';
 import Konva from 'konva';
+import { v4 as uuidv4 } from 'uuid';
 
 /**
  * Utilities to create various konva objects from layer state. These are used by both the raster and regional guidance
@@ -13,12 +14,13 @@ import Konva from 'konva';
  * Creates a konva line for a brush line.
  * @param brushLine The brush line state
  * @param layerObjectGroup The konva layer's object group to add the line to
+ * @param name The konva name for the line
  */
-export const createBrushLine = (brushLine: BrushLine, layerObjectGroup: Konva.Group): Konva.Line => {
+export const createBrushLine = (brushLine: BrushLine, layerObjectGroup: Konva.Group, name: string): Konva.Line => {
   const konvaLine = new Konva.Line({
     id: brushLine.id,
     key: brushLine.id,
-    name: RG_LAYER_LINE_NAME,
+    name,
     strokeWidth: brushLine.strokeWidth,
     tension: 0,
     lineCap: 'round',
@@ -36,12 +38,13 @@ export const createBrushLine = (brushLine: BrushLine, layerObjectGroup: Konva.Gr
  * Creates a konva line for a eraser line.
  * @param eraserLine The eraser line state
  * @param layerObjectGroup The konva layer's object group to add the line to
+ * @param name The konva name for the line
  */
-export const createEraserLine = (eraserLine: EraserLine, layerObjectGroup: Konva.Group): Konva.Line => {
+export const createEraserLine = (eraserLine: EraserLine, layerObjectGroup: Konva.Group, name: string): Konva.Line => {
   const konvaLine = new Konva.Line({
     id: eraserLine.id,
     key: eraserLine.id,
-    name: RG_LAYER_LINE_NAME,
+    name,
     strokeWidth: eraserLine.strokeWidth,
     tension: 0,
     lineCap: 'round',
@@ -58,13 +61,14 @@ export const createEraserLine = (eraserLine: EraserLine, layerObjectGroup: Konva
 /**
  * Creates a konva rect for a rect shape.
  * @param rectShape The rect shape state
- * @param layerObjectGroup The konva layer's object group to add the line to
+ * @param layerObjectGroup The konva layer's object group to add the rect to
+ * @param name The konva name for the rect
  */
-export const createRectShape = (rectShape: RectShape, layerObjectGroup: Konva.Group): Konva.Rect => {
+export const createRectShape = (rectShape: RectShape, layerObjectGroup: Konva.Group, name: string): Konva.Rect => {
   const konvaRect = new Konva.Rect({
     id: rectShape.id,
     key: rectShape.id,
-    name: RG_LAYER_RECT_NAME,
+    name,
     x: rectShape.x,
     y: rectShape.y,
     width: rectShape.width,
@@ -74,4 +78,20 @@ export const createRectShape = (rectShape: RectShape, layerObjectGroup: Konva.Gr
   });
   layerObjectGroup.add(konvaRect);
   return konvaRect;
+};
+
+/**
+ * Creates a konva group for a layer's objects.
+ * @param konvaLayer The konva layer to add the object group to
+ * @param name The konva name for the group
+ * @returns
+ */
+export const createObjectGroup = (konvaLayer: Konva.Layer, name: string): Konva.Group => {
+  const konvaObjectGroup = new Konva.Group({
+    id: getObjectGroupId(konvaLayer.id(), uuidv4()),
+    name,
+    listening: false,
+  });
+  konvaLayer.add(konvaObjectGroup);
+  return konvaObjectGroup;
 };
