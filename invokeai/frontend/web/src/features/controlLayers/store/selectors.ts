d0cde66e9297d7ef6a4eba4d0d4e78f68a650e27
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-06-15 20:25:07 +1000

    refactor(ui): canvas v2 (wip)
    fix entity count select

--- invokeai/frontend/web/src/features/controlLayers/store/selectors.ts
+++ invokeai/frontend/web/src/features/controlLayers/store/selectors.ts
@@ -1,17 +1,8 @@
 import { createSelector } from '@reduxjs/toolkit';
-import { selectControlAdaptersV2Slice } from 'features/controlLayers/store/controlAdaptersSlice';
-import { selectIPAdaptersSlice } from 'features/controlLayers/store/ipAdaptersSlice';
-import { selectLayersSlice } from 'features/controlLayers/store/layersSlice';
-import { selectRegionalGuidanceSlice } from 'features/controlLayers/store/regionalGuidanceSlice';
+import { selectCanvasSlice } from 'features/canvas/store/canvasSlice';
 
-export const selectEntityCount = createSelector(
-  selectRegionalGuidanceSlice,
-  selectControlAdaptersV2Slice,
-  selectIPAdaptersSlice,
-  selectLayersSlice,
-  (rgState, caState, ipaState, layersState) => {
-    return (
-      rgState.regions.length + caState.controlAdapters.length + ipaState.ipAdapters.length + layersState.layers.length
-    );
-  }
-);
+export const selectEntityCount = createSelector(selectCanvasSlice, (canvasV2) => {
+  return (
+    canvasV2.regions.length + canvasV2.controlAdapters.length + canvasV2.ipAdapters.length + canvasV2.layers.length
+  );
+});
