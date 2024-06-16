0586c6bdf2c2e5d58156bef7f30d0c34de3ede3b
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-06-16 12:45:05 +1000

    refactor(ui): fix more types

--- invokeai/frontend/web/src/services/api/hooks/useSelectedModelConfig.ts
+++ invokeai/frontend/web/src/services/api/hooks/useSelectedModelConfig.ts
@@ -1,13 +1,9 @@
-import { createSelector } from '@reduxjs/toolkit';
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useAppSelector } from 'app/store/storeHooks';
-import { selectGenerationSlice } from 'features/canvas/store/canvasSlice';
 import { useGetModelConfigQuery } from 'services/api/endpoints/models';
 
-const selectModelKey = createSelector(selectGenerationSlice, (generation) => generation.model?.key);
-
 export const useSelectedModelConfig = () => {
-  const key = useAppSelector(selectModelKey);
+  const key = useAppSelector((s) => s.canvasV2.params.model?.key);
   const { currentData: modelConfig } = useGetModelConfigQuery(key ?? skipToken);
 
   return modelConfig;
