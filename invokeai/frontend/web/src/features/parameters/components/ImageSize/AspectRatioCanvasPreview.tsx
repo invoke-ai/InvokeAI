06fa65cbf75556649b2667893f1adfdbfb89f3c4
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-06-18 17:25:39 +1000

    refactor(ui): disable the preview renderer for now

--- invokeai/frontend/web/src/features/parameters/components/ImageSize/AspectRatioCanvasPreview.tsx
+++ invokeai/frontend/web/src/features/parameters/components/ImageSize/AspectRatioCanvasPreview.tsx
@@ -1,21 +1,21 @@
-import { Flex } from '@invoke-ai/ui-library';
 import { useStore } from '@nanostores/react';
-import { StageComponent } from 'features/controlLayers/components/StageComponent';
 import { $isPreviewVisible } from 'features/controlLayers/store/canvasV2Slice';
+import { AspectRatioIconPreview } from 'features/parameters/components/ImageSize/AspectRatioIconPreview';
 import { memo } from 'react';
 
 export const AspectRatioCanvasPreview = memo(() => {
   const isPreviewVisible = useStore($isPreviewVisible);
 
+  return <AspectRatioIconPreview />;
   // if (!isPreviewVisible) {
   //   return <AspectRatioIconPreview />;
   // }
 
-  return (
-    <Flex w="full" h="full" alignItems="center" justifyContent="center" position="relative">
-      <StageComponent asPreview />
-    </Flex>
-  );
+  // return (
+  //   <Flex w="full" h="full" alignItems="center" justifyContent="center" position="relative">
+  //     <StageComponent asPreview />
+  //   </Flex>
+  // );
 });
 
 AspectRatioCanvasPreview.displayName = 'AspectRatioCanvasPreview';
