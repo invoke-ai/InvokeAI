f0b587da277c5223364cd9f18ce6a4e69976660a
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-06-21 18:20:45 +1000

    feat(ui): tweak pagination buttons
    - Fix off-by-one error when going to last page
    - Update component to have minimal/no layout shift

--- invokeai/frontend/web/src/features/gallery/components/ImageGrid/GalleryPagination.tsx
+++ invokeai/frontend/web/src/features/gallery/components/ImageGrid/GalleryPagination.tsx
@@ -1,10 +1,23 @@
-import { Button, Flex, IconButton, Text } from '@invoke-ai/ui-library';
-import { useGalleryPagination } from '../../hooks/useGalleryPagination';
-import { PiCaretLeftBold, PiCaretRightBold } from 'react-icons/pi';
+import { Button, Flex, IconButton, Spacer, Text } from '@invoke-ai/ui-library';
+import { useGalleryPagination } from 'features/gallery/hooks/useGalleryPagination';
+import { PiCaretDoubleLeftBold, PiCaretDoubleRightBold, PiCaretLeftBold, PiCaretRightBold } from 'react-icons/pi';
 
 export const GalleryPagination = () => {
-  const { goPrev, goNext, isPrevEnabled, isNextEnabled, pageButtons, goToPage, currentPage, rangeDisplay, total } =
-    useGalleryPagination();
+  const {
+    goPrev,
+    goNext,
+    goToFirst,
+    goToLast,
+    isFirstEnabled,
+    isLastEnabled,
+    isPrevEnabled,
+    isNextEnabled,
+    pageButtons,
+    goToPage,
+    currentPage,
+    rangeDisplay,
+    total,
+  } = useGalleryPagination();
 
   if (!total) {
     return <Flex flexDir="column" alignItems="center" gap="2" height="48px"></Flex>;
@@ -12,7 +25,14 @@ export const GalleryPagination = () => {
 
   return (
     <Flex flexDir="column" alignItems="center" gap="2" height="48px">
-      <Flex gap={2} alignItems="flex-end">
+      <Flex gap={2} alignItems="center" w="full">
+        <IconButton
+          size="sm"
+          aria-label="prev"
+          icon={<PiCaretDoubleLeftBold />}
+          onClick={goToFirst}
+          isDisabled={!isFirstEnabled}
+        />
         <IconButton
           size="sm"
           aria-label="prev"
@@ -20,26 +40,31 @@ export const GalleryPagination = () => {
           onClick={goPrev}
           isDisabled={!isPrevEnabled}
         />
-        {pageButtons.map((page) =>
-          typeof page === 'number' ? (
-            <Button
-              size="sm"
-              key={page}
-              onClick={goToPage.bind(null, page)}
-              variant={currentPage === page ? 'solid' : 'outline'}
-            >
-              {page + 1}
-            </Button>
-          ) : (
-            <Text fontSize="md">...</Text>
-          )
-        )}
+        <Spacer />
+        {pageButtons.map((page) => (
+          <Button
+            size="sm"
+            key={page}
+            onClick={goToPage.bind(null, page)}
+            variant={currentPage === page ? 'solid' : 'outline'}
+          >
+            {page + 1}
+          </Button>
+        ))}
+        <Spacer />
         <IconButton
           size="sm"
           aria-label="next"
           icon={<PiCaretRightBold />}
           onClick={goNext}
           isDisabled={!isNextEnabled}
+        />
+        <IconButton
+          size="sm"
+          aria-label="next"
+          icon={<PiCaretDoubleRightBold />}
+          onClick={goToLast}
+          isDisabled={!isLastEnabled}
         />
       </Flex>
       <Text>{rangeDisplay} Images</Text>
