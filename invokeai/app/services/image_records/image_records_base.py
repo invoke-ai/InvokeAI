9e774c09366f2c6cdfed541af241ba1bc4bdee88
maryhipp <maryhipp@gmail.com>
2024-06-27 12:21:23 -0400

    feat(api): add order_by and order_dir to list images for sorting

--- invokeai/app/services/image_records/image_records_base.py
+++ invokeai/app/services/image_records/image_records_base.py
@@ -4,8 +4,9 @@ from typing import Optional
 
 from invokeai.app.invocations.fields import MetadataField
 from invokeai.app.services.shared.pagination import OffsetPaginatedResults
+from invokeai.app.services.shared.sqlite.sqlite_common import SQLiteDirection
 
-from .image_records_common import ImageCategory, ImageRecord, ImageRecordChanges, ResourceOrigin
+from .image_records_common import ImageCategory, ImageRecord, ImageRecordChanges, OrderByOptions, ResourceOrigin
 
 
 class ImageRecordStorageBase(ABC):
@@ -37,6 +38,8 @@ class ImageRecordStorageBase(ABC):
         self,
         offset: int = 0,
         limit: int = 10,
+        order_by: OrderByOptions = OrderByOptions.CREATED_AT,
+        order_dir: SQLiteDirection = SQLiteDirection.Descending,
         image_origin: Optional[ResourceOrigin] = None,
         categories: Optional[list[ImageCategory]] = None,
         is_intermediate: Optional[bool] = None,
