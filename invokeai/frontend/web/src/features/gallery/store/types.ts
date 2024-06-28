1ce459198c500e6e65d89a0d3a188256d90e86cd
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-06-28 17:18:16 +1000

    chore(ui): knip

--- invokeai/frontend/web/src/features/gallery/store/types.ts
+++ invokeai/frontend/web/src/features/gallery/store/types.ts
@@ -2,7 +2,6 @@ import type { ImageCategory, ImageDTO } from 'services/api/types';
 
 export const IMAGE_CATEGORIES: ImageCategory[] = ['general'];
 export const ASSETS_CATEGORIES: ImageCategory[] = ['control', 'mask', 'user', 'other'];
-export const IMAGE_LIMIT = 15;
 
 export type GalleryView = 'images' | 'assets';
 export type BoardId = 'none' | (string & Record<never, never>);
