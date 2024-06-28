9ba3182d19aa3ec11562b6ed06818bb456dd5071
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-06-28 21:04:15 +1000

    feat(ui): canvas staging area works

--- invokeai/frontend/web/src/features/controlLayers/konva/renderers/stagingArea.ts
+++ invokeai/frontend/web/src/features/controlLayers/konva/renderers/stagingArea.ts
@@ -1,34 +1,58 @@
-import { KonvaImage } from 'features/controlLayers/konva/renderers/objects';
+import { KonvaImage, KonvaProgressImage } from 'features/controlLayers/konva/renderers/objects';
 import type { CanvasV2State } from 'features/controlLayers/store/types';
 import Konva from 'konva';
+import type { InvocationDenoiseProgressEvent } from 'services/events/types';
 import { assert } from 'tsafe';
 
 export class CanvasStagingArea {
   group: Konva.Group;
   image: KonvaImage | null;
+  progressImage: KonvaProgressImage | null;
 
   constructor() {
     this.group = new Konva.Group({ listening: false });
     this.image = null;
+    this.progressImage = null;
   }
 
-  async render(stagingArea: CanvasV2State['stagingArea'], shouldShowStagedImage: boolean) {
-    if (!stagingArea || stagingArea.selectedImageIndex === null) {
-      if (this.image) {
-        this.image.destroy();
-        this.image = null;
+  async render(
+    stagingArea: CanvasV2State['stagingArea'],
+    shouldShowStagedImage: boolean,
+    lastProgressEvent: InvocationDenoiseProgressEvent | null
+  ) {
+    if (stagingArea && lastProgressEvent) {
+      const { invocation, step, progress_image } = lastProgressEvent;
+      const { dataURL } = progress_image;
+      const { x, y, width, height } = stagingArea.bbox;
+      const progressImageId = `${invocation.id}_${step}`;
+      if (this.progressImage) {
+        if (
+          !this.progressImage.isLoading &&
+          !this.progressImage.isError &&
+          this.progressImage.progressImageId !== progressImageId
+        ) {
+          await this.progressImage.updateImageSource(progressImageId, dataURL, x, y, width, height);
+          this.image?.konvaImageGroup.visible(false);
+          this.progressImage.konvaImageGroup.visible(true);
+        }
+      } else {
+        this.progressImage = new KonvaProgressImage({ id: 'progress-image' });
+        this.group.add(this.progressImage.konvaImageGroup);
+        await this.progressImage.updateImageSource(progressImageId, dataURL, x, y, width, height);
+        this.image?.konvaImageGroup.visible(false);
+        this.progressImage.konvaImageGroup.visible(true);
       }
-      return;
-    }
-
-    if (stagingArea.selectedImageIndex !== null) {
+    } else if (stagingArea && stagingArea.selectedImageIndex !== null) {
       const imageDTO = stagingArea.images[stagingArea.selectedImageIndex];
       assert(imageDTO, 'Image must exist');
       if (this.image) {
         if (!this.image.isLoading && !this.image.isError && this.image.imageName !== imageDTO.image_name) {
           await this.image.updateImageSource(imageDTO.image_name);
         }
+        this.image.konvaImageGroup.x(stagingArea.bbox.x);
+        this.image.konvaImageGroup.y(stagingArea.bbox.y);
         this.image.konvaImageGroup.visible(shouldShowStagedImage);
+        this.progressImage?.konvaImageGroup.visible(false);
       } else {
         const { image_name, width, height } = imageDTO;
         this.image = new KonvaImage({
@@ -50,6 +74,14 @@ export class CanvasStagingArea {
         this.group.add(this.image.konvaImageGroup);
         await this.image.updateImageSource(imageDTO.image_name);
         this.image.konvaImageGroup.visible(shouldShowStagedImage);
+        this.progressImage?.konvaImageGroup.visible(false);
+      }
+    } else {
+      if (this.image) {
+        this.image.konvaImageGroup.visible(false);
+      }
+      if (this.progressImage) {
+        this.progressImage.konvaImageGroup.visible(false);
       }
     }
   }
