78663c6c3af5d95c9fa86fe2056c7b4dbfaf8f58
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-02 12:27:53 +1000

    feat(ui): restore loading spinner in search box
    @maryhipp you were right, after trying loading bars and different placements, this feels like the best place for it.

--- invokeai/frontend/web/src/features/gallery/components/ImageGrid/GallerySearch.tsx
+++ invokeai/frontend/web/src/features/gallery/components/ImageGrid/GallerySearch.tsx
@@ -1,18 +1,23 @@
-import { IconButton, Input, InputGroup, InputRightElement } from '@invoke-ai/ui-library';
+import { IconButton, Input, InputGroup, InputRightElement, Spinner } from '@invoke-ai/ui-library';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
+import { selectListImagesQueryArgs } from 'features/gallery/store/gallerySelectors';
 import { searchTermChanged } from 'features/gallery/store/gallerySlice';
 import { debounce } from 'lodash-es';
 import type { ChangeEvent } from 'react';
 import { useCallback, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiXBold } from 'react-icons/pi';
+import { useListImagesQuery } from 'services/api/endpoints/images';
 
 export const GallerySearch = () => {
   const dispatch = useAppDispatch();
-  const { searchTerm } = useAppSelector((s) => s.gallery);
+  const searchTerm = useAppSelector((s) => s.gallery.searchTerm);
   const { t } = useTranslation();
   const [searchTermInput, setSearchTermInput] = useState(searchTerm);
-
+  const queryArgs = useAppSelector(selectListImagesQueryArgs);
+  const { isPending } = useListImagesQuery(queryArgs, {
+    selectFromResult: ({ isLoading, isFetching }) => ({ isPending: isLoading || isFetching }),
+  });
   const debouncedSetSearchTerm = useMemo(() => {
     return debounce((value: string) => {
       dispatch(searchTermChanged(value));
@@ -40,7 +45,12 @@ export const GallerySearch = () => {
         onChange={handleChangeInput}
         data-testid="image-search-input"
       />
-      {searchTermInput.length && (
+      {isPending && (
+        <InputRightElement h="full" pe={2}>
+          <Spinner size="sm" opacity={0.5} />
+        </InputRightElement>
+      )}
+      {!isPending && searchTermInput.length && (
         <InputRightElement h="full" pe={2}>
           <IconButton
             onClick={handleClearInput}
