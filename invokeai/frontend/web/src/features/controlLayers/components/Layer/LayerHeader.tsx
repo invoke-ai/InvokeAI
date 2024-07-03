00d68ac460b234007e4e8aebbf959bfddf674e84
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-03 16:10:49 +1000

    feat(ui): updated layer list component styling

--- invokeai/frontend/web/src/features/controlLayers/components/Layer/LayerHeader.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/Layer/LayerHeader.tsx
@@ -7,7 +7,7 @@ import { CanvasEntityTitle } from 'features/controlLayers/components/common/Canv
 import { LayerActionsMenu } from 'features/controlLayers/components/Layer/LayerActionsMenu';
 import { layerDeleted, layerIsEnabledToggled } from 'features/controlLayers/store/canvasV2Slice';
 import { selectLayerOrThrow } from 'features/controlLayers/store/layersReducers';
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { LayerOpacity } from './LayerOpacity';
@@ -21,17 +21,21 @@ export const LayerHeader = memo(({ id, onToggleVisibility }: Props) => {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
   const isEnabled = useAppSelector((s) => selectLayerOrThrow(s.canvasV2, id).isEnabled);
+  const objectCount = useAppSelector((s) => selectLayerOrThrow(s.canvasV2, id).objects.length);
   const onToggleIsEnabled = useCallback(() => {
     dispatch(layerIsEnabledToggled({ id }));
   }, [dispatch, id]);
   const onDelete = useCallback(() => {
     dispatch(layerDeleted({ id }));
   }, [dispatch, id]);
+  const title = useMemo(() => {
+    return `${t('controlLayers.layer')} (${t('controlLayers.objects', { count: objectCount })})`;
+  }, [objectCount, t]);
 
   return (
     <CanvasEntityHeader onToggle={onToggleVisibility}>
       <CanvasEntityEnabledToggle isEnabled={isEnabled} onToggle={onToggleIsEnabled} />
-      <CanvasEntityTitle title={t('controlLayers.layer')} />
+      <CanvasEntityTitle title={title} />
       <Spacer />
       <LayerOpacity id={id} />
       <LayerActionsMenu id={id} />
