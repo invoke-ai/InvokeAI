7d96b3e89ed67a73ed3fb4c47373430a7f43d536
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-03 16:18:06 +1000

    feat(ui): tweak layer ui component

--- invokeai/frontend/web/src/features/controlLayers/components/Layer/LayerSettings.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/Layer/LayerSettings.tsx
@@ -1,24 +1,12 @@
-import IAIDroppable from 'common/components/IAIDroppable';
 import { CanvasEntitySettings } from 'features/controlLayers/components/common/CanvasEntitySettings';
-import type { LayerImageDropData } from 'features/dnd/types';
-import { memo, useMemo } from 'react';
+import { memo } from 'react';
 
 type Props = {
   id: string;
 };
 
 export const LayerSettings = memo(({ id }: Props) => {
-  const droppableData = useMemo<LayerImageDropData>(
-    () => ({ id, actionType: 'ADD_LAYER_IMAGE', context: { id } }),
-    [id]
-  );
-
-  return (
-    <CanvasEntitySettings>
-      PLACEHOLDER
-      <IAIDroppable data={droppableData} />
-    </CanvasEntitySettings>
-  );
+  return <CanvasEntitySettings>PLACEHOLDER</CanvasEntitySettings>;
 });
 
 LayerSettings.displayName = 'LayerSettings';
