4a556f84e04ed0d34903ee73c5a5e03ee6726ac8
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-05 17:12:56 +1000

    feat(ui): control adapter image rendering

--- invokeai/frontend/web/src/features/controlLayers/konva/filters.ts
+++ invokeai/frontend/web/src/features/controlLayers/konva/filters.ts
@@ -19,3 +19,7 @@ export const LightnessToAlphaFilter = (imageData: ImageData): void => {
     imageData.data[i * 4 + 3] = (cMin + cMax) / 2;
   }
 };
+
+export const FILTER_MAP = {
+  LightnessToAlphaFilter: LightnessToAlphaFilter,
+} as const;
