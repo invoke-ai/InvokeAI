6014382c7b80170e4eb1258671dec051e484b542
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-09 22:37:41 +1000

    feat(ui): select a board when it is created

--- invokeai/frontend/web/src/features/gallery/components/Boards/BoardsList/AddBoardButton.tsx
+++ invokeai/frontend/web/src/features/gallery/components/Boards/BoardsList/AddBoardButton.tsx
@@ -1,4 +1,6 @@
 import { IconButton } from '@invoke-ai/ui-library';
+import { useAppDispatch } from 'app/store/storeHooks';
+import { boardIdSelected } from 'features/gallery/store/gallerySlice';
 import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiPlusBold } from 'react-icons/pi';
@@ -10,11 +12,17 @@ type Props = {
 
 const AddBoardButton = ({ isPrivateBoard }: Props) => {
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
   const [createBoard, { isLoading }] = useCreateBoardMutation();
   const DEFAULT_BOARD_NAME = t('boards.myBoard');
-  const handleCreateBoard = useCallback(() => {
-    createBoard({ board_name: DEFAULT_BOARD_NAME, is_private: isPrivateBoard });
-  }, [createBoard, DEFAULT_BOARD_NAME, isPrivateBoard]);
+  const handleCreateBoard = useCallback(async () => {
+    try {
+      const board = await createBoard({ board_name: DEFAULT_BOARD_NAME, is_private: isPrivateBoard }).unwrap();
+      dispatch(boardIdSelected({ boardId: board.board_id }));
+    } catch {
+      //no-op
+    }
+  }, [createBoard, DEFAULT_BOARD_NAME, isPrivateBoard, dispatch]);
 
   return (
     <IconButton
