d38d513d23f9ef0f1756c1c907a665cb9fed2083
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-09 22:39:32 +1000

    fix(ui): autoadd badge doesn't flex shrink

--- invokeai/frontend/web/src/features/gallery/components/Boards/AutoAddBadge.tsx
+++ invokeai/frontend/web/src/features/gallery/components/Boards/AutoAddBadge.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 export const AutoAddBadge = memo(() => {
   const { t } = useTranslation();
   return (
-    <Badge color="invokeBlue.400" borderColor="invokeBlue.700" borderWidth={1} bg="transparent">
+    <Badge color="invokeBlue.400" borderColor="invokeBlue.700" borderWidth={1} bg="transparent" flexShrink={0}>
       {t('common.auto')}
     </Badge>
   );
