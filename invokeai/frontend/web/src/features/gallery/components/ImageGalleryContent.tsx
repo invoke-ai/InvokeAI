8fdff33cf8347d5f4c9dc7c0d8629eef7cf84cdc
chainchompa <chainchompa@gmail.com>
2024-07-09 21:41:03 -0400

    update board header styling, toggle board search, resizing gallery panels

--- invokeai/frontend/web/src/features/gallery/components/ImageGalleryContent.tsx
+++ invokeai/frontend/web/src/features/gallery/components/ImageGalleryContent.tsx
@@ -4,11 +4,14 @@ import { useStore } from '@nanostores/react';
 import { $galleryHeader } from 'app/store/nanostores/galleryHeader';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
 import { galleryViewChanged } from 'features/gallery/store/gallerySlice';
-import { memo, useCallback } from 'react';
+import ResizeHandle from 'features/ui/components/tabs/ResizeHandle';
+import { memo, useCallback, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiImagesBold, PiMagnifyingGlassBold } from 'react-icons/pi';
+import { Panel, PanelGroup } from 'react-resizable-panels';
 
 import BoardsList from './Boards/BoardsList/BoardsList';
+import BoardsSearch from './Boards/BoardsList/BoardsSearch';
 import GalleryBoardName from './GalleryBoardName';
 import GallerySettingsPopover from './GallerySettingsPopover/GallerySettingsPopover';
 import GalleryImageGrid from './ImageGrid/GalleryImageGrid';
@@ -41,6 +44,7 @@ const ImageGalleryContent = () => {
   const dispatch = useAppDispatch();
   const galleryHeader = useStore($galleryHeader);
   const searchDisclosure = useDisclosure({ defaultIsOpen: false });
+  const boardSearchDisclosure = useDisclosure({ defaultIsOpen: false });
 
   const handleClickImages = useCallback(() => {
     dispatch(galleryViewChanged('images'));
@@ -50,51 +54,73 @@ const ImageGalleryContent = () => {
     dispatch(galleryViewChanged('assets'));
   }, [dispatch]);
 
+  const panelGroupRef = useRef(null);
+
   return (
     <Flex layerStyle="first" position="relative" flexDirection="column" h="full" w="full" p={2} gap={2}>
-      {galleryHeader}
       <Flex alignItems="center" justifyContent="space-between" gap={2}>
-          <GalleryBoardName />
-          <GallerySettingsPopover />
-        </Flex>
-      <BoardsList />
-      <Flex alignItems="center" justifyContent="space-between" gap={2}>
-        <Tabs isFitted index={galleryView === 'images' ? 0 : 1} variant="enclosed" size="sm" w="full" mb="2">
-          <TabList fontSize="sm" borderBottom="none">
-            <Tab sx={baseStyles} _selected={selectedStyles} onClick={handleClickImages} data-testid="images-tab">
-              <Flex alignItems="center" justifyContent="center" gap="2" w="full">
-                <PiImagesBold size="16px" />
-                {t('parameters.images')}
-              </Flex>
-            </Tab>
-            <Tab sx={baseStyles} _selected={selectedStyles} onClick={handleClickAssets} data-testid="assets-tab">
-              <Flex alignItems="center" justifyContent="center" gap="2" w="full">
-                <PiImagesBold size="16px" />
-                {t('gallery.assets')}
-              </Flex>
-            </Tab>
-            <Tab sx={searchIconStyles} onClick={searchDisclosure.onToggle}>
-            <IconButton
-                tooltip={`${t('gallery.displaySearch')}`}
-                aria-label={t('gallery.displaySearch')}
-                icon={<PiMagnifyingGlassBold />}
-                fontSize={16}
-                textAlign="center"
-                color="base.400"
-                variant="unstyled"
-                minW="unset"
-              />
-            </Tab>
-          </TabList>
-        </Tabs>
+        {galleryHeader}
+        <GalleryBoardName />
+        <GalleryBoardName />
+        <GallerySettingsPopover />
+        <IconButton
+          onClick={boardSearchDisclosure.onToggle}
+          tooltip={`${t('gallery.displayBoardSearch')}`}
+          aria-label={t('gallery.displayBoardSearch')}
+          icon={<PiMagnifyingGlassBold />}
+          size="sm"
+        />
       </Flex>
-      <Box mb="1">
-        <Collapse in={searchDisclosure.isOpen}>
-          <GallerySearch />
-        </Collapse>
-      </Box>
-      <GalleryImageGrid />
-      <GalleryPagination />
+      <PanelGroup ref={panelGroupRef} direction="vertical">
+        <Panel>
+          <Collapse in={boardSearchDisclosure.isOpen} >
+          <Box mt="2">
+            <BoardsSearch />
+          </Box>
+          </Collapse>
+          <BoardsList />
+        </Panel>
+        <ResizeHandle orientation="horizontal" />
+        <Panel>
+          <Flex flexDirection="column" alignItems="center" justifyContent="space-between" gap={2} h="full">
+            <Tabs isFitted index={galleryView === 'images' ? 0 : 1} variant="enclosed" size="sm" w="full" my="2">
+              <TabList fontSize="sm" borderBottom="none">
+                <Tab sx={baseStyles} _selected={selectedStyles} onClick={handleClickImages} data-testid="images-tab">
+                  <Flex alignItems="center" justifyContent="center" gap="2" w="full">
+                    <PiImagesBold size="16px" />
+                    {t('parameters.images')}
+                  </Flex>
+                </Tab>
+                <Tab sx={baseStyles} _selected={selectedStyles} onClick={handleClickAssets} data-testid="assets-tab">
+                  <Flex alignItems="center" justifyContent="center" gap="2" w="full">
+                    <PiImagesBold size="16px" />
+                    {t('gallery.assets')}
+                  </Flex>
+                </Tab>
+                <Tab sx={searchIconStyles} onClick={searchDisclosure.onToggle}>
+                  <IconButton
+                    tooltip={`${t('gallery.displaySearch')}`}
+                    aria-label={t('gallery.displaySearch')}
+                    icon={<PiMagnifyingGlassBold />}
+                    fontSize={16}
+                    textAlign="center"
+                    color="base.400"
+                    variant="unstyled"
+                    minW="unset"
+                  />
+                </Tab>
+              </TabList>
+            </Tabs>
+            <Box mb="2" w="full">
+              <Collapse in={searchDisclosure.isOpen}>
+                <GallerySearch />
+              </Collapse>
+            </Box>
+            <GalleryImageGrid />
+            <GalleryPagination />
+          </Flex>
+        </Panel>
+      </PanelGroup>
     </Flex>
   );
 };
