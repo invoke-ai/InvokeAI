499e4d4fded022eaf2f238192c88f2cc165077c3
Sergey Borisov <stalkek7779@yandex.ru>
2024-07-13 00:45:04 +0300

    Add preview extension to check logic

--- invokeai/backend/stable_diffusion/extensions/__init__.py
+++ invokeai/backend/stable_diffusion/extensions/__init__.py
@@ -3,7 +3,10 @@ Initialization file for the invokeai.backend.stable_diffusion.extensions package
 """
 
 from invokeai.backend.stable_diffusion.extensions.base import ExtensionBase
+from invokeai.backend.stable_diffusion.extensions.preview import PipelineIntermediateState, PreviewExt
 
 __all__ = [
     "ExtensionBase",
+    "PipelineIntermediateState",
+    "PreviewExt",
 ]
