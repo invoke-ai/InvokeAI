e50b4280f21ba6e69a00e3b4d81f89d9ed56f186
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-16 17:03:55 +1000

    feat(ui): rip out document size
    barely knew ye

--- invokeai/frontend/web/src/features/parameters/components/DocumentSize/SwapDimensionsButton.tsx
+++ invokeai/frontend/web/src/features/parameters/components/DocumentSize/SwapDimensionsButton.tsx
@@ -1,6 +1,6 @@
 import { IconButton } from '@invoke-ai/ui-library';
 import { useAppDispatch } from 'app/store/storeHooks';
-import { documentDimensionsSwapped } from 'features/controlLayers/store/canvasV2Slice';
+import { bboxDimensionsSwapped } from 'features/controlLayers/store/canvasV2Slice';
 import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiArrowsDownUpBold } from 'react-icons/pi';
@@ -9,7 +9,7 @@ export const SwapDimensionsButton = memo(() => {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
   const onClick = useCallback(() => {
-    dispatch(documentDimensionsSwapped());
+    dispatch(bboxDimensionsSwapped());
   }, [dispatch]);
   return (
     <IconButton
