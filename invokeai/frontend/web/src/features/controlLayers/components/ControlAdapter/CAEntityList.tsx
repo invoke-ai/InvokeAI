7c3d2f55785c06885ecab25b8d7470daba3487f5
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-17 15:14:10 +1000

    feat(ui): canvas entity list headers

--- invokeai/frontend/web/src/features/controlLayers/components/ControlAdapter/CAEntityList.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/ControlAdapter/CAEntityList.tsx
@@ -1,16 +1,22 @@
-/* eslint-disable i18next/no-literal-string */
+import { Text } from '@invoke-ai/ui-library';
 import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
 import { useAppSelector } from 'app/store/storeHooks';
 import { CA } from 'features/controlLayers/components/ControlAdapter/CA';
 import { mapId } from 'features/controlLayers/konva/util';
 import { selectCanvasV2Slice } from 'features/controlLayers/store/canvasV2Slice';
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const selectEntityIds = createMemoizedSelector(selectCanvasV2Slice, (canvasV2) => {
   return canvasV2.controlAdapters.entities.map(mapId).reverse();
 });
 
 export const CAEntityList = memo(() => {
+  const { t } = useTranslation();
+  const isTypeSelected = useAppSelector((s) =>
+    Boolean(s.canvasV2.selectedEntityIdentifier?.type === 'control_adapter')
+  );
+
   const caIds = useAppSelector(selectEntityIds);
 
   if (caIds.length === 0) {
@@ -20,6 +26,13 @@ export const CAEntityList = memo(() => {
   if (caIds.length > 0) {
     return (
       <>
+        <Text
+          color={isTypeSelected ? 'base.100' : 'base.300'}
+          fontWeight={isTypeSelected ? 'semibold' : 'normal'}
+          userSelect="none"
+        >
+          {t('controlLayers.controlAdapters_withCount', { count: caIds.length })}
+        </Text>
         {caIds.map((id) => (
           <CA key={id} id={id} />
         ))}
