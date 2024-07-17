c70437d12060a03118aaba4b273737e57442b034
Lincoln Stein <lstein@gmail.com>
2024-07-17 15:03:00 -0400

    add node to read watermarks

--- invokeai/app/invocations/image.py
+++ invokeai/app/invocations/image.py
@@ -16,7 +16,7 @@ from invokeai.app.invocations.fields import (
     WithBoard,
     WithMetadata,
 )
-from invokeai.app.invocations.primitives import ImageOutput
+from invokeai.app.invocations.primitives import ImageOutput, StringOutput
 from invokeai.app.services.image_records.image_records_common import ImageCategory
 from invokeai.app.services.shared.invocation_context import InvocationContext
 from invokeai.backend.image_util.invisible_watermark import InvisibleWatermark
@@ -540,6 +540,25 @@ class ImageWatermarkInvocation(BaseInvocation, WithMetadata, WithBoard):
         image_dto = context.images.save(image=new_image)
 
         return ImageOutput.build(image_dto)
+
+
+@invocation(
+    "read_watermark",
+    title="Read Invisible Watermark from Image",
+    tags=["image", "watermark"],
+    category="image",
+    version="1.2.2",
+)
+class ReadWatermarkInvocation(BaseInvocation):
+    """Read an invisible watermark from an image"""
+
+    image: ImageField = InputField(description="The image to read the watermark from")
+    watermark_len: int = InputField(default=8, description="length of watermark, in characters")
+
+    def invoke(self, context: InvocationContext) -> StringOutput:
+        image = context.images.get_pil(self.image.image_name)
+        watermark_text = InvisibleWatermark.read_watermark(image, self.watermark_len * 8)
+        return StringOutput(value=watermark_text)
 
 
 @invocation(
