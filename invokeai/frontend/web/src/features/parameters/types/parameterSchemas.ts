0b554682558ae918d6460a3fcc49aa472ed37067
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-07-22 16:41:31 -0400

    knip fix

--- invokeai/frontend/web/src/features/parameters/types/parameterSchemas.ts
+++ invokeai/frontend/web/src/features/parameters/types/parameterSchemas.ts
@@ -126,11 +126,6 @@ const zParameterT2IAdapterModel = zModelIdentifierField;
 export type ParameterT2IAdapterModel = z.infer<typeof zParameterT2IAdapterModel>;
 // #endregion
 
-// #region VAE Model
-export const zParameterSpandrelImageToImageModel = zModelIdentifierField;
-export type ParameterSpandrelImageToImageModel = z.infer<typeof zParameterSpandrelImageToImageModel>;
-// #endregion
-
 // #region Strength (l2l strength)
 export const zParameterStrength = z.number().min(0).max(1);
 export type ParameterStrength = z.infer<typeof zParameterStrength>;
