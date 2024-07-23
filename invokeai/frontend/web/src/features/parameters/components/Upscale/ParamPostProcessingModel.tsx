9f3115f9b90ac8ed85e3f8277572697af7350fdb
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-24 07:32:33 +1000

    feat(ui): rename ad-hoc upscale stuff to post-processing

--- invokeai/frontend/web/src/features/parameters/components/Upscale/ParamPostProcessingModel.tsx
+++ invokeai/frontend/web/src/features/parameters/components/Upscale/ParamPostProcessingModel.tsx
@@ -1,17 +1,17 @@
 import { Box, Combobox, FormControl, FormLabel, Tooltip } from '@invoke-ai/ui-library';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
 import { useModelCombobox } from 'common/hooks/useModelCombobox';
-import { upscaleModelChanged } from 'features/parameters/store/upscaleSlice';
+import { simpleUpscaleModelChanged } from 'features/parameters/store/upscaleSlice';
 import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSpandrelImageToImageModels } from 'services/api/hooks/modelsByType';
 import type { SpandrelImageToImageModelConfig } from 'services/api/types';
 
-const ParamSpandrelModel = () => {
+const ParamPostProcessingModel = () => {
   const { t } = useTranslation();
   const [modelConfigs, { isLoading }] = useSpandrelImageToImageModels();
 
-  const model = useAppSelector((s) => s.upscale.upscaleModel);
+  const model = useAppSelector((s) => s.upscale.simpleUpscaleModel);
   const dispatch = useAppDispatch();
 
   const tooltipLabel = useMemo(() => {
@@ -23,7 +23,7 @@ const ParamSpandrelModel = () => {
 
   const _onChange = useCallback(
     (v: SpandrelImageToImageModelConfig | null) => {
-      dispatch(upscaleModelChanged(v));
+      dispatch(simpleUpscaleModelChanged(v));
     },
     [dispatch]
   );
@@ -37,7 +37,7 @@ const ParamSpandrelModel = () => {
 
   return (
     <FormControl orientation="vertical">
-      <FormLabel>{t('upscaling.upscaleModel')}</FormLabel>
+      <FormLabel>{t('upscaling.postProcessingModel')}</FormLabel>
       <Tooltip label={tooltipLabel}>
         <Box w="full">
           <Combobox
@@ -54,4 +54,4 @@ const ParamSpandrelModel = () => {
   );
 };
 
-export default memo(ParamSpandrelModel);
+export default memo(ParamPostProcessingModel);
