2e0cebb571128ddcffc4ba7fb38c63d025adf696
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-23 10:19:40 +1000

    fix(ui): bug where viewer would disappear on upscaling tab

--- invokeai/frontend/web/src/features/ui/components/tabs/UpscalingTab.tsx
+++ invokeai/frontend/web/src/features/ui/components/tabs/UpscalingTab.tsx
@@ -1,13 +1,11 @@
 import { Box } from '@invoke-ai/ui-library';
 import { ImageViewer } from 'features/gallery/components/ImageViewer/ImageViewer';
-import { useImageViewer } from 'features/gallery/components/ImageViewer/useImageViewer';
 import { memo } from 'react';
 
 const UpscalingTab = () => {
-  const imageViewer = useImageViewer();
   return (
     <Box layerStyle="first" position="relative" w="full" h="full" p={2} borderRadius="base">
-      {imageViewer.isOpen && <ImageViewer />}
+      <ImageViewer />
     </Box>
   );
 };
