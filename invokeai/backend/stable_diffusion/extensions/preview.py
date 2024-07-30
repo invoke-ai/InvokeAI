4d893c9e4eff8b5e7e9a806dc227bd5961f0bf03
Sergey Borisov <stalkek7779@yandex.ru>
2024-07-30 04:34:34 +0300

    Revert preview test code change

--- invokeai/backend/stable_diffusion/extensions/preview.py
+++ invokeai/backend/stable_diffusion/extensions/preview.py
@@ -1,17 +1,15 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, Callable, Optional
+from typing import TYPE_CHECKING, Callable, Optional
 
 import torch
 
 from invokeai.backend.stable_diffusion.extension_callback_type import ExtensionCallbackType
-from invokeai.backend.stable_diffusion.extension_override_type import ExtensionOverrideType
-from invokeai.backend.stable_diffusion.extensions.base import ExtensionBase, callback, override
+from invokeai.backend.stable_diffusion.extensions.base import ExtensionBase, callback
 
 if TYPE_CHECKING:
     from invokeai.backend.stable_diffusion.denoise_context import DenoiseContext
-    from invokeai.backend.stable_diffusion.extensions_manager import ExtensionsManager
 
 
 # TODO: change event to accept image instead of latents
@@ -43,31 +41,6 @@ class PreviewExt(ExtensionBase):
             )
         )
 
-    # TODO: remove when inpaint PR merged
-    @override(ExtensionOverrideType.STEP)
-    def step(self, orig_func: Callable[..., Any], ctx: DenoiseContext, ext_manager: ExtensionsManager):
-        step_output = orig_func(ctx, ext_manager)
-        if hasattr(step_output, "denoised"):
-            predicted_original = step_output.denoised
-        elif hasattr(step_output, "pred_original_sample"):
-            predicted_original = step_output.pred_original_sample
-        else:
-            predicted_original = step_output.prev_sample
-
-        self.callback(
-            PipelineIntermediateState(
-                step=ctx.step_index,
-                order=ctx.scheduler.order,
-                total_steps=len(ctx.inputs.timesteps),
-                timestep=int(ctx.timestep),  # TODO: is there any code which uses it?
-                latents=step_output.prev_sample,
-                predicted_original=predicted_original,  # TODO: is there any reason for additional field?
-            )
-        )
-
-        return step_output
-
-    """
     # do last so that all other changes shown
     @callback(ExtensionCallbackType.POST_STEP, order=1000)
     def step_preview(self, ctx: DenoiseContext):
@@ -88,4 +61,3 @@ class PreviewExt(ExtensionBase):
                 predicted_original=predicted_original,  # TODO: is there any reason for additional field?
             )
         )
-    """
