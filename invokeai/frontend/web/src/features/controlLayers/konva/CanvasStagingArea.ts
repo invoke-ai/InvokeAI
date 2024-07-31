f59c2cf7f5a76bbb6ea42a76a4a60010d255ef32
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-31 20:04:14 +1000

    feat(ui): revised logging and naming setup, fix staging area

--- invokeai/frontend/web/src/features/controlLayers/konva/CanvasStagingArea.ts
+++ invokeai/frontend/web/src/features/controlLayers/konva/CanvasStagingArea.ts
@@ -1,29 +1,30 @@
+import { CanvasEntity } from 'features/controlLayers/konva/CanvasEntity';
 import { CanvasImage } from 'features/controlLayers/konva/CanvasImage';
 import type { CanvasManager } from 'features/controlLayers/konva/CanvasManager';
 import type { StagingAreaImage } from 'features/controlLayers/store/types';
 import Konva from 'konva';
 
-export class CanvasStagingArea {
+export class CanvasStagingArea extends CanvasEntity {
   static NAME_PREFIX = 'staging-area';
   static GROUP_NAME = `${CanvasStagingArea.NAME_PREFIX}_group`;
 
+  type = 'staging_area';
   konva: { group: Konva.Group };
 
   image: CanvasImage | null;
   selectedImage: StagingAreaImage | null;
-  manager: CanvasManager;
 
   constructor(manager: CanvasManager) {
-    this.manager = manager;
+    super('staging-area', manager);
     this.konva = { group: new Konva.Group({ name: CanvasStagingArea.GROUP_NAME, listening: false }) };
     this.image = null;
     this.selectedImage = null;
   }
 
   async render() {
-    const session = this.manager.stateApi.getSession();
-    const bboxRect = this.manager.stateApi.getBbox().rect;
-    const shouldShowStagedImage = this.manager.stateApi.getShouldShowStagedImage();
+    const session = this._manager.stateApi.getSession();
+    const bboxRect = this._manager.stateApi.getBbox().rect;
+    const shouldShowStagedImage = this._manager.stateApi.getShouldShowStagedImage();
 
     this.selectedImage = session.stagedImages[session.selectedStagedImageIndex] ?? null;
 
@@ -32,34 +33,45 @@ export class CanvasStagingArea {
 
       if (!this.image) {
         const { image_name, width, height } = imageDTO;
-        this.image = new CanvasImage({
-          id: 'staging-area-image',
-          type: 'image',
-          x: 0,
-          y: 0,
-          width,
-          height,
-          filters: [],
-          image: {
-            name: image_name,
+        this.image = new CanvasImage(
+          {
+            id: 'staging-area-image',
+            type: 'image',
+            x: 0,
+            y: 0,
             width,
             height,
+            filters: [],
+            image: {
+              name: image_name,
+              width,
+              height,
+            },
           },
-        });
+          this
+        );
         this.konva.group.add(this.image.konva.group);
       }
 
       if (!this.image.isLoading && !this.image.isError && this.image.imageName !== imageDTO.image_name) {
-        this.image.image?.width(imageDTO.width);
-        this.image.image?.height(imageDTO.height);
+        this.image.konva.image?.width(imageDTO.width);
+        this.image.konva.image?.height(imageDTO.height);
         this.image.konva.group.x(bboxRect.x + offsetX);
         this.image.konva.group.y(bboxRect.y + offsetY);
         await this.image.updateImageSource(imageDTO.image_name);
-        this.manager.stateApi.resetLastProgressEvent();
+        this._manager.stateApi.resetLastProgressEvent();
       }
       this.image.konva.group.visible(shouldShowStagedImage);
     } else {
       this.image?.konva.group.visible(false);
     }
+  }
+
+  repr() {
+    return {
+      id: this.id,
+      type: this.type,
+      selectedImage: this.selectedImage,
+    };
   }
 }
