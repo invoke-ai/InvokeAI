200338ed720ca957033b804044f70d3a65536cc6
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-07-31 20:04:14 +1000

    feat(ui): revised logging and naming setup, fix staging area

--- invokeai/frontend/web/src/features/controlLayers/konva/naming.ts
+++ invokeai/frontend/web/src/features/controlLayers/konva/naming.ts
@@ -6,13 +6,13 @@
 export const getRGId = (entityId: string) => `region_${entityId}`;
 export const getLayerId = (entityId: string) => `layer_${entityId}`;
 export const getBrushLineId = (entityId: string, lineId: string, isBuffer?: boolean) =>
-  `${entityId}.${isBuffer ? 'buffer_' : ''}brush_line_${lineId}`;
+  `${isBuffer ? 'buffer_' : ''}brush_line_${lineId}`;
 export const getEraserLineId = (entityId: string, lineId: string, isBuffer?: boolean) =>
-  `${entityId}.${isBuffer ? 'buffer_' : ''}eraser_line_${lineId}`;
+  `${isBuffer ? 'buffer_' : ''}eraser_line_${lineId}`;
 export const getRectShapeId = (entityId: string, rectId: string, isBuffer?: boolean) =>
-  `${entityId}.${isBuffer ? 'buffer_' : ''}rect_${rectId}`;
-export const getImageObjectId = (entityId: string, imageId: string) => `${entityId}.image_${imageId}`;
-export const getObjectGroupId = (entityId: string, groupId: string) => `${entityId}.objectGroup_${groupId}`;
+  `${isBuffer ? 'buffer_' : ''}rect_${rectId}`;
+export const getImageObjectId = (entityId: string, imageId: string) => `image_${imageId}`;
+export const getObjectGroupId = (entityId: string, groupId: string) => `objectGroup_${groupId}`;
 export const getLayerBboxId = (entityId: string) => `${entityId}.bbox`;
 export const getCAId = (entityId: string) => `control_adapter_${entityId}`;
 export const getIPAId = (entityId: string) => `ip_adapter_${entityId}`;
