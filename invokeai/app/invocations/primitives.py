675ffc2757ed5f3f0c615d8d898136a11c5bd894
Ryan Dick <ryanjdick3@gmail.com>
2024-08-01 14:05:44 -0400

    Remove BoundingBoxInvocation field name overrides.

--- invokeai/app/invocations/primitives.py
+++ invokeai/app/invocations/primitives.py
@@ -498,10 +498,10 @@ class BoundingBoxCollectionOutput(BaseInvocationOutput):
 class BoundingBoxInvocation(BaseInvocation):
     """Create a bounding box manually by supplying box coordinates"""
 
-    x_min: int = InputField(default=0, description="x-coordinate of the bounding box's top left vertex", title="X1")
-    y_min: int = InputField(default=0, description="y-coordinate of the bounding box's top left vertex", title="Y1")
-    x_max: int = InputField(default=0, description="x-coordinate of the bounding box's bottom right vertex", title="X2")
-    y_max: int = InputField(default=0, description="y-coordinate of the bounding box's bottom right vertex", title="Y2")
+    x_min: int = InputField(default=0, description="x-coordinate of the bounding box's top left vertex")
+    y_min: int = InputField(default=0, description="y-coordinate of the bounding box's top left vertex")
+    x_max: int = InputField(default=0, description="x-coordinate of the bounding box's bottom right vertex")
+    y_max: int = InputField(default=0, description="y-coordinate of the bounding box's bottom right vertex")
 
     def invoke(self, context: InvocationContext) -> BoundingBoxOutput:
         bounding_box = BoundingBoxField(x_min=self.x_min, y_min=self.y_min, x_max=self.x_max, y_max=self.y_max)
