c7e71038dd6836fd4c8376cbc639a99c00650a45
Sergey Borisov <stalkek7779@yandex.ru>
2024-08-03 17:04:00 +0300

    Revert "Fix bad generation on slice_size not factor of heads count"
    This reverts commit bf2f798341a40ff58b7df9dc10463237593d2d64.

--- invokeai/frontend/web/scripts/typegen.js
+++ invokeai/frontend/web/scripts/typegen.js
@@ -3,7 +3,7 @@ import fs from 'node:fs';
 
 import openapiTS from 'openapi-typescript';
 
-const OPENAPI_URL = 'http://192.168.5.199:9090/openapi.json';
+const OPENAPI_URL = 'http://127.0.0.1:9090/openapi.json';
 const OUTPUT_FILE = 'src/services/api/schema.ts';
 
 async function generateTypes(schema) {
