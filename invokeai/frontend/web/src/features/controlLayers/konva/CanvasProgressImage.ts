2dde7d8925ad23122ab12bbede77279622cffa56
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-08 17:24:16 +1000

    fix(ui): staging area works

--- invokeai/frontend/web/src/features/controlLayers/konva/CanvasProgressImage.ts
+++ invokeai/frontend/web/src/features/controlLayers/konva/CanvasProgressImage.ts
@@ -1,5 +1,9 @@
-import { loadImage } from 'features/controlLayers/konva/util';
+import { Mutex } from 'async-mutex';
+import type { CanvasManager } from 'features/controlLayers/konva/CanvasManager';
+import type { CanvasPreview } from 'features/controlLayers/konva/CanvasPreview';
+import { getPrefixedId, loadImage } from 'features/controlLayers/konva/util';
 import Konva from 'konva';
+import type { InvocationDenoiseProgressEvent } from 'services/events/types';
 
 export class CanvasProgressImage {
   static NAME_PREFIX = 'progress-image';
@@ -7,53 +11,86 @@ export class CanvasProgressImage {
   static IMAGE_NAME = `${CanvasProgressImage.NAME_PREFIX}_image`;
 
   id: string;
-  progressImageId: string | null;
+  parent: CanvasPreview;
+  manager: CanvasManager;
+
+  /**
+   * A set of subscriptions that should be cleaned up when the transformer is destroyed.
+   */
+  subscriptions: Set<() => void> = new Set();
+
+  progressImageId: string | null = null;
   konva: {
     group: Konva.Group;
     image: Konva.Image | null; // The image is loaded asynchronously, so it may not be available immediately
   };
-  isLoading: boolean;
-  isError: boolean;
+  isLoading: boolean = false;
+  isError: boolean = false;
+  imageElement: HTMLImageElement | null = null;
 
-  constructor(arg: { id: string }) {
-    const { id } = arg;
+  lastProgressEvent: InvocationDenoiseProgressEvent | null = null;
+
+  mutex: Mutex = new Mutex();
+
+  constructor(parent: CanvasPreview) {
+    this.id = getPrefixedId(CanvasProgressImage.NAME_PREFIX);
+    this.parent = parent;
+    this.manager = parent.manager;
     this.konva = {
       group: new Konva.Group({ name: CanvasProgressImage.GROUP_NAME, listening: false }),
       image: null,
     };
-    this.id = id;
-    this.progressImageId = null;
-    this.isLoading = false;
-    this.isError = false;
+
+    this.manager.stateApi.$lastProgressEvent.listen((event) => {
+      this.lastProgressEvent = event;
+      this.render();
+    });
   }
 
-  async updateImageSource(
-    progressImageId: string,
-    dataURL: string,
-    x: number,
-    y: number,
-    width: number,
-    height: number
-  ) {
-    if (this.isLoading) {
+  getNodes = () => {
+    return [this.konva.group];
+  };
+
+  render = async () => {
+    const release = await this.mutex.acquire();
+
+    if (!this.lastProgressEvent) {
+      this.konva.group.visible(false);
+      this.imageElement = null;
+      this.isLoading = false;
+      this.isError = false;
+      release();
       return;
     }
+
+    const { isStaging } = this.manager.stateApi.getSession();
+
+    if (!isStaging) {
+      release();
+      return;
+    }
+
     this.isLoading = true;
+
+    const { x, y } = this.manager.stateApi.getBbox().rect;
+    const { dataURL, width, height } = this.lastProgressEvent.progress_image;
     try {
-      const imageEl = await loadImage(dataURL);
+      this.imageElement = await loadImage(dataURL);
       if (this.konva.image) {
+        console.log('UPDATING PROGRESS IMAGE')
         this.konva.image.setAttrs({
-          image: imageEl,
+          image: this.imageElement,
           x,
           y,
           width,
           height,
         });
       } else {
+        console.log('CREATING NEW PROGRESS IMAGE')
         this.konva.image = new Konva.Image({
           name: CanvasProgressImage.IMAGE_NAME,
           listening: false,
-          image: imageEl,
+          image: this.imageElement,
           x,
           y,
           width,
@@ -61,14 +98,19 @@ export class CanvasProgressImage {
         });
         this.konva.group.add(this.konva.image);
       }
-      this.isLoading = false;
-      this.id = progressImageId;
+      this.konva.group.visible(true);
     } catch {
       this.isError = true;
+    } finally {
+      this.isLoading = false;
+      release();
     }
-  }
+  };
 
-  destroy() {
+  destroy = () => {
+    for (const unsubscribe of this.subscriptions) {
+      unsubscribe();
+    }
     this.konva.group.destroy();
-  }
+  };
 }
