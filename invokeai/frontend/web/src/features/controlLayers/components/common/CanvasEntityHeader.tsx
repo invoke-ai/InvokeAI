500f151d96e59802240209ddcd4e00082e2ec0dd
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-13 15:37:33 +1000

    feat(ui): add contextmenu for canvas entities

--- invokeai/frontend/web/src/features/controlLayers/components/common/CanvasEntityHeader.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/common/CanvasEntityHeader.tsx
@@ -1,12 +1,25 @@
 import type { FlexProps } from '@invoke-ai/ui-library';
-import { Flex } from '@invoke-ai/ui-library';
-import { memo } from 'react';
+import { ContextMenu, Flex, MenuList } from '@invoke-ai/ui-library';
+import { CanvasEntityActionMenuItems } from 'features/controlLayers/components/common/CanvasEntityActionMenuItems';
+import { memo, useCallback } from 'react';
 
 export const CanvasEntityHeader = memo(({ children, ...rest }: FlexProps) => {
+  const renderMenu = useCallback(() => {
+    return (
+      <MenuList>
+        <CanvasEntityActionMenuItems />
+      </MenuList>
+    );
+  }, []);
+
   return (
-    <Flex gap={3} alignItems="center" p={3} cursor="pointer" {...rest}>
-      {children}
-    </Flex>
+    <ContextMenu renderMenu={renderMenu}>
+      {(ref) => (
+        <Flex ref={ref} gap={2} alignItems="center" p={2} role="button" {...rest}>
+          {children}
+        </Flex>
+      )}
+    </ContextMenu>
   );
 });
 
