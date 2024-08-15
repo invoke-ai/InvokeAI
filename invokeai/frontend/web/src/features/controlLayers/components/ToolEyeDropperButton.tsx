31ace5fb0c6b08daa3da6774a8babb9e51186954
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-15 20:41:57 +1000

    feat(ui): rough out eyedropper tool
    It's a bit slow bc we are converting the stage to canvas on every mouse move. Also need to improve the visual but it works.

---
+++ invokeai/frontend/web/src/features/controlLayers/components/ToolEyeDropperButton.tsx
@@ -0,0 +1,34 @@
+import { IconButton } from '@invoke-ai/ui-library';
+import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
+import { toolChanged } from 'features/controlLayers/store/canvasV2Slice';
+import { memo, useCallback } from 'react';
+import { useHotkeys } from 'react-hotkeys-hook';
+import { useTranslation } from 'react-i18next';
+import { PiEyedropperBold } from 'react-icons/pi';
+
+export const ToolEyeDropperButton = memo(() => {
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const isDisabled = useAppSelector((s) => s.canvasV2.session.isStaging || s.canvasV2.tool.isTransforming);
+  const isSelected = useAppSelector((s) => s.canvasV2.tool.selected === 'eyeDropper');
+
+  const onClick = useCallback(() => {
+    dispatch(toolChanged('eyeDropper'));
+  }, [dispatch]);
+
+  useHotkeys('i', onClick, { enabled: !isDisabled }, [onClick, isDisabled]);
+
+  return (
+    <IconButton
+      aria-label={`${t('controlLayers.eyeDropper')} (I)`}
+      tooltip={`${t('controlLayers.eyeDropper')} (I)`}
+      icon={<PiEyedropperBold />}
+      colorScheme={isSelected ? 'invokeBlue' : 'base'}
+      variant="outline"
+      onClick={onClick}
+      isDisabled={isDisabled}
+    />
+  );
+});
+
+ToolEyeDropperButton.displayName = 'ToolEyeDropperButton';
