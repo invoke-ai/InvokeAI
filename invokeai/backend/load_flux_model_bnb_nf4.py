99b0f797845ad090030cca0853fdfb87d334e58e
Ryan Dick <ryanjdick3@gmail.com>
2024-08-15 16:30:47 +0000

    Clean up NF4 implementation.

--- invokeai/backend/load_flux_model_bnb_nf4.py
+++ invokeai/backend/load_flux_model_bnb_nf4.py
@@ -1,4 +1,5 @@
 import time
+from contextlib import contextmanager
 from pathlib import Path
 
 import accelerate
@@ -6,99 +7,87 @@ import torch
 from diffusers.models.transformers.transformer_flux import FluxTransformer2DModel
 from safetensors.torch import load_file, save_file
 
-from invokeai.backend.bnb import quantize_model_nf4
-
-# Docs:
-# https://huggingface.co/docs/accelerate/usage_guides/quantization
-# https://huggingface.co/docs/bitsandbytes/v0.43.3/en/integrations#accelerate
+from invokeai.backend.quantization.bnb_nf4 import quantize_model_nf4
 
 
-def get_parameter_device(parameter: torch.nn.Module):
-    return next(parameter.parameters()).device
-
-
-def load_flux_transformer(path: Path) -> FluxTransformer2DModel:
-    model_config = FluxTransformer2DModel.load_config(path, local_files_only=True)
-    with accelerate.init_empty_weights():
-        empty_model = FluxTransformer2DModel.from_config(model_config)
-    assert isinstance(empty_model, FluxTransformer2DModel)
-
-    model_nf4_path = path / "bnb_nf4"
-    if model_nf4_path.exists():
-        # The quantized model already exists, load it and return it.
-        # Note that the model loading code is the same when loading from quantized vs original weights. The only
-        # difference is the weights_location.
-        # model = load_and_quantize_model(
-        #     empty_model,
-        #     weights_location=model_8bit_path,
-        #     bnb_quantization_config=bnb_quantization_config,
-        #     # device_map="auto",
-        #     device_map={"": "cpu"},
-        # )
-
-        # TODO: Handle the keys that were not quantized (get_keys_to_not_convert).
-        with accelerate.init_empty_weights():
-            model = quantize_model_nf4(empty_model, modules_to_not_convert=set(), compute_dtype=torch.bfloat16)
-
-        model.to_empty(device="cpu")
-        sd = load_file(model_nf4_path / "model.safetensors")
-        model.load_state_dict(sd, strict=True)
-        model = model.to("cuda")
-
-    else:
-        # The quantized model does not exist yet, quantize and save it.
-        # model = load_and_quantize_model(
-        #     empty_model,
-        #     weights_location=path,
-        #     bnb_quantization_config=bnb_quantization_config,
-        #     device_map="auto",
-        # )
-
-        # keys_to_not_convert = get_keys_to_not_convert(empty_model)  # TODO
-
-        # model_8bit_path.mkdir(parents=True, exist_ok=True)
-        # accl = accelerate.Accelerator()
-        # accl.save_model(model, model_8bit_path)
-
-        # ---------------------
-
-        with accelerate.init_empty_weights():
-            model = quantize_model_nf4(empty_model, modules_to_not_convert=set(), compute_dtype=torch.bfloat16)
-
-        # Load sharded state dict.
-        files = list(path.glob("*.safetensors"))
-        state_dict = dict()
-        for file in files:
-            sd = load_file(file)
-            state_dict.update(sd)
-
-        # model.to_empty(device="cpu")
-        # model.to(dtype=torch.float16)
-        model.load_state_dict(state_dict, strict=True, assign=True)
-
-        # Load the state dict into the model. The bitsandbytes layers know how to load from both quantized and
-        # non-quantized state dicts.
-        # model.to_empty(device="cpu")
-        # model.to(dtype=torch.float16)
-        # result = model.load_state_dict(state_dict, strict=True)
-        model = model.to("cuda")
-
-        model_nf4_path.mkdir(parents=True, exist_ok=True)
-        save_file(model.state_dict(), model_nf4_path / "model.safetensors")
-
-        # ---------------------
-
-    assert isinstance(model, FluxTransformer2DModel)
-    return model
+@contextmanager
+def log_time(name: str):
+    """Helper context manager to log the time taken by a block of code."""
+    start = time.time()
+    try:
+        yield None
+    finally:
+        end = time.time()
+        print(f"'{name}' took {end - start:.4f} secs")
 
 
 def main():
-    start = time.time()
-    model = load_flux_transformer(
-        Path("/data/invokeai/models/.download_cache/black-forest-labs_flux.1-schnell/FLUX.1-schnell/transformer/")
+    # Load the FLUX transformer model onto the meta device.
+    model_path = Path(
+        "/data/invokeai/models/.download_cache/black-forest-labs_flux.1-schnell/FLUX.1-schnell/transformer/"
     )
-    print(f"Time to load: {time.time() - start}s")
-    print("hi")
+
+    with log_time("Intialize FLUX transformer on meta device"):
+        model_config = FluxTransformer2DModel.load_config(model_path, local_files_only=True)
+        with accelerate.init_empty_weights():
+            empty_model = FluxTransformer2DModel.from_config(model_config)
+        assert isinstance(empty_model, FluxTransformer2DModel)
+
+    # TODO(ryand): We may want to add some modules to not quantize here (e.g. the proj_out layer). See the accelerate
+    # `get_keys_to_not_convert(...)` function for a heuristic to determine which modules to not quantize.
+    modules_to_not_convert: set[str] = set()
+
+    model_nf4_path = model_path / "bnb_nf4"
+    if model_nf4_path.exists():
+        # The quantized model already exists, load it and return it.
+        print(f"A pre-quantized model already exists at '{model_nf4_path}'. Attempting to load it...")
+
+        # Replace the linear layers with NF4 quantized linear layers (still on the meta device).
+        with log_time("Replace linear layers with NF4 layers"), accelerate.init_empty_weights():
+            model = quantize_model_nf4(
+                empty_model, modules_to_not_convert=modules_to_not_convert, compute_dtype=torch.bfloat16
+            )
+
+        with log_time("Load state dict into model"):
+            sd = load_file(model_nf4_path / "model.safetensors")
+            model.load_state_dict(sd, strict=True, assign=True)
+
+        with log_time("Move model to cuda"):
+            model = model.to("cuda")
+
+        print(f"Successfully loaded pre-quantized model from '{model_nf4_path}'.")
+
+    else:
+        # The quantized model does not exist, quantize the model and save it.
+        print(f"No pre-quantized model found at '{model_nf4_path}'. Quantizing the model...")
+
+        with log_time("Replace linear layers with NF4 layers"), accelerate.init_empty_weights():
+            model = quantize_model_nf4(
+                empty_model, modules_to_not_convert=modules_to_not_convert, compute_dtype=torch.bfloat16
+            )
+
+        with log_time("Load state dict into model"):
+            # Load sharded state dict.
+            files = list(model_path.glob("*.safetensors"))
+            state_dict = dict()
+            for file in files:
+                sd = load_file(file)
+                state_dict.update(sd)
+
+            model.load_state_dict(state_dict, strict=True, assign=True)
+
+        with log_time("Move model to cuda and quantize"):
+            model = model.to("cuda")
+
+        with log_time("Save quantized model"):
+            model_nf4_path.mkdir(parents=True, exist_ok=True)
+            output_path = model_nf4_path / "model.safetensors"
+            save_file(model.state_dict(), output_path)
+
+        print(f"Successfully quantized and saved model to '{output_path}'.")
+
+    assert isinstance(model, FluxTransformer2DModel)
+    return model
 
 
 if __name__ == "__main__":
