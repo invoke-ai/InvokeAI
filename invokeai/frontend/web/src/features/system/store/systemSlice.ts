7bf0e554eacf2d6194b38407e4829a4f8d5701ff
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-17 15:41:34 +1000

    tidy(ui): cleanup after events change

--- invokeai/frontend/web/src/features/system/store/systemSlice.ts
+++ invokeai/frontend/web/src/features/system/store/systemSlice.ts
@@ -2,25 +2,13 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
 import type { PersistConfig, RootState } from 'app/store/store';
 import type { LogLevelName } from 'roarr';
-import {
-  socketConnected,
-  socketDisconnected,
-  socketGeneratorProgress,
-  socketInvocationComplete,
-  socketInvocationStarted,
-  socketModelLoadComplete,
-  socketModelLoadStarted,
-  socketQueueItemStatusChanged,
-} from 'services/events/actions';
 
 import type { Language, SystemState } from './types';
 
 const initialSystemState: SystemState = {
   _version: 1,
-  isConnected: false,
   shouldConfirmOnDelete: true,
   enableImageDebugging: false,
-  denoiseProgress: null,
   shouldAntialiasProgressImage: false,
   consoleLogLevel: 'debug',
   shouldLogToConsole: true,
@@ -28,8 +16,6 @@ const initialSystemState: SystemState = {
   shouldUseNSFWChecker: false,
   shouldUseWatermarker: false,
   shouldEnableInformationalPopovers: true,
-  status: 'DISCONNECTED',
-  cancellations: [],
 };
 
 export const systemSlice = createSlice({
@@ -64,82 +50,6 @@ export const systemSlice = createSlice({
       state.shouldEnableInformationalPopovers = action.payload;
     },
   },
-  extraReducers(builder) {
-    /**
-     * Socket Connected
-     */
-    builder.addCase(socketConnected, (state) => {
-      state.isConnected = true;
-      state.denoiseProgress = null;
-      state.status = 'CONNECTED';
-    });
-
-    /**
-     * Socket Disconnected
-     */
-    builder.addCase(socketDisconnected, (state) => {
-      state.isConnected = false;
-      state.denoiseProgress = null;
-      state.status = 'DISCONNECTED';
-    });
-
-    /**
-     * Invocation Started
-     */
-    builder.addCase(socketInvocationStarted, (state) => {
-      state.cancellations = [];
-      state.denoiseProgress = null;
-      state.status = 'PROCESSING';
-    });
-
-    /**
-     * Generator Progress
-     */
-    builder.addCase(socketGeneratorProgress, (state, action) => {
-      const { step, total_steps, progress_image, session_id, batch_id, percentage } = action.payload.data;
-
-      if (state.cancellations.includes(session_id)) {
-        // Do not update the progress if this session has been cancelled. This prevents a race condition where we get a
-        // progress update after the session has been cancelled.
-        return;
-      }
-
-      state.denoiseProgress = {
-        step,
-        total_steps,
-        percentage,
-        progress_image,
-        session_id,
-        batch_id,
-      };
-
-      state.status = 'PROCESSING';
-    });
-
-    /**
-     * Invocation Complete
-     */
-    builder.addCase(socketInvocationComplete, (state) => {
-      state.denoiseProgress = null;
-      state.status = 'CONNECTED';
-    });
-
-    builder.addCase(socketModelLoadStarted, (state) => {
-      state.status = 'LOADING_MODEL';
-    });
-
-    builder.addCase(socketModelLoadComplete, (state) => {
-      state.status = 'CONNECTED';
-    });
-
-    builder.addCase(socketQueueItemStatusChanged, (state, action) => {
-      if (['completed', 'canceled', 'failed'].includes(action.payload.data.status)) {
-        state.status = 'CONNECTED';
-        state.denoiseProgress = null;
-        state.cancellations.push(action.payload.data.session_id);
-      }
-    });
-  },
 });
 
 export const {
@@ -168,5 +78,5 @@ export const systemPersistConfig: PersistConfig<SystemState> = {
   name: systemSlice.name,
   initialState: initialSystemState,
   migrate: migrateSystemState,
-  persistDenylist: ['isConnected', 'denoiseProgress', 'status', 'cancellations'],
+  persistDenylist: [],
 };
