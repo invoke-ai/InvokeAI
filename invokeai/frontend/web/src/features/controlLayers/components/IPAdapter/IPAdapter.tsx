ab3eb32ec82fbbe27a0aa5f3c88dc22a6dd0563d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-20 12:21:36 +1000

    feat(ui): better editable title

--- invokeai/frontend/web/src/features/controlLayers/components/IPAdapter/IPAdapter.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/IPAdapter/IPAdapter.tsx
@@ -1,11 +1,9 @@
 import { Spacer } from '@invoke-ai/ui-library';
-import { useBoolean } from 'common/hooks/useBoolean';
 import { CanvasEntityContainer } from 'features/controlLayers/components/common/CanvasEntityContainer';
 import { CanvasEntityDeleteButton } from 'features/controlLayers/components/common/CanvasEntityDeleteButton';
 import { CanvasEntityEnabledToggle } from 'features/controlLayers/components/common/CanvasEntityEnabledToggle';
 import { CanvasEntityHeader } from 'features/controlLayers/components/common/CanvasEntityHeader';
-import { CanvasEntityTitle } from 'features/controlLayers/components/common/CanvasEntityTitle';
-import { CanvasEntityTitleEdit } from 'features/controlLayers/components/common/CanvasEntityTitleEdit';
+import { CanvasEntityEditableTitle } from 'features/controlLayers/components/common/CanvasEntityTitleEdit';
 import { IPAdapterSettings } from 'features/controlLayers/components/IPAdapter/IPAdapterSettings';
 import { EntityIdentifierContext } from 'features/controlLayers/contexts/EntityIdentifierContext';
 import type { CanvasEntityIdentifier } from 'features/controlLayers/store/types';
@@ -17,14 +15,13 @@ type Props = {
 
 export const IPAdapter = memo(({ id }: Props) => {
   const entityIdentifier = useMemo<CanvasEntityIdentifier>(() => ({ id, type: 'ip_adapter' }), [id]);
-  const editing = useBoolean(false);
 
   return (
     <EntityIdentifierContext.Provider value={entityIdentifier}>
       <CanvasEntityContainer>
-        <CanvasEntityHeader onDoubleClick={editing.setTrue}>
+        <CanvasEntityHeader>
           <CanvasEntityEnabledToggle />
-          {editing.isTrue ? <CanvasEntityTitleEdit onStopEditing={editing.setFalse} /> : <CanvasEntityTitle />}
+          <CanvasEntityEditableTitle />
           <Spacer />
           <CanvasEntityDeleteButton />
         </CanvasEntityHeader>
