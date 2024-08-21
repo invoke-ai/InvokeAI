77f206abe41593b828bccd7f3f3fbc526d5427d1
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-21 19:01:28 +1000

    feat(ui): add contexts/hooks to access entity adapters directly

--- invokeai/frontend/web/src/features/controlLayers/hooks/useEntityAdapter.ts
+++ invokeai/frontend/web/src/features/controlLayers/hooks/useEntityAdapter.ts
@@ -1,8 +1,18 @@
-import { useStore } from '@nanostores/react';
-import { $canvasManager } from 'features/controlLayers/konva/CanvasManager';
+import { useCanvasManager } from 'features/controlLayers/contexts/CanvasManagerProviderGate';
+import type { CanvasLayerAdapter } from 'features/controlLayers/konva/CanvasLayerAdapter';
+import type { CanvasMaskAdapter } from 'features/controlLayers/konva/CanvasMaskAdapter';
 import type { CanvasEntityIdentifier } from 'features/controlLayers/store/types';
+import { useMemo } from 'react';
+import { assert } from 'tsafe';
 
-export const useEntityAdapter = (entityIdentifier: CanvasEntityIdentifier) => {
-  const canvasManager = useStore($canvasManager);
-  console.log(canvasManager);
+export const useEntityAdapter = (entityIdentifier: CanvasEntityIdentifier): CanvasLayerAdapter | CanvasMaskAdapter => {
+  const canvasManager = useCanvasManager();
+
+  const adapter = useMemo(() => {
+    const entity = canvasManager.stateApi.getEntity(entityIdentifier);
+    assert(entity, 'Entity adapter not found');
+    return entity.adapter;
+  }, [canvasManager, entityIdentifier]);
+
+  return adapter;
 };
