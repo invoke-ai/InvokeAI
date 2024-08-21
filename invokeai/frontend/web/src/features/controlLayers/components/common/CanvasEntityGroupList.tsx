3136d89d5274d0fbab65679414f433721212e00b
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-22 09:26:46 +1000

    feat(ui): add entity group hiding

--- invokeai/frontend/web/src/features/controlLayers/components/common/CanvasEntityGroupList.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/common/CanvasEntityGroupList.tsx
@@ -1,41 +1,25 @@
-import { Flex, Switch, Text } from '@invoke-ai/ui-library';
-import { createSelector } from '@reduxjs/toolkit';
-import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
-import {
-  allEntitiesOfTypeToggled,
-  selectAllEntitiesOfType,
-  selectCanvasV2Slice,
-} from 'features/controlLayers/store/canvasV2Slice';
+import { Flex, Spacer, Text } from '@invoke-ai/ui-library';
+import { CanvasEntityTypeIsHiddenToggle } from 'features/controlLayers/components/common/CanvasEntityTypeIsHiddenToggle';
+import { useEntityTypeTitle } from 'features/controlLayers/hooks/useEntityTypeTitle';
 import type { CanvasEntityIdentifier } from 'features/controlLayers/store/types';
 import type { PropsWithChildren } from 'react';
-import { memo, useCallback, useMemo } from 'react';
+import { memo } from 'react';
 
 type Props = PropsWithChildren<{
-  title: string;
   isSelected: boolean;
   type: CanvasEntityIdentifier['type'];
 }>;
 
-export const CanvasEntityGroupList = memo(({ title, isSelected, type, children }: Props) => {
-  const dispatch = useAppDispatch();
-  const selectAreAllEnabled = useMemo(
-    () =>
-      createSelector(selectCanvasV2Slice, (canvasV2) => {
-        return selectAllEntitiesOfType(canvasV2, type).every((entity) => entity.isEnabled);
-      }),
-    [type]
-  );
-  const areAllEnabled = useAppSelector(selectAreAllEnabled);
-  const onChange = useCallback(() => {
-    dispatch(allEntitiesOfTypeToggled({ type }));
-  }, [dispatch, type]);
+export const CanvasEntityGroupList = memo(({ isSelected, type, children }: Props) => {
+  const title = useEntityTypeTitle(type);
   return (
     <Flex flexDir="column" gap={2}>
-      <Flex justifyContent="space-between" alignItems="center">
+      <Flex justifyContent="space-between" alignItems="center" gap={3}>
         <Text color={isSelected ? 'base.200' : 'base.500'} fontWeight="semibold" userSelect="none">
           {title}
         </Text>
-        <Switch size="sm" isChecked={areAllEnabled} onChange={onChange} pe={1} />
+        <Spacer />
+        {type !== 'ip_adapter' && <CanvasEntityTypeIsHiddenToggle type={type} />}
       </Flex>
       {children}
     </Flex>
