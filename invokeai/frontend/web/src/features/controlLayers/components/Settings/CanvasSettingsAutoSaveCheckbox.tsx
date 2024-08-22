9a442918b5dd6ac93e64e14ffde8c36dabe87d6c
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-22 19:13:16 +1000

    feat(ui): add canvas autosave checkbox

---
+++ invokeai/frontend/web/src/features/controlLayers/components/Settings/CanvasSettingsAutoSaveCheckbox.tsx
@@ -0,0 +1,20 @@
+import { Checkbox, FormControl, FormLabel } from '@invoke-ai/ui-library';
+import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
+import { settingsAutoSaveToggled } from 'features/controlLayers/store/canvasV2Slice';
+import { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const CanvasSettingsAutoSaveCheckbox = memo(() => {
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const autoSave = useAppSelector((s) => s.canvasV2.settings.autoSave);
+  const onChange = useCallback(() => dispatch(settingsAutoSaveToggled()), [dispatch]);
+  return (
+    <FormControl w="full">
+      <FormLabel flexGrow={1}>{t('controlLayers.autoSave')}</FormLabel>
+      <Checkbox isChecked={autoSave} onChange={onChange} />
+    </FormControl>
+  );
+});
+
+CanvasSettingsAutoSaveCheckbox.displayName = 'CanvasSettingsAutoSaveCheckbox';
