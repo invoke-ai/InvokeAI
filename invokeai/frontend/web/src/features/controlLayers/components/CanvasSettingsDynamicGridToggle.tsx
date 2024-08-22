f4a798c64f67bb46f8d3cdb3a1a82ab4599230f6
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-22 17:20:28 +1000

    feat(ui): remove "solid" background option

---
+++ invokeai/frontend/web/src/features/controlLayers/components/CanvasSettingsDynamicGridToggle.tsx
@@ -0,0 +1,25 @@
+import { FormControl, FormLabel, Switch } from '@invoke-ai/ui-library';
+import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
+import { settingsDynamicGridToggled } from 'features/controlLayers/store/canvasV2Slice';
+import { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const CanvasSettingsDynamicGridToggle = memo(() => {
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const dynamicGrid = useAppSelector((s) => s.canvasV2.settings.dynamicGrid);
+  const onChange = useCallback(() => {
+    dispatch(settingsDynamicGridToggled());
+  }, [dispatch]);
+
+  return (
+    <FormControl>
+      <FormLabel m={0} flexGrow={1}>
+        {t('controlLayers.dynamicGrid')}
+      </FormLabel>
+      <Switch size="sm" isChecked={dynamicGrid} onChange={onChange} />
+    </FormControl>
+  );
+});
+
+CanvasSettingsDynamicGridToggle.displayName = 'CanvasSettingsDynamicGridToggle';
