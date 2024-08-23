bb4eb70a4a65ce653099724b306455ce55e73121
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-23 11:27:03 +1000

    tidy(ui): remove unused stuff

--- invokeai/frontend/web/src/features/nodes/util/graph/generation/addLayers.ts
+++
@@ -1,5 +0,0 @@
-import type { CanvasRasterLayerState } from 'features/controlLayers/store/types';
-
-export const isValidLayer = (layer: CanvasRasterLayerState) => {
-  return layer.isEnabled && layer.objects.length > 0;
-};
