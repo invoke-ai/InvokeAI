f81c87b685bebfce347a8e5f63105c1dcccc319b
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-23 11:34:13 +1000

    tidy(ui): remove unused stuff 2

--- invokeai/frontend/web/src/common/util/roundDownToMultiple.ts
+++ invokeai/frontend/web/src/common/util/roundDownToMultiple.ts
@@ -1,9 +1,6 @@
 export const roundDownToMultiple = (num: number, multiple: number): number => {
   return Math.floor(num / multiple) * multiple;
 };
-export const roundDownToMultipleMin = (num: number, multiple: number): number => {
-  return Math.max(multiple, Math.floor(num / multiple) * multiple);
-};
 
 export const roundToMultiple = (num: number, multiple: number): number => {
   return Math.round(num / multiple) * multiple;
