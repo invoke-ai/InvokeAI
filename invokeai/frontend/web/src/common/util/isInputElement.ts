a807957967972238ad768dc2ae112383d8857fb2
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-23 11:27:03 +1000

    tidy(ui): remove unused stuff

--- invokeai/frontend/web/src/common/util/isInputElement.ts
+++
@@ -1,7 +0,0 @@
-export const isInputElement = (el: HTMLElement) => {
-  return (
-    el.tagName.toLowerCase() === 'input' ||
-    el.tagName.toLowerCase() === 'textarea' ||
-    el.tagName.toLowerCase() === 'select'
-  );
-};
