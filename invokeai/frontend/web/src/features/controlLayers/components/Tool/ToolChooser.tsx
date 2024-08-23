5ea3c11883a4657af614e99fdd96d404d44dced3
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-23 20:32:49 +1000

    feat(ui): disable most interaction while filtering

--- invokeai/frontend/web/src/features/controlLayers/components/Tool/ToolChooser.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/Tool/ToolChooser.tsx
@@ -1,7 +1,7 @@
 import { ButtonGroup } from '@invoke-ai/ui-library';
 import { ToolBboxButton } from 'features/controlLayers/components/Tool/ToolBboxButton';
 import { ToolBrushButton } from 'features/controlLayers/components/Tool/ToolBrushButton';
-import { ToolColorPickerButton } from 'features/controlLayers/components/Tool/ToolEyeDropperButton';
+import { ToolColorPickerButton } from 'features/controlLayers/components/Tool/ToolColorPickerButton';
 import { ToolMoveButton } from 'features/controlLayers/components/Tool/ToolMoveButton';
 import { ToolRectButton } from 'features/controlLayers/components/Tool/ToolRectButton';
 import { useCanvasDeleteLayerHotkey } from 'features/controlLayers/hooks/useCanvasDeleteLayerHotkey';
