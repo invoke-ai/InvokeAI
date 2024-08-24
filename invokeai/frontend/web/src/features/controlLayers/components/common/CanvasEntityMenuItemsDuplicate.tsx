86a5e2538c0a42f315c12ea44682010b45e77cba
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-24 12:20:35 +1000

    feat(ui): duplicate entity

---
+++ invokeai/frontend/web/src/features/controlLayers/components/common/CanvasEntityMenuItemsDuplicate.tsx
@@ -0,0 +1,25 @@
+import { MenuItem } from '@invoke-ai/ui-library';
+import { useAppDispatch } from 'app/store/storeHooks';
+import { useEntityIdentifierContext } from 'features/controlLayers/contexts/EntityIdentifierContext';
+import { entityDuplicated } from 'features/controlLayers/store/canvasV2Slice';
+import { memo, useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { PiCopyFill } from 'react-icons/pi';
+
+export const CanvasEntityMenuItemsDuplicate = memo(() => {
+  const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const entityIdentifier = useEntityIdentifierContext();
+
+  const onClick = useCallback(() => {
+    dispatch(entityDuplicated({ entityIdentifier }));
+  }, [dispatch, entityIdentifier]);
+
+  return (
+    <MenuItem onClick={onClick} icon={<PiCopyFill />}>
+      {t('controlLayers.duplicate')}
+    </MenuItem>
+  );
+});
+
+CanvasEntityMenuItemsDuplicate.displayName = 'CanvasEntityMenuItemsDuplicate';
