3876f71ff4715b9285e8b185d49d20cbf1526ead
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-28 08:25:37 +1000

    feat(ui): tweak enabled/locked toggle styles

--- invokeai/frontend/web/src/features/controlLayers/components/common/CanvasEntityEnabledToggle.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/common/CanvasEntityEnabledToggle.tsx
@@ -1,34 +1,28 @@
 import { IconButton } from '@invoke-ai/ui-library';
 import { useAppDispatch } from 'app/store/storeHooks';
-import { useBoolean } from 'common/hooks/useBoolean';
 import { useEntityIdentifierContext } from 'features/controlLayers/contexts/EntityIdentifierContext';
 import { useEntityIsEnabled } from 'features/controlLayers/hooks/useEntityIsEnabled';
 import { entityIsEnabledToggled } from 'features/controlLayers/store/canvasSlice';
-import { memo, useCallback, useRef } from 'react';
+import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import { PiCheckBold } from 'react-icons/pi';
+import { PiCircleBold, PiCircleFill } from 'react-icons/pi';
 
 export const CanvasEntityEnabledToggle = memo(() => {
   const { t } = useTranslation();
   const entityIdentifier = useEntityIdentifierContext();
-  const ref = useRef<HTMLButtonElement>(null);
   const isEnabled = useEntityIsEnabled(entityIdentifier);
   const dispatch = useAppDispatch();
   const onClick = useCallback(() => {
     dispatch(entityIsEnabledToggled({ entityIdentifier }));
   }, [dispatch, entityIdentifier]);
-  const isHovered = useBoolean(false);
 
   return (
     <IconButton
-      ref={ref}
       size="sm"
-      onMouseOver={isHovered.setTrue}
-      onMouseOut={isHovered.setFalse}
       aria-label={t(isEnabled ? 'common.enabled' : 'common.disabled')}
       tooltip={t(isEnabled ? 'common.enabled' : 'common.disabled')}
       variant="ghost"
-      icon={isEnabled || isHovered.isTrue ? <PiCheckBold /> : undefined}
+      icon={isEnabled ? <PiCircleFill /> : <PiCircleBold />}
       onClick={onClick}
     />
   );
