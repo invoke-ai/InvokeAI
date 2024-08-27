3001718f9f2f939ec2a50f74323a14cec329f4d3
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-27 19:56:57 +1000

    feat(ui): generalize mask fill, add to action bar

--- invokeai/frontend/web/src/features/controlLayers/components/CanvasEntityList/EntityListActionBar.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/CanvasEntityList/EntityListActionBar.tsx
@@ -1,14 +1,16 @@
 import { Flex, Spacer } from '@invoke-ai/ui-library';
 import { EntityListActionBarAddLayerButton } from 'features/controlLayers/components/CanvasEntityList/EntityListActionBarAddLayerMenuButton';
 import { EntityListActionBarDeleteButton } from 'features/controlLayers/components/CanvasEntityList/EntityListActionBarDeleteButton';
+import { EntityListActionBarSelectedEntityFill } from 'features/controlLayers/components/CanvasEntityList/EntityListActionBarSelectedEntityFill';
 import { SelectedEntityOpacity } from 'features/controlLayers/components/CanvasEntityList/EntityListActionBarSelectedEntityOpacity';
 import { memo } from 'react';
 
 export const EntityListActionBar = memo(() => {
   return (
-    <Flex w="full" py={1} px={1} gap={2}>
+    <Flex w="full" py={1} px={1} gap={2} alignItems="center">
       <SelectedEntityOpacity />
       <Spacer />
+      <EntityListActionBarSelectedEntityFill />
       <EntityListActionBarAddLayerButton />
       <EntityListActionBarDeleteButton />
     </Flex>
