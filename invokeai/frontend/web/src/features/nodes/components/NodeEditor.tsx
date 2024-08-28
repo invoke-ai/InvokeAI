6a62854e7dc1ef3836ffd69135a40c9e62e5d0fc
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-28 21:32:10 +1000

    feat(ui): migrate add node popover to cmdk
    Put this together as a way to figure out the library before moving on to the full app cmdk. Works great.

--- invokeai/frontend/web/src/features/nodes/components/NodeEditor.tsx
+++ invokeai/frontend/web/src/features/nodes/components/NodeEditor.tsx
@@ -2,6 +2,7 @@ import 'reactflow/dist/style.css';
 
 import { Flex } from '@invoke-ai/ui-library';
 import { IAINoContentFallback } from 'common/components/IAIImageFallback';
+import { AddNodeCmdk } from 'features/nodes/components/flow/AddNodeCmdk/AddNodeCmdk';
 import TopPanel from 'features/nodes/components/flow/panels/TopPanel/TopPanel';
 import { LoadWorkflowFromGraphModal } from 'features/workflowLibrary/components/LoadWorkflowFromGraphModal/LoadWorkflowFromGraphModal';
 import { SaveWorkflowAsDialog } from 'features/workflowLibrary/components/SaveWorkflowAsDialog/SaveWorkflowAsDialog';
@@ -10,7 +11,6 @@ import { useTranslation } from 'react-i18next';
 import { MdDeviceHub } from 'react-icons/md';
 import { useGetOpenAPISchemaQuery } from 'services/api/endpoints/appInfo';
 
-import AddNodePopover from './flow/AddNodePopover/AddNodePopover';
 import { Flow } from './flow/Flow';
 import BottomLeftPanel from './flow/panels/BottomLeftPanel/BottomLeftPanel';
 import MinimapPanel from './flow/panels/MinimapPanel/MinimapPanel';
@@ -31,7 +31,7 @@ const NodeEditor = () => {
       {data && (
         <>
           <Flow />
-          <AddNodePopover />
+          <AddNodeCmdk />
           <TopPanel />
           <BottomLeftPanel />
           <MinimapPanel />
