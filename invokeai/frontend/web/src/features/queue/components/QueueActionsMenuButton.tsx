04555f39166e8a0a7992e2f588ad90e456e6d6e1
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-08-28 16:37:50 +1000

    feat(ui): fix queue item count badge positioning
    Previously this badge, floating over the queue menu button next to the invoke button, was rendered within the existing layout. When I initially positioned it, the app layout interfered - it would extend into an area reserved for a flex gap, which cut off the badge.
    
    As a (bad) workaround, I had shifted the whole app down a few pixels to make room for it. What I should have done is what I've done in this commit - render the badge in a portal to take it out of the layout so we don't need that extra vertical padding.
    
    Sleekified some styling a bit too.

--- invokeai/frontend/web/src/features/queue/components/QueueActionsMenuButton.tsx
+++ invokeai/frontend/web/src/features/queue/components/QueueActionsMenuButton.tsx
@@ -7,16 +7,18 @@ import {
   MenuDivider,
   MenuItem,
   MenuList,
+  Portal,
   useDisclosure,
 } from '@invoke-ai/ui-library';
 import { useAppDispatch } from 'app/store/storeHooks';
+import type { Coordinate } from 'features/controlLayers/store/types';
 import { useClearQueueConfirmationAlertDialog } from 'features/queue/components/ClearQueueConfirmationAlertDialog';
 import { useClearQueue } from 'features/queue/hooks/useClearQueue';
 import { usePauseProcessor } from 'features/queue/hooks/usePauseProcessor';
 import { useResumeProcessor } from 'features/queue/hooks/useResumeProcessor';
 import { useFeatureStatus } from 'features/system/hooks/useFeatureStatus';
 import { setActiveTab } from 'features/ui/store/uiSlice';
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiPauseFill, PiPlayFill, PiTrashSimpleBold } from 'react-icons/pi';
 import { RiListCheck, RiPlayList2Fill } from 'react-icons/ri';
@@ -26,6 +28,8 @@ export const QueueActionsMenuButton = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
+  const [badgePos, setBadgePos] = useState<Coordinate | null>(null);
+  const menuButtonRef = useRef<HTMLButtonElement>(null);
   const dialogState = useClearQueueConfirmationAlertDialog();
   const isPauseEnabled = useFeatureStatus('pauseQueue');
   const isResumeEnabled = useFeatureStatus('resumeQueue');
@@ -49,10 +53,17 @@ export const QueueActionsMenuButton = memo(() => {
     dispatch(setActiveTab('queue'));
   }, [dispatch]);
 
+  useEffect(() => {
+    if (menuButtonRef.current) {
+      const { x, y } = menuButtonRef.current.getBoundingClientRect();
+      setBadgePos({ x: x - 10, y: y - 10 });
+    }
+  }, []);
+
   return (
     <Box pos="relative">
       <Menu isOpen={isOpen} onOpen={onOpen} onClose={onClose} placement="bottom-end">
-        <MenuButton as={IconButton} aria-label="Queue Actions Menu" icon={<RiListCheck />} />
+        <MenuButton ref={menuButtonRef} as={IconButton} aria-label="Queue Actions Menu" icon={<RiListCheck />} />
         <MenuList>
           <MenuItem
             isDestructive
@@ -89,10 +100,18 @@ export const QueueActionsMenuButton = memo(() => {
           </MenuItem>
         </MenuList>
       </Menu>
-      {queueSize > 0 && (
-        <Badge pos="absolute" insetInlineStart={-3} insetBlockStart={-1.5} colorScheme="invokeYellow" zIndex="docked">
-          {queueSize}
-        </Badge>
+      {queueSize > 0 && badgePos !== null && (
+        <Portal>
+          <Badge
+            pos="absolute"
+            insetInlineStart={badgePos.x}
+            insetBlockStart={badgePos.y}
+            colorScheme="invokeYellow"
+            zIndex="docked"
+          >
+            {queueSize}
+          </Badge>
+        </Portal>
       )}
     </Box>
   );
