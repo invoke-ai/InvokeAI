f4b654d37c9921617ece722b1bc0cbfd0bd73570
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-09-01 19:19:02 +1000

    tidy(ui): canvas hotkey hooks

--- invokeai/frontend/web/src/features/controlLayers/components/Toolbar/CanvasToolbar.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/Toolbar/CanvasToolbar.tsx
@@ -8,16 +8,20 @@ import { CanvasToolbarResetViewButton } from 'features/controlLayers/components/
 import { CanvasToolbarSaveToGalleryButton } from 'features/controlLayers/components/Toolbar/CanvasToolbarSaveToGalleryButton';
 import { CanvasToolbarScale } from 'features/controlLayers/components/Toolbar/CanvasToolbarScale';
 import { CanvasManagerProviderGate } from 'features/controlLayers/contexts/CanvasManagerProviderGate';
-import { useCanvasEntityQuickSwitch } from 'features/controlLayers/hooks/useCanvasEntityQuickSwitch';
-import { useCanvasUndoRedo } from 'features/controlLayers/hooks/useCanvasUndoRedo';
+import { useCanvasDeleteLayerHotkey } from 'features/controlLayers/hooks/useCanvasDeleteLayerHotkey';
+import { useCanvasEntityQuickSwitchHotkey } from 'features/controlLayers/hooks/useCanvasEntityQuickSwitchHotkey';
+import { useCanvasResetLayerHotkey } from 'features/controlLayers/hooks/useCanvasResetLayerHotkey';
+import { useCanvasUndoRedoHotkeys } from 'features/controlLayers/hooks/useCanvasUndoRedoHotkeys';
 import { useNextPrevEntityHotkeys } from 'features/controlLayers/hooks/useNextPrevEntity';
 import { ToggleProgressButton } from 'features/gallery/components/ImageViewer/ToggleProgressButton';
 import { ViewerToggle } from 'features/gallery/components/ImageViewer/ViewerToggleMenu';
 import { memo } from 'react';
 
 export const CanvasToolbar = memo(() => {
-  useCanvasUndoRedo();
-  useCanvasEntityQuickSwitch();
+  useCanvasResetLayerHotkey();
+  useCanvasDeleteLayerHotkey();
+  useCanvasUndoRedoHotkeys();
+  useCanvasEntityQuickSwitchHotkey();
   useNextPrevEntityHotkeys();
 
   return (
