2538b348c776da893d916d2660a594fa35b02eeb
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-09-01 19:19:02 +1000

    tidy(ui): canvas hotkey hooks

--- invokeai/frontend/web/src/features/controlLayers/components/Tool/ToolChooser.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/Tool/ToolChooser.tsx
@@ -4,16 +4,11 @@ import { ToolBrushButton } from 'features/controlLayers/components/Tool/ToolBrus
 import { ToolColorPickerButton } from 'features/controlLayers/components/Tool/ToolColorPickerButton';
 import { ToolMoveButton } from 'features/controlLayers/components/Tool/ToolMoveButton';
 import { ToolRectButton } from 'features/controlLayers/components/Tool/ToolRectButton';
-import { useCanvasDeleteLayerHotkey } from 'features/controlLayers/hooks/useCanvasDeleteLayerHotkey';
-import { useCanvasResetLayerHotkey } from 'features/controlLayers/hooks/useCanvasResetLayerHotkey';
 
 import { ToolEraserButton } from './ToolEraserButton';
 import { ToolViewButton } from './ToolViewButton';
 
 export const ToolChooser: React.FC = () => {
-  useCanvasResetLayerHotkey();
-  useCanvasDeleteLayerHotkey();
-
   return (
     <>
       <ButtonGroup isAttached>
