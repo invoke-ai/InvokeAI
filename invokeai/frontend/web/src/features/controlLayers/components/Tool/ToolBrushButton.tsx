334709425445fc1cf9323b05c3ed664804eea0b9
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-09-03 11:44:16 +1000

    feat(ui): tool buttons are only disabled when currently selected

--- invokeai/frontend/web/src/features/controlLayers/components/Tool/ToolBrushButton.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/Tool/ToolBrushButton.tsx
@@ -1,29 +1,16 @@
 import { IconButton } from '@invoke-ai/ui-library';
-import { useAppSelector } from 'app/store/storeHooks';
 import { useSelectTool, useToolIsSelected } from 'features/controlLayers/components/Tool/hooks';
-import { useIsFiltering } from 'features/controlLayers/hooks/useIsFiltering';
-import { useIsTransforming } from 'features/controlLayers/hooks/useIsTransforming';
-import { selectIsStaging } from 'features/controlLayers/store/canvasSessionSlice';
-import { selectIsSelectedEntityDrawable } from 'features/controlLayers/store/selectors';
-import { memo, useMemo } from 'react';
+import { memo } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { useTranslation } from 'react-i18next';
 import { PiPaintBrushBold } from 'react-icons/pi';
 
 export const ToolBrushButton = memo(() => {
   const { t } = useTranslation();
-  const isFiltering = useIsFiltering();
-  const isTransforming = useIsTransforming();
-  const isStaging = useAppSelector(selectIsStaging);
-  const selectBrush = useSelectTool('brush');
   const isSelected = useToolIsSelected('brush');
-  const isDrawingToolAllowed = useAppSelector(selectIsSelectedEntityDrawable);
+  const selectBrush = useSelectTool('brush');
 
-  const isDisabled = useMemo(() => {
-    return isTransforming || isFiltering || isStaging || !isDrawingToolAllowed;
-  }, [isDrawingToolAllowed, isFiltering, isStaging, isTransforming]);
-
-  useHotkeys('b', selectBrush, { enabled: !isDisabled || isSelected }, [isDisabled, isSelected, selectBrush]);
+  useHotkeys('b', selectBrush, { enabled: !isSelected }, [isSelected, selectBrush]);
 
   return (
     <IconButton
@@ -31,9 +18,9 @@ export const ToolBrushButton = memo(() => {
       tooltip={`${t('controlLayers.tool.brush')} (B)`}
       icon={<PiPaintBrushBold />}
       colorScheme={isSelected ? 'invokeBlue' : 'base'}
-      variant="outline"
+      variant="solid"
       onClick={selectBrush}
-      isDisabled={isDisabled}
+      isDisabled={isSelected}
     />
   );
 });
--- invokeai/frontend/web/src/features/controlLayers/components/Tool/ToolViewButton.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/Tool/ToolViewButton.tsx
@@ -1,26 +1,16 @@
 import { IconButton } from '@invoke-ai/ui-library';
-import { useAppSelector } from 'app/store/storeHooks';
 import { useSelectTool, useToolIsSelected } from 'features/controlLayers/components/Tool/hooks';
-import { useIsFiltering } from 'features/controlLayers/hooks/useIsFiltering';
-import { useIsTransforming } from 'features/controlLayers/hooks/useIsTransforming';
-import { selectIsStaging } from 'features/controlLayers/store/canvasSessionSlice';
-import { memo, useMemo } from 'react';
+import { memo } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { useTranslation } from 'react-i18next';
 import { PiHandBold } from 'react-icons/pi';
 
 export const ToolViewButton = memo(() => {
   const { t } = useTranslation();
-  const isTransforming = useIsTransforming();
-  const isFiltering = useIsFiltering();
-  const isStaging = useAppSelector(selectIsStaging);
-  const selectView = useSelectTool('view');
   const isSelected = useToolIsSelected('view');
-  const isDisabled = useMemo(() => {
-    return isTransforming || isFiltering || isStaging;
-  }, [isFiltering, isStaging, isTransforming]);
+  const selectView = useSelectTool('view');
 
-  useHotkeys('h', selectView, { enabled: !isDisabled || isSelected }, [selectView, isSelected, isDisabled]);
+  useHotkeys('h', selectView, { enabled: !isSelected }, [selectView, isSelected]);
 
   return (
     <IconButton
@@ -28,9 +18,9 @@ export const ToolViewButton = memo(() => {
       tooltip={`${t('controlLayers.tool.view')} (H)`}
       icon={<PiHandBold />}
       colorScheme={isSelected ? 'invokeBlue' : 'base'}
-      variant="outline"
+      variant="solid"
       onClick={selectView}
-      isDisabled={isDisabled}
+      isDisabled={isSelected}
     />
   );
 });
