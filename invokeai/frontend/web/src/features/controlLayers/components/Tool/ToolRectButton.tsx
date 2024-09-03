a5a077964e2b0f74fcde93a7a4c726eaba10513f
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-09-03 11:44:16 +1000

    feat(ui): tool buttons are only disabled when currently selected

--- invokeai/frontend/web/src/features/controlLayers/components/Tool/ToolRectButton.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/Tool/ToolRectButton.tsx
@@ -1,29 +1,16 @@
 import { IconButton } from '@invoke-ai/ui-library';
-import { useAppSelector } from 'app/store/storeHooks';
 import { useSelectTool, useToolIsSelected } from 'features/controlLayers/components/Tool/hooks';
-import { useIsFiltering } from 'features/controlLayers/hooks/useIsFiltering';
-import { useIsTransforming } from 'features/controlLayers/hooks/useIsTransforming';
-import { selectIsStaging } from 'features/controlLayers/store/canvasSessionSlice';
-import { selectIsSelectedEntityDrawable } from 'features/controlLayers/store/selectors';
-import { memo, useMemo } from 'react';
+import { memo } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
 import { useTranslation } from 'react-i18next';
 import { PiRectangleBold } from 'react-icons/pi';
 
 export const ToolRectButton = memo(() => {
   const { t } = useTranslation();
-  const selectRect = useSelectTool('rect');
   const isSelected = useToolIsSelected('rect');
-  const isFiltering = useIsFiltering();
-  const isTransforming = useIsTransforming();
-  const isStaging = useAppSelector(selectIsStaging);
-  const isDrawingToolAllowed = useAppSelector(selectIsSelectedEntityDrawable);
+  const selectRect = useSelectTool('rect');
 
-  const isDisabled = useMemo(() => {
-    return isTransforming || isFiltering || isStaging || !isDrawingToolAllowed;
-  }, [isDrawingToolAllowed, isFiltering, isStaging, isTransforming]);
-
-  useHotkeys('u', selectRect, { enabled: !isDisabled || isSelected }, [isDisabled, isSelected, selectRect]);
+  useHotkeys('u', selectRect, { enabled: !isSelected }, [isSelected, selectRect]);
 
   return (
     <IconButton
@@ -31,9 +18,9 @@ export const ToolRectButton = memo(() => {
       tooltip={`${t('controlLayers.tool.rectangle')} (U)`}
       icon={<PiRectangleBold />}
       colorScheme={isSelected ? 'invokeBlue' : 'base'}
-      variant="outline"
+      variant="solid"
       onClick={selectRect}
-      isDisabled={isDisabled}
+      isDisabled={isSelected}
     />
   );
 });
