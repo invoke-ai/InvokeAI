0712684dc9a1ccff076869978a821bd42e6e897f
Ryan Dick <ryanjdick3@gmail.com>
2024-09-03 18:04:48 +0000

    Split PEFT layer implementations into separate files.

---
+++ invokeai/backend/peft/layers/any_lora_layer.py
@@ -0,0 +1,10 @@
+from typing import Union
+
+from invokeai.backend.peft.layers.full_layer import FullLayer
+from invokeai.backend.peft.layers.ia3_layer import IA3Layer
+from invokeai.backend.peft.layers.loha_layer import LoHALayer
+from invokeai.backend.peft.layers.lokr_layer import LoKRLayer
+from invokeai.backend.peft.layers.lora_layer import LoRALayer
+from invokeai.backend.peft.layers.norm_layer import NormLayer
+
+AnyLoRALayer = Union[LoRALayer, LoHALayer, LoKRLayer, FullLayer, IA3Layer, NormLayer]
