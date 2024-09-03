d20335dabc9b2fdaf0a7e5c142c1093e2b3fb82c
Brandon Rising <brandon@invoke.ai>
2024-09-03 17:10:34 -0400

    convert_bundle_to_flux_transformer_checkpoint now removes processed keys to decrease memory usage

--- invokeai/backend/model_manager/util/model_util.py
+++ invokeai/backend/model_manager/util/model_util.py
@@ -139,6 +139,8 @@ def convert_bundle_to_flux_transformer_checkpoint(
     transformer_state_dict: dict[str, torch.Tensor],
 ) -> dict[str, torch.Tensor]:
     original_state_dict: dict[str, torch.Tensor] = {}
+    keys_to_remove: list[str] = []
+
     for k, v in transformer_state_dict.items():
         if not k.startswith("model.diffusion_model"):
             continue
@@ -146,5 +148,13 @@ def convert_bundle_to_flux_transformer_checkpoint(
             # Scale math must be done at bfloat16 due to our current flux model
             # support limitations at inference time
             v = v.to(dtype=torch.bfloat16)
-        original_state_dict[k.replace("model.diffusion_model.", "")] = v
+        new_key = k.replace("model.diffusion_model.", "")
+        original_state_dict[new_key] = v
+        keys_to_remove.append(k)
+
+    # Remove processed keys from the original dictionary, leaving others in case
+    # other model state dicts need to be pulled
+    for k in keys_to_remove:
+        del transformer_state_dict[k]
+
     return original_state_dict
