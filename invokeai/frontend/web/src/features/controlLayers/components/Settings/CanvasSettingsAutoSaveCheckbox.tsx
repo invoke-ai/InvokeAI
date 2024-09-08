db30014365c5a8db3b3908880aa5f5d6c1edc0bc
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-09-08 19:38:38 +1000

    tidy(ui): CanvasSettingsAutoSaveCheckbox

--- invokeai/frontend/web/src/features/controlLayers/components/Settings/CanvasSettingsAutoSaveCheckbox.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/Settings/CanvasSettingsAutoSaveCheckbox.tsx
@@ -1,17 +1,19 @@
 import { Checkbox, FormControl, FormLabel } from '@invoke-ai/ui-library';
-import { createSelector } from '@reduxjs/toolkit';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
-import { selectCanvasSettingsSlice, settingsAutoSaveToggled } from 'features/controlLayers/store/canvasSettingsSlice';
+import {
+  selectAutoSave,
+  settingsAutoSaveToggled,
+} from 'features/controlLayers/store/canvasSettingsSlice';
 import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-
-const selectAutoSave = createSelector(selectCanvasSettingsSlice, (canvasSettings) => canvasSettings.autoSave);
 
 export const CanvasSettingsAutoSaveCheckbox = memo(() => {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
   const autoSave = useAppSelector(selectAutoSave);
-  const onChange = useCallback(() => dispatch(settingsAutoSaveToggled()), [dispatch]);
+  const onChange = useCallback(() => {
+    dispatch(settingsAutoSaveToggled());
+  }, [dispatch]);
   return (
     <FormControl w="full">
       <FormLabel flexGrow={1}>{t('controlLayers.autoSave')}</FormLabel>
