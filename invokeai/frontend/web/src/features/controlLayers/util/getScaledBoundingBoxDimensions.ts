6f06ca8c54a7e32c5b00baafa5cb6bc171dd6b1d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-09-08 20:50:29 +1000

    feat(ui): use 64 as grid for auto-scaled bbox

--- invokeai/frontend/web/src/features/controlLayers/util/getScaledBoundingBoxDimensions.ts
+++ invokeai/frontend/web/src/features/controlLayers/util/getScaledBoundingBoxDimensions.ts
@@ -1,24 +1,26 @@
 import { roundToMultiple } from 'common/util/roundDownToMultiple';
 import type { Dimensions } from 'features/controlLayers/store/types';
 
-const CANVAS_GRID_SIZE_FINE = 8;
-
 /**
  * Scales the bounding box dimensions to the optimal dimension. The optimal dimensions should be the trained dimension
  * for the model. For example, 1024 for SDXL or 512 for SD1.5.
  * @param dimensions The un-scaled bbox dimensions
  * @param optimalDimension The optimal dimension to scale the bbox to
  */
-export const getScaledBoundingBoxDimensions = (dimensions: Dimensions, optimalDimension: number): Dimensions => {
+export const getScaledBoundingBoxDimensions = (
+  dimensions: Dimensions,
+  optimalDimension: number,
+  gridSize: number = 64
+): Dimensions => {
   const { width, height } = dimensions;
 
   const scaledDimensions = { width, height };
   const targetArea = optimalDimension * optimalDimension;
   const aspectRatio = width / height;
   let currentArea = width * height;
-  let maxDimension = optimalDimension - CANVAS_GRID_SIZE_FINE;
+  let maxDimension = optimalDimension - gridSize;
   while (currentArea < targetArea) {
-    maxDimension += CANVAS_GRID_SIZE_FINE;
+    maxDimension += gridSize;
     if (width === height) {
       scaledDimensions.width = optimalDimension;
       scaledDimensions.height = optimalDimension;
@@ -26,10 +28,10 @@ export const getScaledBoundingBoxDimensions = (dimensions: Dimensions, optimalDi
     } else {
       if (aspectRatio > 1) {
         scaledDimensions.width = maxDimension;
-        scaledDimensions.height = roundToMultiple(maxDimension / aspectRatio, CANVAS_GRID_SIZE_FINE);
+        scaledDimensions.height = roundToMultiple(maxDimension / aspectRatio, gridSize);
       } else if (aspectRatio < 1) {
         scaledDimensions.height = maxDimension;
-        scaledDimensions.width = roundToMultiple(maxDimension * aspectRatio, CANVAS_GRID_SIZE_FINE);
+        scaledDimensions.width = roundToMultiple(maxDimension * aspectRatio, gridSize);
       }
       currentArea = scaledDimensions.width * scaledDimensions.height;
     }
