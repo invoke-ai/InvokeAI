31a8757e6b3885b28dc2ba0d0a221f5f649871f6
Ryan Dick <ryanjdick3@gmail.com>
2024-09-09 19:02:15 +0000

    Add is_state_dict_likely_in_flux_diffusers_format(...) function with unit test.

--- tests/backend/lora/conversions/test_flux_diffusers_lora_conversion_utils.py
+++ tests/backend/lora/conversions/test_flux_diffusers_lora_conversion_utils.py
@@ -1,16 +1,44 @@
 import torch
 
 from invokeai.backend.lora.conversions.flux_diffusers_lora_conversion_utils import (
+    is_state_dict_likely_in_flux_diffusers_format,
     lora_model_from_flux_diffusers_state_dict,
 )
-from tests.backend.lora.conversions.lora_state_dicts.flux_lora_diffusers_format import state_dict_keys
+from tests.backend.lora.conversions.lora_state_dicts.flux_lora_diffusers_format import (
+    state_dict_keys as flux_diffusers_state_dict_keys,
+)
+from tests.backend.lora.conversions.lora_state_dicts.flux_lora_kohya_format import (
+    state_dict_keys as flux_kohya_state_dict_keys,
+)
+
+
+def test_is_state_dict_likely_in_flux_diffusers_format_true():
+    """Test that is_state_dict_likely_in_flux_diffusers_format() can identify a state dict in the Diffusers FLUX LoRA format."""
+    # Construct a state dict that is in the Diffusers FLUX LoRA format.
+    state_dict: dict[str, torch.Tensor] = {}
+    for k in flux_diffusers_state_dict_keys:
+        state_dict[k] = torch.empty(1)
+
+    assert is_state_dict_likely_in_flux_diffusers_format(state_dict)
+
+
+def test_is_state_dict_likely_in_flux_diffusers_format_false():
+    """Test that is_state_dict_likely_in_flux_diffusers_format() returns False for a state dict that is not in the Kohya
+    FLUX LoRA format.
+    """
+    # Construct a state dict that is not in the Kohya FLUX LoRA format.
+    state_dict: dict[str, torch.Tensor] = {}
+    for k in flux_kohya_state_dict_keys:
+        state_dict[k] = torch.empty(1)
+
+    assert not is_state_dict_likely_in_flux_diffusers_format(state_dict)
 
 
 def test_lora_model_from_flux_diffusers_state_dict():
     """Test that lora_model_from_flux_diffusers_state_dict() can load a state dict in the Diffusers FLUX LoRA format."""
     # Construct a state dict that is in the Diffusers FLUX LoRA format.
     state_dict: dict[str, torch.Tensor] = {}
-    for k in state_dict_keys:
+    for k in flux_diffusers_state_dict_keys:
         state_dict[k] = torch.empty(1)
 
     # Load the state dict into a LoRAModelRaw object.
@@ -18,7 +46,7 @@ def test_lora_model_from_flux_diffusers_state_dict():
 
     # Check that the model has the correct number of LoRA layers.
     expected_lora_layers: set[str] = set()
-    for k in state_dict_keys:
+    for k in flux_diffusers_state_dict_keys:
         k = k.replace("lora_A.weight", "")
         k = k.replace("lora_B.weight", "")
         expected_lora_layers.add(k)
