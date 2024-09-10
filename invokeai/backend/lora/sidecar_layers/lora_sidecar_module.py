0aea4e6800841e18fde52a2ca38829b39f26cb07
Ryan Dick <ryanjdick3@gmail.com>
2024-09-10 21:45:18 +0000

    WIP - adding LoRA sidecar layers

---
+++ invokeai/backend/lora/sidecar_layers/lora_sidecar_module.py
@@ -0,0 +1,17 @@
+import torch
+
+
+class LoRASidecarModule(torch.nn.Module):
+    def __init__(self, orig_module: torch.nn.Module, lora_layers: list[torch.nn.Module]):
+        super().__init__()
+        self._orig_module = orig_module
+        self._lora_layers = lora_layers
+
+    def add_lora_layer(self, lora_layer: torch.nn.Module):
+        self._lora_layers.append(lora_layer)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self._orig_module(x)
+        for lora_layer in self._lora_layers:
+            x += lora_layer(x)
+        return x
