105162ffb541d8388bbc680e1d3865cd32ad5372
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-09-16 22:21:38 +1000

    fix(ui): remove accidentally added staging button

--- invokeai/frontend/web/src/features/controlLayers/components/StagingArea/StagingAreaToolbar.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/StagingArea/StagingAreaToolbar.tsx
@@ -22,7 +22,6 @@ import {
   PiEyeBold,
   PiEyeSlashBold,
   PiFloppyDiskBold,
-  PiStackPlusBold,
   PiTrashSimpleBold,
   PiXBold,
 } from 'react-icons/pi';
@@ -179,14 +178,6 @@ export const StagingAreaToolbar = memo(() => {
           onClick={onSaveStagingImage}
           colorScheme="invokeBlue"
           isDisabled={!selectedImage || !selectedImage.imageDTO.is_intermediate}
-        />
-        <IconButton
-          tooltip={`${t('unifiedCanvas.saveToGallery')} (Shift+S)`}
-          aria-label={t('unifiedCanvas.saveToGallery')}
-          icon={<PiStackPlusBold />}
-          onClick={onSaveStagingImage}
-          colorScheme="invokeBlue"
-          isDisabled={!selectedImage}
         />
         <IconButton
           tooltip={`${t('unifiedCanvas.discardCurrent')}`}
