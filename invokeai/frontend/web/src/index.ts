ac773da0600793c846377df6f779205cb692b48e
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-09-18 10:49:43 -0400

    lint

--- invokeai/frontend/web/src/index.ts
+++ invokeai/frontend/web/src/index.ts
@@ -1,7 +1,7 @@
 export { default as InvokeAIUI } from './app/components/InvokeAIUI';
 export type { PartialAppConfig } from './app/types/invokeai';
 export { default as ParamMainModelSelect } from './features/parameters/components/MainModel/ParamMainModelSelect';
+export { default as HotkeysModal } from './features/system/components/HotkeysModal/HotkeysModal';
 export { default as InvokeAiLogoComponent } from './features/system/components/InvokeAILogoComponent';
 export { default as SettingsModal } from './features/system/components/SettingsModal/SettingsModal';
-export { default as HotkeysModal } from './features/system/components/HotkeysModal/HotkeysModal';
 export { default as StatusIndicator } from './features/system/components/StatusIndicator';
