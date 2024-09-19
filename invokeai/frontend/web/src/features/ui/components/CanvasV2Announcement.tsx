8c93f54a0bc7579b6a5cc132e2f690c298ec3d0a
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-09-19 15:33:15 -0400

    add translations

--- invokeai/frontend/web/src/features/ui/components/CanvasV2Announcement.tsx
+++ invokeai/frontend/web/src/features/ui/components/CanvasV2Announcement.tsx
@@ -1,30 +1,32 @@
 import { Flex, UnorderedList, ListItem, Icon, Text } from '@invoke-ai/ui-library';
+import { useTranslation } from 'react-i18next';
 import { PiArrowSquareOutBold } from 'react-icons/pi';
 
 export const CanvasV2Announcement = () => {
+  const { t } = useTranslation();
   return (
     <Flex gap={4} flexDir="column">
       <UnorderedList fontSize="sm">
-        <ListItem>A poweful new control canvas</ListItem>
-        <ListItem>New layer types for even more control</ListItem>
-        <ListItem>Support for the Flux family of models</ListItem>
+        <ListItem>{t('whatsNew.canvasV2Announcement.newCanvas')}</ListItem>
+        <ListItem>{t('whatsNew.canvasV2Announcement.newLayerTypes')}</ListItem>
+        <ListItem>{t('whatsNew.canvasV2Announcement.fluxSupport')}</ListItem>
       </UnorderedList>
       <Flex flexDir="column" gap={1}>
         <Flex gap={2}>
-          <Text as="a" target="_blank" href="" fontWeight="semibold">
-            Read Release Notes
+          <Text as="a" target="_blank" href="https://github.com/invoke-ai/InvokeAI/releases" fontWeight="semibold">
+            {t('whatsNew.canvasV2Announcement.readReleaseNotes')}
           </Text>
           <Icon as={PiArrowSquareOutBold} />
         </Flex>
         <Flex gap={2}>
-          <Text as="a" target="_blank" href="" fontWeight="semibold">
-            Watch Release Video
+          <Text as="a" target="_blank" href="https://www.youtube.com/@invokeai/videos" fontWeight="semibold">
+            {t('whatsNew.canvasV2Announcement.watchReleaseVideo')}
           </Text>
           <Icon as={PiArrowSquareOutBold} />
         </Flex>
         <Flex gap={2}>
-          <Text as="a" target="_blank" href="" fontWeight="semibold">
-            Watch UI Updates Overview
+          <Text as="a" target="_blank" href="https://www.youtube.com/@invokeai/videos" fontWeight="semibold">
+            {t('whatsNew.canvasV2Announcement.watchUiUpdatesOverview')}
           </Text>
           <Icon as={PiArrowSquareOutBold} />
         </Flex>
