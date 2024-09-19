78016419f0bc76324c795bd6e737b579cd7f96ba
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-09-19 16:01:33 -0400

    more updates for new user experience

--- invokeai/frontend/web/src/features/gallery/components/ImageViewer/NoContentForViewer.tsx
+++ invokeai/frontend/web/src/features/gallery/components/ImageViewer/NoContentForViewer.tsx
@@ -1,46 +1,46 @@
 import { Flex, Image, Text } from '@invoke-ai/ui-library';
 import { useHasImages } from 'features/gallery/hooks/useHasImages';
 import InvokeSymbol from 'public/assets/images/invoke-symbol-char-lrg.svg';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
+import { PiImageBold } from 'react-icons/pi';
+import { IAINoContentFallback } from '../../../../common/components/IAIImageFallback';
 
 export const NoContentForViewer = () => {
   const hasImages = useHasImages();
   const { t } = useTranslation();
 
-  //   if (hasImages()) {
-  //     return <IAINoContentFallback icon={PiImageBold} label={t('gallery.noImageSelected')} />;
-  //   }
+  if (hasImages) {
+    return <IAINoContentFallback icon={PiImageBold} label={t('gallery.noImageSelected')} />;
+  }
+
   return (
     <Flex flexDir="column" gap={4} alignItems="center" textAlign="center" maxW="600px">
       <Image src={InvokeSymbol} w="8rem" h="8rem" />
-      <Text fontSize="md">
-        To get started, enter a prompt in the box and click{' '}
-        <Text as="span" fontWeight="semibold" fontSize="md">
-          Invoke
-        </Text>{' '}
-        to generate your first image. You can choose to save your images directly to the{' '}
-        <Text as="span" fontWeight="semibold" fontSize="md">
-          Gallery
-        </Text>{' '}
-        or edit them on the{' '}
-        <Text as="span" fontWeight="semibold" fontSize="md">
-          Canvas
-        </Text>
-        .
+      <Text fontSize="md" color="base.200">
+        <Trans
+          i18nKey="newUserExperience.toGetStarted"
+          components={{
+            StrongComponent: <Text as="span" color="white" fontSize="md" fontWeight="semibold" />,
+          }}
+        />
       </Text>
-      <Text fontSize="md">
-        Want more guidance? Check out our{' '}
-        <Text
-          as="a"
-          href="http://youtube.com"
-          target="_blank"
-          fontWeight="semibold"
-          fontSize="md"
-          textDecor="underline"
-        >
-          Getting Started Series
-        </Text>{' '}
-        for tips on unlocking the full potential of the Invoke Studio.
+
+      <Text fontSize="md" color="base.200">
+        <Trans
+          i18nKey="newUserExperience.gettingStartedSeries"
+          components={{
+            LinkComponent: (
+              <Text
+                as="a"
+                color="white"
+                fontSize="md"
+                fontWeight="semibold"
+                href="https://www.youtube.com/@invokeai/videos"
+                target="_blank"
+              />
+            ),
+          }}
+        />
       </Text>
     </Flex>
   );
--- invokeai/frontend/web/src/features/gallery/hooks/useHasImages.ts
+++ invokeai/frontend/web/src/features/gallery/hooks/useHasImages.ts
@@ -1,19 +1,27 @@
-import { selectListImagesQueryArgs } from "features/gallery/store/gallerySelectors";
-import { useCallback } from "react"
+import { useMemo } from "react"
 import { useListAllBoardsQuery } from "services/api/endpoints/boards";
 import { useListImagesQuery } from "services/api/endpoints/images";
 
 export const useHasImages = () => {
     const { data: boardList } = useListAllBoardsQuery({ include_archived: true })
-    const { data: uncategorizedImages } = useListImagesQuery({ ...selectListImagesQueryArgs, board_id: "none" })
+    const { data: uncategorizedImages } = useListImagesQuery({
+        board_id: "none",
+        offset: 0,
+        limit: 0,
+        is_intermediate: false,
+    })
 
-    const hasImages = useCallback(() => {
+    const hasImages = useMemo(() => {
         const hasBoards = boardList && boardList.length > 0;
-        if (!hasBoards) {
-            return uncategorizedImages ? uncategorizedImages.total > 0 : false
-        } else {
-            return boardList.filter(board => board.image_count > 0).length > 0
+
+        if (hasBoards) {
+            if (boardList.filter(board => board.image_count > 0).length > 0) {
+                return true
+            }
         }
+        return uncategorizedImages ? uncategorizedImages.total > 0 : false
+
+
     }, [boardList, uncategorizedImages])
 
     return hasImages
