5490151e0cb4f716c4d4ddcc27d8ac91df22adaa
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-09-19 14:59:14 +1000

    feat(ui): reworked send-to toggle

--- invokeai/frontend/web/src/features/queue/components/SendToToggle.tsx
+++ invokeai/frontend/web/src/features/queue/components/SendToToggle.tsx
@@ -1,52 +1,77 @@
 import type { SystemStyleObject } from '@invoke-ai/ui-library';
-import { Box, Flex, IconButton, Text, Tooltip, useToken } from '@invoke-ai/ui-library';
-import { createSelector } from '@reduxjs/toolkit';
-import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
 import {
-  selectCanvasSettingsSlice,
-  settingsSendToCanvasChanged,
-} from 'features/controlLayers/store/canvasSettingsSlice';
+  Box,
+  Button,
+  chakra,
+  Flex,
+  Icon,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Portal,
+  Text,
+  useCheckbox,
+  useToken,
+} from '@invoke-ai/ui-library';
+import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
+import { selectSendToCanvas, settingsSendToCanvasChanged } from 'features/controlLayers/store/canvasSettingsSlice';
+import { selectIsStaging } from 'features/controlLayers/store/canvasStagingAreaSlice';
+import { selectCanvasRightPanelLayersTab } from 'features/controlLayers/store/ephemeral';
+import { useImageViewer } from 'features/gallery/components/ImageViewer/useImageViewer';
+import { setActiveTab } from 'features/ui/store/uiSlice';
+import type { ChangeEvent, PropsWithChildren } from 'react';
 import { memo, useCallback, useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { PiImageBold, PiPaintBrushBold } from 'react-icons/pi';
 
-const TooltipSendToGallery = memo(() => {
-  const { t } = useTranslation();
-
-  return (
-    <Flex flexDir="column">
-      <Text fontWeight="semibold">{t('controlLayers.sendToGallery')}</Text>
-      <Text fontWeight="normal">{t('controlLayers.sendToGalleryDesc')}</Text>
-    </Flex>
-  );
-});
-
-TooltipSendToGallery.displayName = 'TooltipSendToGallery';
-
-const TooltipSendToCanvas = memo(() => {
-  const { t } = useTranslation();
-
-  return (
-    <Flex flexDir="column">
-      <Text fontWeight="semibold">{t('controlLayers.sendToCanvas')}</Text>
-      <Text fontWeight="normal">{t('controlLayers.sendToCanvasDesc')}</Text>
-    </Flex>
-  );
-});
-
-TooltipSendToCanvas.displayName = 'TooltipSendToCanvas';
-
-const selectSendToCanvas = createSelector(selectCanvasSettingsSlice, (canvasSettings) => canvasSettings.sendToCanvas);
-
 const getSx = (padding: string | number): SystemStyleObject => ({
-  transition: 'left 0.1s ease-in-out, transform 0.1s ease-in-out',
-  '&[data-checked="true"]': {
-    left: `calc(100% - ${padding})`,
-    transform: 'translateX(-100%)',
+  bg: 'invokeBlue.500',
+  w: '72px',
+  '&[data-checked]': {
+    bg: 'invokePurple.300',
+    '.checked-bg-slider': {
+      left: `calc(100% - ${padding})`,
+      transform: 'translateX(-100%)',
+    },
+    '.unchecked-icon': {
+      color: 'invokePurple.800',
+      opacity: 0,
+    },
+    '.checked-icon': {
+      color: 'base.900',
+      opacity: 1,
+    },
   },
-  '&[data-checked="false"]': {
+  '&[data-disabled]': {
+    bg: 'base.700',
+    '.checked-bg-slider': {
+      bg: 'base.500',
+    },
+    '.unchecked-icon': {
+      color: 'base.800',
+    },
+    '.checked-icon': {
+      color: 'base.800',
+    },
+  },
+  '.checked-bg-slider': {
+    transition: 'left 0.1s ease-in-out, transform 0.1s ease-in-out',
     left: padding,
     transform: 'translateX(0)',
+    bg: 'base.50',
+    shadow: 'md',
+  },
+  '.unchecked-icon': {
+    transition: 'all 0.15s ease-in-out',
+    color: 'base.900',
+    opacity: 1,
+  },
+  '.checked-icon': {
+    transition: 'all 0.15s ease-in-out',
+    color: 'invokeBlue.800',
+    opacity: 0,
   },
 });
 
@@ -54,77 +79,121 @@ export const SendToToggle = memo(() => {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
   const sendToCanvas = useAppSelector(selectSendToCanvas);
+  const isStaging = useAppSelector(selectIsStaging);
 
-  const gap = useToken('space', 1.5);
+  const gap = useToken('space', 1);
   const sx = useMemo(() => getSx(gap), [gap]);
 
-  const onClickSendToGallery = useCallback(() => {
-    if (!sendToCanvas) {
-      return;
-    }
-    dispatch(settingsSendToCanvasChanged(false));
-  }, [dispatch, sendToCanvas]);
+  const onChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      dispatch(settingsSendToCanvasChanged(e.target.checked));
+    },
+    [dispatch]
+  );
 
-  const onClickSendToCanvas = useCallback(() => {
-    if (sendToCanvas) {
-      return;
-    }
-    dispatch(settingsSendToCanvasChanged(true));
-  }, [dispatch, sendToCanvas]);
+  const { getCheckboxProps, getInputProps, htmlProps } = useCheckbox({
+    onChange,
+    isChecked: sendToCanvas,
+    isDisabled: isStaging,
+  });
 
   return (
-    <Flex
-      position="relative"
-      bg="base.800"
-      borderRadius="base"
-      alignItems="center"
-      justifyContent="center"
-      h="full"
-      p={gap}
-      gap={gap}
-    >
-      <Box
-        position="absolute"
-        borderRadius="base"
-        bg="invokeBlue.400"
-        w={12}
-        top={gap}
-        bottom={gap}
-        data-checked={sendToCanvas}
-        sx={sx}
-      />
-      <Tooltip hasArrow label={<TooltipSendToGallery />}>
-        <IconButton
-          size="sm"
-          fontSize={16}
-          icon={<PiImageBold />}
-          onClick={onClickSendToGallery}
-          variant={!sendToCanvas ? 'solid' : 'ghost'}
-          colorScheme={!sendToCanvas ? 'invokeBlue' : 'base'}
-          aria-label={t('controlLayers.sendToGallery')}
-          data-checked={!sendToCanvas}
-          w={12}
-          alignSelf="stretch"
-          h="auto"
-        />
-      </Tooltip>
-      <Tooltip hasArrow label={<TooltipSendToCanvas />}>
-        <IconButton
-          size="sm"
-          fontSize={16}
-          icon={<PiPaintBrushBold />}
-          onClick={onClickSendToCanvas}
-          variant={sendToCanvas ? 'solid' : 'ghost'}
-          colorScheme={sendToCanvas ? 'invokeGreen' : 'base'}
-          aria-label={t('controlLayers.sendToCanvas')}
-          data-checked={sendToCanvas}
-          w={12}
-          alignSelf="stretch"
-          h="auto"
-        />
-      </Tooltip>
-    </Flex>
+    <Popover trigger="hover">
+      <PopoverTrigger>
+        <chakra.label {...htmlProps}>
+          <Flex
+            position="relative"
+            borderRadius="full"
+            alignItems="center"
+            justifyContent="space-between"
+            h="full"
+            p={gap}
+            gap={gap}
+            sx={sx}
+            {...getCheckboxProps()}
+          >
+            <input {...getInputProps()} hidden />
+            <Box className="checked-bg-slider" position="absolute" borderRadius="full" w={10} top={gap} bottom={gap} />
+            <Flex w={10} h="full" alignItems="center" justifyContent="center" pos="relative">
+              <Icon
+                className="unchecked-icon"
+                w={6}
+                h={6}
+                as={PiImageBold}
+                aria-label={t('controlLayers.sendToGallery')}
+              />
+            </Flex>
+            <Flex w={10} h="full" alignItems="center" justifyContent="center" pos="relative">
+              <Icon
+                className="checked-icon"
+                w={6}
+                h={6}
+                as={PiPaintBrushBold}
+                aria-label={t('controlLayers.sendToCanvas')}
+              />
+            </Flex>
+          </Flex>
+        </chakra.label>
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent maxW={296} p={2} bg="base.200" color="base.800">
+          <PopoverArrow bg="base.200" />
+          <PopoverBody>
+            <TooltipContent sendToCanvas={sendToCanvas} isStaging={isStaging} />
+          </PopoverBody>
+        </PopoverContent>
+      </Portal>
+    </Popover>
   );
 });
 
 SendToToggle.displayName = 'CanvasSendToToggle';
+
+const TooltipContent = memo(({ sendToCanvas, isStaging }: { sendToCanvas: boolean; isStaging: boolean }) => {
+  const { t } = useTranslation();
+
+  if (isStaging) {
+    return (
+      <Flex flexDir="column">
+        <Text fontWeight="semibold">{t('controlLayers.sendingToCanvas')}</Text>
+        <Text fontWeight="normal">
+          <Trans i18nKey="controlLayers.viewProgressOnCanvas" components={{ Btn: <ActivateCanvasButton /> }} />
+        </Text>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex flexDir="column">
+      <Text fontWeight="semibold">
+        {sendToCanvas ? t('controlLayers.sendToCanvas') : t('controlLayers.sendToGallery')}
+      </Text>
+      <Text fontWeight="normal">
+        {sendToCanvas ? t('controlLayers.sendToCanvasDesc') : t('controlLayers.sendToGalleryDesc')}
+      </Text>
+    </Flex>
+  );
+});
+
+TooltipContent.displayName = 'TooltipContent';
+
+const ActivateCanvasButton = (props: PropsWithChildren) => {
+  const dispatch = useAppDispatch();
+  const imageViewer = useImageViewer();
+  const onClick = useCallback(() => {
+    dispatch(setActiveTab('canvas'));
+    selectCanvasRightPanelLayersTab();
+    imageViewer.close();
+  }, [dispatch, imageViewer]);
+  return (
+    <Button
+      onClick={onClick}
+      size="sm"
+      variant="link"
+      color="base.800"
+      _hover={{ color: 'base.900', textDecoration: 'underline' }}
+    >
+      {props.children}
+    </Button>
+  );
+};
