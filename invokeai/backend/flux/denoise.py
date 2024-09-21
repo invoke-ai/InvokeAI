dc101976157fd302be98654fe2f3947045009dc2
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-09-21 19:17:09 +1000

    fix(app): step callbacks for SD, FLUX, MultiDiffusion
    Each of these was a bit off:
    - The SD callback started at `-1` and ended at `i`. Combined w/ the weird math on the previous `calc_percentage` util, this caused the progress bar to never finish.
    - The MultiDiffusion callback had the same problems as SD.
    - The FLUX callback didn't emit a pre-denoising step 0 image. It also reported total_steps as 1 higher than the actual step count.
    
    Each of these now emit the expected events to the frontend:
    - The initial latents at 0%
    - Progress at each step, ending at 100%

--- invokeai/backend/flux/denoise.py
+++ invokeai/backend/flux/denoise.py
@@ -22,7 +22,18 @@ def denoise(
     guidance: float,
     traj_guidance_extension: TrajectoryGuidanceExtension | None,  # noqa: F821
 ):
-    step = 0
+    # step 0 is the initial state
+    total_steps = len(timesteps) - 1
+    step_callback(
+        PipelineIntermediateState(
+            step=0,
+            order=1,
+            total_steps=total_steps,
+            timestep=int(timesteps[0]),
+            latents=img,
+        ),
+    )
+    step = 1
     # guidance_vec is ignored for schnell.
     guidance_vec = torch.full((img.shape[0],), guidance, device=img.device, dtype=img.dtype)
     for t_curr, t_prev in tqdm(list(zip(timesteps[:-1], timesteps[1:], strict=True))):
@@ -49,7 +60,7 @@ def denoise(
             PipelineIntermediateState(
                 step=step,
                 order=1,
-                total_steps=len(timesteps),
+                total_steps=total_steps,
                 timestep=int(t_curr),
                 latents=preview_img,
             ),
