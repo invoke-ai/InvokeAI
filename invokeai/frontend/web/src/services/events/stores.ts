dccbaa45b7da3174b7bee0df28c13d459386eccb
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-09-24 09:33:33 +1000

    chore(ui): lint

--- invokeai/frontend/web/src/services/events/stores.ts
+++ invokeai/frontend/web/src/services/events/stores.ts
@@ -7,7 +7,6 @@ export const $socket = atom<AppSocket | null>(null);
 export const $socketOptions = map<Partial<ManagerOptions & SocketOptions>>({});
 export const $isConnected = atom<boolean>(false);
 export const $lastProgressEvent = atom<S['InvocationProgressEvent'] | null>(null);
-export const $hasProgress = computed($lastProgressEvent, (val) => Boolean(val));
 export const $progressImage = computed($lastProgressEvent, (val) => val?.image ?? null);
 export const $hasProgressImage = computed($lastProgressEvent, (val) => Boolean(val?.image));
 export const $isProgressFromCanvas = computed($lastProgressEvent, (val) => val?.destination === 'canvas');
