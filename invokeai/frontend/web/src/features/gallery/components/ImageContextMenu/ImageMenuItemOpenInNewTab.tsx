3e528c319596c5f4aa04d6de8b8cca6b17ba38c0
Josh Corbett <joshwcorbett@icloud.com>
2024-10-01 17:06:04 -0600

    feat(context menu): :sparkles: condense top row of image context menu

--- invokeai/frontend/web/src/features/gallery/components/ImageContextMenu/ImageMenuItemOpenInNewTab.tsx
+++ invokeai/frontend/web/src/features/gallery/components/ImageContextMenu/ImageMenuItemOpenInNewTab.tsx
@@ -1,17 +1,25 @@
-import { MenuItem } from '@invoke-ai/ui-library';
+import { IconButton } from '@invoke-ai/ui-library';
 import { useImageDTOContext } from 'features/gallery/contexts/ImageDTOContext';
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiArrowSquareOutBold } from 'react-icons/pi';
 
 export const ImageMenuItemOpenInNewTab = memo(() => {
   const { t } = useTranslation();
   const imageDTO = useImageDTOContext();
+  const onClick = useCallback(() => {
+    window.open(imageDTO.image_url, '_blank');
+  }, [imageDTO.image_url]);
 
   return (
-    <MenuItem as="a" href={imageDTO.image_url} target="_blank" icon={<PiArrowSquareOutBold />}>
-      {t('common.openInNewTab')}
-    </MenuItem>
+    <IconButton
+      onClickCapture={onClick}
+      aria-label={t('common.openInNewTab')}
+      tooltip={t('common.openInNewTab')}
+      icon={<PiArrowSquareOutBold />}
+      variant="ghost"
+      colorScheme="base"
+    />
   );
 });
 
