395d35c28707c91c24c36a5b2c65f14a44fa39a7
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-10-02 16:05:11 +1000

    fix(ui): next/prev image buttons layout

--- invokeai/frontend/web/src/features/gallery/components/NextPrevImageButtons.tsx
+++ invokeai/frontend/web/src/features/gallery/components/NextPrevImageButtons.tsx
@@ -7,12 +7,7 @@ import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiCaretLeftBold, PiCaretRightBold } from 'react-icons/pi';
 
-const nextPrevButtonStyles: ChakraProps['sx'] = {
-  color: 'base.100',
-  pointerEvents: 'auto',
-};
-
-const NextPrevImageButtons = () => {
+const NextPrevImageButtons = ({ inset = 8 }: { inset?: ChakraProps['insetInlineStart' | 'insetInlineEnd'] }) => {
   const { t } = useTranslation();
   const { prevImage, nextImage, isOnFirstImageOfView, isOnLastImageOfView } = useGalleryNavigation();
 
@@ -61,32 +56,36 @@ const NextPrevImageButtons = () => {
 
   return (
     <Box pos="relative" h="full" w="full">
-      <Box pos="absolute" top="50%" transform="translate(0, -50%)" insetInlineStart={1}>
-        {shouldShowLeftArrow && (
-          <IconButton
-            aria-label={t('accessibility.previousImage')}
-            icon={<PiCaretLeftBold size={64} />}
-            variant="unstyled"
-            onClick={onClickLeftArrow}
-            boxSize={16}
-            sx={nextPrevButtonStyles}
-            isDisabled={isFetching}
-          />
-        )}
-      </Box>
-      <Box pos="absolute" top="50%" transform="translate(0, -50%)" insetInlineEnd={6}>
-        {shouldShowRightArrow && (
-          <IconButton
-            aria-label={t('accessibility.nextImage')}
-            icon={<PiCaretRightBold size={64} />}
-            variant="unstyled"
-            onClick={onClickRightArrow}
-            boxSize={16}
-            sx={nextPrevButtonStyles}
-            isDisabled={isFetching}
-          />
-        )}
-      </Box>
+      {shouldShowLeftArrow && (
+        <IconButton
+          position="absolute"
+          top="50%"
+          transform="translate(0, -50%)"
+          aria-label={t('accessibility.previousImage')}
+          icon={<PiCaretLeftBold size={64} />}
+          variant="unstyled"
+          onClick={onClickLeftArrow}
+          isDisabled={isFetching}
+          color="base.100"
+          pointerEvents="auto"
+          insetInlineStart={inset}
+        />
+      )}
+      {shouldShowRightArrow && (
+        <IconButton
+          position="absolute"
+          top="50%"
+          transform="translate(0, -50%)"
+          aria-label={t('accessibility.nextImage')}
+          icon={<PiCaretRightBold size={64} />}
+          variant="unstyled"
+          onClick={onClickRightArrow}
+          isDisabled={isFetching}
+          color="base.100"
+          pointerEvents="auto"
+          insetInlineEnd={inset}
+        />
+      )}
     </Box>
   );
 };
