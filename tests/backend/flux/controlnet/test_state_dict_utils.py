1125218bc5fc11c143b1008aca5034a6f9542592
Ryan Dick <ryanjdick3@gmail.com>
2024-10-07 16:18:33 +0000

    Update FLUX ControlNet unit test state dicts to include shapes.

--- tests/backend/flux/controlnet/test_state_dict_utils.py
+++ tests/backend/flux/controlnet/test_state_dict_utils.py
@@ -6,28 +6,28 @@ from invokeai.backend.flux.controlnet.state_dict_utils import (
     is_state_dict_instantx_controlnet,
     is_state_dict_xlabs_controlnet,
 )
-from tests.backend.flux.controlnet.instantx_flux_controlnet_state_dict import instantx_state_dict_keys
-from tests.backend.flux.controlnet.xlabs_flux_controlnet_state_dict import xlabs_state_dict_keys
+from tests.backend.flux.controlnet.instantx_flux_controlnet_state_dict import instantx_sd_shapes
+from tests.backend.flux.controlnet.xlabs_flux_controlnet_state_dict import xlabs_sd_shapes
 
 
 @pytest.mark.parametrize(
-    ["sd_keys", "expected"],
+    ["sd_shapes", "expected"],
     [
-        (xlabs_state_dict_keys, True),
-        (instantx_state_dict_keys, False),
+        (xlabs_sd_shapes, True),
+        (instantx_sd_shapes, False),
         (["foo"], False),
     ],
 )
-def test_is_state_dict_xlabs_controlnet(sd_keys: list[str], expected: bool):
-    sd = {k: None for k in sd_keys}
+def test_is_state_dict_xlabs_controlnet(sd_shapes: dict[str, list[int]], expected: bool):
+    sd = {k: None for k in sd_shapes}
     assert is_state_dict_xlabs_controlnet(sd) == expected
 
 
 @pytest.mark.parametrize(
     ["sd_keys", "expected"],
     [
-        (instantx_state_dict_keys, True),
-        (xlabs_state_dict_keys, False),
+        (instantx_sd_shapes, True),
+        (xlabs_sd_shapes, False),
         (["foo"], False),
     ],
 )
@@ -38,6 +38,6 @@ def test_is_state_dict_instantx_controlnet(sd_keys: list[str], expected: bool):
 
 def test_convert_diffusers_instantx_state_dict_to_bfl_format():
     """Smoke test convert_diffusers_instantx_state_dict_to_bfl_format() to ensure that it handles all of the keys."""
-    sd = {k: torch.zeros(1) for k in instantx_state_dict_keys}
+    sd = {k: torch.zeros(1) for k in instantx_sd_shapes}
     bfl_sd = convert_diffusers_instantx_state_dict_to_bfl_format(sd)
     assert bfl_sd is not None
