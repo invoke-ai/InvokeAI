849b9e8d86c4f1047e806e7f9b7186a7f2bd8c94
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-10-10 10:37:55 +1000

    fix(ui): duplicate copy workflow link modals
    The component state is a global singleton, but each workflow had an instance of the modal. So when you open one, they _all_ opened.

--- invokeai/frontend/web/src/features/nodes/components/sidePanel/WorkflowListMenu/CopyWorkflowLinkModal.tsx
+++ invokeai/frontend/web/src/features/nodes/components/sidePanel/WorkflowListMenu/CopyWorkflowLinkModal.tsx
@@ -13,15 +13,19 @@ import {
 } from '@invoke-ai/ui-library';
 import { useStore } from '@nanostores/react';
 import { $projectUrl } from 'app/store/nanostores/projectId';
-import { buildUseDisclosure } from 'common/hooks/useBoolean';
 import { toast } from 'features/toast/toast';
 import { useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiCopyBold } from 'react-icons/pi';
 
-export const [useCopyWorkflowLinkModal] = buildUseDisclosure(false);
+type Arg = {
+  isOpen: boolean;
+  onClose: () => void;
+  workflowId: string;
+  workflowName: string;
+};
 
-export const CopyWorkflowLinkModal = ({ workflowId, workflowName }: { workflowId: string; workflowName: string }) => {
+export const CopyWorkflowLinkModal = ({ isOpen, onClose, workflowId, workflowName }: Arg) => {
   const projectUrl = useStore($projectUrl);
   const { t } = useTranslation();
 
@@ -29,25 +33,17 @@ export const CopyWorkflowLinkModal = ({ workflowId, workflowName }: { workflowId
     return `${projectUrl}/studio?selectedWorkflowId=${workflowId}`;
   }, [projectUrl, workflowId]);
 
-  const copyWorkflowLinkModal = useCopyWorkflowLinkModal();
-
   const handleCopy = useCallback(() => {
     navigator.clipboard.writeText(workflowLink);
     toast({
       status: 'success',
       title: t('toast.linkCopied'),
     });
-    copyWorkflowLinkModal.close();
-  }, [workflowLink, t, copyWorkflowLinkModal]);
+    onClose();
+  }, [workflowLink, t, onClose]);
 
   return (
-    <Modal
-      isOpen={copyWorkflowLinkModal.isOpen}
-      onClose={copyWorkflowLinkModal.close}
-      isCentered
-      size="lg"
-      useInert={false}
-    >
+    <Modal isOpen={isOpen} onClose={onClose} isCentered size="lg" useInert={false}>
       <ModalContent>
         <ModalHeader>
           <Flex flexDir="column" gap={2}>
@@ -57,14 +53,20 @@ export const CopyWorkflowLinkModal = ({ workflowId, workflowName }: { workflowId
         </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Flex layerStyle="third" p={5} borderRadius="base">
+          <Flex layerStyle="third" p={4} borderRadius="base" alignItems="center">
             <Text fontWeight="semibold">{workflowLink}</Text>
-            <IconButton aria-label="Copy Link" icon={<PiCopyBold />} onClick={handleCopy} />
+            <IconButton
+              variant="ghost"
+              aria-label={t('common.copy')}
+              tooltip={t('common.copy')}
+              icon={<PiCopyBold />}
+              onClick={handleCopy}
+            />
           </Flex>
         </ModalBody>
 
         <ModalFooter>
-          <Button onClick={copyWorkflowLinkModal.close}>{t('common.close')}</Button>
+          <Button onClick={onClose}>{t('common.close')}</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
