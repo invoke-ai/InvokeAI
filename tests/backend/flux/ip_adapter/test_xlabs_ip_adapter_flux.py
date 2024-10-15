05358c3ada7d06fb9c4151af0cfc80a5bb079e66
Ryan Dick <ryanjdick3@gmail.com>
2024-10-15 13:52:07 +0000

    Add logic for loading an Xlabs IP-Adapter from a state dict.

--- tests/backend/flux/ip_adapter/test_xlabs_ip_adapter_flux.py
+++ tests/backend/flux/ip_adapter/test_xlabs_ip_adapter_flux.py
@@ -1,6 +1,10 @@
+import accelerate
 import torch
 
-from invokeai.backend.flux.ip_adapter.xlabs_ip_adapter_flux import infer_xlabs_ip_adapter_params_from_state_dict
+from invokeai.backend.flux.ip_adapter.xlabs_ip_adapter_flux import (
+    XlabsIpAdapterFlux,
+    infer_xlabs_ip_adapter_params_from_state_dict,
+)
 from tests.backend.flux.ip_adapter.xlabs_flux_ip_adapter_state_dict import xlabs_sd_shapes
 
 
@@ -15,3 +19,18 @@ def test_infer_xlabs_ip_adapter_params_from_state_dict():
     assert params.context_dim == 4096
     assert params.hidden_dim == 3072
     assert params.clip_embeddings_dim == 768
+
+
+def test_initialize_xlabs_ip_adapter_flux_from_state_dict():
+    # Construct a dummy state_dict with tensors of the correct shape on the meta device.
+    with torch.device("meta"):
+        sd = {k: torch.zeros(v) for k, v in xlabs_sd_shapes.items()}
+
+    # Initialize the XLabs IP-Adapter from the state_dict.
+    params = infer_xlabs_ip_adapter_params_from_state_dict(sd)
+
+    with accelerate.init_empty_weights():
+        model = XlabsIpAdapterFlux(params=params)
+
+    # Smoke test state_dict loading.
+    model.load_xlabs_state_dict(sd)
