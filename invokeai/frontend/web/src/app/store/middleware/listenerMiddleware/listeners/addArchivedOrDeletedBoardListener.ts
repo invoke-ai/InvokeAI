2b000cb00648c1e1f2ea08868fc5991e475e28a6
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-10-16 17:15:49 +1000

    fix(ui): erroneous board selection when uploading multiple images

--- invokeai/frontend/web/src/app/store/middleware/listenerMiddleware/listeners/addArchivedOrDeletedBoardListener.ts
+++ invokeai/frontend/web/src/app/store/middleware/listenerMiddleware/listeners/addArchivedOrDeletedBoardListener.ts
@@ -29,13 +29,13 @@ export const addArchivedOrDeletedBoardListener = (startAppListening: AppStartLis
       const { autoAddBoardId, selectedBoardId } = state.gallery;
 
       // If the deleted board was currently selected, we should reset the selected board to uncategorized
-      if (deletedBoardId === selectedBoardId) {
+      if (selectedBoardId !== 'none' && deletedBoardId === selectedBoardId) {
         dispatch(boardIdSelected({ boardId: 'none' }));
         dispatch(galleryViewChanged('images'));
       }
 
       // If the deleted board was selected for auto-add, we should reset the auto-add board to uncategorized
-      if (deletedBoardId === autoAddBoardId) {
+      if (autoAddBoardId !== 'none' && deletedBoardId === autoAddBoardId) {
         dispatch(autoAddBoardIdChanged('none'));
       }
     },
@@ -46,11 +46,11 @@ export const addArchivedOrDeletedBoardListener = (startAppListening: AppStartLis
     matcher: boardsApi.endpoints.updateBoard.matchFulfilled,
     effect: (action, { dispatch, getState }) => {
       const state = getState();
-      const { shouldShowArchivedBoards } = state.gallery;
+      const { shouldShowArchivedBoards, selectedBoardId, autoAddBoardId } = state.gallery;
 
       const wasArchived = action.meta.arg.originalArgs.changes.archived === true;
 
-      if (wasArchived && !shouldShowArchivedBoards) {
+      if (selectedBoardId !== 'none' && autoAddBoardId !== 'none' && wasArchived && !shouldShowArchivedBoards) {
         dispatch(autoAddBoardIdChanged('none'));
         dispatch(boardIdSelected({ boardId: 'none' }));
         dispatch(galleryViewChanged('images'));
@@ -80,7 +80,7 @@ export const addArchivedOrDeletedBoardListener = (startAppListening: AppStartLis
 
       // Handle the case where selected board is archived
       const selectedBoard = queryResult.data.find((b) => b.board_id === selectedBoardId);
-      if (!selectedBoard || selectedBoard.archived) {
+      if (selectedBoardId !== 'none' && (!selectedBoard || selectedBoard.archived)) {
         // If we can't find the selected board or it's archived, we should reset the selected board to uncategorized
         dispatch(boardIdSelected({ boardId: 'none' }));
         dispatch(galleryViewChanged('images'));
@@ -88,7 +88,7 @@ export const addArchivedOrDeletedBoardListener = (startAppListening: AppStartLis
 
       // Handle the case where auto-add board is archived
       const autoAddBoard = queryResult.data.find((b) => b.board_id === autoAddBoardId);
-      if (!autoAddBoard || autoAddBoard.archived) {
+      if (autoAddBoardId !== 'none' && (!autoAddBoard || autoAddBoard.archived)) {
         // If we can't find the auto-add board or it's archived, we should reset the selected board to uncategorized
         dispatch(autoAddBoardIdChanged('none'));
       }
@@ -106,13 +106,13 @@ export const addArchivedOrDeletedBoardListener = (startAppListening: AppStartLis
       const { selectedBoardId, autoAddBoardId } = state.gallery;
 
       // Handle the case where selected board isn't in the list of boards
-      if (!boards.find((b) => b.board_id === selectedBoardId)) {
+      if (selectedBoardId !== 'none' && !boards.find((b) => b.board_id === selectedBoardId)) {
         dispatch(boardIdSelected({ boardId: 'none' }));
         dispatch(galleryViewChanged('images'));
       }
 
       // Handle the case where auto-add board isn't in the list of boards
-      if (!boards.find((b) => b.board_id === autoAddBoardId)) {
+      if (autoAddBoardId !== 'none' && !boards.find((b) => b.board_id === autoAddBoardId)) {
         dispatch(autoAddBoardIdChanged('none'));
       }
     },
