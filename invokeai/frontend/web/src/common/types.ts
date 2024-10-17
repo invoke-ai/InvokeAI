d3a997f2d198958f3909a302c8865852ea527a2d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-10-17 19:46:02 +1000

    feat(ui): support readonly arrays in SerializableObject type

--- invokeai/frontend/web/src/common/types.ts
+++ invokeai/frontend/web/src/common/types.ts
@@ -1,4 +1,12 @@
-type SerializableValue = string | number | boolean | null | undefined | SerializableValue[] | SerializableObject;
+type SerializableValue =
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | SerializableValue[]
+  | readonly SerializableValue[]
+  | SerializableObject;
 export type SerializableObject = {
   [k: string | number]: SerializableValue;
 };
