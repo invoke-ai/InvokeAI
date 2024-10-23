586c00bc023c7e1725927fc091eaf33e27df51f3
Ryan Dick <ryanjdick3@gmail.com>
2024-10-23 19:03:33 +0000

    (minor) Remove unused dict.

--- invokeai/backend/model_manager/load/model_loaders/stable_diffusion.py
+++ invokeai/backend/model_manager/load/model_loaders/stable_diffusion.py
@@ -51,13 +51,6 @@ VARIANT_TO_IN_CHANNEL_MAP = {
 class StableDiffusionDiffusersModel(GenericDiffusersLoader):
     """Class to load main models."""
 
-    model_base_to_model_type = {
-        BaseModelType.StableDiffusion1: "FrozenCLIPEmbedder",
-        BaseModelType.StableDiffusion2: "FrozenOpenCLIPEmbedder",
-        BaseModelType.StableDiffusionXL: "SDXL",
-        BaseModelType.StableDiffusionXLRefiner: "SDXL-Refiner",
-    }
-
     def _load_model(
         self,
         config: AnyModelConfig,
