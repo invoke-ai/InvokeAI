ef4f466ccf2aaab3324545646a7f4892672f6745
Ryan Dick <ryanjdick3@gmail.com>
2024-10-24 14:43:48 +0000

    Initial draft of SD3DenoiseInvocation.

--- invokeai/app/invocations/fields.py
+++ invokeai/app/invocations/fields.py
@@ -248,6 +248,12 @@ class FluxConditioningField(BaseModel):
     conditioning_name: str = Field(description="The name of conditioning tensor")
 
 
+class SD3ConditioningField(BaseModel):
+    """A conditioning tensor primitive value"""
+
+    conditioning_name: str = Field(description="The name of conditioning tensor")
+
+
 class ConditioningField(BaseModel):
     """A conditioning tensor primitive value"""
 
