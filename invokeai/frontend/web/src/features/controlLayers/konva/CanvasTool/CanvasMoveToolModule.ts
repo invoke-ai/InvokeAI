c474baa4e1422397b3a18a7dabdb6e06c7c8be6c
Hippalectryon <hippalectryon@protonmail.com>
2024-10-26 00:39:55 +0200

    Allow moving layers using the keyboard

--- invokeai/frontend/web/src/features/controlLayers/konva/CanvasTool/CanvasMoveToolModule.ts
+++ invokeai/frontend/web/src/features/controlLayers/konva/CanvasTool/CanvasMoveToolModule.ts
@@ -4,6 +4,12 @@ import type { CanvasToolModule } from 'features/controlLayers/konva/CanvasTool/C
 import { getPrefixedId } from 'features/controlLayers/konva/util';
 import type { Logger } from 'roarr';
 
+// Typo insurance
+const KEY_LEFT = 'ArrowLeft';
+const KEY_RIGHT = 'ArrowRight';
+const KEY_UP = 'ArrowUp';
+const KEY_DOWN = 'ArrowDown';
+
 export class CanvasMoveToolModule extends CanvasModuleBase {
   readonly type = 'move_tool';
   readonly id: string;
@@ -30,6 +36,33 @@ export class CanvasMoveToolModule extends CanvasModuleBase {
     } else {
       // The cursor is on an entity, defer to transformer to handle the cursor
       selectedEntity.transformer.syncCursorStyle();
+    }
+  };
+
+  onKeyDown = (e: KeyboardEvent) => {
+    // Support moving via arrow keys
+    const selectedEntity = this.manager.stateApi.getSelectedEntityAdapter();
+    let offset_x;
+    let offset_y;
+    switch (e.key) {
+      case KEY_LEFT:
+        offset_x = -1;
+        break;
+      case KEY_RIGHT:
+        offset_x = 1;
+        break;
+      case KEY_UP:
+        offset_y = -1;
+        break;
+      case KEY_DOWN:
+        offset_y = 1;
+        break;
+    }
+    if (offset_x !== undefined) {
+      selectedEntity?.konva.layer.x(selectedEntity?.konva.layer.x() + offset_x);
+    }
+    if (offset_y !== undefined) {
+      selectedEntity?.konva.layer.y(selectedEntity?.konva.layer.y() + offset_y);
     }
   };
 }
