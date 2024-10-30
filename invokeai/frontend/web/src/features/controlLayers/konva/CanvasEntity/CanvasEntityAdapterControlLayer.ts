cdee808c50bf78925ce0ca63a94fb4285190e568
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-10-30 10:02:39 +1000

    chore(ui): lint

--- invokeai/frontend/web/src/features/controlLayers/konva/CanvasEntity/CanvasEntityAdapterControlLayer.ts
+++ invokeai/frontend/web/src/features/controlLayers/konva/CanvasEntity/CanvasEntityAdapterControlLayer.ts
@@ -78,7 +78,12 @@ export class CanvasEntityAdapterControlLayer extends CanvasEntityAdapterBase<
   };
 
   getHashableState = (): SerializableObject => {
-    const keysToOmit: (keyof CanvasControlLayerState)[] = ['name', 'controlAdapter', 'withTransparencyEffect', 'isLocked'];
+    const keysToOmit: (keyof CanvasControlLayerState)[] = [
+      'name',
+      'controlAdapter',
+      'withTransparencyEffect',
+      'isLocked',
+    ];
     return omit(this.state, keysToOmit);
   };
 }
