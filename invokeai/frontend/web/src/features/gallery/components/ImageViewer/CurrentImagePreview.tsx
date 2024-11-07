bdbdb22b74217cbcb60a33ed916bbabb426ef251
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-11-06 20:32:47 -0500

    (ui) add Canvas Alert for invocation progress messages

--- invokeai/frontend/web/src/features/gallery/components/ImageViewer/CurrentImagePreview.tsx
+++ invokeai/frontend/web/src/features/gallery/components/ImageViewer/CurrentImagePreview.tsx
@@ -2,6 +2,7 @@ import { Box, Flex } from '@invoke-ai/ui-library';
 import { useStore } from '@nanostores/react';
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useAppSelector } from 'app/store/storeHooks';
+import { CanvasAlertsInvocationProgress } from 'features/controlLayers/components/CanvasAlerts/CanvasAlertsInvocationProgress';
 import { CanvasAlertsSendingToCanvas } from 'features/controlLayers/components/CanvasAlerts/CanvasAlertsSendingTo';
 import { DndImage } from 'features/dnd/DndImage';
 import ImageMetadataViewer from 'features/gallery/components/ImageMetadataViewer/ImageMetadataViewer';
@@ -49,6 +50,7 @@ const CurrentImagePreview = () => {
     >
       <ImageContent imageDTO={imageDTO} />
       <Box position="absolute" top={0} insetInlineStart={0}>
+        <CanvasAlertsInvocationProgress />
         <CanvasAlertsSendingToCanvas />
       </Box>
       {shouldShowImageDetails && imageDTO && (
