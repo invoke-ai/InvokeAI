22c53dce45ecf84c2b1e9ada950560f2d68f9545
Ryan Dick <ryanjdick3@gmail.com>
2024-11-13 22:17:59 +0000

    Add unit tests for FLUX diffusers LoRA without .proj_mlp layers.

--- tests/backend/lora/conversions/test_flux_diffusers_lora_conversion_utils.py
+++ tests/backend/lora/conversions/test_flux_diffusers_lora_conversion_utils.py
@@ -9,22 +9,26 @@ from invokeai.backend.lora.conversions.flux_lora_constants import FLUX_LORA_TRAN
 from tests.backend.lora.conversions.lora_state_dicts.flux_lora_diffusers_format import (
     state_dict_keys as flux_diffusers_state_dict_keys,
 )
+from tests.backend.lora.conversions.lora_state_dicts.flux_lora_diffusers_no_proj_mlp_format import (
+    state_dict_keys as flux_diffusers_no_proj_mlp_state_dict_keys,
+)
 from tests.backend.lora.conversions.lora_state_dicts.flux_lora_kohya_format import (
     state_dict_keys as flux_kohya_state_dict_keys,
 )
 from tests.backend.lora.conversions.lora_state_dicts.utils import keys_to_mock_state_dict
 
 
-def test_is_state_dict_likely_in_flux_diffusers_format_true():
+@pytest.mark.parametrize("sd_keys", [flux_diffusers_state_dict_keys, flux_diffusers_no_proj_mlp_state_dict_keys])
+def test_is_state_dict_likely_in_flux_diffusers_format_true(sd_keys: dict[str, list[int]]):
     """Test that is_state_dict_likely_in_flux_diffusers_format() can identify a state dict in the Diffusers FLUX LoRA format."""
     # Construct a state dict that is in the Diffusers FLUX LoRA format.
-    state_dict = keys_to_mock_state_dict(flux_diffusers_state_dict_keys)
+    state_dict = keys_to_mock_state_dict(sd_keys)
 
     assert is_state_dict_likely_in_flux_diffusers_format(state_dict)
 
 
 def test_is_state_dict_likely_in_flux_diffusers_format_false():
-    """Test that is_state_dict_likely_in_flux_diffusers_format() returns False for a state dict that is not in the Kohya
+    """Test that is_state_dict_likely_in_flux_diffusers_format() returns False for a state dict that is in the Kohya
     FLUX LoRA format.
     """
     # Construct a state dict that is not in the Kohya FLUX LoRA format.
@@ -33,16 +37,17 @@ def test_is_state_dict_likely_in_flux_diffusers_format_false():
     assert not is_state_dict_likely_in_flux_diffusers_format(state_dict)
 
 
-def test_lora_model_from_flux_diffusers_state_dict():
+@pytest.mark.parametrize("sd_keys", [flux_diffusers_state_dict_keys, flux_diffusers_no_proj_mlp_state_dict_keys])
+def test_lora_model_from_flux_diffusers_state_dict(sd_keys: dict[str, list[int]]):
     """Test that lora_model_from_flux_diffusers_state_dict() can load a state dict in the Diffusers FLUX LoRA format."""
     # Construct a state dict that is in the Diffusers FLUX LoRA format.
-    state_dict = keys_to_mock_state_dict(flux_diffusers_state_dict_keys)
+    state_dict = keys_to_mock_state_dict(sd_keys)
     # Load the state dict into a LoRAModelRaw object.
     model = lora_model_from_flux_diffusers_state_dict(state_dict, alpha=8.0)
 
     # Check that the model has the correct number of LoRA layers.
     expected_lora_layers: set[str] = set()
-    for k in flux_diffusers_state_dict_keys:
+    for k in sd_keys:
         k = k.replace("lora_A.weight", "")
         k = k.replace("lora_B.weight", "")
         expected_lora_layers.add(k)
