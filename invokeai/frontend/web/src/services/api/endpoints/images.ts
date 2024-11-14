a29d9e821f290dbd0f84b478fd157ff95d1c235b
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-11-14 12:35:17 -0800

    chore(ui): knip

--- invokeai/frontend/web/src/services/api/endpoints/images.ts
+++ invokeai/frontend/web/src/services/api/endpoints/images.ts
@@ -545,7 +545,6 @@ export const {
   useClearIntermediatesMutation,
   useAddImagesToBoardMutation,
   useRemoveImagesFromBoardMutation,
-  useChangeImageIsIntermediateMutation,
   useDeleteBoardAndImagesMutation,
   useDeleteBoardMutation,
   useStarImagesMutation,
