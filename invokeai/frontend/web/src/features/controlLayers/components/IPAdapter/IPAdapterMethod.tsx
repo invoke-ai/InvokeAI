965cd76e338e1959efd0eea3d760846e86c6e1e5
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2024-11-25 15:01:25 -0500

    lint fix

--- invokeai/frontend/web/src/features/controlLayers/components/IPAdapter/IPAdapterMethod.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/IPAdapter/IPAdapterMethod.tsx
@@ -1,13 +1,13 @@
 import type { ComboboxOnChange } from '@invoke-ai/ui-library';
 import { Combobox, FormControl, FormLabel } from '@invoke-ai/ui-library';
+import { useAppSelector } from 'app/store/storeHooks';
 import { InformationalPopover } from 'common/components/InformationalPopover/InformationalPopover';
 import type { IPMethodV2 } from 'features/controlLayers/store/types';
 import { isIPMethodV2 } from 'features/controlLayers/store/types';
+import { selectSystemShouldEnableModelDescriptions } from 'features/system/store/systemSlice';
 import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { assert } from 'tsafe';
-import { useAppSelector } from '../../../../app/store/storeHooks';
-import { selectSystemShouldEnableModelDescriptions } from '../../../system/store/systemSlice';
 
 type Props = {
   method: IPMethodV2;
@@ -20,9 +20,21 @@ export const IPAdapterMethod = memo(({ method, onChange }: Props) => {
 
   const options: { label: string; value: IPMethodV2 }[] = useMemo(
     () => [
-      { label: t('controlLayers.ipAdapterMethod.full'), value: 'full', description: shouldShowModelDescriptions ? t('controlLayers.ipAdapterMethod.fullDesc'): undefined },
-      { label: t('controlLayers.ipAdapterMethod.style'), value: 'style', description: shouldShowModelDescriptions ? t('controlLayers.ipAdapterMethod.styleDesc') : undefined },
-      { label: t('controlLayers.ipAdapterMethod.composition'), value: 'composition', description: shouldShowModelDescriptions ? t('controlLayers.ipAdapterMethod.compositionDesc'): undefined },
+      {
+        label: t('controlLayers.ipAdapterMethod.full'),
+        value: 'full',
+        description: shouldShowModelDescriptions ? t('controlLayers.ipAdapterMethod.fullDesc') : undefined,
+      },
+      {
+        label: t('controlLayers.ipAdapterMethod.style'),
+        value: 'style',
+        description: shouldShowModelDescriptions ? t('controlLayers.ipAdapterMethod.styleDesc') : undefined,
+      },
+      {
+        label: t('controlLayers.ipAdapterMethod.composition'),
+        value: 'composition',
+        description: shouldShowModelDescriptions ? t('controlLayers.ipAdapterMethod.compositionDesc') : undefined,
+      },
     ],
     [t, shouldShowModelDescriptions]
   );
