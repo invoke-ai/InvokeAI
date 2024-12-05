a39bcf7e854e36e042f754e91400ec0f89b7da70
Ryan Dick <ryanjdick3@gmail.com>
2024-12-05 16:11:40 +0000

    Move lock(...) and unlock(...) logic from ModelLocker to the ModelCache and make a bunch of ModelCache properties/methods private.

--- invokeai/backend/model_manager/load/model_cache/model_locker.py
+++ invokeai/backend/model_manager/load/model_cache/model_locker.py
@@ -12,15 +12,7 @@ from invokeai.backend.model_manager.load.model_cache.model_cache import ModelCac
 
 
 class ModelLocker:
-    """Internal class that mediates movement in and out of GPU."""
-
     def __init__(self, cache: ModelCache, cache_entry: CacheRecord):
-        """
-        Initialize the model locker.
-
-        :param cache: The ModelCache object
-        :param cache_entry: The entry in the model cache
-        """
         self._cache = cache
         self._cache_entry = cache_entry
 
@@ -35,27 +27,9 @@ class ModelLocker:
 
     def lock(self) -> AnyModel:
         """Move the model into the execution device (GPU) and lock it."""
-        self._cache_entry.lock()
-        try:
-            if self._cache.lazy_offloading:
-                self._cache.offload_unlocked_models(self._cache_entry.size)
-            self._cache.move_model_to_device(self._cache_entry, self._cache.execution_device)
-            self._cache_entry.loaded = True
-            self._cache.logger.debug(f"Locking {self._cache_entry.key} in {self._cache.execution_device}")
-            self._cache.print_cuda_stats()
-        except torch.cuda.OutOfMemoryError:
-            self._cache.logger.warning("Insufficient GPU memory to load model. Aborting")
-            self._cache_entry.unlock()
-            raise
-        except Exception:
-            self._cache_entry.unlock()
-            raise
-
+        self._cache.lock(self._cache_entry.key)
         return self.model
 
     def unlock(self) -> None:
-        """Call upon exit from context."""
-        self._cache_entry.unlock()
-        if not self._cache.lazy_offloading:
-            self._cache.offload_unlocked_models(0)
-            self._cache.print_cuda_stats()
+        """Unlock a model."""
+        self._cache.unlock(self._cache_entry.key)
