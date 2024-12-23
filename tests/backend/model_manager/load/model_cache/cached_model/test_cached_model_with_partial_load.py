f8ab414f99eee4f21e893b0eb57f65c1778abd1c
Ryan Dick <ryanjdick3@gmail.com>
2024-12-23 18:36:13 +0000

    Add CachedModelOnlyFullLoad to mirror the CachedModelWithPartialLoad for models that cannot or should not be partially loaded.

--- tests/backend/model_manager/load/model_cache/cached_model/test_cached_model_with_partial_load.py
+++ tests/backend/model_manager/load/model_cache/cached_model/test_cached_model_with_partial_load.py
@@ -1,6 +1,5 @@
 import itertools
 
-import pytest
 import torch
 
 from invokeai.backend.model_manager.load.model_cache.cached_model.cached_model_with_partial_load import (
@@ -8,35 +7,7 @@ from invokeai.backend.model_manager.load.model_cache.cached_model.cached_model_w
 )
 from invokeai.backend.model_manager.load.model_cache.torch_module_autocast.autocast_modules import CustomLinear
 from invokeai.backend.util.calc_tensor_size import calc_tensor_size
-
-
-class DummyModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.linear1 = torch.nn.Linear(10, 32)
-        self.linear2 = torch.nn.Linear(32, 64)
-        self.register_buffer("buffer1", torch.ones(64))
-        # Non-persistent buffers are not included in the state dict. We need to make sure that this case is handled
-        # correctly by the partial loading code.
-        self.register_buffer("buffer2", torch.ones(64), persistent=False)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.linear1(x)
-        x = self.linear2(x)
-        x = x + self.buffer1
-        x = x + self.buffer2
-        return x
-
-
-parameterize_mps_and_cuda = pytest.mark.parametrize(
-    ("device"),
-    [
-        pytest.param(
-            "mps", marks=pytest.mark.skipif(not torch.backends.mps.is_available(), reason="MPS is not available.")
-        ),
-        pytest.param("cuda", marks=pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA is not available.")),
-    ],
-)
+from tests.backend.model_manager.load.model_cache.cached_model.utils import DummyModule, parameterize_mps_and_cuda
 
 
 @parameterize_mps_and_cuda
