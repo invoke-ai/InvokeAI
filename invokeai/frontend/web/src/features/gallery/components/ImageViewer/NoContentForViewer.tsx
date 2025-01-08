5246b3134758a4142102985bf732585c6bd6205c
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-01-08 18:10:59 +1100

    feat(ui): add low vram link to first run page

--- invokeai/frontend/web/src/features/gallery/components/ImageViewer/NoContentForViewer.tsx
+++ invokeai/frontend/web/src/features/gallery/components/ImageViewer/NoContentForViewer.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Flex, Spinner, Text } from '@invoke-ai/ui-library';
+import { Button, Divider, Flex, Icon, Link, Spinner, Text } from '@invoke-ai/ui-library';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
 import { IAINoContentFallback } from 'common/components/IAIImageFallback';
 import { InvokeLogoIcon } from 'common/components/InvokeLogoIcon';
@@ -7,10 +7,20 @@ import { $installModelsTab } from 'features/modelManagerV2/subpanels/InstallMode
 import { useFeatureStatus } from 'features/system/hooks/useFeatureStatus';
 import { selectIsLocal } from 'features/system/store/configSlice';
 import { setActiveTab } from 'features/ui/store/uiSlice';
+import type { PropsWithChildren } from 'react';
 import { memo, useCallback, useMemo } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { PiImageBold } from 'react-icons/pi';
+import { PiArrowSquareOutBold, PiImageBold } from 'react-icons/pi';
 import { useMainModels } from 'services/api/hooks/modelsByType';
+
+const ExternalLink = (props: PropsWithChildren<{ href: string }>) => {
+  return (
+    <Link isExternal display="inline-flex" alignItems="center" href={props.href} color="base.50">
+      {props.children}
+      <Icon display="inline" verticalAlign="middle" marginInlineStart={2} as={PiArrowSquareOutBold} />
+    </Link>
+  );
+};
 
 export const NoContentForViewer = memo(() => {
   const hasImages = useHasImages();
@@ -54,23 +64,35 @@ export const NoContentForViewer = memo(() => {
     <Flex flexDir="column" gap={4} alignItems="center" textAlign="center" maxW="600px">
       <InvokeLogoIcon w={40} h={40} />
       <Flex flexDir="column" gap={8} alignItems="center" textAlign="center">
-        <Text fontSize="md" color="base.200" pt={16}>
-          {isLocal ? (
-            <Trans
-              i18nKey="newUserExperience.toGetStartedLocal"
-              components={{
-                StrongComponent: <Text as="span" color="white" fontSize="md" fontWeight="semibold" />,
-              }}
-            />
-          ) : (
+        {isLocal ? (
+          <>
+            <Text fontSize="md" color="base.200" pt={16}>
+              <Trans
+                i18nKey="newUserExperience.toGetStartedLocal"
+                components={{
+                  StrongComponent: <Text as="span" color="white" fontSize="md" fontWeight="semibold" />,
+                }}
+              />
+            </Text>
+            <Text fontSize="md" color="base.200">
+              <Trans
+                i18nKey="newUserExperience.lowVRAMMode"
+                components={{
+                  LinkComponent: <ExternalLink href="https://invoke-ai.github.io/InvokeAI/features/low-vram/" />,
+                }}
+              />
+            </Text>
+          </>
+        ) : (
+          <Text fontSize="md" color="base.200" pt={16}>
             <Trans
               i18nKey="newUserExperience.toGetStarted"
               components={{
                 StrongComponent: <Text as="span" color="white" fontSize="md" fontWeight="semibold" />,
               }}
             />
-          )}
-        </Text>
+          </Text>
+        )}
 
         {showStarterBundles && (
           <Flex flexDir="column" gap={2} alignItems="center">
@@ -98,14 +120,7 @@ export const NoContentForViewer = memo(() => {
             i18nKey="newUserExperience.gettingStartedSeries"
             components={{
               LinkComponent: (
-                <Text
-                  as="a"
-                  color="white"
-                  fontSize="md"
-                  fontWeight="semibold"
-                  href="https://www.youtube.com/playlist?list=PLvWK1Kc8iXGrQy8r9TYg6QdUuJ5MMx-ZO"
-                  target="_blank"
-                />
+                <ExternalLink href="https://www.youtube.com/playlist?list=PLvWK1Kc8iXGrQy8r9TYg6QdUuJ5MMx-ZO" />
               ),
             }}
           />
