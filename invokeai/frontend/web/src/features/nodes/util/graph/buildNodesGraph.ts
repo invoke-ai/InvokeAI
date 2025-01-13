38e50cc7aa86e664495017ec5a7819d45c09e93f
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-01-13 19:25:39 +1100

    tidy(ui): abstract out batch detection logic

--- invokeai/frontend/web/src/features/nodes/util/graph/buildNodesGraph.ts
+++ invokeai/frontend/web/src/features/nodes/util/graph/buildNodesGraph.ts
@@ -1,29 +1,11 @@
 import { logger } from 'app/logging/logger';
 import type { NodesState } from 'features/nodes/store/types';
-import type { InvocationNode } from 'features/nodes/types/invocation';
-import { isInvocationNode } from 'features/nodes/types/invocation';
-import { omit, reduce } from 'lodash-es';
+import { isBatchNode, isInvocationNode } from 'features/nodes/types/invocation';
+import { negate, omit, reduce } from 'lodash-es';
 import type { AnyInvocation, Graph } from 'services/api/types';
 import { v4 as uuidv4 } from 'uuid';
 
 const log = logger('workflows');
-
-// These nodes are not executable, they exist for the frontend only
-export const filterNonExecutableNodes = (node: InvocationNode) => {
-  if (node.data.type === 'image_batch') {
-    return false;
-  }
-  if (node.data.type === 'string_batch') {
-    return false;
-  }
-  if (node.data.type === 'integer_batch') {
-    return false;
-  }
-  if (node.data.type === 'float_batch') {
-    return false;
-  }
-  return true;
-};
 
 /**
  * Builds a graph from the node editor state.
@@ -32,7 +14,7 @@ export const buildNodesGraph = (nodesState: NodesState): Graph => {
   const { nodes, edges } = nodesState;
 
   // Exclude all batch nodes - we will handle these in the batch setup in a diff function
-  const filteredNodes = nodes.filter(isInvocationNode).filter(filterNonExecutableNodes);
+  const filteredNodes = nodes.filter(isInvocationNode).filter(negate(isBatchNode));
 
   // Reduce the node editor nodes into invocation graph nodes
   const parsedNodes = filteredNodes.reduce<NonNullable<Graph['nodes']>>((nodesAccumulator, node) => {
