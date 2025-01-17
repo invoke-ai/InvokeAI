9bc9cc96a6fa667b74e7a0e1e41ed4e1a3e90be3
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-01-17 16:31:08 +1100

    feat(ui): add notes state to fields

--- invokeai/frontend/web/src/features/nodes/types/field.ts
+++ invokeai/frontend/web/src/features/nodes/types/field.ts
@@ -38,6 +38,7 @@ const zFieldUIComponent = z.enum(['none', 'textarea', 'slider']);
 const zFieldInputInstanceBase = z.object({
   name: z.string().trim().min(1),
   label: z.string().nullish(),
+  notes: z.string().nullish(),
 });
 const zFieldTemplateBase = z.object({
   name: z.string().min(1),
