f1de722beb63a536e28dbe9598b9a843ddec69ca
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-01-17 17:33:27 +1100

    feat(ui): add ViewContext so components can know where they are being rendered (user-linear view, editor-linear view, or editor-nodes view)

---
+++ invokeai/frontend/web/src/features/nodes/contexts/ViewContext.tsx
@@ -0,0 +1,19 @@
+import type { PropsWithChildren } from 'react';
+import { createContext, memo, useContext } from 'react';
+import { assert } from 'tsafe';
+
+type ViewType = 'user-linear' | 'editor-linear' | 'editor-nodes';
+
+const ViewContext = createContext<ViewType | null>(null);
+
+export const ViewContextProvider = memo((props: PropsWithChildren<{ viewType: ViewType }>) => {
+  return <ViewContext.Provider value={props.viewType}>{props.children}</ViewContext.Provider>;
+});
+
+ViewContextProvider.displayName = 'ViewContextProvider';
+
+export const useViewContext = () => {
+  const context = useContext(ViewContext);
+  assert(context !== null, 'useViewContext must be used within a ViewContextProvider');
+  return context;
+};
