7f75315a8c5114e819e9a86a1c5ad92fc02ecb96
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-01-19 10:14:30 +1100

    feat(api): add seed field to dynamicprompts

--- invokeai/app/api/routers/utilities.py
+++ invokeai/app/api/routers/utilities.py
@@ -25,6 +25,7 @@ async def parse_dynamicprompts(
     prompt: str = Body(description="The prompt to parse with dynamicprompts"),
     max_prompts: int = Body(ge=1, le=10000, default=1000, description="The max number of prompts to generate"),
     combinatorial: bool = Body(default=True, description="Whether to use the combinatorial generator"),
+    seed: int | None = Body(None, description="The seed to use for random generation. Only used if not combinatorial"),
 ) -> DynamicPromptsResponse:
     """Creates a batch process"""
     max_prompts = min(max_prompts, 10000)
@@ -35,7 +36,7 @@ async def parse_dynamicprompts(
             generator = CombinatorialPromptGenerator()
             prompts = generator.generate(prompt, max_prompts=max_prompts)
         else:
-            generator = RandomPromptGenerator()
+            generator = RandomPromptGenerator(seed=seed)
             prompts = generator.generate(prompt, num_images=max_prompts)
     except ParseException as e:
         prompts = [prompt]
