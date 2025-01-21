d988c26e3b78ac1a6ba41022628d13d186d80f8d
skunkworxdark <neil_hodgson@hotmail.com>
2025-01-21 09:48:22 +0000

    fix typegen error
    fix typegen

--- invokeai/app/invocations/model.py
+++ invokeai/app/invocations/model.py
@@ -261,7 +261,7 @@ class LoRACollectionLoader(BaseInvocation):
     """Applies a collection of LoRAs to the provided UNet and CLIP models."""
 
     loras: Optional[LoRAField | list[LoRAField]] = InputField(
-        default=None, description="LoRA models and weights. May be a single LoRA or collection.", title="LoRAs"
+        default=[], description="LoRA models and weights. May be a single LoRA or collection.", title="LoRAs"
     )
     unet: Optional[UNetField] = InputField(
         default=None,
@@ -287,8 +287,6 @@ class LoRACollectionLoader(BaseInvocation):
             output.clip = self.clip.model_copy(deep=True)
 
         for lora in loras:
-            if lora is None:
-                continue
             assert type(lora) is LoRAField
             if lora.lora.key in added_loras:
                 continue
@@ -409,7 +407,7 @@ class SDXLLoRACollectionLoader(BaseInvocation):
     """Applies a collection of SDXL LoRAs to the provided UNet and CLIP models."""
 
     loras: Optional[LoRAField | list[LoRAField]] = InputField(
-        default=None, description="LoRA models and weights. May be a single LoRA or collection.", title="LoRAs"
+        default=[], description="LoRA models and weights. May be a single LoRA or collection.", title="LoRAs"
     )
     unet: Optional[UNetField] = InputField(
         default=None,
@@ -445,8 +443,6 @@ class SDXLLoRACollectionLoader(BaseInvocation):
             output.clip2 = self.clip2.model_copy(deep=True)
 
         for lora in loras:
-            if lora is None:
-                continue
             assert type(lora) is LoRAField
             if lora.lora.key in added_loras:
                 continue
