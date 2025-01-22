81e385a756697fdcc5d0f85a6fec2e118370b4c6
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-01-23 07:13:08 +1100

    feat(ui): iterate on builder (WIP)

--- invokeai/frontend/web/src/features/nodes/components/sidePanel/builder/DividerElementComponent.tsx
+++ invokeai/frontend/web/src/features/nodes/components/sidePanel/builder/DividerElementComponent.tsx
@@ -1,20 +1,18 @@
+import type { SystemStyleObject } from '@invoke-ai/ui-library';
 import { Flex } from '@invoke-ai/ui-library';
-import { useContainerDirectionContext } from 'features/nodes/components/sidePanel/builder/ContainerContext';
+import { getPrefixedId } from 'features/controlLayers/konva/util';
 import { memo } from 'react';
 
-export const DividerElementComponent = memo(({ id }: { id: string }) => {
-  const containerDirection = useContainerDirectionContext();
+export const DIVIDER_CLASS_NAME = getPrefixedId('divider');
 
-  return (
-    <Flex
-      flex="0 0 auto"
-      id={id}
-      h={containerDirection === 'column' ? '1px' : undefined}
-      w={containerDirection === 'column' ? undefined : '1px'}
-      bg="base.700"
-      flexShrink={0}
-    />
-  );
+const sx: SystemStyleObject = {
+  flex: '0 0 1px',
+  bg: 'base.700',
+  flexShrink: 0,
+};
+
+export const DividerElementComponent = memo(({ id }: { id: string }) => {
+  return <Flex id={id} className={DIVIDER_CLASS_NAME} sx={sx} />;
 });
 
 DividerElementComponent.displayName = 'DividerElementComponent';
