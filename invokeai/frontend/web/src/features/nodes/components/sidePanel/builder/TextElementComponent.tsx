9f07e83a23f21abcd2f8f261d291d519de2ee877
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-01-23 16:35:57 +1100

    feat(ui): iterate on builder (WIP)

--- invokeai/frontend/web/src/features/nodes/components/sidePanel/builder/TextElementComponent.tsx
+++ invokeai/frontend/web/src/features/nodes/components/sidePanel/builder/TextElementComponent.tsx
@@ -1,16 +1,31 @@
 import { Flex, Text } from '@invoke-ai/ui-library';
-import { useElement } from 'features/nodes/store/workflowSlice';
+import { useAppSelector } from 'app/store/storeHooks';
+import { FormElementEditModeWrapper } from 'features/nodes/components/sidePanel/builder/FormElementEditModeWrapper';
+import { selectWorkflowFormMode, useElement } from 'features/nodes/store/workflowSlice';
+import type { TextElement } from 'features/nodes/types/workflow';
 import { isTextElement, TEXT_CLASS_NAME } from 'features/nodes/types/workflow';
 import { memo } from 'react';
 
 export const TextElementComponent = memo(({ id }: { id: string }) => {
   const el = useElement(id);
+  const mode = useAppSelector(selectWorkflowFormMode);
 
   if (!el || !isTextElement(el)) {
     return null;
   }
 
-  const { content, fontSize } = el.data;
+  if (mode === 'view') {
+    return <TextElementComponentViewMode el={el} />;
+  }
+
+  // mode === 'edit'
+  return <TextElementComponentEditMode el={el} />;
+});
+TextElementComponent.displayName = 'TextElementComponent';
+
+export const TextElementComponentViewMode = memo(({ el }: { el: TextElement }) => {
+  const { id, data } = el;
+  const { content, fontSize } = data;
 
   return (
     <Flex id={id} className={TEXT_CLASS_NAME}>
@@ -18,5 +33,18 @@ export const TextElementComponent = memo(({ id }: { id: string }) => {
     </Flex>
   );
 });
+TextElementComponentViewMode.displayName = 'TextElementComponentViewMode';
 
-TextElementComponent.displayName = 'TextElementComponent';
+export const TextElementComponentEditMode = memo(({ el }: { el: TextElement }) => {
+  const { id, data } = el;
+  const { content, fontSize } = data;
+
+  return (
+    <Flex id={id} className={TEXT_CLASS_NAME}>
+      <FormElementEditModeWrapper element={el}>
+        <Text fontSize={fontSize}>{content}</Text>
+      </FormElementEditModeWrapper>
+    </Flex>
+  );
+});
+TextElementComponentEditMode.displayName = 'TextElementComponentEditMode';
