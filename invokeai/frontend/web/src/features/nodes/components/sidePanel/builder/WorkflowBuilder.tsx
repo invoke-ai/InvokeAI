2aa1fc9301fa6d5b87fce4bef184ee555f5f0925
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-01-24 14:57:07 +1100

    feat(ui): dnd mostly working (WIP)

--- invokeai/frontend/web/src/features/nodes/components/sidePanel/builder/WorkflowBuilder.tsx
+++ invokeai/frontend/web/src/features/nodes/components/sidePanel/builder/WorkflowBuilder.tsx
@@ -2,6 +2,7 @@ import { Button, Flex } from '@invoke-ai/ui-library';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
 import ScrollableContent from 'common/components/OverlayScrollbars/ScrollableContent';
 import { FormElementComponent } from 'features/nodes/components/sidePanel/builder/ContainerElementComponent';
+import { useMonitorForFormElementDnd } from 'features/nodes/components/sidePanel/builder/use-builder-dnd';
 import { formLoaded, formModeToggled, selectWorkflowFormMode } from 'features/nodes/store/workflowSlice';
 import { elements, rootElementId } from 'features/nodes/types/workflow';
 import { memo, useCallback, useEffect } from 'react';
@@ -9,14 +10,16 @@ import { memo, useCallback, useEffect } from 'react';
 export const WorkflowBuilder = memo(() => {
   const dispatch = useAppDispatch();
   const mode = useAppSelector(selectWorkflowFormMode);
+  useMonitorForFormElementDnd();
 
   useEffect(() => {
+    // dispatch(formReset());
     dispatch(formLoaded({ elements, rootElementId }));
   }, [dispatch]);
   return (
     <ScrollableContent>
       <Flex w="full" justifyContent="center">
-        <Flex flexDir="column" w={mode === 'view' ? '768px' : 'min-content'} minW='768px'>
+        <Flex flexDir="column" w={mode === 'view' ? '512px' : 'min-content'} minW="512px">
           <ToggleModeButton />
           {rootElementId && <FormElementComponent id={rootElementId} />}
         </Flex>
