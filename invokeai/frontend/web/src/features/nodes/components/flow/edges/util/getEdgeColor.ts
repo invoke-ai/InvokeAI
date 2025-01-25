fb77d271abe36efdde7d02ffd5f95cac19f7a78e
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-01-26 10:53:39 +1100

    refactor(ui): edge rendering
    - Fix issues with positioning of labels
    - Optimize styling to be less reliant on JS

--- invokeai/frontend/web/src/features/nodes/components/flow/edges/util/getEdgeColor.ts
+++ invokeai/frontend/web/src/features/nodes/components/flow/edges/util/getEdgeColor.ts
@@ -1,7 +1,6 @@
 import { colorTokenToCssVar } from 'common/util/colorTokenToCssVar';
 import { FIELD_COLORS } from 'features/nodes/types/constants';
 import type { FieldType } from 'features/nodes/types/field';
-import type { CSSProperties } from 'react';
 
 export const getFieldColor = (fieldType: FieldType | null): string => {
   if (!fieldType) {
@@ -11,16 +10,3 @@ export const getFieldColor = (fieldType: FieldType | null): string => {
 
   return color ? colorTokenToCssVar(color) : colorTokenToCssVar('base.500');
 };
-
-export const getEdgeStyles = (
-  stroke: string,
-  selected: boolean,
-  shouldAnimateEdges: boolean,
-  areConnectedNodesSelected: boolean
-): CSSProperties => ({
-  strokeWidth: 3,
-  stroke,
-  opacity: selected ? 1 : 0.5,
-  animation: shouldAnimateEdges ? 'dashdraw 0.5s linear infinite' : undefined,
-  strokeDasharray: selected || areConnectedNodesSelected ? 5 : 'none',
-});
