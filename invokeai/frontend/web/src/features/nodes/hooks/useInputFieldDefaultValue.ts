ce286363d0e1639b97dbd76ec00cf00c85bb2e4e
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-02-16 09:29:52 +1000

    perf(ui): optimize checking if a field value is changed by wrapping in single selector

--- invokeai/frontend/web/src/features/nodes/hooks/useInputFieldDefaultValue.ts
+++ invokeai/frontend/web/src/features/nodes/hooks/useInputFieldDefaultValue.ts
@@ -1,19 +1,29 @@
-import { useAppDispatch } from 'app/store/storeHooks';
+import { createSelector } from '@reduxjs/toolkit';
+import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
 import { useInputFieldTemplate } from 'features/nodes/hooks/useInputFieldTemplate';
-import { useInputFieldValue } from 'features/nodes/hooks/useInputFieldValue';
 import { fieldValueReset } from 'features/nodes/store/nodesSlice';
+import { selectNodesSlice } from 'features/nodes/store/selectors';
+import { isInvocationNode } from 'features/nodes/types/invocation';
 import { isEqual } from 'lodash-es';
 import { useCallback, useMemo } from 'react';
 
 export const useInputFieldDefaultValue = (nodeId: string, fieldName: string) => {
   const dispatch = useAppDispatch();
 
-  const value = useInputFieldValue(nodeId, fieldName);
   const fieldTemplate = useInputFieldTemplate(nodeId, fieldName);
-
-  const isValueChanged = useMemo(() => {
-    return !isEqual(value, fieldTemplate.default);
-  }, [value, fieldTemplate.default]);
+  const selectIsChanged = useMemo(
+    () =>
+      createSelector(selectNodesSlice, (nodes) => {
+        const node = nodes.nodes.find((node) => node.id === nodeId);
+        if (!isInvocationNode(node)) {
+          return;
+        }
+        const value = node.data.inputs[fieldName]?.value;
+        return !isEqual(value, fieldTemplate.default);
+      }),
+    [fieldName, fieldTemplate.default, nodeId]
+  );
+  const isValueChanged = useAppSelector(selectIsChanged);
 
   const resetToDefaultValue = useCallback(() => {
     dispatch(fieldValueReset({ nodeId, fieldName, value: fieldTemplate.default }));
