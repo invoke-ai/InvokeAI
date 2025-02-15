68a6374929d41744a3cb5485dabee593b4a76345
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-02-15 20:51:49 +1000

    perf(ui): optimized invocation node component structure

--- invokeai/frontend/web/src/features/nodes/hooks/useInputFieldNamesByStatus.ts
+++ invokeai/frontend/web/src/features/nodes/hooks/useInputFieldNamesByStatus.ts
@@ -3,37 +3,56 @@ import { useNodeTemplate } from 'features/nodes/hooks/useNodeTemplate';
 import type { FieldInputTemplate } from 'features/nodes/types/field';
 import { isSingleOrCollection } from 'features/nodes/types/field';
 import { TEMPLATE_BUILDER_MAP } from 'features/nodes/util/schema/buildFieldInputTemplate';
-import { difference, filter, keys } from 'lodash-es';
 import { useMemo } from 'react';
 
 const isConnectionInputField = (field: FieldInputTemplate) => {
   return (
-    (field.input === 'connection' && !isSingleOrCollection(field.type)) ||
-    !keys(TEMPLATE_BUILDER_MAP).includes(field.type.name)
+    (field.input === 'connection' && !isSingleOrCollection(field.type)) || !(field.type.name in TEMPLATE_BUILDER_MAP)
   );
 };
 
 const isAnyOrDirectInputField = (field: FieldInputTemplate) => {
   return (
     (['any', 'direct'].includes(field.input) || isSingleOrCollection(field.type)) &&
-    keys(TEMPLATE_BUILDER_MAP).includes(field.type.name)
+    field.type.name in TEMPLATE_BUILDER_MAP
   );
 };
 
-export const useInputFieldNamesByStatus = (nodeId: string) => {
+export const useInputFieldNamesMissing = (nodeId: string) => {
   const template = useNodeTemplate(nodeId);
   const node = useNodeData(nodeId);
   const fieldNames = useMemo(() => {
-    const instanceFields = keys(node.inputs);
-    const allTemplateFields = keys(template.inputs);
-    const missingFields = difference(instanceFields, allTemplateFields);
-    const connectionFields = filter(template.inputs, isConnectionInputField).map((f) => f.name);
-    const anyOrDirectFields = filter(template.inputs, isAnyOrDirectInputField).map((f) => f.name);
-    return {
-      missingFields,
-      connectionFields,
-      anyOrDirectFields,
-    };
+    const instanceFields = new Set(Object.keys(node.inputs));
+    const allTemplateFields = new Set(Object.keys(template.inputs));
+    return Array.from(instanceFields.difference(allTemplateFields));
   }, [node.inputs, template.inputs]);
+  return fieldNames;
+};
+
+export const useInputFieldNamesAnyOrDirect = (nodeId: string) => {
+  const template = useNodeTemplate(nodeId);
+  const fieldNames = useMemo(() => {
+    const anyOrDirectFields: string[] = [];
+    for (const [fieldName, fieldTemplate] of Object.entries(template.inputs)) {
+      if (isAnyOrDirectInputField(fieldTemplate)) {
+        anyOrDirectFields.push(fieldName);
+      }
+    }
+    return anyOrDirectFields;
+  }, [template.inputs]);
+  return fieldNames;
+};
+
+export const useInputFieldNamesConnection = (nodeId: string) => {
+  const template = useNodeTemplate(nodeId);
+  const fieldNames = useMemo(() => {
+    const connectionFields: string[] = [];
+    for (const [fieldName, fieldTemplate] of Object.entries(template.inputs)) {
+      if (isConnectionInputField(fieldTemplate)) {
+        connectionFields.push(fieldName);
+      }
+    }
+    return connectionFields;
+  }, [template.inputs]);
   return fieldNames;
 };
