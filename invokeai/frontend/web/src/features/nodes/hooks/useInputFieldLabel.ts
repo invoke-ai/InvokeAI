c379d768443072cc24651cc03387b58171418db1
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-02-20 11:34:00 +1000

    feat(ui): add "unsafe" version of field instance selector

--- invokeai/frontend/web/src/features/nodes/hooks/useInputFieldLabel.ts
+++ invokeai/frontend/web/src/features/nodes/hooks/useInputFieldLabel.ts
@@ -1,13 +1,13 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { useAppSelector } from 'app/store/storeHooks';
-import { selectFieldInputInstance, selectNodesSlice } from 'features/nodes/store/selectors';
+import { selectFieldInputInstanceSafe, selectNodesSlice } from 'features/nodes/store/selectors';
 import { useMemo } from 'react';
 
 export const useInputFieldLabel = (nodeId: string, fieldName: string): string => {
   const selector = useMemo(
     () =>
       createSelector(selectNodesSlice, (nodes) => {
-        return selectFieldInputInstance(nodes, nodeId, fieldName)?.label ?? '';
+        return selectFieldInputInstanceSafe(nodes, nodeId, fieldName)?.label ?? '';
       }),
     [fieldName, nodeId]
   );
