a3f172546a4f4f8f8815243b88c6850ac59067e0
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-02-24 06:53:25 +1000

    fix(ui): star button not working on Chrome
    Not sure why the perf optimisation doesn't work on Chrome but I reverted it.

--- invokeai/frontend/web/src/features/gallery/components/ImageGrid/GalleryImageHoverIcons.tsx
+++ invokeai/frontend/web/src/features/gallery/components/ImageGrid/GalleryImageHoverIcons.tsx
@@ -1,22 +1,19 @@
-import { useStore } from '@nanostores/react';
 import { useAppSelector } from 'app/store/storeHooks';
 import { GalleryImageDeleteIconButton } from 'features/gallery/components/ImageGrid/GalleryImageDeleteIconButton';
 import { GalleryImageOpenInViewerIconButton } from 'features/gallery/components/ImageGrid/GalleryImageOpenInViewerIconButton';
 import { GalleryImageSizeBadge } from 'features/gallery/components/ImageGrid/GalleryImageSizeBadge';
 import { GalleryImageStarIconButton } from 'features/gallery/components/ImageGrid/GalleryImageStarIconButton';
 import { selectAlwaysShouldImageSizeBadge } from 'features/gallery/store/gallerySelectors';
-import type { Atom } from 'nanostores';
 import { memo } from 'react';
 import type { ImageDTO } from 'services/api/types';
 
 type Props = {
   imageDTO: ImageDTO;
-  $isHovered: Atom<boolean>;
+  isHovered: boolean;
 };
 
-export const GalleryImageHoverIcons = memo(({ imageDTO, $isHovered }: Props) => {
+export const GalleryImageHoverIcons = memo(({ imageDTO, isHovered }: Props) => {
   const alwaysShowImageSizeBadge = useAppSelector(selectAlwaysShouldImageSizeBadge);
-  const isHovered = useStore($isHovered);
 
   return (
     <>
