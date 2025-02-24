6469f426c59ece351d574d26e817b01f3e41beef
Ryan Dick <ryanjdick3@gmail.com>
2025-02-24 14:20:13 -0500

    Mark test_configure_torch_cuda_allocator_raises_if_torch_is_already_imported() to only run if CUDA is available.

--- tests/app/util/test_torch_cuda_allocator.py
+++ tests/app/util/test_torch_cuda_allocator.py
@@ -1,10 +1,12 @@
 import pytest
+import torch
 
 from invokeai.app.util.torch_cuda_allocator import configure_torch_cuda_allocator
 
 
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="Requires CUDA device.")
 def test_configure_torch_cuda_allocator_raises_if_torch_is_already_imported():
-    """Test that enable_torch_cuda_malloc() raises a RuntimeError if torch is already imported."""
+    """Test that configure_torch_cuda_allocator() raises a RuntimeError if torch is already imported."""
     import torch  # noqa: F401
 
     with pytest.raises(RuntimeError, match="Failed to configure the PyTorch CUDA memory allocator."):
