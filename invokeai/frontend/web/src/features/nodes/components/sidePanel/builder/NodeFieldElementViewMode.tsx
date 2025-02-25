b666ef41ff0147b21f19368c3c6ab05014a4940b
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-02-25 16:26:00 +1000

    fix(ui): various styling fixes

--- invokeai/frontend/web/src/features/nodes/components/sidePanel/builder/NodeFieldElementViewMode.tsx
+++ invokeai/frontend/web/src/features/nodes/components/sidePanel/builder/NodeFieldElementViewMode.tsx
@@ -1,5 +1,7 @@
+import type { SystemStyleObject } from '@invoke-ai/ui-library';
 import { Flex, FormControl, FormHelperText } from '@invoke-ai/ui-library';
 import { InputFieldRenderer } from 'features/nodes/components/flow/nodes/Invocation/fields/InputFieldRenderer';
+import { useContainerContext } from 'features/nodes/components/sidePanel/builder/contexts';
 import { NodeFieldElementLabel } from 'features/nodes/components/sidePanel/builder/NodeFieldElementLabel';
 import { useInputFieldDescription } from 'features/nodes/hooks/useInputFieldDescription';
 import { useInputFieldTemplate } from 'features/nodes/hooks/useInputFieldTemplate';
@@ -7,11 +9,22 @@ import type { NodeFieldElement } from 'features/nodes/types/workflow';
 import { NODE_FIELD_CLASS_NAME } from 'features/nodes/types/workflow';
 import { memo, useMemo } from 'react';
 
+const sx: SystemStyleObject = {
+  '&[data-parent-layout="column"]': {
+    w: 'full',
+    h: 'min-content',
+  },
+  '&[data-parent-layout="row"]': {
+    flex: '1 1 0',
+  },
+};
+
 export const NodeFieldElementViewMode = memo(({ el }: { el: NodeFieldElement }) => {
   const { id, data } = el;
   const { fieldIdentifier, showDescription } = data;
   const description = useInputFieldDescription(fieldIdentifier.nodeId, fieldIdentifier.fieldName);
   const fieldTemplate = useInputFieldTemplate(fieldIdentifier.nodeId, fieldIdentifier.fieldName);
+  const containerCtx = useContainerContext();
 
   const _description = useMemo(
     () => description || fieldTemplate.description,
@@ -19,7 +32,7 @@ export const NodeFieldElementViewMode = memo(({ el }: { el: NodeFieldElement }) 
   );
 
   return (
-    <Flex id={id} className={NODE_FIELD_CLASS_NAME} flex="1 0 0">
+    <Flex id={id} className={NODE_FIELD_CLASS_NAME} sx={sx} data-parent-layout={containerCtx.layout}>
       <FormControl flex="1 1 0" orientation="vertical">
         <NodeFieldElementLabel el={el} />
         <Flex w="full" gap={4}>
