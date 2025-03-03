766430b20f58147a539fb2cf098999b35bd52802
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-03-03 15:37:10 +1000

    tidy(app): make workflow thumbnails base class an ABC, move it to own file

--- invokeai/app/services/workflow_thumbnails/workflow_thumbnails_common.py
+++ invokeai/app/services/workflow_thumbnails/workflow_thumbnails_common.py
@@ -1,8 +1,3 @@
-from pathlib import Path
-
-from PIL import Image
-
-
 class WorkflowThumbnailFileNotFoundException(Exception):
     """Raised when a workflow thumbnail file is not found"""
 
@@ -25,23 +20,3 @@ class WorkflowThumbnailFileDeleteException(Exception):
     def __init__(self, message: str = "Workflow thumbnail file cannot be deleted"):
         self.message = message
         super().__init__(self.message)
-
-
-class WorkflowThumbnailServiceBase:
-    """Base class for workflow thumbnail services"""
-
-    def get_path(self, workflow_id: str) -> Path:
-        """Gets the path to a workflow thumbnail"""
-        raise NotImplementedError
-
-    def get_url(self, workflow_id: str) -> str | None:
-        """Gets the URL of a workflow thumbnail"""
-        raise NotImplementedError
-
-    def save(self, workflow_id: str, image: Image.Image) -> None:
-        """Saves a workflow thumbnail"""
-        raise NotImplementedError
-
-    def delete(self, workflow_id: str) -> None:
-        """Deletes a workflow thumbnail"""
-        raise NotImplementedError
