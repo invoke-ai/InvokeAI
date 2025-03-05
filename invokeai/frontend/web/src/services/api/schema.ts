faa143b08c4bc3da2b62dbb2e8d6a7699a03c35d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-03-06 09:19:02 +1000

    experiment: simulate typegen-checks failure

--- invokeai/frontend/web/src/services/api/schema.ts
+++ invokeai/frontend/web/src/services/api/schema.ts
@@ -16521,7 +16521,7 @@ export type components = {
              * @constant
              * @enum {string}
              */
-            type: "pair_tile_image";
+            type: "pair_tile_imageaaa";
         };
         /** PairTileImageOutput */
         PairTileImageOutput: {
