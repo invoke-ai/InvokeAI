1a84a2fb7e71f87f1ae6658b57e43a161be8c783
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-03-06 14:21:51 +1000

    feat(ui): restore share workflow button

--- invokeai/frontend/web/src/features/nodes/components/sidePanel/workflow/WorkflowLibrary/WorkflowLibraryListItemActions/ShareWorkflow.tsx
+++ invokeai/frontend/web/src/features/nodes/components/sidePanel/workflow/WorkflowLibrary/WorkflowLibraryListItemActions/ShareWorkflow.tsx
@@ -1,46 +1,25 @@
 import { IconButton, Tooltip } from '@invoke-ai/ui-library';
-import { useStore } from '@nanostores/react';
-import { $projectUrl } from 'app/store/nanostores/projectId';
 import { useShareWorkflow } from 'features/nodes/components/sidePanel/workflow/WorkflowLibrary/ShareWorkflowModal';
 import type { MouseEvent } from 'react';
-import { useCallback } from 'react';
+import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiShareFatBold } from 'react-icons/pi';
 import type { WorkflowRecordListItemWithThumbnailDTO } from 'services/api/types';
 
-export const ViewWorkflow = ({
-  isHovered,
-  setIsHovered,
-  workflow,
-}: {
-  isHovered: boolean;
-  setIsHovered: (isHovered: boolean) => void;
-  workflow: WorkflowRecordListItemWithThumbnailDTO;
-}) => {
-  const projectUrl = useStore($projectUrl);
+export const ShareWorkflowButton = memo(({ workflow }: { workflow: WorkflowRecordListItemWithThumbnailDTO }) => {
   const shareWorkflow = useShareWorkflow();
   const { t } = useTranslation();
 
   const handleClickShare = useCallback(
     (e: MouseEvent<HTMLButtonElement>) => {
       e.stopPropagation();
-      setIsHovered(false);
       shareWorkflow(workflow);
     },
-    [shareWorkflow, workflow, setIsHovered]
+    [shareWorkflow, workflow]
   );
 
-  if (!projectUrl || !workflow.workflow_id || workflow.category === 'user') {
-    return null;
-  }
-
   return (
-    <Tooltip
-      label={t('workflows.copyShareLink')}
-      // This prevents an issue where the tooltip isn't closed after the modal is opened
-      isOpen={!isHovered ? false : undefined}
-      closeOnScroll
-    >
+    <Tooltip label={t('workflows.copyShareLink')} closeOnScroll>
       <IconButton
         size="sm"
         variant="ghost"
@@ -50,4 +29,6 @@ export const ViewWorkflow = ({
       />
     </Tooltip>
   );
-};
+});
+
+ShareWorkflowButton.displayName = 'ShareWorkflowButton';
