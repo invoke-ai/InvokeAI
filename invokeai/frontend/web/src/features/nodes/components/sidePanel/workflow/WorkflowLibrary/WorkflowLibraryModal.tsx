95a9bb6c7b41f601591af0d08e10b74413ca367a
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-03-06 14:16:41 +1000

    fix(ui): missing translation

--- invokeai/frontend/web/src/features/nodes/components/sidePanel/workflow/WorkflowLibrary/WorkflowLibraryModal.tsx
+++ invokeai/frontend/web/src/features/nodes/components/sidePanel/workflow/WorkflowLibrary/WorkflowLibraryModal.tsx
@@ -9,12 +9,14 @@ import {
   ModalOverlay,
 } from '@invoke-ai/ui-library';
 import { useWorkflowLibraryModal } from 'features/nodes/store/workflowLibraryModal';
+import { useTranslation } from 'react-i18next';
 
 import { WorkflowLibrarySideNav } from './WorkflowLibrarySideNav';
 import { WorkflowLibraryTopNav } from './WorkflowLibraryTopNav';
 import { WorkflowList } from './WorkflowList';
 
 export const WorkflowLibraryModal = () => {
+  const { t } = useTranslation();
   const workflowLibraryModal = useWorkflowLibraryModal();
   return (
     <Modal isOpen={workflowLibraryModal.isOpen} onClose={workflowLibraryModal.close} isCentered>
@@ -25,7 +27,7 @@ export const WorkflowLibraryModal = () => {
         h="calc(100% - var(--invoke-sizes-40))"
         maxH="calc(100% - var(--invoke-sizes-40))"
       >
-        <ModalHeader>Workflow Library</ModalHeader>
+        <ModalHeader>{t('workflows.workflowLibrary')}</ModalHeader>
         <ModalCloseButton />
         <ModalBody pb={6}>
           <Flex gap={4} h="100%">
