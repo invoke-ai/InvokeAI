89fbddc12b571c2040104256d30763de1b54caa0
simpletrontdip <thongitbk@gmail.com>
2025-03-07 19:39:44 +0700

    feat: add new layer type for diffusers-ada-ln

---
+++ invokeai/backend/patches/layers/diffusers_ada_ln_lora_layer.py
@@ -0,0 +1,16 @@
+import torch
+
+from invokeai.backend.patches.layers.lora_layer import LoRALayer
+
+class DiffusersAdaLN_LoRALayer(LoRALayer):
+    '''LoRA layer converted from Diffusers AdaLN, weight is shift-scale swapped'''
+
+    def get_weight(self, orig_weight: torch.Tensor) -> torch.Tensor: 
+        # In SD3 and Flux implementation of AdaLayerNormContinuous, it split linear projection output into shift, scale;
+        # while in diffusers it split into scale, shift. 
+        # So we swap the linear projection weights in order to be able to use Flux implementation
+
+        weight = super().get_weight(orig_weight) 
+        scale, shift = weight.chunk(2, dim=0) 
+        
+        return torch.cat([shift, scale], dim=0)
