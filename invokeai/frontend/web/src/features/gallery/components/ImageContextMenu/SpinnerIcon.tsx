09c6c7ceb2387b4204b7f9c3872d95deb597941e
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-03-12 20:06:29 +1000

    chore(ui): lint

--- invokeai/frontend/web/src/features/gallery/components/ImageContextMenu/SpinnerIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { Flex, Spinner } from '@invoke-ai/ui-library';
-
-export const SpinnerIcon = () => (
-  <Flex w="14px" alignItems="center" justifyContent="center">
-    <Spinner size="xs" />
-  </Flex>
-);
