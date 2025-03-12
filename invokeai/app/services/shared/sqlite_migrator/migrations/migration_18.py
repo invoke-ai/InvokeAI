3334652acc6781906c20bab6262ceb1d66fca102
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-03-12 10:05:49 +1000

    feat(db): drop the opened_at column instead of marking deprecated

--- invokeai/app/services/shared/sqlite_migrator/migrations/migration_18.py
+++ invokeai/app/services/shared/sqlite_migrator/migrations/migration_18.py
@@ -10,15 +10,15 @@ class Migration18Callback:
     def _make_workflow_opened_at_nullable(self, cursor: sqlite3.Cursor) -> None:
         """
         Make the `opened_at` column nullable in the `workflow_library` table. This is accomplished by:
-            - Renaming the existing column to `opened_at_deprecated`
-            - Drop the existing `idx_workflow_library_opened_at` index
+            - Dropping the existing `idx_workflow_library_opened_at` index (must be done before dropping the column)
+            - Dropping the existing `opened_at` column
             - Adding a new nullable column `opened_at` (no data migration needed, all values will be NULL)
-            - Recreate the `idx_workflow_library_opened_at` index on the `opened_at` column
+            - Adding a new `idx_workflow_library_opened_at` index on the `opened_at` column
         """
-        # Rename existing column to deprecated
-        cursor.execute("ALTER TABLE workflow_library RENAME COLUMN opened_at TO opened_at_deprecated;")
         # For index renaming in SQLite, we need to drop and recreate
         cursor.execute("DROP INDEX IF EXISTS idx_workflow_library_opened_at;")
+        # Rename existing column to deprecated
+        cursor.execute("ALTER TABLE workflow_library DROP COLUMN opened_at;")
         # Add new nullable column - all values will be NULL - no migration of data needed
         cursor.execute("ALTER TABLE workflow_library ADD COLUMN opened_at DATETIME;")
         # Create new index on the new column
@@ -33,10 +33,10 @@ def build_migration_18() -> Migration:
 
     This migration does the following:
         - Make the `opened_at` column nullable in the `workflow_library` table. This is accomplished by:
-            - Renaming the existing column to `opened_at_deprecated`
-            - Drop the existing `idx_workflow_library_opened_at` index
+            - Dropping the existing `idx_workflow_library_opened_at` index (must be done before dropping the column)
+            - Dropping the existing `opened_at` column
             - Adding a new nullable column `opened_at` (no data migration needed, all values will be NULL)
-            - Recreate the `idx_workflow_library_opened_at` index on the `opened_at` column
+            - Adding a new `idx_workflow_library_opened_at` index on the `opened_at` column
     """
     migration_18 = Migration(
         from_version=17,
