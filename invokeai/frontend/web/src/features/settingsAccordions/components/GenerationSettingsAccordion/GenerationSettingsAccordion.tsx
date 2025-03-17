02ad11a17a102bf5115504de18bf1a4d4693724c
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2025-03-17 15:01:13 -0400

    update available params for cogview4

--- invokeai/frontend/web/src/features/settingsAccordions/components/GenerationSettingsAccordion/GenerationSettingsAccordion.tsx
+++ invokeai/frontend/web/src/features/settingsAccordions/components/GenerationSettingsAccordion/GenerationSettingsAccordion.tsx
@@ -4,7 +4,7 @@ import { EMPTY_ARRAY } from 'app/store/constants';
 import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
 import { useAppSelector } from 'app/store/storeHooks';
 import { selectLoRAsSlice } from 'features/controlLayers/store/lorasSlice';
-import { selectIsFLUX, selectIsSD3 } from 'features/controlLayers/store/paramsSlice';
+import { selectIsCogView4, selectIsFLUX, selectIsSD3 } from 'features/controlLayers/store/paramsSlice';
 import { LoRAList } from 'features/lora/components/LoRAList';
 import LoRASelect from 'features/lora/components/LoRASelect';
 import ParamCFGScale from 'features/parameters/components/Core/ParamCFGScale';
@@ -33,6 +33,8 @@ export const GenerationSettingsAccordion = memo(() => {
   const activeTabName = useAppSelector(selectActiveTab);
   const isFLUX = useAppSelector(selectIsFLUX);
   const isSD3 = useAppSelector(selectIsSD3);
+  const isCogView4 = useAppSelector(selectIsCogView4)
+
   const isUpscaling = useMemo(() => {
     return activeTabName === 'upscaling';
   }, [activeTabName]);
@@ -80,7 +82,7 @@ export const GenerationSettingsAccordion = memo(() => {
         <Expander label={t('accordions.advanced.options')} isOpen={isOpenExpander} onToggle={onToggleExpander}>
           <Flex gap={4} flexDir="column" pb={4}>
             <FormControlGroup formLabelProps={formLabelProps}>
-              {!isFLUX && !isSD3 && !isUpscaling && <ParamScheduler />}
+              {!isFLUX && !isSD3 && !isCogView4 && !isUpscaling && <ParamScheduler />}
               {isUpscaling && <ParamUpscaleScheduler />}
               <ParamSteps />
               {isFLUX && <ParamGuidance />}
