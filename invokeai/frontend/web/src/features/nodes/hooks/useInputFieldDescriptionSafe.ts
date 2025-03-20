64ff0b5a9020fcb55bb007950825e0b8982eecd9
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-03-21 09:21:26 +1000

    chore(ui): "useInputFieldDescription" -> "useInputFieldDescriptionSafe"
    Also update docstrings

---
+++ invokeai/frontend/web/src/features/nodes/hooks/useInputFieldDescriptionSafe.ts
@@ -0,0 +1,26 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { useAppSelector } from 'app/store/storeHooks';
+import { selectFieldInputInstanceSafe, selectNodesSlice } from 'features/nodes/store/selectors';
+import { useMemo } from 'react';
+
+/**
+ * Gets the user-defined description of an input field for a given node.
+ *
+ * If the node doesn't exist or is not an invocation node, an empty string is returned.
+ *
+ * @param nodeId The ID of the node
+ * @param fieldName The name of the field
+ */
+export const useInputFieldDescriptionSafe = (nodeId: string, fieldName: string) => {
+  const selector = useMemo(
+    () =>
+      createSelector(
+        selectNodesSlice,
+        (nodes) => selectFieldInputInstanceSafe(nodes, nodeId, fieldName)?.description ?? ''
+      ),
+    [fieldName, nodeId]
+  );
+
+  const description = useAppSelector(selector);
+  return description;
+};
