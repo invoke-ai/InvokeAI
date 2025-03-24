b9ddf6785398f177bae4ad5d4c94aaee414d23cd
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-03-24 17:50:28 +1000

    refactor(ui): rejiggle enqueue actions to support api validation runs

--- invokeai/frontend/web/src/app/store/actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-import type { TabName } from 'features/ui/store/uiTypes';
-
-export const enqueueRequested = createAction<{
-  tabName: TabName;
-  prepend: boolean;
-}>('app/enqueueRequested');
