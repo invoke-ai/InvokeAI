eaa1d8eb71357a2588f5774ea282cae9933a6637
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-03-27 05:33:57 +1000

    tidy(backend): errant comments

--- invokeai/backend/model_manager/load/model_cache/torch_module_autocast/cast_to_dtype.py
+++ invokeai/backend/model_manager/load/model_cache/torch_module_autocast/cast_to_dtype.py
@@ -7,13 +7,9 @@ T = TypeVar("T", torch.Tensor, None, torch.Tensor | None)
 
 def cast_to_dtype(t: T, to_dtype: torch.dtype) -> T:
     """Helper function to cast an optional tensor to a target dtype."""
-
     if t is None:
-        # If the tensor is None, return it as is.
         return t
 
     if t.dtype != to_dtype:
-        # The tensor is on the wrong device and we don't care about the dtype - or the dtype is already correct.
         return t.to(dtype=to_dtype)
-
     return t
