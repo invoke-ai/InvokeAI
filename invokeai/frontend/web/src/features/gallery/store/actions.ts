4a83700fe4003ce44f4f6d8b94ed59018360a4c9
Mary Hipp <maryhipp@Marys-MacBook-Air.local>
2025-03-27 15:08:57 -0400

    if clientSideUploading is enabled, handle bulk uploads using that flow

--- invokeai/frontend/web/src/features/gallery/store/actions.ts
+++ invokeai/frontend/web/src/features/gallery/store/actions.ts
@@ -1,4 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
+import type { ImageDTO } from 'services/api/types';
 
 export const sentImageToCanvas = createAction('gallery/sentImageToCanvas');
 
@@ -7,3 +8,9 @@ export const imageDownloaded = createAction('gallery/imageDownloaded');
 export const imageCopiedToClipboard = createAction('gallery/imageCopiedToClipboard');
 
 export const imageOpenedInNewTab = createAction('gallery/imageOpenedInNewTab');
+
+export const imageUploadedClientSide = createAction<{
+  imageDTO: ImageDTO;
+  silent: boolean;
+  isFirstUploadOfBatch: boolean;
+}>('gallery/imageUploadedClientSide');
