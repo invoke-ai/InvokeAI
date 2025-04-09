a23d90187b9bfe56ac433a3857220a5dfe8ad7bc
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-04-09 17:07:26 +1000

    feat(ui): allow send-image-to-canvas to work when canvas is uninitialized
    Add `useCanvasIsBusySafe()` hook. This is like `useCanvasIsBusy()`, but when the canvas is not initialized, it gracefully falls back to false instead of raising.
    
    Because app tabs are lazy-loaded, the canvas is not initialized until the user visits that tab. If the page loads up on the workflows tab, the canvas will be uninitialized until the user clicks on it.
    
    This graceful fallback behaviour allows actions like sending an image to canvas to work even when the canvas is not yet initialized. These actions are exposed in the image context menu, and previously were hidden when the canvas was not initialized. We can now show these actions and use them even when the canvas is uninitialized.
    
    - Add `useCanvasIsBusySafe()` hook
    - Use the new hook in the image context menu for send to canvas actions
    - Do not use `<CanvasManagerProviderGate />` in the image context menu (this was hiding the actions when canvas was uninitialized)

--- invokeai/frontend/web/src/features/controlLayers/hooks/useCanvasIsBusy.ts
+++ invokeai/frontend/web/src/features/controlLayers/hooks/useCanvasIsBusy.ts
@@ -1,16 +1,35 @@
 import { useStore } from '@nanostores/react';
-import { useCanvasManager } from 'features/controlLayers/contexts/CanvasManagerProviderGate';
+import { $false } from 'app/store/nanostores/util';
+import { useCanvasManager, useCanvasManagerSafe } from 'features/controlLayers/contexts/CanvasManagerProviderGate';
 
+/**
+ * Returns a boolena indicating whether the canvas is busy:
+ * - While staging
+ * - While an entity is transforming
+ * - While an entity is filtering
+ * - While the canvas is doing some other long-running operation, like rasterizing a layer
+ *
+ * This hook will throw an error if the canvas manager is not initialized.
+ */
 export const useCanvasIsBusy = () => {
   const canvasManager = useCanvasManager();
-  /**
-   * Whether the canvas is busy:
-   * - While staging
-   * - While an entity is transforming
-   * - While an entity is filtering
-   * - While the canvas is doing some other long-running operation, like rasterizing a layer
-   */
   const isBusy = useStore(canvasManager.$isBusy);
+
+  return isBusy;
+};
+
+/**
+ * Returns a boolena indicating whether the canvas is busy:
+ * - While staging
+ * - While an entity is transforming
+ * - While an entity is filtering
+ * - While the canvas is doing some other long-running operation, like rasterizing a layer
+ *
+ * This hook will fall back to false if the canvas manager is not initialized.
+ */
+export const useCanvasIsBusySafe = () => {
+  const canvasManager = useCanvasManagerSafe();
+  const isBusy = useStore(canvasManager?.$isBusy ?? $false);
 
   return isBusy;
 };
