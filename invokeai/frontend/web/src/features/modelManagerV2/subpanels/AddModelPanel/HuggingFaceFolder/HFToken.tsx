ee4002607c88e1097edb8d279bc5ac0956d955ab
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-05-05 15:14:46 +1000

    feat(ui): add UI to reset hf token

--- invokeai/frontend/web/src/features/modelManagerV2/subpanels/AddModelPanel/HuggingFaceFolder/HFToken.tsx
+++ invokeai/frontend/web/src/features/modelManagerV2/subpanels/AddModelPanel/HuggingFaceFolder/HFToken.tsx
@@ -12,63 +12,45 @@ import { skipToken } from '@reduxjs/toolkit/query';
 import { useFeatureStatus } from 'features/system/hooks/useFeatureStatus';
 import { toast } from 'features/toast/toast';
 import type { ChangeEvent } from 'react';
-import { useCallback, useMemo, useState } from 'react';
+import { memo, useCallback, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useGetHFTokenStatusQuery, useSetHFTokenMutation } from 'services/api/endpoints/models';
-import { UNAUTHORIZED_TOAST_ID } from 'services/events/onModelInstallError';
+import {
+  useGetHFTokenStatusQuery,
+  useResetHFTokenMutation,
+  useSetHFTokenMutation,
+} from 'services/api/endpoints/models';
+import type { Equals } from 'tsafe';
+import { assert } from 'tsafe';
 
 export const HFToken = () => {
   const { t } = useTranslation();
   const isHFTokenEnabled = useFeatureStatus('hfToken');
-  const [token, setToken] = useState('');
   const { currentData } = useGetHFTokenStatusQuery(isHFTokenEnabled ? undefined : skipToken);
-  const [trigger, { isLoading, isUninitialized }] = useSetHFTokenMutation();
-  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setToken(e.target.value);
-  }, []);
-  const onClick = useCallback(() => {
-    trigger({ token })
-      .unwrap()
-      .then((res) => {
-        if (res === 'valid') {
-          setToken('');
-          toast({
-            id: UNAUTHORIZED_TOAST_ID,
-            title: t('modelManager.hfTokenSaved'),
-            status: 'success',
-            duration: 3000,
-          });
-        }
-      });
-  }, [t, token, trigger]);
 
   const error = useMemo(() => {
-    if (!currentData || isUninitialized || isLoading) {
-      return null;
+    switch (currentData) {
+      case 'invalid':
+        return t('modelManager.hfTokenInvalidErrorMessage');
+      case 'unknown':
+        return t('modelManager.hfTokenUnableToVerifyErrorMessage');
+      case 'valid':
+      case undefined:
+        return null;
+      default:
+        assert<Equals<never, typeof currentData>>(false, 'Unexpected HF token status');
     }
-    if (currentData === 'invalid') {
-      return t('modelManager.hfTokenInvalidErrorMessage');
-    }
-    if (currentData === 'unknown') {
-      return t('modelManager.hfTokenUnableToVerifyErrorMessage');
-    }
-    return null;
-  }, [currentData, isLoading, isUninitialized, t]);
+  }, [currentData, t]);
 
-  if (!currentData || currentData === 'valid') {
+  if (!currentData) {
     return null;
   }
 
   return (
     <Flex borderRadius="base" w="full">
-      <FormControl isInvalid={!isUninitialized && Boolean(error)} orientation="vertical">
+      <FormControl isInvalid={Boolean(error)} orientation="vertical">
         <FormLabel>{t('modelManager.hfTokenLabel')}</FormLabel>
-        <Flex gap={3} alignItems="center" w="full">
-          <Input type="password" value={token} onChange={onChange} />
-          <Button onClick={onClick} size="sm" isDisabled={token.trim().length === 0} isLoading={isLoading}>
-            {t('common.save')}
-          </Button>
-        </Flex>
+        {error && <SetHFTokenInput />}
+        {!error && <ResetHFTokenButton />}
         <FormHelperText>
           <ExternalLink label={t('modelManager.hfTokenHelperText')} href="https://huggingface.co/settings/tokens" />
         </FormHelperText>
@@ -77,3 +59,73 @@ export const HFToken = () => {
     </Flex>
   );
 };
+
+const PLACEHOLDER_TOKEN = Array.from({ length: 37 }, () => 'a').join('');
+
+const ResetHFTokenButton = memo(() => {
+  const { t } = useTranslation();
+  const [resetHFToken, { isLoading }] = useResetHFTokenMutation();
+
+  const onClick = useCallback(() => {
+    resetHFToken()
+      .unwrap()
+      .then(() => {
+        toast({
+          title: t('modelManager.hfTokenReset'),
+          status: 'info',
+        });
+      });
+  }, [resetHFToken, t]);
+
+  return (
+    <Flex gap={3} alignItems="center" w="full">
+      <Input type="password" value={PLACEHOLDER_TOKEN} isDisabled />
+      <Button onClick={onClick} size="sm" isLoading={isLoading}>
+        {t('common.reset')}
+      </Button>
+    </Flex>
+  );
+});
+ResetHFTokenButton.displayName = 'ResetHFTokenButton';
+
+const SetHFTokenInput = memo(() => {
+  const { t } = useTranslation();
+  const [token, setToken] = useState('');
+  const [trigger, { isLoading }] = useSetHFTokenMutation();
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setToken(e.target.value);
+  }, []);
+  const onClick = useCallback(() => {
+    trigger({ token })
+      .unwrap()
+      .then((res) => {
+        switch (res) {
+          case 'valid':
+            setToken('');
+            toast({
+              title: t('modelManager.hfTokenSaved'),
+              status: 'success',
+            });
+            break;
+          case 'invalid':
+          case 'unknown':
+          default:
+            toast({
+              title: t('modelManager.hfTokenUnableToVerify'),
+              status: 'error',
+            });
+            break;
+        }
+      });
+  }, [t, token, trigger]);
+
+  return (
+    <Flex gap={3} alignItems="center" w="full">
+      <Input type="password" value={token} onChange={onChange} />
+      <Button onClick={onClick} size="sm" isDisabled={token.trim().length === 0} isLoading={isLoading}>
+        {t('common.save')}
+      </Button>
+    </Flex>
+  );
+});
+SetHFTokenInput.displayName = 'SetHFTokenInput';
