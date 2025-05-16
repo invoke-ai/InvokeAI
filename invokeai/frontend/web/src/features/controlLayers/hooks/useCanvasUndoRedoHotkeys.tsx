9c8af8c39d9cd06f22f4759c9f34a81e4b3db86f
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-05-16 12:00:39 +1000

    feat(ui): viewer is a modal (wip)

--- invokeai/frontend/web/src/features/controlLayers/hooks/useCanvasUndoRedoHotkeys.tsx
+++ invokeai/frontend/web/src/features/controlLayers/hooks/useCanvasUndoRedoHotkeys.tsx
@@ -3,7 +3,6 @@ import { useAssertSingleton } from 'common/hooks/useAssertSingleton';
 import { useCanvasIsBusy } from 'features/controlLayers/hooks/useCanvasIsBusy';
 import { canvasRedo, canvasUndo } from 'features/controlLayers/store/canvasSlice';
 import { selectCanvasMayRedo, selectCanvasMayUndo } from 'features/controlLayers/store/selectors';
-import { useImageViewer } from 'features/gallery/components/ImageViewer/useImageViewer';
 import { useRegisteredHotkeys } from 'features/system/components/HotkeysModal/useHotkeyData';
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
@@ -12,7 +11,6 @@ export const useCanvasUndoRedoHotkeys = () => {
   useAssertSingleton('useCanvasUndoRedo');
   const dispatch = useDispatch();
   const isBusy = useCanvasIsBusy();
-  const imageViewer = useImageViewer();
 
   const mayUndo = useAppSelector(selectCanvasMayUndo);
   const handleUndo = useCallback(() => {
@@ -22,8 +20,8 @@ export const useCanvasUndoRedoHotkeys = () => {
     id: 'undo',
     category: 'canvas',
     callback: handleUndo,
-    options: { enabled: mayUndo && !isBusy && !imageViewer.isOpen, preventDefault: true },
-    dependencies: [mayUndo, isBusy, handleUndo, imageViewer.isOpen],
+    options: { enabled: mayUndo && !isBusy, preventDefault: true },
+    dependencies: [mayUndo, isBusy, handleUndo],
   });
 
   const mayRedo = useAppSelector(selectCanvasMayRedo);
@@ -34,7 +32,7 @@ export const useCanvasUndoRedoHotkeys = () => {
     id: 'redo',
     category: 'canvas',
     callback: handleRedo,
-    options: { enabled: mayRedo && !isBusy && !imageViewer.isOpen, preventDefault: true },
-    dependencies: [mayRedo, handleRedo, isBusy, imageViewer.isOpen],
+    options: { enabled: mayRedo && !isBusy, preventDefault: true },
+    dependencies: [mayRedo, handleRedo, isBusy],
   });
 };
