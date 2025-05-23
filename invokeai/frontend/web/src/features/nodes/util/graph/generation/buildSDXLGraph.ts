51f6af166082a1de40d9c583dd103de468a949b7
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-05-23 13:43:31 +1000

    refactor(ui): canvas flow (wip)

--- invokeai/frontend/web/src/features/nodes/util/graph/generation/buildSDXLGraph.ts
+++ invokeai/frontend/web/src/features/nodes/util/graph/generation/buildSDXLGraph.ts
@@ -323,9 +323,9 @@ export const buildSDXLGraph = async (state: RootState, manager?: CanvasManager |
 
   g.updateNode(canvasOutput, {
     id: getPrefixedId(CANVAS_OUTPUT_PREFIX),
-    is_intermediate,
+    is_intermediate: true,
     use_cache: false,
-    board,
+    board: undefined,
   });
 
   g.setMetadataReceivingNode(canvasOutput);
