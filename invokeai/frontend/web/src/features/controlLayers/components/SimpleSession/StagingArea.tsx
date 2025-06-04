f66f2b3c71cd81d8956c1f1b040be5e327569f58
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-06-04 23:17:33 +1000

    feat(ui): modularize all staging area logic so it can be shared w/ canvas more easily

--- invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/StagingArea.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/StagingArea.tsx
@@ -1,129 +1,36 @@
 /* eslint-disable i18next/no-literal-string */
 
-import { Divider, Flex, Text } from '@invoke-ai/ui-library';
+import { Divider, Flex } from '@invoke-ai/ui-library';
 import { useStore } from '@nanostores/react';
-import { EMPTY_ARRAY } from 'app/store/constants';
 import { useCanvasSessionContext } from 'features/controlLayers/components/SimpleSession/context';
 import { getQueueItemElementId } from 'features/controlLayers/components/SimpleSession/shared';
 import { StagingAreaContent } from 'features/controlLayers/components/SimpleSession/StagingAreaContent';
 import { StagingAreaHeader } from 'features/controlLayers/components/SimpleSession/StagingAreaHeader';
+import { StagingAreaNoItems } from 'features/controlLayers/components/SimpleSession/StagingAreaNoItems';
+import { useProgressEvents } from 'features/controlLayers/components/SimpleSession/use-progress-events';
 import { useStagingAreaKeyboardNav } from 'features/controlLayers/components/SimpleSession/use-staging-keyboard-nav';
-import { memo, useCallback, useEffect, useMemo, useState } from 'react';
-import { useListAllQueueItemsQuery } from 'services/api/endpoints/queue';
-import type { S } from 'services/api/types';
-import { $socket, setProgress } from 'services/events/stores';
-
-const LIST_ALL_OPTIONS = {
-  selectFromResult: ({ data }) => {
-    if (!data) {
-      return { items: EMPTY_ARRAY };
-    }
-    return { items: data.filter(({ status }) => status !== 'canceled') };
-  },
-} satisfies Parameters<typeof useListAllQueueItemsQuery>[1];
+import { memo, useEffect } from 'react';
 
 export const StagingArea = memo(() => {
   const ctx = useCanvasSessionContext();
-  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
-  const [autoSwitch, setAutoSwitch] = useState(true);
-  const { items } = useListAllQueueItemsQuery({ destination: ctx.session.id }, LIST_ALL_OPTIONS);
-  const selectedItem = useMemo(() => {
-    if (items.length === 0) {
-      return null;
-    }
-    if (selectedItemId === null) {
-      return null;
-    }
-    return items.find(({ item_id }) => item_id === selectedItemId) ?? null;
-  }, [items, selectedItemId]);
-  const selectedItemIndex = useMemo(() => {
-    if (items.length === 0) {
-      return null;
-    }
-    if (selectedItemId === null) {
-      return null;
-    }
-    return items.findIndex(({ item_id }) => item_id === selectedItemId) ?? null;
-  }, [items, selectedItemId]);
-
-  const onSelectItemId = useCallback((item_id: number | null) => {
-    setSelectedItemId(item_id);
-    if (item_id !== null) {
-      document.getElementById(getQueueItemElementId(item_id))?.scrollIntoView();
-    }
-  }, []);
-
-  useStagingAreaKeyboardNav(items, selectedItemId, onSelectItemId);
+  const hasItems = useStore(ctx.$hasItems);
+  useProgressEvents();
+  useStagingAreaKeyboardNav();
 
   useEffect(() => {
-    if (items.length === 0) {
-      onSelectItemId(null);
-      return;
-    }
-    if (selectedItemId === null && items.length > 0) {
-      onSelectItemId(items[0]?.item_id ?? null);
-      return;
-    }
-  }, [items, onSelectItemId, selectedItem, selectedItemId]);
-
-  const socket = useStore($socket);
-  useEffect(() => {
-    if (!socket) {
-      return;
-    }
-
-    const onQueueItemStatusChanged = (data: S['QueueItemStatusChangedEvent']) => {
-      if (data.destination !== ctx.session.id) {
-        return;
+    return ctx.$selectedItemId.listen((id) => {
+      if (id !== null) {
+        document.getElementById(getQueueItemElementId(id))?.scrollIntoView();
       }
-      if (data.status === 'in_progress' && autoSwitch) {
-        onSelectItemId(data.item_id);
-      }
-    };
-
-    socket.on('queue_item_status_changed', onQueueItemStatusChanged);
-
-    return () => {
-      socket.off('queue_item_status_changed', onQueueItemStatusChanged);
-    };
-  }, [autoSwitch, ctx.$progressData, ctx.session.id, onSelectItemId, socket]);
-
-  useEffect(() => {
-    if (!socket) {
-      return;
-    }
-    const onProgress = (data: S['InvocationProgressEvent']) => {
-      if (data.destination !== ctx.session.id) {
-        return;
-      }
-      setProgress(ctx.$progressData, data);
-    };
-    socket.on('invocation_progress', onProgress);
-
-    return () => {
-      socket.off('invocation_progress', onProgress);
-    };
-  }, [ctx.$progressData, ctx.session.id, socket]);
+    });
+  }, [ctx.$selectedItemId]);
 
   return (
     <Flex flexDir="column" gap={2} w="full" h="full" minW={0} minH={0}>
-      <StagingAreaHeader autoSwitch={autoSwitch} setAutoSwitch={setAutoSwitch} />
+      <StagingAreaHeader />
       <Divider />
-      {items.length > 0 && (
-        <StagingAreaContent
-          items={items}
-          selectedItem={selectedItem}
-          selectedItemId={selectedItemId}
-          selectedItemIndex={selectedItemIndex}
-          onChangeAutoSwitch={setAutoSwitch}
-          onSelectItemId={onSelectItemId}
-        />
-      )}
-      {items.length === 0 && (
-        <Flex w="full" h="full" alignItems="center" justifyContent="center">
-          <Text>No generations</Text>
-        </Flex>
-      )}
+      {hasItems && <StagingAreaContent />}
+      {!hasItems && <StagingAreaNoItems />}
     </Flex>
   );
 });
--- invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/use-staging-keyboard-nav.ts
+++ invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/use-staging-keyboard-nav.ts
@@ -1,51 +1,54 @@
+import { useCanvasSessionContext } from 'features/controlLayers/components/SimpleSession/context';
 import { useCallback } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
-import type { S } from 'services/api/types';
 
-export const useStagingAreaKeyboardNav = (
-  items: S['SessionQueueItem'][],
-  selectedItemId: number | null,
-  onSelectItemId: (item_id: number) => void
-) => {
+export const useStagingAreaKeyboardNav = () => {
+  const ctx = useCanvasSessionContext();
   const onNext = useCallback(() => {
+    const selectedItemId = ctx.$selectedItemId.get();
     if (selectedItemId === null) {
       return;
     }
+    const items = ctx.$items.get();
     const currentIndex = items.findIndex((item) => item.item_id === selectedItemId);
     const nextIndex = (currentIndex + 1) % items.length;
     const nextItem = items[nextIndex];
     if (!nextItem) {
       return;
     }
-    onSelectItemId(nextItem.item_id);
-  }, [items, onSelectItemId, selectedItemId]);
+    ctx.$selectedItemId.set(nextItem.item_id);
+  }, [ctx.$items, ctx.$selectedItemId]);
   const onPrev = useCallback(() => {
+    const selectedItemId = ctx.$selectedItemId.get();
     if (selectedItemId === null) {
       return;
     }
+    const items = ctx.$items.get();
     const currentIndex = items.findIndex((item) => item.item_id === selectedItemId);
     const prevIndex = (currentIndex - 1 + items.length) % items.length;
     const prevItem = items[prevIndex];
     if (!prevItem) {
       return;
     }
-    onSelectItemId(prevItem.item_id);
-  }, [items, onSelectItemId, selectedItemId]);
+    ctx.$selectedItemId.set(prevItem.item_id);
+  }, [ctx.$items, ctx.$selectedItemId]);
 
   const onFirst = useCallback(() => {
+    const items = ctx.$items.get();
     const first = items.at(0);
     if (!first) {
       return;
     }
-    onSelectItemId(first.item_id);
-  }, [items, onSelectItemId]);
+    ctx.$selectedItemId.set(first.item_id);
+  }, [ctx.$items, ctx.$selectedItemId]);
   const onLast = useCallback(() => {
+    const items = ctx.$items.get();
     const last = items.at(-1);
     if (!last) {
       return;
     }
-    onSelectItemId(last.item_id);
-  }, [items, onSelectItemId]);
+    ctx.$selectedItemId.set(last.item_id);
+  }, [ctx.$items, ctx.$selectedItemId]);
 
   useHotkeys('left', onPrev, { preventDefault: true });
   useHotkeys('right', onNext, { preventDefault: true });
