a038c09bb360228f8b9afd3ef7c9232f6e32c6ea
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-06-04 11:44:37 +1000

    tidy(ui): app layout components

--- invokeai/frontend/web/src/features/ui/components/FloatingLeftPanelButtons.tsx
+++ invokeai/frontend/web/src/features/ui/components/FloatingLeftPanelButtons.tsx
@@ -2,15 +2,14 @@ import { ButtonGroup, Flex, Icon, IconButton, spinAnimation, Tooltip, useShiftMo
 import { useAppSelector } from 'app/store/storeHooks';
 import { ToolChooser } from 'features/controlLayers/components/Tool/ToolChooser';
 import { CanvasManagerProviderGate } from 'features/controlLayers/contexts/CanvasManagerProviderGate';
-import { selectCanvasSessionType } from 'features/controlLayers/store/canvasStagingAreaSlice';
+import { selectCanvasSession } from 'features/controlLayers/store/canvasStagingAreaSlice';
 import { useCancelAllExceptCurrentQueueItemDialog } from 'features/queue/components/CancelAllExceptCurrentQueueItemConfirmationAlertDialog';
 import { useClearQueueDialog } from 'features/queue/components/ClearQueueConfirmationAlertDialog';
 import { InvokeButtonTooltip } from 'features/queue/components/InvokeButtonTooltip/InvokeButtonTooltip';
 import { useCancelCurrentQueueItem } from 'features/queue/hooks/useCancelCurrentQueueItem';
 import { useInvoke } from 'features/queue/hooks/useInvoke';
-import { useFeatureStatus } from 'features/system/hooks/useFeatureStatus';
 import { selectActiveTab } from 'features/ui/store/uiSelectors';
-import { memo, useMemo } from 'react';
+import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
   PiCircleNotchBold,
@@ -23,60 +22,48 @@ import {
 } from 'react-icons/pi';
 import { useGetQueueStatusQuery } from 'services/api/endpoints/queue';
 
-type Props = {
-  togglePanel: () => void;
-};
-
-const FloatingSidePanelButtons = ({ togglePanel }: Props) => {
-  const { t } = useTranslation();
+export const FloatingLeftPanelButtons = memo((props: { onToggle: () => void }) => {
   const tab = useAppSelector(selectActiveTab);
-  const isCancelAndClearAllEnabled = useFeatureStatus('cancelAndClearAll');
-  const sessionType = useAppSelector(selectCanvasSessionType);
+  const session = useAppSelector(selectCanvasSession);
 
   return (
     <Flex pos="absolute" transform="translate(0, -50%)" top="50%" insetInlineStart={2} direction="column" gap={2}>
-      {tab === 'canvas' && sessionType === 'advanced' && (
+      {tab === 'canvas' && session?.type === 'advanced' && (
         <CanvasManagerProviderGate>
           <ToolChooser />
         </CanvasManagerProviderGate>
       )}
       <ButtonGroup orientation="vertical" h={48}>
-        <Tooltip label={t('accessibility.toggleLeftPanel')} placement="end">
-          <IconButton
-            aria-label={t('accessibility.toggleLeftPanel')}
-            onClick={togglePanel}
-            icon={<PiSlidersHorizontalBold />}
-            flexGrow={1}
-          />
-        </Tooltip>
+        <ToggleLeftPanelButton onToggle={props.onToggle} />
         <InvokeIconButton />
         <CancelCurrentIconButton />
-        {/* Show the cancel all except current button instead of cancel and clear all when it is disabled */}
-        {isCancelAndClearAllEnabled && <CancelAndClearAllIconButton />}
-        {!isCancelAndClearAllEnabled && <CancelAllExceptCurrentIconButton />}
+        <CancelAllExceptCurrentIconButton />
       </ButtonGroup>
     </Flex>
   );
-};
+});
 
-export default memo(FloatingSidePanelButtons);
+FloatingLeftPanelButtons.displayName = 'FloatingLeftPanelButtons';
+
+const ToggleLeftPanelButton = memo((props: { onToggle: () => void }) => {
+  const { t } = useTranslation();
+  return (
+    <Tooltip label={t('accessibility.toggleLeftPanel')} placement="end">
+      <IconButton
+        aria-label={t('accessibility.toggleLeftPanel')}
+        onClick={props.onToggle}
+        icon={<PiSlidersHorizontalBold />}
+        flexGrow={1}
+      />
+    </Tooltip>
+  );
+});
+ToggleLeftPanelButton.displayName = 'ToggleLeftPanelButton';
 
 const InvokeIconButton = memo(() => {
   const { t } = useTranslation();
   const queue = useInvoke();
   const shift = useShiftModifier();
-  const { data: queueStatus } = useGetQueueStatusQuery();
-
-  const queueButtonIcon = useMemo(() => {
-    const isProcessing = (queueStatus?.queue.in_progress ?? 0) > 0;
-    if (!queue.isDisabled && isProcessing) {
-      return <Icon boxSize={6} as={PiCircleNotchBold} animation={spinAnimation} />;
-    }
-    if (shift) {
-      return <PiLightningFill />;
-    }
-    return <PiSparkleFill />;
-  }, [queue.isDisabled, queueStatus?.queue.in_progress, shift]);
 
   return (
     <InvokeButtonTooltip prepend={shift} placement="end">
@@ -85,7 +72,7 @@ const InvokeIconButton = memo(() => {
         onClick={shift ? queue.enqueueFront : queue.enqueueBack}
         isLoading={queue.isLoading}
         isDisabled={queue.isDisabled}
-        icon={queueButtonIcon}
+        icon={<InvokeIconButtonIcon />}
         colorScheme="invokeYellow"
         flexGrow={1}
       />
@@ -93,6 +80,30 @@ const InvokeIconButton = memo(() => {
   );
 });
 InvokeIconButton.displayName = 'InvokeIconButton';
+
+const InvokeIconButtonIcon = memo(() => {
+  const shift = useShiftModifier();
+  const queue = useInvoke();
+  const { isProcessing } = useGetQueueStatusQuery(undefined, {
+    selectFromResult: ({ data }) => {
+      if (!data) {
+        return { isProcessing: false };
+      }
+      return { isProcessing: data.queue.in_progress > 0 };
+    },
+  });
+
+  if (!queue.isDisabled && isProcessing) {
+    return <Icon boxSize={6} as={PiCircleNotchBold} animation={spinAnimation} />;
+  }
+
+  if (shift) {
+    return <PiLightningFill />;
+  }
+
+  return <PiSparkleFill />;
+});
+InvokeIconButtonIcon.displayName = 'InvokeIconButtonIcon';
 
 const CancelCurrentIconButton = memo(() => {
   const { t } = useTranslation();
