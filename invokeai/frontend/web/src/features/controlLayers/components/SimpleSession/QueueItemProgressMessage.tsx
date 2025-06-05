f240f1a5d044463daa002901b3fb62744538ebb4
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-06-05 11:54:06 +1000

    feat: canvas flow rework (wip)

--- invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/QueueItemProgressMessage.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/QueueItemProgressMessage.tsx
@@ -1,34 +1,33 @@
 /* eslint-disable i18next/no-literal-string */
 import type { TextProps } from '@invoke-ai/ui-library';
 import { Text } from '@invoke-ai/ui-library';
-import { useCanvasSessionContext } from 'features/controlLayers/components/SimpleSession/context';
+import { useCanvasSessionContext, useProgressData } from 'features/controlLayers/components/SimpleSession/context';
 import { DROP_SHADOW, getProgressMessage } from 'features/controlLayers/components/SimpleSession/shared';
 import { memo } from 'react';
 import type { S } from 'services/api/types';
-import { useProgressData } from 'services/events/stores';
 
-export const QueueItemProgressMessage = memo(
-  ({ session_id, status, ...rest }: { session_id: string; status: S['SessionQueueItem']['status'] } & TextProps) => {
-    const { $progressData } = useCanvasSessionContext();
-    const { progressEvent } = useProgressData($progressData, session_id);
+type Props = { itemId: number; status: S['SessionQueueItem']['status'] } & TextProps;
 
-    if (status === 'completed' || status === 'failed' || status === 'canceled') {
-      return null;
-    }
+export const QueueItemProgressMessage = memo(({ itemId, status, ...rest }: Props) => {
+  const ctx = useCanvasSessionContext();
+  const { progressEvent } = useProgressData(ctx.$progressData, itemId);
 
-    if (status === 'pending') {
-      return (
-        <Text pointerEvents="none" userSelect="none" filter={DROP_SHADOW} {...rest}>
-          Waiting to start...
-        </Text>
-      );
-    }
+  if (status === 'completed' || status === 'failed' || status === 'canceled') {
+    return null;
+  }
 
+  if (status === 'pending') {
     return (
       <Text pointerEvents="none" userSelect="none" filter={DROP_SHADOW} {...rest}>
-        {getProgressMessage(progressEvent)}
+        Waiting to start...
       </Text>
     );
   }
-);
+
+  return (
+    <Text pointerEvents="none" userSelect="none" filter={DROP_SHADOW} {...rest}>
+      {getProgressMessage(progressEvent)}
+    </Text>
+  );
+});
 QueueItemProgressMessage.displayName = 'QueueItemProgressMessage';
