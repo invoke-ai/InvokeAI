8f4ba3d91fa6bcf7425efe1b6cc6d563d1beb142
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-06-06 20:16:56 +1000

    feat(ui): add startover button to canvas toolbar

--- invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/StagingAreaHeader.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/StagingAreaHeader.tsx
@@ -1,20 +1,14 @@
 /* eslint-disable i18next/no-literal-string */
-import { Button, Divider, Flex, FormControl, FormLabel, Spacer, Switch, Text } from '@invoke-ai/ui-library';
+import { Divider, Flex, FormControl, FormLabel, Spacer, Switch, Text } from '@invoke-ai/ui-library';
 import { useStore } from '@nanostores/react';
-import { useAppDispatch } from 'app/store/storeHooks';
 import { useCanvasSessionContext } from 'features/controlLayers/components/SimpleSession/context';
-import { canvasSessionTypeChanged } from 'features/controlLayers/store/canvasStagingAreaSlice';
+import { StartOverButton } from 'features/controlLayers/components/StartOverButton';
 import type { ChangeEvent } from 'react';
 import { memo, useCallback } from 'react';
 
 export const StagingAreaHeader = memo(() => {
   const ctx = useCanvasSessionContext();
   const autoSwitch = useStore(ctx.$autoSwitch);
-  const dispatch = useAppDispatch();
-
-  const startOver = useCallback(() => {
-    dispatch(canvasSessionTypeChanged({ type: 'simple' }));
-  }, [dispatch]);
 
   const onChangeAutoSwitch = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
@@ -24,19 +18,17 @@ export const StagingAreaHeader = memo(() => {
   );
 
   return (
-    <Flex gap={4} w="full" alignItems="center" px={2}>
+    <Flex gap={2} w="full" alignItems="center" px={2}>
       <Text fontSize="lg" fontWeight="bold">
         Staging Area
       </Text>
       <Spacer />
-      <FormControl w="min-content">
+      <FormControl w="min-content" me={2}>
         <FormLabel m={0}>Auto-switch</FormLabel>
         <Switch size="sm" isChecked={autoSwitch} onChange={onChangeAutoSwitch} />
       </FormControl>
       <Divider orientation="vertical" />
-      <Button size="sm" variant="link" onClick={startOver}>
-        Start Over
-      </Button>
+      <StartOverButton />
     </Flex>
   );
 });
