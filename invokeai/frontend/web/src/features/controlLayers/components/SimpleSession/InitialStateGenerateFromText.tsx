9a06ffe3f5631ebad2d7a9d827197225be5edbe3
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-06-11 12:47:46 +1000

    feat(ui): simple session initial state cards are buttons

--- invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/InitialStateGenerateFromText.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/InitialStateGenerateFromText.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable i18next/no-literal-string */
 
-import { Flex, Heading, Icon, IconButton, Text } from '@invoke-ai/ui-library';
+import { Flex, Heading, Icon, Text } from '@invoke-ai/ui-library';
+import { InitialStateButtonGridItem } from 'features/controlLayers/components/SimpleSession/InitialStateButtonGridItem';
 import { memo } from 'react';
 import { PiCursorTextBold, PiTextAaBold } from 'react-icons/pi';
 
@@ -14,20 +15,14 @@ const focusOnPrompt = () => {
 
 export const InitialStateGenerateFromText = memo(() => {
   return (
-    <>
+    <InitialStateButtonGridItem onClick={focusOnPrompt}>
       <Icon as={PiTextAaBold} boxSize={8} color="base.500" />
       <Heading size="sm">Generate from Text</Heading>
       <Text color="base.300">Enter a prompt and Invoke.</Text>
-      <Flex w="full" justifyContent="flex-end">
-        <IconButton
-          onClick={focusOnPrompt}
-          aria-label="Focus on prompt"
-          icon={<PiCursorTextBold />}
-          variant="link"
-          h={8}
-        />
+      <Flex w="full" justifyContent="flex-end" p={2}>
+        <PiCursorTextBold />
       </Flex>
-    </>
+    </InitialStateButtonGridItem>
   );
 });
 InitialStateGenerateFromText.displayName = 'InitialStateGenerateFromText';
