2e0824a799bff3c5166f5297d0430febf0b8fcce
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-06-16 17:41:47 +1000

    feat(ui): make autoswitch on/off
    When the invocation cache is used, we might skip all progress images. This can prevent auto-switch-on-first-progress from working, as we don't get any of those events.
    
    It's much easier to only support auto-switch on complete.

--- invokeai/frontend/web/src/features/controlLayers/components/StagingArea/StagingAreaToolbarMenuAutoSwitch.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/StagingArea/StagingAreaToolbarMenuAutoSwitch.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable i18next/no-literal-string */
 import { MenuItemOption, MenuOptionGroup } from '@invoke-ai/ui-library';
 import { useStore } from '@nanostores/react';
-import { useCanvasSessionContext, zAutoSwitchMode } from 'features/controlLayers/components/SimpleSession/context';
+import { useCanvasSessionContext } from 'features/controlLayers/components/SimpleSession/context';
 import { memo, useCallback } from 'react';
 
 export const StagingAreaToolbarMenuAutoSwitch = memo(() => {
@@ -10,22 +10,18 @@ export const StagingAreaToolbarMenuAutoSwitch = memo(() => {
 
   const onChange = useCallback(
     (val: string | string[]) => {
-      const newAutoSwitch = zAutoSwitchMode.parse(val);
-      ctx.$autoSwitch.set(newAutoSwitch);
+      ctx.$autoSwitch.set(val === 'on');
     },
     [ctx.$autoSwitch]
   );
 
   return (
-    <MenuOptionGroup value={autoSwitch} onChange={onChange} title="Auto Switch" type="radio">
+    <MenuOptionGroup value={autoSwitch ? 'on' : 'off'} onChange={onChange} title="Auto Switch" type="radio">
       <MenuItemOption value="off" closeOnSelect={false}>
         Off
       </MenuItemOption>
-      <MenuItemOption value="first_progress" closeOnSelect={false}>
-        First Progress
-      </MenuItemOption>
-      <MenuItemOption value="completed" closeOnSelect={false}>
-        Completed
+      <MenuItemOption value="on" closeOnSelect={false}>
+        On
       </MenuItemOption>
     </MenuOptionGroup>
   );
