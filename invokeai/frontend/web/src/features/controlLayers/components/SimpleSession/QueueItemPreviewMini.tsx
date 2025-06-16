94bad3ea9ffcf468a441da894f70ee21ba366891
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-06-16 18:25:37 +1000

    fix(ui): progress image fixes

--- invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/QueueItemPreviewMini.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/QueueItemPreviewMini.tsx
@@ -1,13 +1,17 @@
 import type { SystemStyleObject } from '@invoke-ai/ui-library';
 import { Flex } from '@invoke-ai/ui-library';
-import { useCanvasSessionContext, useOutputImageDTO } from 'features/controlLayers/components/SimpleSession/context';
+import {
+  useCanvasSessionContext,
+  useOutputImageDTO,
+  useProgressData,
+} from 'features/controlLayers/components/SimpleSession/context';
 import { QueueItemCircularProgress } from 'features/controlLayers/components/SimpleSession/QueueItemCircularProgress';
 import { QueueItemNumber } from 'features/controlLayers/components/SimpleSession/QueueItemNumber';
 import { QueueItemProgressImage } from 'features/controlLayers/components/SimpleSession/QueueItemProgressImage';
 import { QueueItemStatusLabel } from 'features/controlLayers/components/SimpleSession/QueueItemStatusLabel';
 import { getQueueItemElementId } from 'features/controlLayers/components/SimpleSession/shared';
 import { DndImage } from 'features/dnd/DndImage';
-import { memo, useCallback, useState } from 'react';
+import { memo, useCallback } from 'react';
 import type { S } from 'services/api/types';
 
 const sx = {
@@ -35,7 +39,7 @@ type Props = {
 
 export const QueueItemPreviewMini = memo(({ item, isSelected, number }: Props) => {
   const ctx = useCanvasSessionContext();
-  const [imageLoaded, setImageLoaded] = useState(false);
+  const { imageLoaded } = useProgressData(ctx.$progressData, item.item_id);
   const imageDTO = useOutputImageDTO(item);
 
   const onClick = useCallback(() => {
@@ -43,15 +47,12 @@ export const QueueItemPreviewMini = memo(({ item, isSelected, number }: Props) =
   }, [ctx.$selectedItemId, item.item_id]);
 
   const onLoad = useCallback(() => {
-    setImageLoaded(true);
-    if (ctx.$progressData.get()[item.item_id]) {
-      ctx.$lastLoadedItemId.set(item.item_id);
-    }
-  }, [ctx.$lastLoadedItemId, ctx.$progressData, item.item_id]);
+    ctx.onImageLoad(item.item_id);
+  }, [ctx, item.item_id]);
 
   return (
     <Flex id={getQueueItemElementId(item.item_id)} sx={sx} data-selected={isSelected} onClick={onClick}>
-      <QueueItemStatusLabel status={item.status} position="absolute" margin="auto" />
+      <QueueItemStatusLabel item={item} position="absolute" margin="auto" />
       {imageDTO && <DndImage imageDTO={imageDTO} onLoad={onLoad} asThumbnail />}
       {!imageLoaded && <QueueItemProgressImage itemId={item.item_id} position="absolute" />}
       <QueueItemNumber number={number} position="absolute" top={0} left={1} />
