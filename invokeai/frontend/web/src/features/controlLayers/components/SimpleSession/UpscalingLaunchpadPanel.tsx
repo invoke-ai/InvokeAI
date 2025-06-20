1bd9515b6cb9810d896620998b5c28b556505395
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-06-20 16:41:49 +1000

    fix(ui): launchpad layouts

--- invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/UpscalingLaunchpadPanel.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/UpscalingLaunchpadPanel.tsx
@@ -3,8 +3,8 @@ import { memo } from 'react';
 
 export const UpscalingLaunchpadPanel = memo(() => {
   return (
-    <Flex flexDir="column" h="full" w="full" alignItems="center" justifyContent="center" gap={2}>
-      <Flex flexDir="column" w="full" h="full" justifyContent="center" gap={4} px={14} maxW={768}>
+    <Flex flexDir="column" h="full" w="full" alignItems="center" gap={2}>
+      <Flex flexDir="column" w="full" gap={4} px={14} maxW={768} pt="20vh">
         <Heading mb={4}>Upscale and add detail.</Heading>
       </Flex>
     </Flex>
