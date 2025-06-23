5f8e21e8094605849e141d54cb28adb134d4dbb5
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-06-23 11:34:26 +1000

    feat(ui): re-implement multiple auto-switch modes

--- invokeai/frontend/web/src/features/controlLayers/components/StagingArea/StagingAreaToolbarMenuAutoSwitch.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/StagingArea/StagingAreaToolbarMenuAutoSwitch.tsx
@@ -1,7 +1,8 @@
 import { MenuItemOption, MenuOptionGroup } from '@invoke-ai/ui-library';
 import { useStore } from '@nanostores/react';
-import { useCanvasSessionContext } from 'features/controlLayers/components/SimpleSession/context';
+import { isAutoSwitchMode, useCanvasSessionContext } from 'features/controlLayers/components/SimpleSession/context';
 import { memo, useCallback } from 'react';
+import { assert } from 'tsafe';
 
 export const StagingAreaToolbarMenuAutoSwitch = memo(() => {
   const ctx = useCanvasSessionContext();
@@ -9,18 +10,22 @@ export const StagingAreaToolbarMenuAutoSwitch = memo(() => {
 
   const onChange = useCallback(
     (val: string | string[]) => {
-      ctx.$autoSwitch.set(val === 'on');
+      assert(isAutoSwitchMode(val));
+      ctx.$autoSwitch.set(val);
     },
     [ctx.$autoSwitch]
   );
 
   return (
-    <MenuOptionGroup value={autoSwitch ? 'on' : 'off'} onChange={onChange} title="Auto Switch" type="radio">
+    <MenuOptionGroup value={autoSwitch} onChange={onChange} title="Auto Switch" type="radio">
       <MenuItemOption value="off" closeOnSelect={false}>
         Off
       </MenuItemOption>
-      <MenuItemOption value="on" closeOnSelect={false}>
-        On
+      <MenuItemOption value="switch_on_start" closeOnSelect={false}>
+        Switch on Start
+      </MenuItemOption>
+      <MenuItemOption value="switch_on_finish" closeOnSelect={false}>
+        Switch on Finish
       </MenuItemOption>
     </MenuOptionGroup>
   );
