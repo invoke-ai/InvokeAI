7cd2f32e1e6586e3c98e0df7434b305d8e978a8a
Billy <billy@invoke.ai>
2025-06-26 17:25:37 +1000

    OMI files

---
+++ invokeai/backend/model_manager/omi/__init__.py
@@ -0,0 +1,6 @@
+from invokeai.backend.model_manager.omi.vendor.model_spec.architecture import (
+    stable_diffusion_xl_1_lora,
+    flux_dev_1_lora
+)
+
+from invokeai.backend.model_manager.omi.omi import convert_from_omi
