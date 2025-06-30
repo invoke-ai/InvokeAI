11fe3b6332d75f02a49e2fd425bfb16a10bf975d
Kent Keirsey <31807370+hipsterusername@users.noreply.github.com>
2025-06-29 21:44:31 -0400

    Comments

--- invokeai/frontend/web/src/features/controlLayers/components/RasterLayer/RasterLayerExportPSDButton.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/RasterLayer/RasterLayerExportPSDButton.tsx
@@ -1,8 +1,6 @@
 import { IconButton } from '@invoke-ai/ui-library';
-import { useAppSelector } from 'app/store/storeHooks';
 import { useCanvasIsBusy } from 'features/controlLayers/hooks/useCanvasIsBusy';
 import { useExportCanvasToPSD } from 'features/controlLayers/hooks/useExportCanvasToPSD';
-import { selectRasterLayerEntities } from 'features/controlLayers/store/selectors';
 import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiFileArrowDownBold } from 'react-icons/pi';
@@ -10,18 +8,11 @@ import { PiFileArrowDownBold } from 'react-icons/pi';
 export const RasterLayerExportPSDButton = memo(() => {
   const { t } = useTranslation();
   const isBusy = useCanvasIsBusy();
-  const rasterLayers = useAppSelector(selectRasterLayerEntities);
   const { exportCanvasToPSD } = useExportCanvasToPSD();
 
   const onClick = useCallback(() => {
     exportCanvasToPSD();
   }, [exportCanvasToPSD]);
-
-  const hasActiveLayers = rasterLayers.some((layer) => layer.isEnabled);
-
-  if (!hasActiveLayers) {
-    return null;
-  }
 
   return (
     <IconButton
