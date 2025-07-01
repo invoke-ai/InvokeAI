d0ee09fad9d944b242bb70014dfbf4f70bf24dfb
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-07-01 14:01:01 +1000

    tidy(ui): clean up focus/layout container

--- invokeai/frontend/web/src/features/ui/layouts/AutoLayoutPanelContainer.tsx
+++ invokeai/frontend/web/src/features/ui/layouts/AutoLayoutPanelContainer.tsx
@@ -1,18 +1,13 @@
-import { Box, type BoxProps, type SystemStyleObject } from '@invoke-ai/ui-library';
+import { Box, type SystemStyleObject } from '@invoke-ai/ui-library';
 import { useAppSelector } from 'app/store/storeHooks';
-import { type FocusRegionName, useFocusRegion, useIsRegionFocused } from 'common/hooks/focus';
+import { useFocusRegion, useIsRegionFocused } from 'common/hooks/focus';
 import type { IDockviewPanelProps, IGridviewPanelProps } from 'dockview';
 import { selectSystemShouldEnableHighlightFocusedRegions } from 'features/system/store/systemSlice';
 import type { PanelParameters } from 'features/ui/layouts/auto-layout-context';
 import type { PropsWithChildren } from 'react';
-import { memo, useMemo, useRef } from 'react';
+import { memo, useRef } from 'react';
 
-interface FocusRegionWrapperProps extends BoxProps {
-  region: FocusRegionName;
-  focusOnMount?: boolean;
-}
-
-export const BASE_FOCUS_REGION_STYLES: SystemStyleObject = {
+const sx: SystemStyleObject = {
   position: 'relative',
   w: 'full',
   h: 'full',
@@ -32,30 +27,6 @@ export const BASE_FOCUS_REGION_STYLES: SystemStyleObject = {
   },
 };
 
-export const FocusRegionWrapper = memo((props: FocusRegionWrapperProps) => {
-  const { region, focusOnMount = false, sx: _sx, children, ...boxProps } = props;
-
-  const ref = useRef<HTMLDivElement>(null);
-
-  const sx = useMemo(() => ({ ...BASE_FOCUS_REGION_STYLES, ..._sx }), [_sx]);
-
-  const shouldHighlightFocusedRegions = useAppSelector(selectSystemShouldEnableHighlightFocusedRegions);
-
-  const options = useMemo(() => ({ focusOnMount }), [focusOnMount]);
-
-  useFocusRegion(region, ref, options);
-  const isFocused = useIsRegionFocused(region);
-  const isHighlighted = isFocused && shouldHighlightFocusedRegions;
-
-  return (
-    <Box ref={ref} tabIndex={-1} sx={sx} data-highlighted={isHighlighted} {...boxProps}>
-      {children}
-    </Box>
-  );
-});
-
-FocusRegionWrapper.displayName = 'FocusRegionWrapper';
-
 export const AutoLayoutPanelContainer = memo(
   (
     props:
@@ -71,7 +42,7 @@ export const AutoLayoutPanelContainer = memo(
     const isHighlighted = isFocused && shouldHighlightFocusedRegions;
 
     return (
-      <Box ref={ref} tabIndex={-1} sx={BASE_FOCUS_REGION_STYLES} data-highlighted={isHighlighted}>
+      <Box ref={ref} tabIndex={-1} sx={sx} data-highlighted={isHighlighted}>
         {props.children}
       </Box>
     );
