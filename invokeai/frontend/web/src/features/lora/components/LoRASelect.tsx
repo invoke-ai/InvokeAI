910576f7c414a397e6b84212e199a394308a4f55
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-07-04 01:39:59 +1000

    perf(ui): lora components

--- invokeai/frontend/web/src/features/lora/components/LoRASelect.tsx
+++ invokeai/frontend/web/src/features/lora/components/LoRASelect.tsx
@@ -1,6 +1,6 @@
 import { FormControl, FormLabel } from '@invoke-ai/ui-library';
-import { createSelector } from '@reduxjs/toolkit';
 import { EMPTY_ARRAY } from 'app/store/constants';
+import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
 import { useAppDispatch, useAppSelector } from 'app/store/storeHooks';
 import { InformationalPopover } from 'common/components/InformationalPopover/InformationalPopover';
 import type { GroupStatusMap } from 'common/components/Picker/Picker';
@@ -13,13 +13,15 @@ import { useTranslation } from 'react-i18next';
 import { useLoRAModels } from 'services/api/hooks/modelsByType';
 import type { LoRAModelConfig } from 'services/api/types';
 
-const selectLoRAs = createSelector(selectLoRAsSlice, (loras) => loras.loras);
+const selectLoRAModelKeys = createMemoizedSelector(selectLoRAsSlice, ({ loras }) =>
+  loras.map(({ model }) => model.key)
+);
 
 const LoRASelect = () => {
   const dispatch = useAppDispatch();
   const [modelConfigs, { isLoading }] = useLoRAModels();
   const { t } = useTranslation();
-  const addedLoRAs = useAppSelector(selectLoRAs);
+  const addedLoRAModelKeys = useAppSelector(selectLoRAModelKeys);
 
   const currentBaseModel = useAppSelector(selectBase);
 
@@ -33,10 +35,10 @@ const LoRASelect = () => {
 
   const getIsDisabled = useCallback(
     (model: LoRAModelConfig): boolean => {
-      const isAdded = Boolean(addedLoRAs.find((lora) => lora.model.key === model.key));
+      const isAdded = addedLoRAModelKeys.includes(model.key);
       return isAdded;
     },
-    [addedLoRAs]
+    [addedLoRAModelKeys]
   );
 
   const onChange = useCallback(
