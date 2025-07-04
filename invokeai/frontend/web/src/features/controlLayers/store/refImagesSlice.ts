b86e2838bfb00f9bc713f38fe120bb717103f96d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-07-04 17:27:29 +1000

    fix(ui): do not attempt to recall ref images from canvas metadata

--- invokeai/frontend/web/src/features/controlLayers/store/refImagesSlice.ts
+++ invokeai/frontend/web/src/features/controlLayers/store/refImagesSlice.ts
@@ -5,7 +5,6 @@ import { createMemoizedSelector } from 'app/store/createMemoizedSelector';
 import type { PersistConfig, RootState } from 'app/store/store';
 import { clamp } from 'es-toolkit/compat';
 import { getPrefixedId } from 'features/controlLayers/konva/util';
-import { canvasMetadataRecalled } from 'features/controlLayers/store/canvasSlice';
 import type { FLUXReduxImageInfluence, RefImagesState } from 'features/controlLayers/store/types';
 import { zModelIdentifierField } from 'features/nodes/types/common';
 import type { ApiModelConfig, FLUXReduxModelConfig, ImageDTO, IPAdapterModelConfig } from 'services/api/types';
@@ -231,12 +230,6 @@ export const refImagesSlice = createSlice({
       entity.isEnabled = !entity.isEnabled;
     },
     refImagesReset: () => getInitialRefImagesState(),
-  },
-  extraReducers(builder) {
-    builder.addCase(canvasMetadataRecalled, (state, action) => {
-      const { referenceImages } = action.payload;
-      state.entities = referenceImages;
-    });
   },
 });
 
