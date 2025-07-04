352704dbbc08adc78201b2da4abfc2de26f721e0
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-07-04 12:20:10 +1000

    feat(ui): display cached metadata if it exists instead of always waiting for debounce

--- invokeai/frontend/web/src/services/api/hooks/useDebouncedMetadata.ts
+++ invokeai/frontend/web/src/services/api/hooks/useDebouncedMetadata.ts
@@ -1,14 +1,18 @@
 import { skipToken } from '@reduxjs/toolkit/query';
 import { useAppSelector } from 'app/store/storeHooks';
 import { selectMetadataFetchDebounce } from 'features/system/store/configSlice';
-import { useGetImageMetadataQuery } from 'services/api/endpoints/images';
+import { imagesApi, useGetImageMetadataQuery } from 'services/api/endpoints/images';
 import { useDebounce } from 'use-debounce';
 
 export const useDebouncedMetadata = (imageName?: string | null) => {
   const metadataFetchDebounce = useAppSelector(selectMetadataFetchDebounce);
 
   const [debouncedImageName] = useDebounce(imageName, metadataFetchDebounce);
-  const { data, isFetching } = useGetImageMetadataQuery(debouncedImageName ?? skipToken);
+  const { currentData: cachedData } = imagesApi.endpoints.getImageMetadata.useQueryState(imageName ?? skipToken);
+  const { currentData: data, isFetching } = useGetImageMetadataQuery(debouncedImageName ?? skipToken);
 
-  return { metadata: data, isLoading: isFetching || imageName !== debouncedImageName };
+  return {
+    metadata: cachedData ?? data,
+    isLoading: cachedData ? false : isFetching || imageName !== debouncedImageName,
+  };
 };
