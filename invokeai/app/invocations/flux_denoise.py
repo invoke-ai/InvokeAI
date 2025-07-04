0fe1bf0933d528ee62dcd73c714aabee25956790
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-07-04 19:49:19 +1000

    fix(nodes): revert unnecessary version bump

--- invokeai/app/invocations/flux_denoise.py
+++ invokeai/app/invocations/flux_denoise.py
@@ -63,7 +63,7 @@ from invokeai.backend.util.devices import TorchDevice
     title="FLUX Denoise",
     tags=["image", "flux"],
     category="image",
-    version="4.1.0",
+    version="4.0.0",
 )
 class FluxDenoiseInvocation(BaseInvocation):
     """Run denoising process with a FLUX transformer model."""
