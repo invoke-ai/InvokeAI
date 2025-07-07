971ff6f7dc62e4e2349c0b7c100b7959d3d7cfcc
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-07-07 22:57:20 +1000

    feat(ui): clean up auto layout context for panels

--- invokeai/frontend/web/src/features/ui/layouts/auto-layout-context.tsx
+++ invokeai/frontend/web/src/features/ui/layouts/auto-layout-context.tsx
@@ -1,22 +1,21 @@
 import type { FocusRegionName } from 'common/hooks/focus';
 import type { IDockviewPanelProps, IGridviewPanelProps } from 'dockview';
 import type { TabName } from 'features/ui/store/uiTypes';
-import type { FunctionComponent, PropsWithChildren, RefObject } from 'react';
+import type { FunctionComponent, PropsWithChildren } from 'react';
 import { createContext, memo, useContext, useMemo } from 'react';
 
 import { AutoLayoutPanelContainer } from './AutoLayoutPanelContainer';
 
 type AutoLayoutContextValue = {
   tab: TabName;
-  rootRef: RefObject<HTMLDivElement>;
 };
 
 const AutoLayoutContext = createContext<AutoLayoutContextValue | null>(null);
 
 export const AutoLayoutProvider = (props: PropsWithChildren<AutoLayoutContextValue>) => {
-  const { tab, rootRef, children } = props;
+  const { tab, children } = props;
 
-  const value = useMemo<AutoLayoutContextValue>(() => ({ tab, rootRef }), [tab, rootRef]);
+  const value = useMemo<AutoLayoutContextValue>(() => ({ tab }), [tab]);
   return <AutoLayoutContext.Provider value={value}>{children}</AutoLayoutContext.Provider>;
 };
 
