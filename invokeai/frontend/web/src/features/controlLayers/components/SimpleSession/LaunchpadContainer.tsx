8117d27ea0b8df5bb4cda65650a26ac677175e7d
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-07-08 07:18:13 +1000

    feat(ui): tighten up launchpad content to fit better

--- invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/LaunchpadContainer.tsx
+++ invokeai/frontend/web/src/features/controlLayers/components/SimpleSession/LaunchpadContainer.tsx
@@ -4,10 +4,10 @@ import { memo } from 'react';
 
 export const LaunchpadContainer = memo((props: PropsWithChildren<{ heading: string }>) => {
   return (
-    <Flex flexDir="column" h="full" w="full" alignItems="center" gap={2}>
-      <Flex flexDir="column" w="full" gap={4} px={14} maxW={768} pt="20vh">
-        <Heading mb={4}>{props.heading}</Heading>
-        <Flex flexDir="column" gap={8}>
+    <Flex flexDir="column" h="full" w="full" alignItems="center" justifyContent="center" gap={2}>
+      <Flex flexDir="column" w="full" gap={4} px={14} maxW={768}>
+        <Heading>{props.heading}</Heading>
+        <Flex flexDir="column" gap={4}>
           {props.children}
         </Flex>
       </Flex>
