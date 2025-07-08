e8817a6f44085a70abf4a998ca1267444b16451b
Kent Keirsey <31807370+hipsterusername@users.noreply.github.com>
2025-07-07 20:53:26 -0400

    Address comments

--- invokeai/frontend/web/src/features/system/components/VideosModal/VideosModal.tsx
+++ invokeai/frontend/web/src/features/system/components/VideosModal/VideosModal.tsx
@@ -90,7 +90,7 @@ export const VideosModal = memo(() => {
   return (
     <Modal isOpen={videosModal.isOpen} onClose={videosModal.close} size="2xl" isCentered useInert={false}>
       <ModalOverlay />
-      <ModalContent maxH="40vh" h="40vh">
+      <ModalContent maxH="60vh" h="60vh">
         <ModalHeader bg="none">{t('supportVideos.supportVideos')}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
@@ -98,10 +98,7 @@ export const VideosModal = memo(() => {
             <Flex flexDir="column" gap={4}>
               <Flex flexDir="column" gap={2} pb={2}>
                 <Text fontSize="md">
-                  <Trans i18nKey="supportVideos.studioSessionsDesc1" components={components} />
-                </Text>
-                <Text fontSize="md">
-                  <Trans i18nKey="supportVideos.studioSessionsDesc2" components={components} />
+                  <Trans i18nKey="supportVideos.studioSessionsDesc" components={components} />
                 </Text>
               </Flex>
               <VideoCardList category={t('supportVideos.supportVideos')} videos={supportVideos} />
