8f27a393d851ecf69d99acfaf665b07f76bdb349
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-07-23 21:19:15 +1000

    wip

--- invokeai/frontend/web/src/app/store/enhancers/reduxRemember/driver.ts
+++ invokeai/frontend/web/src/app/store/enhancers/reduxRemember/driver.ts
@@ -3,9 +3,16 @@ import { StorageError } from 'app/store/enhancers/reduxRemember/errors';
 import { $authToken } from 'app/store/nanostores/authToken';
 import { $projectId } from 'app/store/nanostores/projectId';
 import { $queueId } from 'app/store/nanostores/queueId';
+import { atom } from 'nanostores';
 import type { Driver } from 'redux-remember';
 import { getBaseUrl } from 'services/api';
 import { buildAppInfoUrl } from 'services/api/endpoints/appInfo';
+
+export type StorageDriverApi = {
+  getItem: (key: string) => Promise<any>;
+  setItem: (key: string, value: any) => Promise<any>;
+  clear: () => Promise<void>;
+};
 
 const log = logger('system');
 
@@ -47,77 +54,94 @@ const getHeaders = (extra?: Record<string, string>) => {
   return headers;
 };
 
-export const serverBackedDriver: Driver = {
-  getItem: async (key) => {
-    try {
-      const url = getUrl(key);
-      const headers = getHeaders();
-      const res = await fetch(url, { headers, method: 'GET' });
-      if (!res.ok) {
-        throw new Error(`Response status: ${res.status}`);
+export const buildDriver = (api?: StorageDriverApi): Driver => {
+  return {
+    getItem: async (key) => {
+      try {
+        if (api) {
+          log.trace(`Using provided API to get item for key "${key}"`);
+          return await api.getItem(key);
+        }
+        const url = getUrl(key);
+        const headers = getHeaders();
+        const res = await fetch(url, { headers, method: 'GET' });
+        if (!res.ok) {
+          throw new Error(`Response status: ${res.status}`);
+        }
+        const json = await res.json();
+        return json;
+      } catch (originalError) {
+        throw new StorageError({
+          key,
+          projectId: $projectId.get(),
+          originalError,
+        });
       }
-      const json = await res.json();
-      return json;
-    } catch (originalError) {
-      throw new StorageError({
-        key,
-        projectId: $projectId.get(),
-        originalError,
-      });
-    }
-  },
-  setItem: async (key, value) => {
-    try {
-      persistRefCount++;
-      // Deep equality check to avoid noop persist network requests.
-      //
-      // `redux-remember` persists individual slices of state, so we can implicity denylist a slice by not giving it a
-      // persist config.
-      //
-      // However, we may need to avoid persisting individual _fields_ of a slice. `redux-remember` does not provide a
-      // way to do this directly.
-      //
-      // To accomplish this, we add a layer of logic on top of the `redux-remember`. In the state serializer function
-      // provided to `redux-remember`, we can omit certain fields from the state that we do not want to persist. See
-      // the implementation in `store.ts` for this logic.
-      //
-      // This logic is unknown to `redux-remember`. When an omitted field changes, it will still attempt to persist the
-      // whole slice, even if the final, _serialized_ slice value is unchanged.
-      //
-      // To avoid unnecessary network requests, we keep track of the last persisted state for each key. If the value to
-      // be persisted is the same as the last persisted value, we skip the network request.
-      if (lastPersistedState.get(key) === value) {
-        log.trace(`Skipping persist for key "${key}" as value is unchanged.`);
+    },
+    setItem: async (key, value) => {
+      try {
+        persistRefCount++;
+        if (api) {
+          log.trace(`Using provided API to get item for key "${key}"`);
+          return await api.setItem(key, value);
+        }
+        // Deep equality check to avoid noop persist network requests.
+        //
+        // `redux-remember` persists individual slices of state, so we can implicity denylist a slice by not giving it a
+        // persist config.
+        //
+        // However, we may need to avoid persisting individual _fields_ of a slice. `redux-remember` does not provide a
+        // way to do this directly.
+        //
+        // To accomplish this, we add a layer of logic on top of the `redux-remember`. In the state serializer function
+        // provided to `redux-remember`, we can omit certain fields from the state that we do not want to persist. See
+        // the implementation in `store.ts` for this logic.
+        //
+        // This logic is unknown to `redux-remember`. When an omitted field changes, it will still attempt to persist the
+        // whole slice, even if the final, _serialized_ slice value is unchanged.
+        //
+        // To avoid unnecessary network requests, we keep track of the last persisted state for each key. If the value to
+        // be persisted is the same as the last persisted value, we skip the network request.
+        if (lastPersistedState.get(key) === value) {
+          log.trace(`Skipping persist for key "${key}" as value is unchanged.`);
+          return value;
+        }
+        const url = getUrl(key);
+        const headers = getHeaders({ 'content-type': 'application/json' });
+        const res = await fetch(url, { headers, method: 'POST', body: JSON.stringify(value) });
+        if (!res.ok) {
+          throw new Error(`Response status: ${res.status}`);
+        }
+        lastPersistedState.set(key, value);
         return value;
+      } catch (originalError) {
+        throw new StorageError({
+          key,
+          value,
+          projectId: $projectId.get(),
+          originalError,
+        });
+      } finally {
+        persistRefCount--;
+        if (persistRefCount < 0) {
+          log.warn('Persist ref count is negative, resetting to 0');
+          persistRefCount = 0;
+        }
       }
-      const url = getUrl(key);
-      const headers = getHeaders({ 'content-type': 'application/json' });
-      const res = await fetch(url, { headers, method: 'POST', body: JSON.stringify(value) });
-      if (!res.ok) {
-        throw new Error(`Response status: ${res.status}`);
-      }
-      lastPersistedState.set(key, value);
-      return value;
-    } catch (originalError) {
-      throw new StorageError({
-        key,
-        value,
-        projectId: $projectId.get(),
-        originalError,
-      });
-    } finally {
-      persistRefCount--;
-      if (persistRefCount < 0) {
-        log.warn('Persist ref count is negative, resetting to 0');
-        persistRefCount = 0;
-      }
-    }
-  },
+    },
+  };
 };
 
-export const resetClientState = async () => {
+export const $resetClientState = atom(() => {});
+
+export const buildResetClientState = (api?: StorageDriverApi) => async () => {
   try {
     persistRefCount++;
+    if (api) {
+      log.trace('Using provided API to reset client state');
+      await api.clear();
+      return;
+    }
     const url = getUrl();
     const headers = getHeaders();
     const res = await fetch(url, { headers, method: 'DELETE' });
