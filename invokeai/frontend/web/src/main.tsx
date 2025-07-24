28e7a83f987073859b2ca8c613d6a0677516ebd4
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-07-24 13:58:10 +1000

    revert(ui): temp changes to main.tsx for testing

--- invokeai/frontend/web/src/main.tsx
+++ invokeai/frontend/web/src/main.tsx
@@ -1,23 +1,5 @@
-import type { StorageDriverApi } from 'app/store/enhancers/reduxRemember/driver';
 import ReactDOM from 'react-dom/client';
 
 import InvokeAIUI from './app/components/InvokeAIUI';
 
-let state: Record<string, any> = {};
-const storageDriverApi: StorageDriverApi = {
-  getItem: (key: string) => {
-    return Promise.resolve(state[key]);
-  },
-  setItem: (key: string, value: any) => {
-    state[key] = value;
-    return Promise.resolve(value);
-  },
-  clear: () => {
-    state = {};
-    return Promise.resolve();
-  },
-};
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <InvokeAIUI storageDriverApi={storageDriverApi} />
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<InvokeAIUI />);
