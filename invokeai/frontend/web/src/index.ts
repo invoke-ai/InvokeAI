5a181ee0fd356123eea0ae35fb9d0b07872d703f
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-07-29 19:56:33 +1000

    build(ui): export loading component

--- invokeai/frontend/web/src/index.ts
+++ invokeai/frontend/web/src/index.ts
@@ -32,6 +32,7 @@ export { default as InvokeAIUI } from './app/components/InvokeAIUI';
 export type { StudioInitAction } from './app/hooks/useStudioInitAction';
 export type { LoggingOverrides } from './app/logging/logger';
 export type { PartialAppConfig } from './app/types/invokeai';
+export { default as Loading } from './common/components/Loading/Loading';
 export { default as HotkeysModal } from './features/system/components/HotkeysModal/HotkeysModal';
 export { default as InvokeAiLogoComponent } from './features/system/components/InvokeAILogoComponent';
 export { default as SettingsModal } from './features/system/components/SettingsModal/SettingsModal';
