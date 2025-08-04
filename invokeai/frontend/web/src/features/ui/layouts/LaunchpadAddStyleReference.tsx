a026355f783160ae9e27f05ddbcfbef0888770c8
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-08-05 08:38:33 +1000

    fix(ui): add style ref launchpad using wrong dnd config
    I don't think this actually caused problems bc the two DND targets were
    very similar, but it was wrong.

--- invokeai/frontend/web/src/features/ui/layouts/LaunchpadAddStyleReference.tsx
+++ invokeai/frontend/web/src/features/ui/layouts/LaunchpadAddStyleReference.tsx
@@ -4,7 +4,7 @@ import { useImageUploadButton } from 'common/hooks/useImageUploadButton';
 import { getDefaultRefImageConfig } from 'features/controlLayers/hooks/addLayerHooks';
 import { refImageAdded } from 'features/controlLayers/store/refImagesSlice';
 import { imageDTOToImageWithDims } from 'features/controlLayers/store/util';
-import { addGlobalReferenceImageDndTarget, newCanvasFromImageDndTarget } from 'features/dnd/dnd';
+import { addGlobalReferenceImageDndTarget } from 'features/dnd/dnd';
 import { DndDropTarget } from 'features/dnd/DndDropTarget';
 import { LaunchpadButton } from 'features/ui/layouts/LaunchpadButton';
 import { memo, useMemo } from 'react';
@@ -39,13 +39,13 @@ export const LaunchpadAddStyleReference = memo((props: { extraAction?: () => voi
       <Icon as={PiUserCircleGearBold} boxSize={8} color="base.500" />
       <Flex flexDir="column" alignItems="flex-start" gap={2}>
         <Heading size="sm">{t('ui.launchpad.addStyleRef.title')}</Heading>
-        <Text color="base.300">{t('ui.launchpad.addStyleRef.description')}</Text>
+        <Text>{t('ui.launchpad.addStyleRef.description')}</Text>
       </Flex>
       <Flex position="absolute" right={3} bottom={3}>
         <PiUploadBold />
         <input {...uploadApi.getUploadInputProps()} />
       </Flex>
-      <DndDropTarget dndTarget={newCanvasFromImageDndTarget} dndTargetData={dndTargetData} label="Drop" />
+      <DndDropTarget dndTarget={addGlobalReferenceImageDndTarget} dndTargetData={dndTargetData} label="Drop" />
     </LaunchpadButton>
   );
 });
