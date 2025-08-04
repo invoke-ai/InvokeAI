68e30a98647171c4e73e7e7531c458c7a344aded
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-08-05 08:38:05 +1000

    feat(ui): prevent creating new canvases while staging
    Disable these items while staging:
    - New Canvas From Image context menu
    - Edit image hook & launchpad button
    - Generate from Text launchpad button (only while on canvas tab)
    - Use a Layout Image launchpad button

--- invokeai/frontend/web/src/features/ui/layouts/LaunchpadGenerateFromTextButton.tsx
+++ invokeai/frontend/web/src/features/ui/layouts/LaunchpadGenerateFromTextButton.tsx
@@ -1,5 +1,8 @@
 import { Flex, Heading, Icon, Text } from '@invoke-ai/ui-library';
+import { useAppSelector } from 'app/store/storeHooks';
+import { useCanvasIsStaging } from 'features/controlLayers/store/canvasStagingAreaSlice';
 import { LaunchpadButton } from 'features/ui/layouts/LaunchpadButton';
+import { selectActiveTab } from 'features/ui/store/uiSelectors';
 import { memo, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PiCursorTextBold, PiTextAaBold } from 'react-icons/pi';
@@ -14,16 +17,19 @@ const focusOnPrompt = () => {
 
 export const LaunchpadGenerateFromTextButton = memo((props: { extraAction?: () => void }) => {
   const { t } = useTranslation();
+  const tab = useAppSelector(selectActiveTab);
+  const isStaging = useCanvasIsStaging();
+
   const onClick = useCallback(() => {
     focusOnPrompt();
     props.extraAction?.();
   }, [props]);
   return (
-    <LaunchpadButton onClick={onClick} position="relative" gap={8}>
+    <LaunchpadButton onClick={onClick} position="relative" gap={8} isDisabled={tab === 'canvas' && isStaging}>
       <Icon as={PiTextAaBold} boxSize={8} color="base.500" />
       <Flex flexDir="column" alignItems="flex-start" gap={2}>
         <Heading size="sm">{t('ui.launchpad.generateFromText.title')}</Heading>
-        <Text color="base.300">{t('ui.launchpad.generateFromText.description')}</Text>
+        <Text>{t('ui.launchpad.generateFromText.description')}</Text>
       </Flex>
       <Flex position="absolute" right={3} bottom={3}>
         <PiCursorTextBold />
