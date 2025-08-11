8f2799d5e83a0c55ea8096118a2f7ef85fe38db0
Mary Hipp <maryhipp@Mac.lan>
2025-08-11 13:40:51 -0400

    split out RunwayVideoOutput from VideoOutput

--- invokeai/invocation_api/__init__.py
+++ invokeai/invocation_api/__init__.py
@@ -68,6 +68,7 @@ from invokeai.app.invocations.primitives import (
     LatentsOutput,
     StringCollectionOutput,
     StringOutput,
+    RunwayVideoOutput,
     VideoOutput,
 )
 from invokeai.app.invocations.scheduler import SchedulerOutput
@@ -157,6 +158,7 @@ __all__ = [
     "LatentsOutput",
     "StringCollectionOutput",
     "StringOutput",
+    "RunwayVideoOutput",
     "VideoOutput",
     # invokeai.app.services.image_records.image_records_common
     "ImageCategory",
