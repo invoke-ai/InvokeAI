94ba8409486672b4363b404a5a0f0bcfd5f1b266
Mary Hipp <maryhipp@Mac.lan>
2025-08-11 16:58:11 -0400

    push up updates for VideoField

--- invokeai/app/invocations/fields.py
+++ invokeai/app/invocations/fields.py
@@ -229,7 +229,10 @@ class ImageField(BaseModel):
 class VideoField(BaseModel):
     """A video primitive field"""
 
-    video_name: str = Field(description="The name of the video")
+    video_id: str = Field(description="The id of the video")
+    width: int = Field(description="The width of the video in pixels")
+    height: int = Field(description="The height of the video in pixels")
+    duration_seconds: float = Field(description="The duration of the video in seconds")
 
 
 class BoardField(BaseModel):
