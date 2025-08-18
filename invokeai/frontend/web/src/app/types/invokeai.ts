1f8a60ded26adcffa67ca32d09776a9c0cf0aef7
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-08-18 10:54:42 +1000

    fix(ui): export NumericalParameterConfig type

--- invokeai/frontend/web/src/app/types/invokeai.ts
+++ invokeai/frontend/web/src/app/types/invokeai.ts
@@ -58,6 +58,7 @@ const zNumericalParameterConfig = z.object({
   fineStep: z.number().default(8),
   coarseStep: z.number().default(64),
 });
+export type NumericalParameterConfig = z.infer<typeof zNumericalParameterConfig>;
 
 /**
  * Configuration options for the InvokeAI UI.
