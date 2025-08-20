6ef1c2a5e1138fcdc3e7fc83e5aea491b30be3e5
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-08-20 18:42:14 +1000

    feat(ui): consolidated gallery (wip)

--- invokeai/frontend/web/src/features/gallery/components/use-gallery-image-names.ts
+++ invokeai/frontend/web/src/features/gallery/components/use-gallery-image-names.ts
@@ -1,7 +1,14 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { skipToken } from '@reduxjs/toolkit/query';
 import { EMPTY_ARRAY } from 'app/store/constants';
 import { useAppSelector } from 'app/store/storeHooks';
-import { selectGetImageNamesQueryArgs } from 'features/gallery/store/gallerySelectors';
+import {
+  selectGalleryView,
+  selectGetImageNamesQueryArgs,
+  selectGetVideoIdsQueryArgs,
+} from 'features/gallery/store/gallerySelectors';
 import { useGetImageNamesQuery } from 'services/api/endpoints/images';
+import { useGetVideoIdsQuery } from 'services/api/endpoints/videos';
 import { useDebounce } from 'use-debounce';
 
 const getImageNamesQueryOptions = {
@@ -13,9 +20,33 @@ const getImageNamesQueryOptions = {
   }),
 } satisfies Parameters<typeof useGetImageNamesQuery>[1];
 
+const getVideoIdsQueryOptions = {
+  refetchOnReconnect: true,
+  selectFromResult: ({ currentData, isLoading, isFetching }) => ({
+    video_ids: currentData?.video_ids ?? EMPTY_ARRAY,
+    isLoading,
+    isFetching,
+  }),
+} satisfies Parameters<typeof useGetVideoIdsQuery>[1];
+
 export const useGalleryImageNames = () => {
+  const galleryView = useAppSelector(selectGalleryView);
   const _queryArgs = useAppSelector(selectGetImageNamesQueryArgs);
   const [queryArgs] = useDebounce(_queryArgs, 300);
-  const { imageNames, isLoading, isFetching } = useGetImageNamesQuery(queryArgs, getImageNamesQueryOptions);
+  const { imageNames, isLoading, isFetching } = useGetImageNamesQuery(
+    galleryView !== 'videos' ? queryArgs : skipToken,
+    getImageNamesQueryOptions
+  );
   return { imageNames, isLoading, isFetching, queryArgs };
+};
+
+export const useGalleryVideoIds = () => {
+  const galleryView = useAppSelector(selectGalleryView);
+  const _queryArgs = useAppSelector(selectGetVideoIdsQueryArgs);
+  const [queryArgs] = useDebounce(_queryArgs, 300);
+  const { video_ids, isLoading, isFetching } = useGetVideoIdsQuery(
+    galleryView === 'videos' ? queryArgs : skipToken,
+    getVideoIdsQueryOptions
+  );
+  return { video_ids, isLoading, isFetching, queryArgs };
 };
