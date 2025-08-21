dced66535a72f56d91fc10fab0646f4192deaa62
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-08-21 21:47:27 +1000

    Revert "feat(ui): consolidated gallery (wip)"
    This reverts commit 12b70bca678c58a8917ec29cc04d229d4b664385.

--- invokeai/frontend/web/src/features/gallery/components/use-gallery-image-names.ts
+++ invokeai/frontend/web/src/features/gallery/components/use-gallery-image-names.ts
@@ -1,14 +1,7 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { skipToken } from '@reduxjs/toolkit/query';
 import { EMPTY_ARRAY } from 'app/store/constants';
 import { useAppSelector } from 'app/store/storeHooks';
-import {
-  selectGalleryView,
-  selectGetImageNamesQueryArgs,
-  selectGetVideoIdsQueryArgs,
-} from 'features/gallery/store/gallerySelectors';
+import { selectGetImageNamesQueryArgs } from 'features/gallery/store/gallerySelectors';
 import { useGetImageNamesQuery } from 'services/api/endpoints/images';
-import { useGetVideoIdsQuery } from 'services/api/endpoints/videos';
 import { useDebounce } from 'use-debounce';
 
 const getImageNamesQueryOptions = {
@@ -20,33 +13,9 @@ const getImageNamesQueryOptions = {
   }),
 } satisfies Parameters<typeof useGetImageNamesQuery>[1];
 
-const getVideoIdsQueryOptions = {
-  refetchOnReconnect: true,
-  selectFromResult: ({ currentData, isLoading, isFetching }) => ({
-    video_ids: currentData?.video_ids ?? EMPTY_ARRAY,
-    isLoading,
-    isFetching,
-  }),
-} satisfies Parameters<typeof useGetVideoIdsQuery>[1];
-
 export const useGalleryImageNames = () => {
-  const galleryView = useAppSelector(selectGalleryView);
   const _queryArgs = useAppSelector(selectGetImageNamesQueryArgs);
   const [queryArgs] = useDebounce(_queryArgs, 300);
-  const { imageNames, isLoading, isFetching } = useGetImageNamesQuery(
-    galleryView !== 'videos' ? queryArgs : skipToken,
-    getImageNamesQueryOptions
-  );
+  const { imageNames, isLoading, isFetching } = useGetImageNamesQuery(queryArgs, getImageNamesQueryOptions);
   return { imageNames, isLoading, isFetching, queryArgs };
-};
-
-export const useGalleryVideoIds = () => {
-  const galleryView = useAppSelector(selectGalleryView);
-  const _queryArgs = useAppSelector(selectGetVideoIdsQueryArgs);
-  const [queryArgs] = useDebounce(_queryArgs, 300);
-  const { video_ids, isLoading, isFetching } = useGetVideoIdsQuery(
-    galleryView === 'videos' ? queryArgs : skipToken,
-    getVideoIdsQueryOptions
-  );
-  return { video_ids, isLoading, isFetching, queryArgs };
 };
