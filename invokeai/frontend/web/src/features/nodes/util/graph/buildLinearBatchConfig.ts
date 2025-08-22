85f6ca3bafe1bc77ee1adcd675634790dcef1ce6
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-08-22 19:01:39 +1000

    fix(ui): iterations works for video models

--- invokeai/frontend/web/src/features/nodes/util/graph/buildLinearBatchConfig.ts
+++ invokeai/frontend/web/src/features/nodes/util/graph/buildLinearBatchConfig.ts
@@ -4,22 +4,22 @@ import { range } from 'es-toolkit/compat';
 import type { SeedBehaviour } from 'features/dynamicPrompts/store/dynamicPromptsSlice';
 import type { ModelIdentifierField } from 'features/nodes/types/common';
 import type { Graph } from 'features/nodes/util/graph/generation/Graph';
-import { API_BASE_MODELS } from 'features/parameters/types/constants';
+import { API_BASE_MODELS, VIDEO_BASE_MODELS } from 'features/parameters/types/constants';
 import type { components } from 'services/api/schema';
-import type { Batch, EnqueueBatchArg, Invocation } from 'services/api/types';
+import type { AnyModelConfig, BaseModelType, Batch, EnqueueBatchArg, Invocation } from 'services/api/types';
 import { assert } from 'tsafe';
 
 const getExtendedPrompts = (arg: {
   seedBehaviour: SeedBehaviour;
   iterations: number;
   prompts: string[];
-  model: ModelIdentifierField;
+  base: BaseModelType;
 }): string[] => {
-  const { seedBehaviour, iterations, prompts, model } = arg;
+  const { seedBehaviour, iterations, prompts, base } = arg;
   // Normally, the seed behaviour implicity determines the batch size. But when we use models without seeds (like
   // ChatGPT 4o) in conjunction with the per-prompt seed behaviour, we lose out on that implicit batch size. To rectify
   // this, we need to create a batch of the right size by repeating the prompts.
-  if (seedBehaviour === 'PER_PROMPT' || API_BASE_MODELS.includes(model.base)) {
+  if (seedBehaviour === 'PER_PROMPT' || API_BASE_MODELS.includes(base) || VIDEO_BASE_MODELS.includes(base)) {
     return range(iterations).flatMap(() => prompts);
   }
   return prompts;
@@ -29,16 +29,15 @@ export const prepareLinearUIBatch = (arg: {
   state: RootState;
   g: Graph;
   prepend: boolean;
+  base: BaseModelType;
   positivePromptNode: Invocation<'string'>;
   seedNode?: Invocation<'integer'>;
   origin: string;
   destination: string;
 }): EnqueueBatchArg => {
-  const { state, g, prepend, positivePromptNode, seedNode, origin, destination } = arg;
-  const { iterations, model, shouldRandomizeSeed, seed } = state.params;
+  const { state, g, base, prepend, positivePromptNode, seedNode, origin, destination } = arg;
+  const { iterations, shouldRandomizeSeed, seed } = state.params;
   const { prompts, seedBehaviour } = state.dynamicPrompts;
-
-  assert(model, 'No model found in state when preparing batch');
 
   const data: Batch['data'] = [];
   const firstBatchDatumList: components['schemas']['BatchDatum'][] = [];
@@ -63,6 +62,7 @@ export const prepareLinearUIBatch = (arg: {
       start: shouldRandomizeSeed ? undefined : seed,
     });
 
+    console.log(seeds);
     secondBatchDatumList.push({
       node_path: seedNode.id,
       field_name: 'value',
@@ -71,7 +71,7 @@ export const prepareLinearUIBatch = (arg: {
     data.push(secondBatchDatumList);
   }
 
-  const extendedPrompts = getExtendedPrompts({ seedBehaviour, iterations, prompts, model });
+  const extendedPrompts = getExtendedPrompts({ seedBehaviour, iterations, prompts, base });
 
   // zipped batch of prompts
   firstBatchDatumList.push({
--- invokeai/frontend/web/src/features/queue/hooks/useEnqueueVideo.ts
+++ invokeai/frontend/web/src/features/queue/hooks/useEnqueueVideo.ts
@@ -4,7 +4,8 @@ import { logger } from 'app/logging/logger';
 import type { AppStore } from 'app/store/store';
 import { useAppStore } from 'app/store/storeHooks';
 import { extractMessageFromAssertionError } from 'common/util/extractMessageFromAssertionError';
-import { withResultAsync } from 'common/util/result';
+import { withResult, withResultAsync } from 'common/util/result';
+import { prepareLinearUIBatch } from 'features/nodes/util/graph/buildLinearBatchConfig';
 import { buildRunwayVideoGraph } from 'features/nodes/util/graph/generation/buildRunwayVideoGraph';
 import { buildVeo3VideoGraph } from 'features/nodes/util/graph/generation/buildVeo3VideoGraph';
 import { selectCanvasDestination } from 'features/nodes/util/graph/graphBuilderUtils';
@@ -17,21 +18,23 @@ import { enqueueMutationFixedCacheKeyOptions, queueApi } from 'services/api/endp
 import { assert, AssertionError } from 'tsafe';
 
 const log = logger('generation');
-export const enqueueRequestedCanvas = createAction('app/enqueueRequestedCanvas');
+export const enqueueRequestedVideos = createAction('app/enqueueRequestedVideos');
 
 const enqueueVideo = async (store: AppStore, prepend: boolean) => {
   const { dispatch, getState } = store;
 
-  dispatch(enqueueRequestedCanvas());
+  dispatch(enqueueRequestedVideos());
 
   const state = getState();
 
-  const destination = selectCanvasDestination(state);
+  const model = state.video.videoModel;
+  if (!model) {
+    log.error('No model found in state');
+    return;
+  }
+  const base = model.base;
 
   const buildGraphResult = await withResultAsync(async () => {
-    const model = state.video.videoModel;
-    assert(model, 'No model found in state');
-    const base = model.base;
     const graphBuilderArg: GraphBuilderArg = { generationMode: 'txt2img', state, manager: null };
 
     switch (base) {
@@ -65,36 +68,37 @@ const enqueueVideo = async (store: AppStore, prepend: boolean) => {
     return;
   }
 
-  const { g } = buildGraphResult.value;
+  const { g, positivePrompt, seed } = buildGraphResult.value;
 
-  // const prepareBatchResult = withResult(() =>
-  //   prepareLinearUIBatch({
-  //     state,
-  //     g,
-  //     prepend,
-  //     seedNode: seed,
-  //     positivePromptNode: positivePrompt,
-  //     origin: 'canvas',
+  const prepareBatchResult = withResult(() =>
+    prepareLinearUIBatch({
+      state,
+      g,
+      base,
+      prepend,
+      seedNode: seed,
+      positivePromptNode: positivePrompt,
+      origin: 'videos',
+      destination: 'gallery',
+    })
+  );
+
+  if (prepareBatchResult.isErr()) {
+    log.error({ error: serializeError(prepareBatchResult.error) }, 'Failed to prepare batch');
+    return;
+  }
+
+  const batchConfig = prepareBatchResult.value;
+
+  // const batchConfig = {
+  //   prepend,
+  //   batch: {
+  //     graph: g.getGraph(),
+  //     runs: 1,
+  //     origin,
   //     destination,
-  //   })
-  // );
-
-  // if (prepareBatchResult.isErr()) {
-  //   log.error({ error: serializeError(prepareBatchResult.error) }, 'Failed to prepare batch');
-  //   return;
-  // }
-
-  // const batchConfig = prepareBatchResult.value;
-
-  const batchConfig = {
-    prepend,
-    batch: {
-      graph: g.getGraph(),
-      runs: 1,
-      origin,
-      destination,
-    },
-  };
+  //   },
+  // };
 
   const req = dispatch(
     queueApi.endpoints.enqueueBatch.initiate(batchConfig, {
