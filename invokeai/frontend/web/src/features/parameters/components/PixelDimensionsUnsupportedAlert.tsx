82729f5ae905cd8e39f0b02abdbfd30d568ecb38
Mary Hipp <maryhipp@Mac.lan>
2025-08-27 09:19:22 -0400

    update copy for API models without w/h controls

--- invokeai/frontend/web/src/features/parameters/components/PixelDimensionsUnsupportedAlert.tsx
+++ invokeai/frontend/web/src/features/parameters/components/PixelDimensionsUnsupportedAlert.tsx
@@ -4,7 +4,9 @@ import { memo } from 'react';
 export const PixelDimensionsUnsupportedAlert = memo(() => {
   return (
     <Alert status="info" borderRadius="base" flexDir="column" gap={2} overflow="unset">
-      <Text fontSize="md">This model does not support user-defined width and height.</Text>
+      <Text fontSize="sm" color="base.100">
+        Select an aspect ratio to control the size of the resulting image from this model.
+      </Text>
     </Alert>
   );
 });
