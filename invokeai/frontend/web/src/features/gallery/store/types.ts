bad6eea0778863ed4f1fbf95a3115477c7dc323f
Mary Hipp Rogers <maryhipp@gmail.com>
2025-08-28 08:26:09 -0400

    Revert "integrating video into gallery - thinking maybe a new category of image would make more senes"
    This reverts commit 5c93e53195e6a7f4333be22696c8e26ba8862e15.

--- invokeai/frontend/web/src/features/gallery/store/types.ts
+++ invokeai/frontend/web/src/features/gallery/store/types.ts
@@ -1,7 +1,7 @@
 import type { ImageCategory } from 'services/api/types';
 import z from 'zod';
 
-const zGalleryView = z.enum(['images', 'assets', 'videos']);
+const zGalleryView = z.enum(['images', 'assets']);
 export type GalleryView = z.infer<typeof zGalleryView>;
 const zBoardId = z.string();
 // TS hack to get autocomplete for "none" but accept any string
