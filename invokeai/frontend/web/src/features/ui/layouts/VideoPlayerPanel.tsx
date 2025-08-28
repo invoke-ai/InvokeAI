db48befbe0c98cfdc4bd964c6648667a7050caa6
Mary Hipp Rogers <maryhipp@gmail.com>
2025-08-28 08:26:09 -0400

    Revert "build out adhoc video saving graph"
    This reverts commit 07565d401531abb1464d35e1e69841f4b09d28a2.

--- invokeai/frontend/web/src/features/ui/layouts/VideoPlayerPanel.tsx
+++ invokeai/frontend/web/src/features/ui/layouts/VideoPlayerPanel.tsx
@@ -1,55 +1,33 @@
-import { Box, Button, Flex, Text } from '@invoke-ai/ui-library';
+import { Box, Flex, Text } from '@invoke-ai/ui-library';
 import { useFocusRegion } from 'common/hooks/focus';
-import { memo, useCallback, useRef } from 'react';
+import { memo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import ReactPlayer from 'react-player';
-import { useAppSelector, useAppStore } from 'app/store/storeHooks';
-import { selectGeneratedVideo } from 'features/parameters/store/videoSlice';
-import { PiCheckBold } from 'react-icons/pi';
-import { useDispatch } from 'react-redux';
-import { saveVideo } from 'features/video/saveVideo';
+import { useAppSelector } from 'app/store/storeHooks';
+import { selectGeneratedVideoUrl } from 'features/parameters/store/videoSlice';
 
 
 export const VideoPlayerPanel = memo(() => {
   const { t } = useTranslation();
   const ref = useRef<HTMLDivElement>(null);
-  const generatedVideo = useAppSelector(selectGeneratedVideo);
-
+  const generatedVideoUrl = useAppSelector(selectGeneratedVideoUrl);
+  
   useFocusRegion('video', ref);
-
-  const { dispatch, getState } = useAppStore();
-
-  const handleSaveVideo = useCallback(() => {
-    console.log('generatedVideo', generatedVideo);
-    if (!generatedVideo?.taskId) {
-      return
-    }
-    console.log('saving video', generatedVideo.taskId);
-    saveVideo({ dispatch, getState, taskId: `${generatedVideo.taskId}` });
-  }, [dispatch, getState, generatedVideo]);
 
 
 
   return (
     <Flex ref={ref} w="full" h="full" flexDirection="column" gap={4}>
-
-      {generatedVideo &&
-        <>
-          <Box flex={0.75} position="relative" >
-            <ReactPlayer
-              src={generatedVideo.url}
-              width="75%"
-              height="75%"
-              controls={true}
-              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '900px' }}
-            />
-          </Box>
-      
-            <Button leftIcon={<PiCheckBold />} colorScheme="invokeBlue" onClick={handleSaveVideo}>Keep</Button>
-      
-        </>}
-      {!generatedVideo && <Text>No video generated</Text>}
-
+      <Box flex={1} position="relative">
+       {generatedVideoUrl && <ReactPlayer
+          src={generatedVideoUrl}
+          width="75%"
+          height="75%"
+          controls={true}
+          style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '900px'}}
+        />}
+        {!generatedVideoUrl && <Text>No video generated</Text>}
+      </Box>
     </Flex>
   );
 });
--- invokeai/frontend/web/src/services/events/onInvocationComplete.tsx
+++ invokeai/frontend/web/src/services/events/onInvocationComplete.tsx
@@ -12,7 +12,7 @@ import { boardIdSelected, galleryViewChanged, imageSelected } from 'features/gal
 import { $nodeExecutionStates, upsertExecutionState } from 'features/nodes/hooks/useNodeExecutionState';
 import { isImageField, isImageFieldCollection } from 'features/nodes/types/common';
 import { zNodeStatus } from 'features/nodes/types/invocation';
-import { generatedVideoChanged } from 'features/parameters/store/videoSlice';
+import { generatedVideoUrlChanged } from 'features/parameters/store/videoSlice';
 import type { LRUCache } from 'lru-cache';
 import { boardsApi } from 'services/api/endpoints/boards';
 import { getImageDTOSafe, imagesApi } from 'services/api/endpoints/images';
@@ -205,11 +205,11 @@ export const buildOnInvocationComplete = (
     return imageDTOs;
   };
 
-  const getResultVideoDTOs = async (data: S['InvocationCompleteEvent']): Promise<{url: string , taskId: number} | null> => {
+  const getResultVideoDTOs = async (data: S['InvocationCompleteEvent']): Promise<string | null> => {
     // @ts-expect-error: This is a workaround to get the video name from the result
     if (data.invocation.type === 'runway_generate_video') {
       // @ts-expect-error: This is a workaround to get the video name from the result
-      return {url: data.result.video_url, taskId: data.result.runway_task_id};
+      return data.result.video.video_name;
     }
     return null;
   };
@@ -235,9 +235,9 @@ export const buildOnInvocationComplete = (
 
     await addImagesToGallery(data);
 
-    const videoResult = await getResultVideoDTOs(data);
-    if (videoResult) {
-      dispatch(generatedVideoChanged(videoResult));
+    const videoUrl = await getResultVideoDTOs(data);
+    if (videoUrl) {
+      dispatch(generatedVideoUrlChanged(videoUrl));
     }
 
     $lastProgressEvent.set(null);
