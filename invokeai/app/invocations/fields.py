144c54a6c8b9460f28c330609b9586c3601adaea
Mary Hipp Rogers <maryhipp@gmail.com>
2025-08-28 08:26:09 -0400

    Revert "video_output support"
    This reverts commit 453ef1a220e54887e59cfcd7c068ac3450b0b3e9.

--- invokeai/app/invocations/fields.py
+++ invokeai/app/invocations/fields.py
@@ -71,7 +71,6 @@ class UIType(str, Enum, metaclass=MetaEnum):
     # region Misc Field Types
     Scheduler = "SchedulerField"
     Any = "AnyField"
-    Video = "VideoField"
     # endregion
 
     # region Internal Field Types
@@ -224,12 +223,6 @@ class ImageField(BaseModel):
     """An image primitive field"""
 
     image_name: str = Field(description="The name of the image")
-
-
-class VideoField(BaseModel):
-    """A video primitive field"""
-
-    video_name: str = Field(description="The name of the video")
 
 
 class BoardField(BaseModel):
