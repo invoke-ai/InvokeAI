44b1d8d1fcdd7e1a30cf77057c1afa8bcaa7a7fe
Mary Hipp <maryhipp@Mac.lan>
2025-09-02 15:00:48 -0400

    remove video base models from image aspect/ratio logic

--- invokeai/frontend/web/src/features/parameters/components/Dimensions/DimensionsAspectRatioSelect.tsx
+++ invokeai/frontend/web/src/features/parameters/components/Dimensions/DimensionsAspectRatioSelect.tsx
@@ -17,10 +17,7 @@ import {
   zFluxKontextAspectRatioID,
   zGemini2_5AspectRatioID,
   zImagen3AspectRatioID,
-  zRunwayAspectRatioID,
-  zVeo3AspectRatioID,
 } from 'features/controlLayers/store/types';
-import { selectIsRunway, selectIsVeo3 } from 'features/parameters/store/videoSlice';
 import type { ChangeEventHandler } from 'react';
 import { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -35,8 +32,6 @@ export const DimensionsAspectRatioSelect = memo(() => {
   const isImagen4 = useAppSelector(selectIsImagen4);
   const isFluxKontext = useAppSelector(selectIsFluxKontext);
   const isGemini2_5 = useAppSelector(selectIsGemini2_5);
-  const isVeo3 = useAppSelector(selectIsVeo3);
-  const isRunway = useAppSelector(selectIsRunway);
 
   const options = useMemo(() => {
     // Imagen3 and ChatGPT4o have different aspect ratio options, and do not support freeform sizes
@@ -52,15 +47,9 @@ export const DimensionsAspectRatioSelect = memo(() => {
     if (isGemini2_5) {
       return zGemini2_5AspectRatioID.options;
     }
-    if (isVeo3) {
-      return zVeo3AspectRatioID.options;
-    }
-    if (isRunway) {
-      return zRunwayAspectRatioID.options;
-    }
     // All other models
     return zAspectRatioID.options;
-  }, [isImagen3, isChatGPT4o, isImagen4, isFluxKontext, isGemini2_5, isRunway, isVeo3]);
+  }, [isImagen3, isChatGPT4o, isImagen4, isFluxKontext, isGemini2_5]);
 
   const onChange = useCallback<ChangeEventHandler<HTMLSelectElement>>(
     (e) => {
