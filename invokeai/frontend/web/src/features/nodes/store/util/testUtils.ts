c305e79fee38f3bc9b2e0437fd93c0217478be51
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-09-18 12:07:50 +1000

    tests(ui): update tests to reflect new model parsing logic

--- invokeai/frontend/web/src/features/nodes/store/util/testUtils.ts
+++ invokeai/frontend/web/src/features/nodes/store/util/testUtils.ts
@@ -247,18 +247,12 @@ export const main_model_loader: InvocationTemplate = {
       fieldKind: 'input',
       input: 'direct',
       ui_hidden: false,
-      ui_model_base: ['sd-1', 'sdxl'],
+      ui_model_base: ['sd-1'],
       ui_model_type: ['main'],
       type: {
         name: 'ModelIdentifierField',
         cardinality: 'SINGLE',
         batch: false,
-
-        originalType: {
-          name: 'ModelIdentifierField',
-          cardinality: 'SINGLE',
-          batch: false,
-        },
       },
     },
   },
@@ -797,7 +791,8 @@ export const schema = {
             input: 'direct',
             orig_required: true,
             ui_hidden: false,
-            ui_type: 'MainModelField',
+            ui_model_base: ['sd-1'],
+            ui_model_type: ['main'],
           },
           type: {
             type: 'string',
