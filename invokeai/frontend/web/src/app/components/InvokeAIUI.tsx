f37f817fd17e1528f7b051717655a2b75ab1c659
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2025-10-15 09:59:52 +1100

    fix(ui): move logging setup to react code

--- invokeai/frontend/web/src/app/components/InvokeAIUI.tsx
+++ invokeai/frontend/web/src/app/components/InvokeAIUI.tsx
@@ -1,11 +1,21 @@
 import 'i18n';
 
+import { configureLogging } from 'app/logging/logger';
 import { addStorageListeners } from 'app/store/enhancers/reduxRemember/driver';
 import { $store } from 'app/store/nanostores/store';
 import { createStore } from 'app/store/store';
 import Loading from 'common/components/Loading/Loading';
 import React, { lazy, memo, useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
+
+/*
+ * We need to configure logging before anything else happens - useLayoutEffect ensures we set this at the first
+ * possible opportunity.
+ *
+ * Once redux initializes, we will check the user's settings and update the logging config accordingly. See
+ * `useSyncLoggingConfig`.
+ */
+configureLogging(true, 'debug', '*');
 
 const App = lazy(() => import('./App'));
 
