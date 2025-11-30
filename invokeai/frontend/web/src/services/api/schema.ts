13ac16e2c03e272545f0020e38497b6a6cdc3163
Alexander Eichhorn <alex@eichhorn.dev>
2025-12-01 00:30:53 +0100

    fix windows path again.

--- invokeai/frontend/web/src/services/api/schema.ts
+++ invokeai/frontend/web/src/services/api/schema.ts
@@ -12961,14 +12961,14 @@ export type components = {
              * Convert Cache Dir
              * Format: path
              * @description Path to the converted models cache directory (DEPRECATED, but do not delete because it is needed for migration from previous versions).
-             * @default models\.convert_cache
+             * @default models/.convert_cache
              */
             convert_cache_dir?: string;
             /**
              * Download Cache Dir
              * Format: path
              * @description Path to the directory that contains dynamically downloaded models.
-             * @default models\.download_cache
+             * @default models/.download_cache
              */
             download_cache_dir?: string;
             /**
