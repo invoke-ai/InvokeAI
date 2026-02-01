9566f9a23f9e5a7f67151a761752aa17e3ddd39f
DustyShoe <38873282+DustyShoe@users.noreply.github.com>
2026-02-01 23:37:53 +0200

    Feat(UI): Reintroduce paged gallery view as option (#8772)
    * Switched to use v5.x gallery pagination design.
    
    * Improved pagination UX and gallery grid calculation
    
    * Minor bug fix
    
    * Formatting...
    
    * Fixed Jump to page input behavior and "Locate in gallery" logic.
    
    * Changed Jump input field to select text on click for better UX.

--- invokeai/frontend/web/src/features/ui/store/uiSelectors.ts
+++ invokeai/frontend/web/src/features/ui/store/uiSelectors.ts
@@ -4,4 +4,5 @@ import { selectUiSlice } from 'features/ui/store/uiSlice';
 export const selectActiveTab = createSelector(selectUiSlice, (ui) => ui.activeTab);
 export const selectShouldShowItemDetails = createSelector(selectUiSlice, (ui) => ui.shouldShowItemDetails);
 export const selectShouldShowProgressInViewer = createSelector(selectUiSlice, (ui) => ui.shouldShowProgressInViewer);
+export const selectShouldUsePagedGalleryView = createSelector(selectUiSlice, (ui) => ui.shouldUsePagedGalleryView);
 export const selectPickerCompactViewStates = createSelector(selectUiSlice, (ui) => ui.pickerCompactViewStates);
