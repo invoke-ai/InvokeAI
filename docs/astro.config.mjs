00c713a6c5eef1a299885b64a96264f3ed830f8e
joshistoast <me@joshcorbett.com>
2026-02-19 15:20:08 -0700

    feat(docs): add more docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,29 +3,39 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // Plugins
+import markdoc from '@astrojs/markdoc';
+// import { strategy } from 'sharp';
 // import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    // rehypePlugins: [
-    //   [
-    //     rehypeExternalLinks,
-    //     {
-    //       content: { type: 'text', value: ' 🔗' },
-    //     },
-    //   ],
-    // ],
-  },
+  // markdown: {},
 
   integrations: [
     starlight({
       // Content
       title: 'Invoke',
+      logo: {
+        src: './src/assets/invoke-icon-wide.svg',
+        alt: 'InvokeAI Logo',
+        replacesTitle: true,
+      },
+      favicon: './src/assets/invoke-icon.svg',
+      editLink: {
+        baseUrl: 'https://github.com/invoke-ai/InvokeAI/tree/main/docs',
+      },
+      // locales: {
+      //   en: {
+      //     label: 'English',
+      //   },
+      // },
       social: [
         { icon: 'github', label: 'GitHub', href: 'https://github.com/invoke-ai/InvokeAI' },
         { icon: 'discord', label: 'Discord', href: 'https://discord.gg/ZmtBAhwWhy' },
       ],
+      tableOfContents: {
+        maxHeadingLevel: 4,
+      },
       sidebar: [
         {
           label: 'Getting Started',
@@ -36,10 +46,15 @@ export default defineConfig({
           autogenerate: { directory: 'configuration' },
         },
         {
+          label: 'Development',
+          autogenerate: { directory: 'development' },
+        },
+        {
           label: 'Contributing',
           autogenerate: { directory: 'contributing' },
         },
       ],
     }),
+    markdoc(),
   ],
 });
