4c283f2bfec435bb94978c57a2138ad2ea89e8cd
dunkeroni <dunkeroni@gmail.com>
2026-05-27 21:53:44 -0400

    update schema

--- invokeai/frontend/web/src/services/api/schema.ts
+++ invokeai/frontend/web/src/services/api/schema.ts
@@ -5145,7 +5145,10 @@ export type components = {
              */
             type: "infill_cv2";
         };
-        /** CacheStats */
+        /**
+         * CacheStats
+         * @description Collect statistics on cache performance.
+         */
         CacheStats: {
             /**
              * Hits
